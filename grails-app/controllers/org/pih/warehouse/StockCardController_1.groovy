0aa5eec443ceb358b10f08e0f8cd9b08aec92f8f
jmiranda <jmiranda@f0ac6d2b-dfc5-4988-9158-fc266d298d87>
2010-04-15 16:27:16 +0000

    warehouse: cleaning up more unnecessary files
    git-svn-id: https://dev.pih-emr.org/svn/repository/projects/warehouse/trunk@8793 f0ac6d2b-dfc5-4988-9158-fc266d298d87

--- grails-app/controllers/org/pih/warehouse/StockCardController_1.groovy
+++
@@ -1,6 +0,0 @@
-package org.pih.warehouse
-
-class StockCardController {
-
-    def scaffold = StockCard
-}
--- grails-app/controllers/org/pih/warehouse/web/controller/ProductController.groovy
+++
@@ -1,8 +0,0 @@
-package org.pih.warehouse.web.controller.admin
-
-class ProductController {
-
-    //def index = { }
-
-    def scaffold = Product
-}
