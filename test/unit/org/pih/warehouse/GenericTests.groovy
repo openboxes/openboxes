dd21c1bf0a83826a1635b233ac6c042b7cc6f4f6
jmiranda <jmiranda@f0ac6d2b-dfc5-4988-9158-fc266d298d87>
2010-05-04 19:49:14 +0000

    warehouse: Added uncommitted tests and YUI plugin libraries.
    git-svn-id: https://dev.pih-emr.org/svn/repository/projects/warehouse/trunk@8812 f0ac6d2b-dfc5-4988-9158-fc266d298d87

---
+++ test/unit/org/pih/warehouse/GenericTests.groovy
@@ -0,0 +1,17 @@
+package org.pih.warehouse
+
+import grails.test.*
+
+class GenericTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/org/pih/warehouse/ShipmentTests.groovy
@@ -0,0 +1,17 @@
+package org.pih.warehouse
+
+import grails.test.*
+
+class ShipmentTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
