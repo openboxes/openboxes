013e8180a5182f15182a03d0ba173ca65369aeff
mseaton <mseaton@f0ac6d2b-dfc5-4988-9158-fc266d298d87>
2011-08-22 01:25:38 +0000

    Fixing a bug which prevents the default from working on the message tag.
    git-svn-id: https://dev.pih-emr.org/svn/repository/projects/warehouse/trunk@10696 f0ac6d2b-dfc5-4988-9158-fc266d298d87

--- grails-app/taglib/org/pih/warehouse/MessageTagLib.groovy
+++ grails-app/taglib/org/pih/warehouse/MessageTagLib.groovy
@@ -7,7 +7,8 @@ class MessageTagLib {
 	Locale defaultLocale = new Locale(grailsApplication.config.locale.defaultLocale)
 	
 	def message = { attrs, body ->
-		Locale l = attrs.locale  ?: session?.user?.locale ?: defaultLocale; 
-		out << g.message(code:attrs.code, args: attrs.args, encodeAs: attrs.encodeAs, error: attrs.error, message: attrs.message, locale: l)
+		Locale l = attrs.locale  ?: session?.user?.locale ?: defaultLocale;
+		String translation = g.message(code:attrs.code, args: attrs.args, encodeAs: attrs.encodeAs, error: attrs.error, message: attrs.message, locale: l)
+		out << (translation == attrs.code ? (attrs.default ?: attrs.code) : translation);
 	}
 }
