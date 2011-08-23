a281244daf5340ebb6b65f225cf30b432104a74b
jmiranda <jmiranda@f0ac6d2b-dfc5-4988-9158-fc266d298d87>
2011-08-23 03:16:12 +0000

    Added shipment type controller (hidden for now).
    git-svn-id: https://dev.pih-emr.org/svn/repository/projects/warehouse/trunk@10716 f0ac6d2b-dfc5-4988-9158-fc266d298d87

---
+++ grails-app/controllers/org/pih/warehouse/shipping/ShipmentTypeController.groovy
@@ -0,0 +1,42 @@
+package org.pih.warehouse.shipping;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import grails.converters.JSON
+import grails.validation.ValidationException;
+import groovy.sql.Sql;
+import au.com.bytecode.opencsv.CSVWriter;
+
+import org.apache.commons.collections.FactoryUtils;
+import org.apache.commons.collections.list.LazyList;
+import org.codehaus.groovy.grails.validation.Validateable;
+import org.pih.warehouse.core.Comment;
+import org.pih.warehouse.core.Document;
+import org.pih.warehouse.core.Event;
+import org.pih.warehouse.core.EventCode;
+import org.pih.warehouse.core.EventType;
+import org.pih.warehouse.core.Location;
+import org.pih.warehouse.core.Person;
+import org.pih.warehouse.core.User;
+import org.pih.warehouse.inventory.InventoryItem;
+import org.pih.warehouse.inventory.Warehouse;
+import org.pih.warehouse.product.Product;
+import org.pih.warehouse.receiving.Receipt;
+import org.pih.warehouse.receiving.ReceiptItem;
+import org.pih.warehouse.core.ListCommand;
+
+import com.ocpsoft.pretty.time.PrettyTime;
+
+
+class ShipmentTypeController {	
+	def scaffold = ShipmentType
+	
+}
+
+
+
+
+
