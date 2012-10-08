f11ac570cf5c8aab634072d146ed526fff28e70f
jmiranda <jmiranda@pih.org>
2012-10-07 22:28:45 -0400

    [PIMS-1646] Property [actualDeliveryDate] of domain class org.pih.warehouse.receiving.Receipt has type [java.util.Date] and doesn't support constraint [blank]. This constraint will not be checked during validation.

--- grails-app/domain/org/pih/warehouse/receiving/Receipt.groovy
+++ grails-app/domain/org/pih/warehouse/receiving/Receipt.groovy
@@ -34,14 +34,14 @@ class Receipt implements Serializable {
 	// Constraints
 	static constraints = {
 		expectedDeliveryDate(nullable:true)
-		actualDeliveryDate(blank:false, 
+		actualDeliveryDate(nullable:true, 
 			// can't be delivered in the future
 			// can't be delivered before it is shipped!
 			validator: { value, obj-> 
-				println "max: " + it + " <= " + new Date();				
-				println "obj.shipment.actualShippingDate is notNull: " + obj.shipment.actualShippingDate 
-				println "value + 1: " + (value + 1) 
-				println "(value + 1).after(obj.shipment.actualShippingDate): " + (value + 1).after(obj.shipment.actualShippingDate) 
+				//println "max: " + it + " <= " + new Date();				
+				//println "obj.shipment.actualShippingDate is notNull: " + obj.shipment.actualShippingDate 
+				//println "value + 1: " + (value + 1) 
+				//println "(value + 1).after(obj.shipment.actualShippingDate): " + (value + 1).after(obj.shipment.actualShippingDate) 
 				if (!(value <= new Date())) {
 					//println "value <= new Date(): " + (value <= new Date())
 					return ["invalid.mustOccurOnOrBeforeToday", value, new Date()]
