87e9ad10389c965817737fd0daa8eaf5cbc6c3e1
PZhao <pzhao@thoughtworks.com>
2012-11-22 10:10:57 -0500

    add requisitionItem spec

--- spec/RequisitionProcessor.spec.js
+++ spec/RequisitionProcessor.spec.js
@@ -77,29 +77,7 @@ describe('Requisition Processor View Model', function(){
 
     });
 
-    it("should get quantity picked, remaining and status from requisition item", function(){
-      var requisitionItem = new openboxes.requisition.RequisitionItem({id: "1234", quantity:5000});
-      expect(requisitionItem.status()).toBe("Incomplete");
-      var picklistItem1 = new openboxes.requisition.PicklistItem({
-        quantityPicked: 1000,
-        quantityOnHand: 2000
-      });
-      requisitionItem.picklistItems.push(picklistItem1);
-
-      expect(requisitionItem.quantityPicked()).toBe(1000);
-      expect(requisitionItem.quantityRemaining()).toBe(4000);
-      expect(requisitionItem.status()).toBe("PartiallyComplete");
-
-      var picklistItem2 = new openboxes.requisition.PicklistItem({
-        quantityPicked: 4000,
-        quantityOnHand: 4000
-      });
-      requisitionItem.picklistItems.push(picklistItem2);
-      expect(requisitionItem.quantityPicked()).toBe(5000);
-      expect(requisitionItem.quantityRemaining()).toBe(0);
-      expect(requisitionItem.status()).toBe("Complete");
-    });
-
+  
     describe("should save picklist", function(){
       it("should get picklist items from requisition", function(){});
          //var viewModel = new openboxes.requisition.ProcessViewModel(requisitionData, picklistData, inventoryItemsData);
