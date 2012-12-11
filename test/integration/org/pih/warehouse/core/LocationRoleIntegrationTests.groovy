0cdad38dcc1385688f94fefab545a37187097719
PZhao <pzhao@thoughtworks.com>
2012-12-11 16:24:23 -0500

    try to fix integration tests for ci server

--- test/integration/org/pih/warehouse/core/LocationRoleIntegrationTests.groovy
+++ test/integration/org/pih/warehouse/core/LocationRoleIntegrationTests.groovy
@@ -4,6 +4,7 @@ package org.pih.warehouse.core
 class LocationRoleIntegrationTests extends GroovyTestCase {
   void test_save(){
     def user = User.list().first()
+    user.locale = new Locale("en")
     def location = Location.list().first()
     def role = Role.list().first()
     def locationRole = new LocationRole(location: location, role: role)
