f3f79c76e9c51da34ae4634d027b5cbf3786466c
jmiranda <jmiranda@pih.org>
2013-05-15 07:15:23 -0400

    PIMS-2421 Reverted change that removes all picklist items when a requisition item is changed or canceled (due to an exception)

--- grails-app/domain/org/pih/warehouse/requisition/RequisitionItem.groovy
+++ grails-app/domain/org/pih/warehouse/requisition/RequisitionItem.groovy
@@ -294,10 +294,11 @@ class RequisitionItem implements Comparable<RequisitionItem>, Serializable {
             throw new ValidationException("Validation errors on requisition item", errors)
         }
 
-        def picklistItems = getPicklistItems()
-        picklistItems.each {
-            it.picklist.removeFromPicklistItems(it)
-        }
+        //def picklistItems = getPicklistItems()
+        //picklistItems.each {
+        //    it.picklist.removeFromPicklistItems(it)
+        //    it.delete()
+        //}
         quantityCanceled = quantity
         cancelReasonCode = reasonCode
         cancelComments = comments
