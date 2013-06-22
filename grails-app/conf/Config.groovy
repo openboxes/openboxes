d44b5540ab223f888fb7f5b99a21a0d9754dd2a5
jmiranda <jmiranda@pih.org>
2013-06-21 22:39:03 -0400

    Improved the exception logging email format

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -181,8 +181,9 @@ log4j = {
 				"IP address: %X{ipAddress}  http://whatismyipaddress.com/ip/%X{ipAddress}%n" +
 				"Request URI: %X{requestUri}%n" +
 				"Query string: %X{queryString}%n" +
-                "Clickstream: %X{clickStream}%n" +
-                "Stacktrace:%n%m%n%n"
+                "Clickstream: %n%X{clickStream}%n" +
+                "Stacktrace: %n%m%n%n"
+
 			// The 'alternate' appender is the best, but only works on localhost w/o authentication
 			if ("alternate".equals(mail.error.appender)&&"localhost".equals(mail.error.server)) {
 				smtpAppender = new AlternateSMTPAppender(
