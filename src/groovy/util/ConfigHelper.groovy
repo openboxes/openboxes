e0dc806e2600c33c44ef541c15e44682ddefd777
jmiranda <jcm62@columbia.edu>
2014-07-16 22:48:39 -0400

    Added utility class to help deal with configuration properties

---
+++ src/groovy/util/ConfigHelper.groovy
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2012 Partners In Health.  All rights reserved.
+ * The use and distribution terms for this software are covered by the
+ * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ * which can be found in the file epl-v10.html at the root of this distribution.
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ * You must not remove this notice, or any other, from this software.
+ **/
+package util
+
+// See http://jira.codehaus.org/browse/GRAILS-6515
+class ConfigHelper {
+    public static booleanValue(def value) {
+        if (value.class == java.lang.Boolean) {
+            // because 'true.toBoolean() == false' !!!
+            return value
+        } else {
+            return value.toBoolean()
+        }
+    }
+
+    public static listValue(def value) {
+        if (value instanceof java.lang.String) {
+            return value?.split(",")
+        }
+        else {
+            return value;
+        }
+
+    }
+
+}
