5bfc3ffb9074672138f4a814df7ed01d21b18bb6
jmiranda <jcm62@columbia.edu>
2015-09-14 12:50:14 -0500

    excluded cache-headers on cached-resources plugin to resolve dependency issue occuring on travis-ci

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -103,7 +103,7 @@ grails.project.dependency.resolution = {
         runtime(":resources:1.1.6")
 		runtime(":cache-headers:1.1.5")
         runtime(":zipped-resources:1.0") { excludes 'resources' }
-        runtime(":cached-resources:1.0") { excludes 'resources' }
+        runtime(":cached-resources:1.0") { excludes 'resources', 'cache-headers' }
         runtime(":jquery:1.7.2")
         runtime(":jquery-ui:1.8.7") { excludes 'jquery' }
 
