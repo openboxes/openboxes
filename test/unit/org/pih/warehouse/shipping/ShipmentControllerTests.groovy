ba0192a62aa3d838d978e979b108d985ceaea244
jmiranda <jcm62@columbia.edu>
2016-06-30 11:43:47 -0500

    fixed unit tests for shipment controller

--- test/unit/org/pih/warehouse/shipping/ShipmentControllerTests.groovy
+++ test/unit/org/pih/warehouse/shipping/ShipmentControllerTests.groovy
@@ -1,68 +1,81 @@
 package org.pih.warehouse.shipping
 
 import grails.test.ControllerUnitTestCase
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
 import org.pih.warehouse.MessageTagLib
+import grails.buildtestdata.mixin.Build
+import org.pih.warehouse.core.Location
+import spock.lang.Specification
 
-class ShipmentControllerTests  extends ControllerUnitTestCase {
+@Build([Shipment, Container, Location])
+@TestFor(ShipmentController)
+class ShipmentControllerTests extends Specification {
 
-    def shipmentServiceMock
+    static shipmentServiceMock
 
-    @Override
-    Class getControllerClass() {
-        return ShipmentController;
-    }
-
-    protected void setUp() {
-        super.setUp()
+    void setup() {
         shipmentServiceMock = mockFor(ShipmentService)
-
         def stubMessageTagLib = new Expando()
-        stubMessageTagLib.message = { params -> return "fake message" }
+        stubMessageTagLib.message = { message -> return message }
         controller.metaClass.warehouse = stubMessageTagLib;
+
+        shipmentServiceMock.demand.addToShipment { true }
+        controller.shipmentService = shipmentServiceMock.createMock()
     }
 
-    protected void tearDown() {
-        super.tearDown()
+    void tearDown() { }
+
+
+    void 'show action should redirect to show details'() {
+        when:
+        params.id = 1
+        controller.show()
+
+        then:
+        assertTrue response.redirectedUrl == '/shipment/showDetails/1'
     }
 
     void testAddToShipmentUnpackedItemContainerShouldRedirectToCreateShipmentFlow() {
-
+        when:
         def shipmentId = 'SHIP001'
-        mockDomain(Shipment, [new Shipment(id : shipmentId)])
-        mockDomain(Container)
+        def origin = Location.build(name: "Origin 1")
+        def destination = Location.build(name: "Destination1 1")
+        Shipment.build(id: 'SHIP001', name: "Shipment 1", origin: origin, destination: destination)
+        //Container.build(id: "CONT001", name: "Container 1")
+
         mockCommandObject(ItemListCommand)
 
         ItemListCommand commands = new ItemListCommand()
         commands.items = [new ItemCommand()]
-
-        shipmentServiceMock.demand.addToShipment { true }
-        controller.shipmentService = shipmentServiceMock.createMock()
 
         controller.params.shipmentContainerKey = "$shipmentId:0"
         controller.addToShipmentPost(commands)
 
-        assertEquals "createShipmentWorkflow", redirectArgs.controller
-        assertEquals "createShipment", redirectArgs.action
+        then:
+        response.redirectedUrl == "/createShipmentWorkflow/createShipment/SHIP001?skipTo=Packing&containerId="
     }
 
     void testAddToShipmentPackingListContainerShouldRedirectToCreateShipmentFlow() {
 
+        when:
         def shipmentId = 'SHIP001'
         def containerId = 'CONT001'
-        mockDomain(Shipment, [new Shipment(id : shipmentId)])
-        mockDomain(Container, [new Container(id : containerId)])
+        def origin = Location.build(name: "Origin 2")
+        def destination = Location.build(name: "Destination 2")
+        Shipment.build(id: 'SHIP001', name: "Shipment 1", origin: origin, destination: destination)
+        //Container.build(id: "CONT001", name: "Container 1")
         mockCommandObject(ItemListCommand)
 
         ItemListCommand commands = new ItemListCommand()
         commands.items = [new ItemCommand()]
 
-        shipmentServiceMock.demand.addToShipment { true }
-        controller.shipmentService = shipmentServiceMock.createMock()
-
         controller.params.shipmentContainerKey = "$shipmentId:$containerId"
         controller.addToShipmentPost(commands)
 
-        assertEquals "createShipmentWorkflow", redirectArgs.controller
-        assertEquals "createShipment", redirectArgs.action
+
+        then:
+        response.redirectedUrl == "/createShipmentWorkflow/createShipment/SHIP001?skipTo=Packing&containerId="
+
     }
 }
