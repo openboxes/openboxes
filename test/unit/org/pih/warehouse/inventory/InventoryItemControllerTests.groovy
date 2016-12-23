75ef4dcc939917a0d6d0dc1ab1a3b63d794d0798
jmiranda <jcm62@columbia.edu>
2016-12-23 12:31:56 -0600

    #94 fixed inventory item controller tests (all tests are now passing)

--- test/unit/org/pih/warehouse/inventory/InventoryItemControllerTests.groovy
+++ test/unit/org/pih/warehouse/inventory/InventoryItemControllerTests.groovy
@@ -2,38 +2,34 @@ package org.pih.warehouse.inventory
 
 import grails.converters.JSON
 import grails.test.ControllerUnitTestCase
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import org.junit.Test
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.product.Product
 
-class InventoryItemControllerTests extends ControllerUnitTestCase {
 
+@TestFor(InventoryItemController)
+@Mock([Product, Location, Inventory, InventoryService])
+class InventoryItemControllerTests {
+
+    @Test
     void test_showRecordInventory() {
 
         def inventory = new Inventory(id: "inventory1")
         def myLocation = new Location(id: "1234", inventory: inventory)
         Product p = new Product(id:"pro1", name:"product1");
-        RecordInventoryCommand com = new RecordInventoryCommand(productInstance: p, inventoryInstance: inventory)
         InventoryItem i1 = new InventoryItem(id: "item1", product: p)
         InventoryItem i2 = new InventoryItem(id: "item2", product: p)
         InventoryItem i3 = new InventoryItem(id: "item3", product: p)
         InventoryItem i4 = new InventoryItem(id: "item4", product: p)
-        mockDomain(Product, [p])
-        mockDomain(InventoryItem, [i1, i2, i3, i4])
-        mockDomain(InventoryLevel, [])
-        mockCommandObject(RecordInventoryCommand)
-        mockDomain(Location, [myLocation])
-        mockDomain(Inventory, [inventory])
+
+        RecordInventoryCommand com = new RecordInventoryCommand(productInstance: p, inventoryInstance: inventory)
 
         def inventoryServiceMock = mockFor(InventoryService)
-        inventoryServiceMock.demand.populateRecordInventoryCommand(1) { commandInstance, userInventory ->
-
-        }
-        inventoryServiceMock.demand.getTransactionEntriesByInventoryAndProduct(1) { inventoryInstance, productInstance ->
-
-        }
-        inventoryServiceMock.demand.getQuantityByProductMap(1) { transactionEntryList ->
-            return [:]
-        }
+        inventoryServiceMock.demand.populateRecordInventoryCommand(1) { commandInstance, userInventory -> }
+        inventoryServiceMock.demand.getTransactionEntriesByInventoryAndProduct(1) { inventoryInstance, productInstance -> }
+        inventoryServiceMock.demand.getQuantityByProductMap(1) { transactionEntryList -> return [:] }
         inventoryServiceMock.demand.getInventoryItemsWithQuantity(1) { product, inv ->
             return ["pro1": [[id:"item1"], [id:"item2"], [id:"item3"], [id:"item4"]]]
         }
@@ -48,6 +44,5 @@ class InventoryItemControllerTests extends ControllerUnitTestCase {
 
         assert json.product
         assert json.inventoryItems
-                                          //Kyle make this test better!
     }
 }
