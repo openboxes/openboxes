734764fc6de9544312b8bd8d14a3c693eb2a671e
Justin Miranda <jcm62@columbia.edu>
2017-05-04 09:24:50 -0500

    Cannot see bin under current stock unless you are an Admin (OBPIH-514)

--- grails-app/taglib/LinkTagLib.groovy
+++ grails-app/taglib/LinkTagLib.groovy
@@ -12,7 +12,11 @@ class LinkTagLib extends ApplicationTagLib {
         if (!SecurityFilters.actionsWithAuthUserNotRequired.contains(actionName)) {
             def missManager = RoleFilters.needManager(controllerName, actionName) && !userService.isUserManager(session.user)
             def missAdmin = RoleFilters.needAdmin(controllerName, actionName) && !userService.isUserAdmin(session.user)
-            if (missManager || missAdmin) return
+            println "missManager ${missManager}, missAdmin ${missAdmin}"
+            if (missManager || missAdmin) {
+                out << body()
+                return;
+            }
         }
 
         def applicationTagLib = grailsApplication.mainContext.getBean('org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib')
