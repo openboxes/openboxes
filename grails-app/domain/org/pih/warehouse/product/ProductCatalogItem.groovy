5f20344924bc03b0b831577e23ee5d54b84a27b7
Justin Miranda <jcm62@columbia.edu>
2018-03-25 22:09:18 -0500

    OBPIH-713 Added product catalog domain

---
+++ grails-app/domain/org/pih/warehouse/product/ProductCatalogItem.groovy
@@ -0,0 +1,44 @@
+package org.pih.warehouse.product
+
+class ProductCatalogItem implements Comparable, Serializable {
+
+    String id
+    Product product
+    Boolean active = Boolean.TRUE
+
+    // Auditing fields
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+        product(nullable:false)
+        active(nullable:false)
+    }
+
+    static belongsTo = [productCatalog:ProductCatalog]
+
+    static mapping = {
+        id generator: 'uuid'
+        cache true
+    }
+
+    /**
+     * Returns a string representation of the catalog.
+     *
+     * @return
+     */
+    String toString() {
+        return "${id}";
+    }
+
+    /**
+     * Sort by name
+     */
+    int compareTo(obj) {
+        def sortOrder = id <=> obj?.id
+        return sortOrder;
+
+    }
+
+
+}
