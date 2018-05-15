634af9806266fb37c3197d360f892e7332c97ff9
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-05-15 11:21:23 +0200

    OBPIH-849: Add redux and redux-form to the project

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,19 +1,28 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
 
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/main.scss';
 
+import rootReducer from './reducers';
 import StockMovement from './components/StockMovement';
 import MainLayoutRoute from './components/Layout/MainLayoutRoute';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <MainLayoutRoute path="/**/stockMovement" component={StockMovement} />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <MainLayoutRoute path="/**/stockMovement" component={StockMovement} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'),
 );
