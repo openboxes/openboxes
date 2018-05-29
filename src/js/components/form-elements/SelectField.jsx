f9e5fedbf1cc642d7bab41c2b6e56ad08b9c2643
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-05-29 11:38:17 +0200

    Add possibility to store objects in Select value

--- src/js/components/form-elements/SelectField.jsx
+++ src/js/components/form-elements/SelectField.jsx
@@ -1,27 +1,50 @@
 import _ from 'lodash';
 import React from 'react';
 import Select from 'react-select';
+import PropTypes from 'prop-types';
 
 import 'react-select/dist/react-select.css';
 
 import BaseReactField from './BaseReactField';
 
 const SelectField = (props) => {
-  const renderInput = (attributes) => {
-    const options = _.map(attributes.options, value => (typeof value === 'string' ? { value, label: value } : value));
+  const renderInput = ({ objectValue, ...attributes }) => {
+    const options = _.map(attributes.options, (value) => {
+      if (typeof value === 'string') {
+        return { value, label: value };
+      } else if (objectValue) {
+        return { ...value, value: JSON.stringify(value.value) };
+      }
+
+      return value;
+    });
 
     const onChange = (selectValue) => {
       if (attributes.multi) {
-        const values = _.map(selectValue, val => val.value);
+        const values = _.map(selectValue, val => (objectValue ? JSON.parse(val.value) : val.value));
         attributes.onChange(values);
       } else if (selectValue !== null && selectValue !== undefined) {
-        attributes.onChange(selectValue.value);
+        attributes.onChange(objectValue ? JSON.parse(selectValue.value) : selectValue.value);
       } else {
         attributes.onChange(null);
       }
     };
 
     const delimiter = attributes.delimiter || ';';
+    let { value } = attributes;
+
+    if (objectValue) {
+      value = attributes.multi ? _.map(attributes.value, val => JSON.stringify(val))
+        : JSON.stringify(attributes.value);
+    }
+
+    renderInput.propTypes = {
+      objectValue: PropTypes.bool,
+    };
+
+    renderInput.defaultProps = {
+      objectValue: false,
+    };
 
     return (
       <Select
@@ -29,7 +52,7 @@ const SelectField = (props) => {
         {...attributes}
         options={options}
         delimiter={delimiter}
-        value={attributes.multi ? _.join(attributes.value, delimiter) : attributes.value}
+        value={attributes.multi ? _.join(value, delimiter) : value}
         onChange={onChange}
       />
     );
