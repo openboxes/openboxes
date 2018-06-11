9356a096f523c4380caec8d22122775ec155f399
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-06-11 14:03:44 +0200

    OBPIH-889: Remove empty line items from step 2 when going next

--- src/js/components/stock-movement-wizard/AddItemsPage.jsx
+++ src/js/components/stock-movement-wizard/AddItemsPage.jsx
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { reduxForm, initialize, formValueSelector } from 'redux-form';
+import { reduxForm, initialize, formValueSelector, change } from 'redux-form';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -165,10 +165,21 @@ class AddItemsPage extends Component {
     return NO_STOCKLIST_FIELDS;
   }
 
+  nextPage(formValues) {
+    const lineItems = _.filter(formValues.lineItems, val => !_.isEmpty(val));
+    this.props.change('stock-movement-wizard', 'lineItems', lineItems);
+
+    if (this.props.origin.type === 'Supplier') {
+      this.props.goToPage(5);
+    } else {
+      this.props.onSubmit();
+    }
+  }
+
   render() {
     const { handleSubmit, previousPage } = this.props;
     return (
-      <form onSubmit={handleSubmit(() => (this.props.origin.type === 'Supplier' ? this.props.goToPage(5) : this.props.onSubmit()))}>
+      <form onSubmit={handleSubmit(values => this.nextPage(values))}>
         {_.map(this.getFields(), (fieldConfig, fieldName) =>
           renderFormField(fieldConfig, fieldName, { stockList: this.props.stockList }))}
         <div>
@@ -194,10 +205,11 @@ export default reduxForm({
   destroyOnUnmount: false,
   forceUnregisterOnUnmount: true,
   validate,
-})(connect(mapStateToProps, { initialize })(AddItemsPage));
+})(connect(mapStateToProps, { initialize, change })(AddItemsPage));
 
 AddItemsPage.propTypes = {
   initialize: PropTypes.func.isRequired,
+  change: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   previousPage: PropTypes.func.isRequired,
   goToPage: PropTypes.func.isRequired,
