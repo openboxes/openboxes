8116d1e3c01caffe371f8df6c2899130d6f4af48
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-07-02 18:13:42 +0200

    OBPIH-896: Fix issues with non form fields re-renders

--- src/js/components/form-elements/FieldArrayComponent.jsx
+++ src/js/components/form-elements/FieldArrayComponent.jsx
@@ -1,66 +1,51 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import PickPageFieldArrayComponent from './PickPageFieldArrayComponent';
 import TableBody from './TableBody';
 import LineItemsRowKeyFieldArrayComponent from './LineItemsRowKeyFieldArrayComponent';
 
-class FieldArrayComponent extends Component {
-  shouldComponentUpdate(nextProps) {
-    if (this.props.fields.length !== nextProps.fields.length) {
-      return true;
-    }
+const FieldArrayComponent = (props) => {
+  const { fieldsConfig, properties, fields } = props;
+  const AddButton = fieldsConfig.addButton;
+  const addRow = (row = {}) => fields.push(row);
 
-    return !_.isEqualWith(this.props.properties, nextProps.properties, (objValue, othValue) => {
-      if (typeof objValue === 'function' || typeof othValue === 'function') {
-        return true;
-      }
-
-      return undefined;
-    });
+  if (fieldsConfig.pickPage) {
+    return (
+      <PickPageFieldArrayComponent
+        fieldsConfig={fieldsConfig}
+        properties={properties}
+        fields={fields}
+      />
+    );
   }
 
-  render() {
-    const { fieldsConfig, properties, fields } = this.props;
-    const AddButton = fieldsConfig.addButton;
-    const addRow = (row = {}) => fields.push(row);
-
-    if (fieldsConfig.pickPage) {
-      return (
-        <PickPageFieldArrayComponent
-          fieldsConfig={fieldsConfig}
-          properties={properties}
-          fields={fields}
-        />
-      );
-    }
-
-    if (fieldsConfig.lineItemsRowKey) {
-      return (
-        <LineItemsRowKeyFieldArrayComponent
-          fieldsConfig={fieldsConfig}
-          properties={properties}
-          fields={fields}
-        />
-      );
-    }
-
+  if (fieldsConfig.lineItemsRowKey) {
     return (
-      <div>
-        <div className="text-center border">
-          <div className="d-flex flex-row border-bottom font-weight-bold py-2">
-            { _.map(fieldsConfig.fields, (config, name) =>
-              <div key={name} className="mx-1" style={{ flex: '1 1 0' }}>{config.label}</div>) }
-          </div>
-          <TableBody
-            fields={fields}
-            properties={properties}
-            addRow={addRow}
-            fieldsConfig={fieldsConfig}
-          />
+      <LineItemsRowKeyFieldArrayComponent
+        fieldsConfig={fieldsConfig}
+        properties={properties}
+        fields={fields}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <div className="text-center border">
+        <div className="d-flex flex-row border-bottom font-weight-bold py-2">
+          { _.map(fieldsConfig.fields, (config, name) =>
+            <div key={name} className="mx-1" style={{ flex: '1 1 0' }}>{config.label}</div>) }
         </div>
-        { AddButton &&
+        <TableBody
+          fields={fields}
+          properties={properties}
+          addRow={addRow}
+          fieldsConfig={fieldsConfig}
+        />
+      </div>
+      { AddButton &&
         <div className="text-center">
           {
             typeof AddButton === 'string' ?
@@ -71,10 +56,9 @@ class FieldArrayComponent extends Component {
           }
         </div>
         }
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 FieldArrayComponent.propTypes = {
   fieldsConfig: PropTypes.shape({}).isRequired,
--- src/js/components/form-elements/TableRow.jsx
+++ src/js/components/form-elements/TableRow.jsx
@@ -1,33 +1,46 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { renderFormField } from '../../utils/form-utils';
 
-const TableRow = (props) => {
-  const {
-    fieldsConfig, index, field, addRow, properties, removeRow, rowValues = {},
-  } = props;
+class TableRow extends Component {
+  shouldComponentUpdate(nextProps) {
+    return !_.isEqualWith(this.props, nextProps, (objValue, othValue) => {
+      if (typeof objValue === 'function' || typeof othValue === 'function') {
+        return true;
+      }
 
-  const dynamicAttr = fieldsConfig.getDynamicRowAttr ? fieldsConfig.getDynamicRowAttr(props) : {};
-  const rowIndex = properties.parentIndex || index;
-  const className = `table-row ${rowIndex % 2 === 0 ? 'even-row' : ''} ${dynamicAttr.className ? dynamicAttr.className : ''}`;
+      return undefined;
+    });
+  }
 
-  return (
-    <div {...dynamicAttr} className={`d-flex flex-row border-bottom ${className}`}>
-      { _.map(fieldsConfig.fields, (config, name) => (
-        <div key={`${field}.${name}`} className="align-self-center mx-1" style={{ flex: '1 1 0', minWidth: 0 }}>
-          { renderFormField(config, `${field}.${name}`, {
-            ...properties,
-            arrayField: true,
-            addRow,
-            removeRow,
-            rowIndex: index,
-            fieldValue: _.get(rowValues, name),
-          })}
-        </div>
-      ))}
-    </div>);
-};
+  render() {
+    const {
+      fieldsConfig, index, field, addRow, properties, removeRow, rowValues = {},
+    } = this.props;
+
+    const dynamicAttr = fieldsConfig.getDynamicRowAttr ?
+      fieldsConfig.getDynamicRowAttr({ ...properties, index, rowValues }) : {};
+    const rowIndex = properties.parentIndex || index;
+    const className = `table-row ${rowIndex % 2 === 0 ? 'even-row' : ''} ${dynamicAttr.className ? dynamicAttr.className : ''}`;
+
+    return (
+      <div {...dynamicAttr} className={`d-flex flex-row border-bottom ${className}`}>
+        {_.map(fieldsConfig.fields, (config, name) => (
+          <div key={`${field}.${name}`} className="align-self-center mx-1" style={{ flex: '1 1 0', minWidth: 0 }}>
+            {renderFormField(config, `${field}.${name}`, {
+              ...properties,
+              arrayField: true,
+              addRow,
+              removeRow,
+              rowIndex: index,
+              fieldValue: config.fieldKey === '' ? rowValues : _.get(rowValues, config.fieldKey || name),
+            })}
+          </div>
+        ))}
+      </div>);
+  }
+}
 
 export default TableRow;
 
