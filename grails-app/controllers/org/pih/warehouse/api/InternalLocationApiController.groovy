f94602e9243de4e4a83bad5e798dd9fa5cf517b4
Justin Miranda <jcm62@columbia.edu>
2018-07-10 16:34:45 -0500

    OBPIH-947 Allow user to create internal locations of any type from the Edit Location page

---
+++ grails-app/controllers/org/pih/warehouse/api/InternalLocationApiController.groovy
@@ -0,0 +1,34 @@
+/**
+* Copyright (c) 2012 Partners In Health.  All rights reserved.
+* The use and distribution terms for this software are covered by the
+* Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+* which can be found in the file epl-v10.html at the root of this distribution.
+* By using this software in any fashion, you are agreeing to be bound by
+* the terms of this license.
+* You must not remove this notice, or any other, from this software.
+**/ 
+package org.pih.warehouse.api
+
+import grails.converters.JSON
+import org.hibernate.ObjectNotFoundException
+import org.pih.warehouse.core.Location
+import org.pih.warehouse.core.LocationTypeCode
+
+class LocationApiController extends BaseDomainApiController {
+
+    def locationService
+
+    def list = {
+        def fields = params.fields ? params.fields.split(",") : null
+        def locations
+
+        if (params.type=="INTERNAL")
+            locations = locationService.getInternalLocations()
+        else
+            locations = locationService.getAllLocations(fields)
+
+        render ([data:locations] as JSON)
+	}
+
+
+}
