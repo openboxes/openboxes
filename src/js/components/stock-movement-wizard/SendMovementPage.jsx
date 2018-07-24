87dd9d54089fdae53ebc105033aac456a885e6fc
Artur Walkowiak <awalkowiak@soldevelo.com>
2018-07-24 15:34:29 +0200

    Step 5 - fetch all required stock movement data

--- src/js/components/stock-movement-wizard/SendMovementPage.jsx
+++ src/js/components/stock-movement-wizard/SendMovementPage.jsx
@@ -50,15 +50,53 @@ class SendMovementPage extends Component {
     super(props);
     this.state = {
       shipmentTypes: [],
+      stockMovementData: {},
+      tableItems: [],
+      supplier: false,
+      printDeliveryNote: '',
+      printPackingList: '',
+      printCertOfDonation: '',
     };
+    this.props.showSpinner();
   }
 
   componentDidMount() {
+    this.props.showSpinner();
     this.fetchShipmentTypes();
+    this.fetchStockMovementData()
+      .then((response) => {
+        const stockMovementData = response.data.data;
+        const { associations } = response.data.data;
+
+        let tableItems;
+        let supplier;
+        if (!_.isEmpty(stockMovementData) && stockMovementData.pickPage.pickPageItems.length) {
+          tableItems = _.reduce(
+            stockMovementData.pickPage.pickPageItems,
+            (result, item) => _.concat(result, item.picklistItems), [],
+          );
+          supplier = false;
+        } else {
+          tableItems = this.props.lineItems;
+          supplier = true;
+        }
+        const printDeliveryNote = _.find(associations.documents, doc => doc.name === 'Delivery Note');
+        const printPackingList = _.find(associations.documents, doc => doc.name === 'Download Packing List');
+        const printCertOfDonation = _.find(associations.documents, doc => doc.name === 'Download Suitcase Letter');
+
+        this.setState({
+          stockMovementData,
+          tableItems,
+          supplier,
+          printDeliveryNote: !_.isEmpty(printDeliveryNote) ? printDeliveryNote.uri : '',
+          printPackingList: !_.isEmpty(printPackingList) ? printPackingList.uri : '',
+          printCertOfDonation: !_.isEmpty(printCertOfDonation) ? printCertOfDonation.uri : '',
+        }, () => this.props.hideSpinner());
+      })
+      .catch(() => this.props.hideSpinner());
   }
 
   fetchShipmentTypes() {
-    this.props.showSpinner();
     const url = '/openboxes/api/generic/shipmentType';
 
     return apiClient.get(url)
@@ -71,33 +109,45 @@ class SendMovementPage extends Component {
       .catch(() => this.props.hideSpinner());
   }
 
+  fetchStockMovementData() {
+    const url = `/openboxes/api/stockMovements/${this.props.stockMovementId}?stepNumber=5`;
+
+    return apiClient.get(url);
+  }
+
   render() {
     const {
-      handleSubmit, pristine, previousPage, submitting, pickPageItems, lineItems,
-      description, origin, destination, stockList, requestedBy, dateRequested, movementNumber,
+      handleSubmit, pristine, previousPage, submitting,
     } = this.props;
-
-    let tableItems;
-    if (pickPageItems.length) {
-      tableItems = _.filter(pickPageItems, pick => !!pick.lot && !pick.crossedOut);
-    } else { tableItems = lineItems; }
 
     return (
       <div>
         <hr />
         <div className="print-buttons-container">
-          <button type="button" className="py-1 mb-1 btn btn-outline-secondary d-print-none">
+          <a
+            href={this.state.printDeliveryNote}
+            className="py-1 mb-1 btn btn-outline-secondary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span><i className="fa fa-print pr-2" />Print Delivery Note</span>
-          </button>
-          <button type="button" className="py-1 mb-1 btn btn-outline-secondary d-print-none">
+          </a>
+          <a
+            href={this.state.printPackingList}
+            className="py-1 mb-1 btn btn-outline-secondary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span><i className="fa fa-print pr-2" />Print Packing List</span>
-          </button>
-          <button type="button" className="py-1 mb-1 btn btn-outline-secondary d-print-none">
+          </a>
+          <a
+            href={this.state.printCertOfDonation}
+            className="py-1 mb-1 btn btn-outline-secondary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span><i className="fa fa-print pr-2" />Print Certificate of Donation</span>
-          </button>
-          <button type="button" className="py-1 mb-1 btn btn-outline-secondary d-print-none">
-            <span><i className="fa fa-upload pr-2" />Upload documents</span>
-          </button>
+          </a>
         </div>
         <form onSubmit={handleSubmit}>
           <div className="row">
@@ -105,7 +155,7 @@ class SendMovementPage extends Component {
             Description
             </span>
             <span className="col-md-4 align-self-center">
-              {description}
+              {this.state.stockMovementData.description}
             </span>
           </div>
           <div className="row">
@@ -113,7 +163,7 @@ class SendMovementPage extends Component {
             From
             </span>
             <span className="col-md-4 align-self-center">
-              {origin.name}
+              {this.state.stockMovementData.origin ? this.state.stockMovementData.origin.name : ''}
             </span>
           </div>
           <div className="row">
@@ -121,7 +171,7 @@ class SendMovementPage extends Component {
             To
             </span>
             <span className="col-md-4 align-self-center">
-              {destination.name}
+              {this.state.stockMovementData.destination ? this.state.stockMovementData.destination.name : ''}
             </span>
           </div>
           <div className="row">
@@ -129,7 +179,7 @@ class SendMovementPage extends Component {
             Stock List
             </span>
             <span className="col-md-4 align-self-center">
-              {stockList}
+              {this.state.stockMovementData.stockList ? this.state.stockMovementData.stockList.name : ''}
             </span>
           </div>
           <div className="row">
@@ -137,7 +187,7 @@ class SendMovementPage extends Component {
             Requested by
             </span>
             <span className="col-md-4 align-self-center">
-              {requestedBy}
+              {this.state.stockMovementData.requestedBy ? this.state.stockMovementData.requestedBy.name : ''}
             </span>
           </div>
           <div className="row">
@@ -145,7 +195,7 @@ class SendMovementPage extends Component {
             Date requested
             </span>
             <span className="col-md-4 align-self-center">
-              {dateRequested}
+              {this.state.stockMovementData.dateRequested}
             </span>
           </div>
           <div className="row">
@@ -153,7 +203,7 @@ class SendMovementPage extends Component {
             Shipment name
             </span>
             <span className="col-md-4 align-self-center">
-              {`"${origin.name}.${destination.name}.${dateRequested}.${stockList}.${movementNumber}.${description}"`}
+              {this.state.stockMovementData.name}
             </span>
           </div>
           <hr />
@@ -164,30 +214,48 @@ class SendMovementPage extends Component {
             <table className="table table-striped text-center border">
               <thead>
                 <tr>
-                  <th>Product</th>
-                  <th>Serial / Lot number</th>
+                  <th>Code</th>
+                  <th>Product Name</th>
+                  <th>Lot number</th>
                   <th>Expiry Date</th>
-                  <th>Quantity</th>
-                  <th>Bin</th>
+                  <th>Quantity Picked</th>
+                  {(this.state.supplier) &&
+                    <th>Pallet</th>
+                  }
+                  {(this.state.supplier) &&
+                    <th>Box</th>
+                  }
+                  {!(this.state.supplier) &&
+                    <th>Bin</th>
+                  }
                   <th>Recipient</th>
                 </tr>
               </thead>
               <tbody>
                 {
                 _.map(
-                  tableItems,
+                  this.state.tableItems,
                   (item, index) =>
                     (
                       <tr key={index}>
-                        <td>{item.product.name}</td>
-                        <td>{item.lot}</td>
+                        <td>{item.productCode || item.product.productCode}</td>
+                        <td>{item['product.name'] || item.product.name}</td>
+                        <td>{item.lotNumber}</td>
                         <td>
-                          {item.expiryDate || item.expiry}
+                          {item.expirationDate}
                         </td>
                         <td>
-                          {item.qtyPicked || item.quantity || item.quantityRequested}
+                          {item.quantityPicked || item.quantity}
                         </td>
-                        <td>{item.bin}</td>
+                        {(this.state.supplier) &&
+                          <td>{item.pallet}</td>
+                        }
+                        {(this.state.supplier) &&
+                          <td>{item.box}</td>
+                        }
+                        {!(this.state.supplier) &&
+                          <td>{item['binLocation.name']}</td>
+                        }
                         <td>
                           {item.recipient ? <span className="fa fa-user" /> : null}
                         </td>
@@ -212,15 +280,8 @@ class SendMovementPage extends Component {
 const selector = formValueSelector('stock-movement-wizard');
 
 const mapStateToProps = state => ({
-  description: selector(state, 'description'),
-  origin: selector(state, 'origin'),
-  destination: selector(state, 'destination'),
-  stockList: selector(state, 'stockList'),
-  requestedBy: selector(state, 'requestedBy'),
-  dateRequested: selector(state, 'dateRequested'),
-  pickPageItems: selector(state, 'pickPageItems'),
   lineItems: selector(state, 'lineItems'),
-  movementNumber: selector(state, 'movementNumber'),
+  stockMovementId: selector(state, 'requisitionId'),
 });
 
 export default reduxForm({
@@ -231,18 +292,6 @@ export default reduxForm({
 })(connect(mapStateToProps, { showSpinner, hideSpinner })(SendMovementPage));
 
 SendMovementPage.propTypes = {
-  description: PropTypes.string.isRequired,
-  origin: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-  destination: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-  stockList: PropTypes.string,
-  requestedBy: PropTypes.string.isRequired,
-  dateRequested: PropTypes.string.isRequired,
-  movementNumber: PropTypes.string.isRequired,
-  pickPageItems: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   lineItems: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   handleSubmit: PropTypes.func.isRequired,
   previousPage: PropTypes.func.isRequired,
@@ -250,8 +299,5 @@ SendMovementPage.propTypes = {
   submitting: PropTypes.bool.isRequired,
   showSpinner: PropTypes.func.isRequired,
   hideSpinner: PropTypes.func.isRequired,
-};
-
-SendMovementPage.defaultProps = {
-  stockList: 'New Stock List',
+  stockMovementId: PropTypes.string.isRequired,
 };
