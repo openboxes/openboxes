201cef85b964e11a0e4b9f9b4153755e44458716
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-07-26 14:03:09 +0200

    OBPIH-919: Debounce field preview rendering during scrolling to improve performance

--- src/js/components/form-elements/TableRow.jsx
+++ src/js/components/form-elements/TableRow.jsx
@@ -4,8 +4,26 @@ import PropTypes from 'prop-types';
 import { renderFormField } from '../../utils/form-utils';
 
 class TableRow extends Component {
-  shouldComponentUpdate(nextProps) {
-    return !_.isEqualWith(this.props, nextProps, (objValue, othValue) => {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fieldPreview: _.isNil(props.fieldPreview) ? false : props.fieldPreview,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.fieldPreview && this.state.fieldPreview) {
+      this.setState({ fieldPreview: nextProps.fieldPreview });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.fieldPreview !== nextState.fieldPreview) {
+      return true;
+    }
+
+    return !_.isEqualWith(_.omit(this.props, 'fieldPreview'), _.omit(nextProps, 'fieldPreview'), (objValue, othValue) => {
       if (typeof objValue === 'function' || typeof othValue === 'function') {
         return true;
       }
@@ -20,7 +38,9 @@ class TableRow extends Component {
     } = this.props;
 
     const dynamicAttr = fieldsConfig.getDynamicRowAttr ?
-      fieldsConfig.getDynamicRowAttr({ ...properties, index, rowValues }) : {};
+      fieldsConfig.getDynamicRowAttr({
+        ...properties, index, rowValues, fieldPreview: this.state.fieldPreview,
+      }) : {};
     const rowIndex = !_.isNil(properties.parentIndex) ? properties.parentIndex : index;
     const className = `table-row ${rowIndex % 2 === 0 ? 'even-row' : ''} ${dynamicAttr.className ? dynamicAttr.className : ''}`;
 
@@ -31,6 +51,7 @@ class TableRow extends Component {
             <div key={`${field}.${name}`} className="align-self-center mx-1" style={{ flex: '1 1 0', minWidth: 0 }}>
               {renderFormField(config, `${field}.${name}`, {
                 ...properties,
+                fieldPreview: this.state.fieldPreview,
                 arrayField: true,
                 addRow,
                 removeRow,
@@ -56,8 +77,10 @@ TableRow.propTypes = {
   removeRow: PropTypes.func.isRequired,
   properties: PropTypes.shape({}).isRequired,
   rowValues: PropTypes.shape({}),
+  fieldPreview: PropTypes.bool,
 };
 
 TableRow.defaultProps = {
   rowValues: {},
+  fieldPreview: false,
 };
