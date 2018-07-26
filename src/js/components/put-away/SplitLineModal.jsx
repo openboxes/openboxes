b23e3ef1286d7d9d6149146318746b6b067e0162
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-07-26 17:10:10 +0200

    OBPIH-924: Change validation and add confirmation dialog to split lines

--- src/js/components/put-away/SplitLineModal.jsx
+++ src/js/components/put-away/SplitLineModal.jsx
@@ -3,23 +3,56 @@ import React, { Component } from 'react';
 import { Tooltip } from 'react-tippy';
 import update from 'immutability-helper';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
+import { confirmAlert } from 'react-confirm-alert';
 
-import ModalWrapper from '../form-elements/ModalWrapper';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
 import Input from '../../utils/Input';
 import Select from '../../utils/Select';
+
+Modal.setAppElement('#root');
 
 class SplitLineModal extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { splitItems: [] };
+    this.state = { splitItems: [], showModal: false };
 
-    this.onOpen = this.onOpen.bind(this);
+    this.openModal = this.openModal.bind(this);
     this.onSave = this.onSave.bind(this);
+    this.closeModal = this.closeModal.bind(this);
     this.isValid = this.isValid.bind(this);
   }
 
-  onOpen() {
+  onSave() {
+    const putAwayQty = this.calculatePutAwayQty();
+
+    if (putAwayQty < this.props.putawayItem.quantity) {
+      confirmAlert({
+        title: 'Confirm split line',
+        message: 'There is still stock in the receiving bin. Do you want to put away the rest of this line?',
+        buttons: [
+          {
+            label: 'Yes',
+          },
+          {
+            label: 'No',
+            onClick: () => this.save(),
+          },
+        ],
+      });
+    } else {
+      this.save();
+    }
+  }
+
+  save() {
+    this.props.saveSplitItems(_.filter(this.state.splitItems, item => item.quantity && item.quantity !== '0'));
+    this.closeModal();
+  }
+
+  openModal() {
     let splitItems = [];
 
     if (this.props.splitItems && this.props.splitItems.length > 0) {
@@ -38,109 +71,140 @@ class SplitLineModal extends Component {
       });
     }
 
-    this.setState({ splitItems });
+    this.setState({ splitItems, showModal: true });
   }
 
-  onSave() {
-    this.props.saveSplitItems(_.filter(this.state.splitItems, item => item.quantity && item.quantity !== '0'));
+  closeModal() {
+    this.setState({ showModal: false });
   }
 
   isValid() {
     const qtySum = _.reduce(this.state.splitItems, (sum, val) =>
       (sum + (val.quantity ? _.toInteger(val.quantity) : 0)), 0);
 
-    return qtySum === _.toInteger(this.props.putawayItem.quantity);
+    return qtySum <= _.toInteger(this.props.putawayItem.quantity);
+  }
+
+  calculatePutAwayQty() {
+    return _.reduce(this.state.splitItems, (sum, val) =>
+      (sum + (val.quantity ? _.toInteger(val.quantity) : 0)), 0);
   }
 
   render() {
     return (
-      <ModalWrapper
-        btnOpenText="Split line"
-        btnOpenClassName="btn btn-outline-success"
-        onOpen={this.onOpen}
-        onSave={this.onSave}
-        btnSaveDisabled={!this.isValid()}
-        title={() => (
+      <div>
+        <button
+          type="button"
+          className="btn btn-outline-success"
+          onClick={() => this.openModal()}
+        >Split line
+        </button>
+        <Modal
+          isOpen={this.state.showModal}
+          onRequestClose={this.closeModal}
+          className="modal-content-custom"
+          shouldCloseOnOverlayClick={false}
+        >
           <div>
             <h3 className="font-weight-bold">{`${this.props.putawayItem.product.productCode} ${this.props.putawayItem.product.name}`}</h3>
             <div className="font-weight-bold">Expiry: {this.props.putawayItem.inventoryItem.expirationDate}</div>
             <div className="font-weight-bold">Total QTY: {this.props.putawayItem.quantity}</div>
-          </div>)}
-      >
-        <div className="text-center">
-          <table className="table table-striped text-center border">
-            <thead>
-              <tr>
-                <th>Put Away Bin</th>
-                <th>Quantity</th>
-              </tr>
-            </thead>
-            <tbody>
-              { _.map(this.state.splitItems, (item, index) => (
-                <tr
-                  // eslint-disable-next-line react/no-array-index-key
-                  key={index}
-                >
-                  <td className="align-middle">
-                    <Select
-                      options={this.props.bins}
-                      value={item.putawayLocation ? item.putawayLocation.id : null}
-                      onChange={value => this.setState({
-                        splitItems: update(this.state.splitItems, {
-                          [index]: {
-                            putawayLocation: { id: { $set: value } },
-                          },
-                        }),
-                      })}
-                    />
-                  </td>
-                  <td className="align-middle">
-                    <Tooltip
-                      // eslint-disable-next-line max-len
-                      html={(<div>Sum of all split items quantities should equal original put-away item quantity</div>)}
-                      disabled={this.isValid()}
-                      theme="transparent"
-                      arrow="true"
-                      delay="150"
-                      duration="250"
-                      hideDelay="50"
-                    >
-                      <div className={!this.isValid() ? 'has-error' : ''}>
-                        <Input
-                          type="number"
-                          className="form-control"
-                          value={!item.quantity ? 0 : item.quantity}
-                          onChange={value => this.setState({
-                            splitItems: update(this.state.splitItems, {
-                              [index]: { quantity: { $set: value } },
-                            }),
-                          })}
-                        />
-                      </div>
-                    </Tooltip>
-                  </td>
+            <div className="font-weight-bold">Put away QTY: {this.calculatePutAwayQty()}</div>
+          </div>
+          <hr />
+
+          <div className="text-center">
+            <table className="table table-striped text-center border">
+              <thead>
+                <tr>
+                  <th>Put Away Bin</th>
+                  <th>Quantity</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <button
-            className="btn btn-outline-success"
-            onClick={() => this.setState({
-              splitItems: update(this.state.splitItems, {
-                $push: [{
-                  quantity: 0,
-                  putawayFacility: {
-                    id: this.props.putawayItem.putawayFacility
-                      ? this.props.putawayItem.putawayFacility.id : null,
-                  },
-                  putawayLocation: { id: null },
-                }],
-              }),
-            })}
-          >Add Line
-          </button>
-        </div>
-      </ModalWrapper>
+              </thead>
+              <tbody>
+                { _.map(this.state.splitItems, (item, index) => (
+                  <tr
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={index}
+                  >
+                    <td className="align-middle">
+                      <Select
+                        options={this.props.bins}
+                        value={item.putawayLocation ? item.putawayLocation.id : null}
+                        onChange={value => this.setState({
+                          splitItems: update(this.state.splitItems, {
+                            [index]: {
+                              putawayLocation: { id: { $set: value } },
+                            },
+                          }),
+                        })}
+                      />
+                    </td>
+                    <td className="align-middle">
+                      <Tooltip
+                        // eslint-disable-next-line max-len
+                        html={(<div>Sum of all split items quantities cannot be higher than original put-away item quantity</div>)}
+                        disabled={this.isValid()}
+                        theme="transparent"
+                        arrow="true"
+                        delay="150"
+                        duration="250"
+                        hideDelay="50"
+                      >
+                        <div className={!this.isValid() ? 'has-error' : ''}>
+                          <Input
+                            type="number"
+                            className="form-control"
+                            value={!item.quantity ? 0 : item.quantity}
+                            onChange={value => this.setState({
+                              splitItems: update(this.state.splitItems, {
+                                [index]: { quantity: { $set: value } },
+                              }),
+                            })}
+                          />
+                        </div>
+                      </Tooltip>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button
+              className="btn btn-outline-success"
+              onClick={() => this.setState({
+                  splitItems: update(this.state.splitItems, {
+                    $push: [{
+                      quantity: 0,
+                      putawayFacility: {
+                        id: this.props.putawayItem.putawayFacility
+                          ? this.props.putawayItem.putawayFacility.id : null,
+                      },
+                      putawayLocation: { id: null },
+                    }],
+                  }),
+                })}
+            >Add Line
+            </button>
+          </div>
+
+          <hr />
+          <div className="btn-group float-right" role="group">
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              disabled={!this.isValid()}
+              onClick={() => this.onSave()}
+            >Save
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={() => this.closeModal()}
+            >Cancel
+            </button>
+          </div>
+        </Modal>
+      </div>
     );
   }
 }
