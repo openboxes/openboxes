a44e78cabbd003291b446c39f87639c8dc1d79b7
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-09-26 18:05:57 +0200

    OBPIH-1217: Change format of expiry date on all stock movement screens

--- src/groovy/org/pih/warehouse/api/StockAdjustment.groovy
+++ src/groovy/org/pih/warehouse/api/StockAdjustment.groovy
@@ -20,7 +20,7 @@ class StockAdjustment {
         return [
                 "inventoryItem.id": inventoryItem?.id,
                 "inventoryItem.lotNumber": inventoryItem?.lotNumber,
-                "inventoryItem.expirationDate": inventoryItem?.expirationDate,
+                "inventoryItem.expirationDate": inventoryItem?.expirationDate?.format("MM/dd/yyyy"),
                 "product.id": inventoryItem?.product?.id,
                 "product.productCode": inventoryItem?.product?.productCode,
                 "product.name": inventoryItem?.product?.name,
