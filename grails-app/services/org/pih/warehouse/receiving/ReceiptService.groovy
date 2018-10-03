927756cda25ba299d265d6908b497bba73f0529d
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-10-03 16:03:44 +0200

    OBPIH-888: Process split line items on backend

--- grails-app/services/org/pih/warehouse/receiving/ReceiptService.groovy
+++ grails-app/services/org/pih/warehouse/receiving/ReceiptService.groovy
@@ -9,14 +9,12 @@
 **/ 
 package org.pih.warehouse.receiving
 
-import grails.validation.ValidationException
 import org.pih.warehouse.api.PartialReceipt
 import org.pih.warehouse.api.PartialReceiptContainer
 import org.pih.warehouse.api.PartialReceiptItem
 import org.pih.warehouse.core.EventCode
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.inventory.InventoryItem
-import org.pih.warehouse.shipping.Container
 import org.pih.warehouse.shipping.Shipment
 import org.pih.warehouse.shipping.ShipmentItem
 
@@ -35,7 +33,7 @@ class ReceiptService {
         partialReceipt.shipment = shipment
         partialReceipt.recipient = shipment.recipient
         partialReceipt.dateShipped = shipment.actualShippingDate
-        partialReceipt.dateDelivered = shipment.actualDeliveryDate
+        partialReceipt.dateDelivered = shipment.actualDeliveryDate ?: new Date()
 
         Location defaultBinLocation =
                 locationService.findInternalLocation(shipment.destination, "Receiving ${shipment.shipmentNumber}")
@@ -48,19 +46,85 @@ class ReceiptService {
             partialReceipt.partialReceiptContainers.add(partialReceiptContainer)
 
             shipmentItems.collect { ShipmentItem shipmentItem ->
-                PartialReceiptItem partialReceiptItem = new PartialReceiptItem()
-                partialReceiptItem.shipmentItem = shipmentItem
-                partialReceiptItem.recipient = shipmentItem.recipient
-                if (defaultBinLocation) {
-                    partialReceiptItem.binLocation = defaultBinLocation
+                if (shipmentItem.receiptItems) {
+                    shipmentItem.receiptItems.collect { ReceiptItem receiptItem ->
+                        partialReceiptContainer.partialReceiptItems.add(buildPartialReceiptItem(receiptItem))
+                    }
+                } else {
+                    partialReceiptContainer.partialReceiptItems.add(buildPartialReceiptItem(shipmentItem, defaultBinLocation))
                 }
-                partialReceiptContainer.partialReceiptItems.add(partialReceiptItem)
             }
         }
         return partialReceipt
     }
 
+    PartialReceiptItem buildPartialReceiptItem(ShipmentItem shipmentItem, Location binLocation) {
+        PartialReceiptItem partialReceiptItem = new PartialReceiptItem()
+        partialReceiptItem.shipmentItem = shipmentItem
+        partialReceiptItem.recipient = shipmentItem.recipient
+        if (binLocation) {
+            partialReceiptItem.binLocation = binLocation
+        }
+        partialReceiptItem.lotNumber = shipmentItem.inventoryItem?.lotNumber
+        partialReceiptItem.expirationDate = shipmentItem.inventoryItem?.expirationDate
+        partialReceiptItem.quantityShipped = shipmentItem?.quantity?:0
 
+        return partialReceiptItem
+    }
+
+    PartialReceiptItem buildPartialReceiptItem(ReceiptItem receiptItem) {
+        PartialReceiptItem partialReceiptItem = new PartialReceiptItem()
+        partialReceiptItem.shipmentItem = receiptItem.shipmentItem
+        partialReceiptItem.receiptItem = receiptItem
+        partialReceiptItem.recipient = receiptItem.recipient
+        partialReceiptItem.binLocation = receiptItem.binLocation
+        partialReceiptItem.quantityReceiving = receiptItem.quantityReceived
+
+        partialReceiptItem.lotNumber = receiptItem.inventoryItem?.lotNumber
+        partialReceiptItem.expirationDate = receiptItem.inventoryItem?.expirationDate
+        partialReceiptItem.quantityShipped = receiptItem?.quantityShipped?:0
+
+        return partialReceiptItem
+    }
+
+    ReceiptItem createReceiptItem(PartialReceiptItem partialReceiptItem) {
+        ShipmentItem shipmentItem = partialReceiptItem.shipmentItem
+        if (!partialReceiptItem.shipmentItem) {
+            throw new IllegalArgumentException("Cannot receive item without valid shipment item")
+        }
+
+        InventoryItem inventoryItem =
+                inventoryService.findOrCreateInventoryItem(partialReceiptItem.product, partialReceiptItem.lotNumber, partialReceiptItem.expirationDate)
+
+        if (!inventoryItem) {
+            throw new IllegalArgumentException("Cannot receive item without valid inventory item")
+        }
+
+        ReceiptItem receiptItem
+
+        if (partialReceiptItem.receiptItem) {
+            receiptItem = partialReceiptItem.receiptItem
+        } else {
+            receiptItem = new ReceiptItem()
+        }
+
+        receiptItem.binLocation = partialReceiptItem.binLocation
+        receiptItem.recipient = partialReceiptItem.recipient
+        receiptItem.quantityShipped = partialReceiptItem.quantityShipped
+        receiptItem.quantityReceived = partialReceiptItem.quantityReceiving
+        receiptItem.lotNumber = partialReceiptItem.lotNumber
+        receiptItem.expirationDate = partialReceiptItem.expirationDate
+        receiptItem.product = inventoryItem.product
+        receiptItem.inventoryItem = inventoryItem
+        receiptItem.shipmentItem = partialReceiptItem.shipmentItem
+
+        if (partialReceiptItem.cancelRemaining) {
+            receiptItem.quantityCanceled = shipmentItem.quantityRemaining - partialReceiptItem.quantityReceiving
+        }
+
+        partialReceiptItem.receiptItem = receiptItem
+        return receiptItem
+    }
 
     void savePartialReceipt(PartialReceipt partialReceipt) {
 
@@ -84,33 +148,9 @@ class ReceiptService {
 
             log.info "Saving partial receipt item " + partialReceiptItem
             if (partialReceiptItem.quantityReceiving != null) {
-                ShipmentItem shipmentItem = partialReceiptItem.shipmentItem
-                if (!shipmentItem) {
-                    throw new IllegalArgumentException("Cannot receive item without valid shipment item")
-                }
-
-                InventoryItem inventoryItem =
-                        inventoryService.findOrCreateInventoryItem(shipmentItem.product, shipmentItem.lotNumber, shipmentItem.expirationDate)
-
-                if (!inventoryItem) {
-                    throw new IllegalArgumentException("Cannot receive item without valid inventory item")
-                }
-
-                ReceiptItem receiptItem = new ReceiptItem();
-                receiptItem.binLocation = partialReceiptItem.binLocation
-                receiptItem.recipient = partialReceiptItem.recipient
-                receiptItem.quantityShipped = shipmentItem.quantity;
-                receiptItem.quantityReceived = partialReceiptItem.quantityReceiving
-                receiptItem.lotNumber = shipmentItem.lotNumber;
-                receiptItem.product = inventoryItem.product
-                receiptItem.inventoryItem = inventoryItem
-                receiptItem.shipmentItem = shipmentItem
-
-                if (partialReceiptItem.cancelRemaining) {
-                    receiptItem.quantityCanceled = shipmentItem.quantityRemaining - partialReceiptItem.quantityReceiving
-                }
-
+                ReceiptItem receiptItem = createReceiptItem(partialReceiptItem)
                 receipt.addToReceiptItems(receiptItem)
+                ShipmentItem shipmentItem = partialReceiptItem.shipmentItem
                 shipmentItem.addToReceiptItems(receiptItem)
             }
         }
--- src/groovy/org/pih/warehouse/api/PartialReceiptItem.groovy
+++ src/groovy/org/pih/warehouse/api/PartialReceiptItem.groovy
@@ -2,24 +2,25 @@ package org.pih.warehouse.api
 
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.core.Person
+import org.pih.warehouse.inventory.InventoryItem
+import org.pih.warehouse.product.Product
 import org.pih.warehouse.receiving.ReceiptItem
 import org.pih.warehouse.shipping.ShipmentItem
 
 class PartialReceiptItem {
 
+    ReceiptItem receiptItem
     ShipmentItem shipmentItem
-//    Integer quantityShipped
-//    Integer quantityReceived
     Integer quantityReceiving
 
     Location binLocation
     Person recipient
     Boolean cancelRemaining = Boolean.FALSE
 
-
-    Integer getQuantityShipped() {
-        return shipmentItem?.quantity?:0
-    }
+    String lotNumber
+    Date expirationDate
+    Integer quantityShipped
+    Product product
 
     Integer getQuantityReceived() {
         def receiptItems = ReceiptItem.findAllByShipmentItem(shipmentItem)
@@ -40,20 +41,27 @@ class PartialReceiptItem {
         return !cancelRemaining ? (quantityRemaining > 0) ? quantityRemaining : 0 : 0
     }
 
+    InventoryItem getInventoryItem() {
+        receiptItem ? receiptItem.inventoryItem : shipmentItem?.inventoryItem
+    }
+
+    Product getProduct() {
+        return product ?: inventoryItem?.product
+    }
 
     Map toJson() {
         return [
-                "shipmentItem.id": shipmentItem?.id,
+                "shipmentItemId": shipmentItem?.id,
+                "receiptItemId": receiptItem?.id,
                 "container.id": shipmentItem?.container?.id,
                 "container.name": shipmentItem?.container?.name,
                 "parentContainer.id": shipmentItem?.container?.parentContainer?.id,
                 "parentContainer.name": shipmentItem?.container?.parentContainer?.name,
-                "product.id": shipmentItem?.inventoryItem?.product?.id,
-                "product.productCode": shipmentItem?.inventoryItem?.product?.productCode,
-                "product.name": shipmentItem?.inventoryItem?.product?.name,
-                "inventoryItem.id": shipmentItem?.inventoryItem?.id,
-                "inventoryItem.lotNumber": shipmentItem?.inventoryItem?.lotNumber,
-                "inventoryItem.expirationDate": shipmentItem?.inventoryItem?.expirationDate?.format("MM/dd/yyyy"),
+                "product.id": inventoryItem?.product?.id,
+                "product.productCode": inventoryItem?.product?.productCode,
+                "product.name": inventoryItem?.product?.name,
+                "lotNumber": lotNumber,
+                "expirationDate": expirationDate?.format("MM/dd/yyyy"),
                 "binLocation.id": binLocation?.id,
                 "binLocation.name": binLocation?.name,
                 "recipient.id": recipient?.id,
