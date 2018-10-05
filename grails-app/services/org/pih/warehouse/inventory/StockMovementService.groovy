77be665f0872ffa0088aac64ee93590b66a12544
Justin Miranda <jcm62@columbia.edu>
2018-10-04 23:48:47 -0500

    OBPIH-1232 Recipient is not pulled through to Step 5 if an item has been modified / substituted

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -771,7 +771,8 @@ class StockMovementService {
 
         if (stockMovement.origin.isSupplier()) {
             stockMovement.lineItems.collect { StockMovementItem stockMovementItem ->
-                log.info "Process item ${stockMovementItem.toJson()}"
+                log.info "Process stock movement item " + (new JSONObject(stockMovementItem.toJson())).toString(4)
+
                 if (stockMovementItem.delete) {
                     log.info "Delete item ${stockMovementItem}"
                     ShipmentItem shipmentItem = ShipmentItem.get(stockMovementItem?.id)
@@ -857,7 +858,8 @@ class StockMovementService {
         shipmentItem.product = picklistItem?.inventoryItem?.product
         shipmentItem.quantity = picklistItem?.quantity
         shipmentItem.requisitionItem = picklistItem.requisitionItem
-        shipmentItem.recipient = picklistItem?.requisitionItem?.recipient
+        shipmentItem.recipient = picklistItem?.requisitionItem?.recipient?:
+                picklistItem?.requisitionItem?.parentRequisitionItem?.recipient
         shipmentItem.inventoryItem = picklistItem?.inventoryItem
         shipmentItem.binLocation = picklistItem?.binLocation
         return shipmentItem
--- src/groovy/org/pih/warehouse/api/StockMovementItem.groovy
+++ src/groovy/org/pih/warehouse/api/StockMovementItem.groovy
@@ -176,7 +176,7 @@ class StockMovementItem {
                 substitutionItems: substitutionItems,
                 reasonCode: requisitionItem.cancelReasonCode,
                 comments: requisitionItem.cancelComments,
-                recipient: requisitionItem.recipient,
+                recipient: requisitionItem.recipient?:requisitionItem?.parentRequisitionItem?.recipient,
                 palletName: requisitionItem?.palletName?:"",
                 boxName: requisitionItem?.boxName?:"",
                 lotNumber: requisitionItem?.lotNumber?:"",
