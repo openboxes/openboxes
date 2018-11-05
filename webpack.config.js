196e19f623d0812b088edddcad01b69adab8dbb3
Artur Walkowiak <awalkowiak@soldevelo.com>
2018-11-05 19:12:26 +0100

    OBPIH-1253: Resources caching with hash in name

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,17 @@ const path = require('path');
 
 const ROOT = path.resolve(__dirname, 'src');
 const SRC = path.resolve(ROOT, 'js');
-const DEST = path.resolve(__dirname, 'web-app/js');
+const ASSETS = path.resolve(ROOT, 'assets');
+const JS_DEST = path.resolve(__dirname, 'web-app/js');
+const CSS_DEST = path.resolve(__dirname, 'web-app/css');
+const GRAILS_VIEWS = path.resolve(__dirname, 'grails-app/views');
+const STOCK_MOVEMENT_VIEW = path.resolve(GRAILS_VIEWS, 'stockMovement');
+const PUT_AWAY_VIEW = path.resolve(GRAILS_VIEWS, 'putAway');
+const RECEIVING_VIEW = path.resolve(GRAILS_VIEWS, 'partialReceiving');
+
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -12,8 +21,8 @@ module.exports = {
     app: `${SRC}/index.jsx`,
   },
   output: {
-    path: DEST,
-    filename: 'bundle.js',
+    path: JS_DEST,
+    filename: 'bundle.[hash].js',
     publicPath: '/js/',
   },
   stats: {
@@ -24,8 +33,44 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery',
     }),
-    new ExtractTextPlugin('../css/bundle.css', {
+    new ExtractTextPlugin({
+      filename: '../css/bundle.[hash].css',
       allChunks: true,
+    }),
+    new CleanWebpackPlugin([`${JS_DEST}/bundle.*`, `${CSS_DEST}/bundle.*`]),
+    new HtmlWebpackPlugin({
+      filename: `${STOCK_MOVEMENT_VIEW}/_create.gsp`,
+      template: `${ASSETS}/grails-template.html`,
+      inject: false,
+      templateParameters: compilation => ({
+        jsSource: `\${createLinkTo(dir:'/js', file:'bundle.${compilation.hash}.js')}`,
+        cssSource: `\${createLinkTo(dir:'css/', file:'bundle.${compilation.hash}.css')}`,
+        receivingIfStatement: '',
+      }),
+    }),
+    new HtmlWebpackPlugin({
+      filename: `${PUT_AWAY_VIEW}/_create.gsp`,
+      template: `${ASSETS}/grails-template.html`,
+      inject: false,
+      templateParameters: compilation => ({
+        jsSource: `\${createLinkTo(dir:'/js', file:'bundle.${compilation.hash}.js')}`,
+        cssSource: `\${createLinkTo(dir:'css/', file:'bundle.${compilation.hash}.css')}`,
+        receivingIfStatement: '',
+      }),
+    }),
+    new HtmlWebpackPlugin({
+      filename: `${RECEIVING_VIEW}/_create.gsp`,
+      template: `${ASSETS}/grails-template.html`,
+      inject: false,
+      templateParameters: compilation => ({
+        jsSource: `\${createLinkTo(dir:'/js', file:'bundle.${compilation.hash}.js')}`,
+        cssSource: `\${createLinkTo(dir:'css/', file:'bundle.${compilation.hash}.css')}`,
+        receivingIfStatement:
+          // eslint-disable-next-line no-template-curly-in-string
+          '<g:if test="${!params.id}">' +
+          'You can access the Partial Receiving feature through the details page for an inbound shipment.' +
+          '</g:if>',
+      }),
     }),
   ],
   module: {
