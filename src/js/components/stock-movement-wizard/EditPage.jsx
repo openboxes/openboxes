815f5dc529857bac489b9e8f560e6211144d0d20
Dominika Hatała <39065822+DominikaHatala@users.noreply.github.com>
2018-11-06 21:22:21 +0100

    OBPIH-1408 Enable save button on edit page (#605)

--- src/js/components/stock-movement-wizard/EditPage.jsx
+++ src/js/components/stock-movement-wizard/EditPage.jsx
@@ -272,7 +272,8 @@ class EditItemsPage extends Component {
             revision => revision.requisitionItemId === item.requisitionItemId,
           );
           return _.isEmpty(oldRevision) ? true :
-            (oldRevision.quantityRevised !== item.quantityRevised);
+            ((oldRevision.quantityRevised !== item.quantityRevised) ||
+             (oldRevision.reasonCode !== item.reasonCode));
         }
         return false;
       },
@@ -437,7 +438,7 @@ class EditItemsPage extends Component {
         validate={validate}
         mutators={{ ...arrayMutators }}
         initialValues={this.state.values}
-        render={({ handleSubmit, values, invalid }) => (
+        render={({ handleSubmit, values }) => (
           <div className="d-flex flex-column">
             <span>
               <button
@@ -449,7 +450,6 @@ class EditItemsPage extends Component {
               </button>
               <button
                 type="button"
-                disabled={invalid}
                 onClick={() => this.save(values)}
                 className="float-right mb-1 btn btn-outline-secondary align-self-end btn-xs"
               >
