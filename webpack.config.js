74acf640957080b0ef41e53e645f968a4e657210
Paweł Muchowski <pmuchowski@soldevelo.com>
2018-11-07 21:51:25 +0100

    OBPIH-942: Update webpack version and use React code splitting (#608)

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,66 @@
-const webpack = require('webpack');
 const path = require('path');
 
 const ROOT = path.resolve(__dirname, 'src');
 const SRC = path.resolve(ROOT, 'js');
-const DEST = path.resolve(__dirname, 'web-app/js');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const DEST = path.resolve(__dirname, 'web-app');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
   entry: {
     app: `${SRC}/index.jsx`,
   },
   output: {
     path: DEST,
-    filename: 'bundle.js',
-    publicPath: '/js/',
+    filename: 'js/bundle.js',
+    chunkFilename: 'js/bundle.[name].js',
+    publicPath: '/openboxes/',
   },
   stats: {
     colors: true,
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
+    new MiniCssExtractPlugin({
+      filename: 'css/bundle.css',
+      chunkFilename: 'css/bundle.[name].css',
     }),
-    new ExtractTextPlugin('../css/bundle.css', {
-      allChunks: true,
-    }),
+    new OptimizeCSSAssetsPlugin({}),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         enforce: 'pre',
         test: /\.jsx$/,
         exclude: /node_modules/,
-        loaders: ['eslint-loader'],
+        loader: 'eslint-loader',
       },
       {
         test: /\.jsx$/,
-        loaders: ['babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-1'],
+        loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-1',
         include: SRC,
         exclude: /node_modules/,
       },
       {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract('css-loader!sass-loader'),
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader'] }),
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=./fonts/[hash].[ext]',
       },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader?name=./fonts/[hash].[ext]' },
-      { test: /\.(woff|woff2)$/, use: 'url-loader?prefix=font/&limit=5000&name=./fonts/[hash].[ext]' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[hash].[ext]' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml&name=./fonts/[hash].[ext]' },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: 'url-loader?prefix=font/&limit=5000&name=./fonts/[hash].[ext]',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[hash].[ext]',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=./fonts/[hash].[ext]',
+      },
     ],
   },
   resolve: {
