fa6f61ad9ff3ee7ec9c82f1e6efdac10a48a21e7
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-11-23 18:11:15 +0100

    OBPIH-1424/OBPIH-1425: Add buttons in stocklist SubComponent and button to create new stocklist items

--- src/js/components/stock-list-management/StocklistManagement.jsx
+++ src/js/components/stock-list-management/StocklistManagement.jsx
@@ -1,13 +1,16 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 import treeTableHOC from 'react-table/lib/hoc/treeTable';
+import update from 'immutability-helper';
 
 import 'react-table/react-table.css';
 
-import apiClient from './../../utils/apiClient';
+import apiClient, { parseResponse, flattenRequest } from './../../utils/apiClient';
 import { hideSpinner, showSpinner } from '../../actions';
+import StocklistTable from './StocklistTable';
 
 const TreeTable = treeTableHOC(ReactTable);
 
@@ -21,26 +24,8 @@ const COLUMNS = [
   {
     Header: 'Monthly demand',
     accessor: 'monthlyDemand',
-  },
-  {
-    Header: 'Stocklist Name',
-    accessor: 'name',
-  },
-  {
-    Header: 'Manager',
-    accessor: 'manager.name',
-  },
-  {
-    Header: 'Replenishment period',
-    accessor: 'replenishmentPeriod',
-  },
-  {
-    Header: 'Maximum  Quantity',
-    accessor: 'maxQuantity',
-  },
-  {
-    Header: 'Unit of measure',
-    accessor: 'uom',
+    aggregate: vals => _.sum(vals),
+    className: 'text-center',
   },
 ];
 
@@ -49,6 +34,14 @@ class StocklistManagement extends Component {
     super(props);
 
     this.state = { data: [] };
+
+    this.addItem = this.addItem.bind(this);
+    this.editItem = this.editItem.bind(this);
+    this.updateItemField = this.updateItemField.bind(this);
+    this.saveItem = this.saveItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.printItem = this.printItem.bind(this);
+    this.mailItem = this.mailItem.bind(this);
   }
 
   componentWillMount() {
@@ -57,13 +50,128 @@ class StocklistManagement extends Component {
 
   fetchData() {
     this.props.showSpinner();
-    // TODO: add proper endpoint here once it will be added in backend
-    const url = '';
+    const url = `/openboxes/api/stocklistItems?product.id=${this.props.match.params.productId || ''}`;
 
     return apiClient.get(url)
-      // TODO: extract data from response
-      .then(() => this.setState({ data: [] }))
+      .then((response) => {
+        this.setState({ data: parseResponse(response.data.data) });
+      })
       .catch(this.props.hideSpinner());
+  }
+
+  addItem(locationIndex) {
+    const location = this.state.data[locationIndex];
+
+    this.setState({
+      data: update(this.state.data, {
+        [locationIndex]: {
+          stocklistItems: {
+            $push: [{
+              name: '',
+              location: {
+                id: location.location.id,
+              },
+              replenishmentPeriod: null,
+              maxQuantity: null,
+              new: true,
+            }],
+          },
+        },
+      }),
+    });
+  }
+
+  editItem(locationIndex, itemIndex) {
+    this.setState({
+      data: update(this.state.data, {
+        [locationIndex]: {
+          stocklistItems: {
+            [itemIndex]: {
+              edit: { $set: true },
+            },
+          },
+        },
+      }),
+    });
+  }
+
+  updateItemField(locationIndex, itemIndex, itemField, fieldValue) {
+    this.setState({
+      data: update(this.state.data, {
+        [locationIndex]: {
+          stocklistItems: {
+            [itemIndex]: {
+              [itemField]: { $set: fieldValue },
+            },
+          },
+        },
+      }),
+    });
+  }
+
+  saveItem(locationIndex, itemIndex, item) {
+    let url = `/openboxes/api/stocklistItems?product.id=${this.props.match.params.productId || ''}`;
+
+    if (!item.new) {
+      url = `/openboxes/api/stocklistItems/${item.requisitionItem.id}`;
+    }
+
+    apiClient.post(url, flattenRequest(item))
+      .then((response) => {
+        this.setState({
+          data: update(this.state.data, {
+            [locationIndex]: {
+              stocklistItems: {
+                [itemIndex]: {
+                  $set: parseResponse(response.data.data),
+                },
+              },
+            },
+          }),
+        });
+      })
+      .catch(this.props.hideSpinner());
+  }
+
+  deleteItem(locationIndex, itemIndex) {
+    const item = this.state.data[locationIndex].stocklistItems[itemIndex];
+
+    if (item.new) {
+      this.removeItem(locationIndex, itemIndex);
+    } else {
+      this.props.showSpinner();
+      const url = `/openboxes/api/stocklistItems/${item.requisitionItem.id}`;
+
+      apiClient.delete(url)
+        .then(() => {
+          this.removeItem(locationIndex, itemIndex);
+        })
+        .catch(this.props.hideSpinner());
+    }
+  }
+
+  // eslint-disable-next-line no-unused-vars,class-methods-use-this
+  printItem(locationIndex, itemIndex) {
+    // TODO add proper implementation
+  }
+
+  // eslint-disable-next-line no-unused-vars,class-methods-use-this
+  mailItem(locationIndex, itemIndex) {
+    // TODO add proper implementation
+  }
+
+  removeItem(locationIndex, itemIndex) {
+    this.setState({
+      data: update(this.state.data, {
+        [locationIndex]: {
+          stocklistItems: {
+            $splice: [
+              [itemIndex, 1],
+            ],
+          },
+        },
+      }),
+    });
   }
 
   render() {
@@ -72,10 +180,31 @@ class StocklistManagement extends Component {
       <TreeTable
         className="stocklist-table"
         data={data}
-        pivotBy={['locationGroup.name', 'location.name']}
+        pivotBy={['locationGroup.name']}
         columns={COLUMNS}
         defaultPageSize={20}
-        SubComponent={() => <div>Stock List Component</div>}
+        sortable={false}
+        collapseOnDataChange={false}
+        resolveData={values => values.map((row) => {
+          if (!_.get(row, 'locationGroup.name')) {
+            return { ...row, locationGroup: { name: 'No location group' } };
+          }
+
+          return row;
+        })}
+        SubComponent={({ original, index }) => (
+          <StocklistTable
+            data={original.stocklistItems}
+            availableStocklists={original.availableStocklists}
+            parentIndex={index}
+            addItem={this.addItem}
+            editItem={this.editItem}
+            updateItemField={this.updateItemField}
+            saveItem={this.saveItem}
+            deleteItem={this.deleteItem}
+            printItem={this.printItem}
+            mailItem={this.mailItem}
+          />)}
       />
     );
   }
@@ -84,6 +213,12 @@ class StocklistManagement extends Component {
 export default connect(null, { showSpinner, hideSpinner })(StocklistManagement);
 
 StocklistManagement.propTypes = {
-  hideSpinner: PropTypes.func.isRequired,
+  /** React router's object which contains information about url varaiables and params */
+  match: PropTypes.shape({
+    params: PropTypes.shape({ productId: PropTypes.string }),
+  }).isRequired,
+  /** Function called when data is loading */
   showSpinner: PropTypes.func.isRequired,
+  /** Function called when data has loaded */
+  hideSpinner: PropTypes.func.isRequired,
 };
--- src/js/components/stock-list-management/StocklistTable.jsx
+++ src/js/components/stock-list-management/StocklistTable.jsx
@@ -1,52 +1,181 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 
 import 'react-table/react-table.css';
 
-const COLUMNS = [
-  {
-    Header: 'Stocklist Name',
-    accessor: 'name',
-  },
-  {
-    Header: 'Manager',
-    accessor: 'manager.name',
-  },
-  {
-    Header: 'Replenishment period',
-    accessor: 'replenishmentPeriod',
-  },
-  {
-    Header: 'Maximum  Quantity',
-    accessor: 'maxQuantity',
-  },
-  {
-    Header: 'Unit of measure',
-    accessor: 'uom',
-  },
-];
+import Select from '../../utils/Select';
+import Input from '../../utils/Input';
+import { debouncedUsersFetch } from '../../utils/option-utils';
 
-const StocklistTable = ({ data }) => (
-  <div>
+const StocklistTable = ({
+  data, availableStocklists, parentIndex, addItem, editItem,
+  updateItemField, saveItem, deleteItem, printItem, mailItem,
+}) => {
+  const COLUMNS = [
     {
-      data && !!data.length &&
-      <ReactTable
-        data={data}
-        columns={COLUMNS}
-        showPagination={false}
-        minRows={0}
-      />
-    }
-  </div>
-);
+      Header: 'Stocklist Name',
+      accessor: 'name',
+      // eslint-disable-next-line react/prop-types
+      Cell: ({ index, original }) => {
+        if (!original.new) {
+          return original.name || '';
+        }
+
+        return (
+          <Select
+            value={original.stocklistId}
+            onChange={value => updateItemField(parentIndex, index, 'stocklistId', value)}
+            options={_.map(availableStocklists, val => ({ value: val.id, label: val.name }))}
+            className="select-xs"
+          />
+        );
+      },
+    },
+    {
+      Header: 'Manager',
+      accessor: 'manager.name',
+      // eslint-disable-next-line react/prop-types
+      Cell: ({ index, original }) => {
+        if (!original.new && !original.edit) {
+          return _.get(original, 'manager.name') || '';
+        }
+
+        return (
+          <Select
+            value={original.manager}
+            onChange={value => updateItemField(parentIndex, index, 'manager', value)}
+            async
+            openOnClick={false}
+            autoload={false}
+            loadOptions={debouncedUsersFetch}
+            cache={false}
+            options={[]}
+            labelKey="name"
+            className="select-xs"
+          />
+        );
+      },
+    },
+    {
+      Header: 'Replenishment period',
+      accessor: 'replenishmentPeriod',
+      // eslint-disable-next-line react/prop-types
+      Cell: ({ index, original }) => {
+        if (!original.new && !original.edit) {
+          return _.isNil(original.replenishmentPeriod) ? '' : original.replenishmentPeriod;
+        }
+
+        return (
+          <Input
+            value={original.replenishmentPeriod || ''}
+            onChange={value => updateItemField(parentIndex, index, 'replenishmentPeriod', value)}
+          />
+        );
+      },
+      className: 'text-center',
+    },
+    {
+      Header: 'Maximum  Quantity',
+      accessor: 'maxQuantity',
+      // eslint-disable-next-line react/prop-types
+      Cell: ({ index, original }) => {
+        if (!original.new && !original.edit) {
+          return _.isNil(original.maxQuantity) ? '' : original.maxQuantity;
+        }
+
+        return (
+          <Input
+            value={original.maxQuantity || ''}
+            onChange={value => updateItemField(parentIndex, index, 'maxQuantity', value)}
+          />
+        );
+      },
+      className: 'text-center',
+    },
+    {
+      Header: 'Unit of measure',
+      accessor: 'uom',
+      className: 'text-center',
+    },
+    {
+      Header: 'Actions',
+      accessor: 'edit',
+      // eslint-disable-next-line react/prop-types
+      Cell: ({ index, original }) => (
+        <div>
+          <button
+            className="btn btn-outline-primary btn-xs mx-1"
+            disabled={original.edit || original.new}
+            onClick={() => editItem(parentIndex, index)}
+          >Edit
+          </button>
+          <button
+            className="btn btn-outline-primary btn-xs mx-1"
+            disabled={(!original.edit && !original.new) || !original.stocklistId
+              || _.isNil(original.maxQuantity) || original.maxQuantity === ''}
+            onClick={() => saveItem(parentIndex, index, original)}
+          >Save
+          </button>
+          <button
+            className="btn btn-outline-danger btn-xs mx-1"
+            onClick={() => deleteItem(parentIndex, index)}
+          >Delete
+          </button>
+          <button
+            className="btn btn-outline-secondary btn-xs mx-1"
+            disabled={original.edit || original.new}
+            onClick={() => printItem(parentIndex, index)}
+          >Print
+          </button>
+          <button
+            className="btn btn-outline-secondary btn-xs mx-1"
+            disabled={original.edit || original.new}
+            onClick={() => mailItem(parentIndex, index)}
+          >Email
+          </button>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      {
+        data && !!data.length &&
+        <ReactTable
+          data={data}
+          columns={COLUMNS}
+          showPagination={false}
+          minRows={0}
+        />
+      }
+      <button
+        className="btn btn-outline-success btn-xs my-1"
+        onClick={() => addItem(parentIndex)}
+      >Add Stocklist
+      </button>
+    </div>
+  );
+};
 
 export default StocklistTable;
 
 StocklistTable.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({})),
+  availableStocklists: PropTypes.arrayOf(PropTypes.shape({})),
+  parentIndex: PropTypes.number.isRequired,
+  addItem: PropTypes.func.isRequired,
+  editItem: PropTypes.func.isRequired,
+  updateItemField: PropTypes.func.isRequired,
+  saveItem: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  printItem: PropTypes.func.isRequired,
+  mailItem: PropTypes.func.isRequired,
 };
 
 StocklistTable.defaultProps = {
   data: [],
+  availableStocklists: [],
 };
