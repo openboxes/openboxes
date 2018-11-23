bcc60ed7c2ae3cd88723181783f0e5a6298d8c35
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-11-23 18:08:03 +0100

    OBPIH-1478: Create API to manage Stocklist Items

--- src/groovy/org/pih/warehouse/api/StocklistLocation.groovy
+++ src/groovy/org/pih/warehouse/api/StocklistLocation.groovy
@@ -1,11 +1,14 @@
 package org.pih.warehouse.api
 
 import org.pih.warehouse.core.Location
+import org.pih.warehouse.requisition.Requisition
 
 class StocklistLocation {
 
     Location location
-    List<Stocklist> stocklists
+    List<StocklistItem> stocklistItems
+
+    List<Requisition> availableStocklists
 
     Map toJson() {
         return [
@@ -13,7 +16,9 @@ class StocklistLocation {
                 "location.name": location?.name,
                 "locationGroup.id": location?.locationGroup?.id,
                 "locationGroup.name": location?.locationGroup?.name,
-                stocklists: stocklists
+                stocklistItems: stocklistItems,
+                monthlyDemand: stocklistItems?.collect { it.requisitionItem?.quantity ?: 0 }?.sum(),
+                availableStocklists: availableStocklists.collect { [ id: it.id, name: it.name ] },
         ]
     }
 }
