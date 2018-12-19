a0ae022f2de7eb2f5945ffdbdfc993664292b674
Justin Miranda <jcm62@columbia.edu>
2018-12-19 11:43:18 -0600

    OBPIH-1515 Add items page item search not working

--- grails-app/controllers/org/pih/warehouse/api/ProductApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/ProductApiController.groovy
@@ -21,12 +21,8 @@ class ProductApiController extends BaseDomainApiController {
     def inventoryService
 
     def list = {
-
-        String locationId = params?.location?.id ?: session?.warehouse?.id
-        Location location = locationId ? Location.get(locationId) : null
         String [] terms = params?.name?.split(",| ")?.findAll { it }
-        def products = inventoryService.getProductsByTermsAndCategories(terms, [], true, location?.inventory, 25, 0)
-        log.info "Products " + products
+        def products = productService.searchProducts(terms, [])
 		render ([data:products] as JSON)
 	}
 
