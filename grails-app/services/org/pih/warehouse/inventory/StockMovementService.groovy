f2b27d39a29da0a51f46ab1827e016496b443c99
awalkowiak <awalkowiak@soldevelo.com>
2019-01-07 18:03:21 +0100

    OBPIH-1342: some stock movement documents hidden (#734)

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -1177,7 +1177,8 @@ class StockMovementService {
                             documentType: DocumentGroupCode.EXPORT.name(),
                             contentType : "text/csv",
                             stepNumber  : 2,
-                            uri         : g.createLink(controller: 'stockMovement', action: "exportCsv", id: stockMovement?.requisition?.id, absolute: true)
+                            uri         : g.createLink(controller: 'stockMovement', action: "exportCsv", id: stockMovement?.requisition?.id, absolute: true),
+                            hidden      : true
                     ],
                     [
                         name        : g.message(code: "picklist.button.print.label"),
@@ -1191,7 +1192,8 @@ class StockMovementService {
                             documentType: DocumentGroupCode.PICKLIST.name(),
                             contentType : "application/pdf",
                             stepNumber  : 4,
-                            uri         : g.createLink(controller: 'picklist', action: "renderPdf", id: stockMovement?.requisition?.id, absolute: true)
+                            uri         : g.createLink(controller: 'picklist', action: "renderPdf", id: stockMovement?.requisition?.id, absolute: true),
+                            hidden      : true
                     ],
                     [
                             name        : g.message(code: "deliveryNote.label", default: "Delivery Note"),
