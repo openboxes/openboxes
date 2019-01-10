05ea4d5159945921aaac0ed1535065f70811ce4a
Justin Miranda <jcm62@columbia.edu>
2019-01-10 00:12:38 -0600

    OBPIH-1569 Cleaned up session variables that are no longer used

--- grails-app/conf/InitializationFilters.groovy
+++ grails-app/conf/InitializationFilters.groovy
@@ -21,35 +21,18 @@ class InitializationFilters {
 
 					// Only initialize session if a user has logged in.
 					if (session.user) {
-						if (!session.hostname) {
-							session.hostname = InetAddress.getLocalHost().getHostName() + " (" + request.getHeader('Host') + ")"
-						}
 
-						// Make sure all session variables are initialized
-						if (!session.loginLocations || session.loginLocations.isEmpty() || params?.reset) {
-							log.info "Initializing login locations ..."
-							Location currentLocation = Location.get(session?.warehouse?.id)
+						if (!session.hasProperty("_showTime")) {
 							session._showTime = true
-							session.loginLocations = locationService.getLoginLocations(currentLocation)
-							session.loginLocationsMap = locationService.getLoginLocationsMap(currentLocation)
 						}
 
-						if (!session.rootCategory) {
-							session.rootCategory = productService.getRootCategory()
-							//session.quickCategories = productService.getQuickCategories()
-						}
-						if (!session.inventoryCategoryFilters) {
-							session.inventoryCategoryFilters = [];
-						}
-						if (!session.productCategoryFilters) {
-							session.productCategoryFilters = [];
+						if (!session.hasProperty("hostname")) {
+							session.hostname = InetAddress.getLocalHost().getHostName() + " (" + request.getHeader('Host') + ")"
 						}
 					}
 				} catch (Exception e) { 
 					log.error "Unable to initialize session variables: " + e.message, e
 				}				
-									
-
 			}
 		}
 
