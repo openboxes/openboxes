ac51b9e5d84d2534db3be60b521d1d0c6a1808fc
Justin Miranda <jcm62@columbia.edu>
2019-04-08 11:19:29 -0500

    OBPIH-1890 Using transaction events instead of transaction entry events

--- grails-app/domain/org/pih/warehouse/inventory/TransactionEntry.groovy
+++ grails-app/domain/org/pih/warehouse/inventory/TransactionEntry.groovy
@@ -16,22 +16,22 @@ import org.pih.warehouse.product.Product
 class TransactionEntry implements Comparable, Serializable {
 
 	// FIXME This is a temporary solution that should be replaced by an event queue in a future version
-	def afterInsert = {
-		RefreshInventorySnapshotJob.triggerNow([transactionEntryId: id])
-	}
+//	def afterInsert = {
+//		RefreshInventorySnapshotJob.triggerNow([transactionEntryId: id])
+//	}
 
 	// beforeUpdate is used instead of afterUpdate so we can check whether certain fields are dirty. However, this
 	// might cause some issues if the data has not been flushed to the database yet. Therefore we might need to add a
     // delay on the background job.
-	def beforeUpdate = {
-		if (isDirty('quantity') || isDirty('binLocation')) {
-			RefreshInventorySnapshotJob.triggerNow([transactionEntryId: id])
-		}
-	}
+//	def beforeUpdate = {
+//		if (isDirty('quantity') || isDirty('binLocation')) {
+//			RefreshInventorySnapshotJob.triggerNow([transactionEntryId: id])
+//		}
+//	}
 
-	def afterDelete = {
-		RefreshInventorySnapshotJob.triggerNow([transactionEntryId: id, deleted: true])
-	}
+//	def afterDelete = {
+//		RefreshInventorySnapshotJob.triggerNow([transactionEntryId: id, deleted: true])
+//	}
 
 	String id
     Integer quantity
