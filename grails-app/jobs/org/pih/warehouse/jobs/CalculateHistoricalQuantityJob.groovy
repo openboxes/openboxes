74717492635e420173228d7016b23e363162d370
Justin Miranda <jcm62@columbia.edu>
2019-04-15 14:15:32 -0500

    Fixed minor bug with the calculate historical quantity quartz job

--- grails-app/jobs/org/pih/warehouse/jobs/CalculateHistoricalQuantityJob.groovy
+++ grails-app/jobs/org/pih/warehouse/jobs/CalculateHistoricalQuantityJob.groovy
@@ -3,8 +3,6 @@ package org.pih.warehouse.jobs
 import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 import org.quartz.JobExecutionContext
 
-import liquibase.Liquibase
-import liquibase.database.DatabaseFactory
 import util.LiquibaseUtil
 
 class CalculateHistoricalQuantityJob {
@@ -33,7 +31,7 @@ class CalculateHistoricalQuantityJob {
         log.info "Executing calculate historical quantity job at ${new Date()} with context ${context}"
         if (!dates) {
             // Filter down to the transaction dates within the last 18 months
-            def daysToProcess = ConfigHolder.config.openboxes.jobs.calculateHistoricalQuantityJob.daysToProcess
+            def daysToProcess = CH.config.openboxes.jobs.calculateHistoricalQuantityJob.daysToProcess
             def startDate = new Date() - daysToProcess
             def transactionDates = inventoryService.getTransactionDates()
             transactionDates = transactionDates.findAll { it >= startDate }
