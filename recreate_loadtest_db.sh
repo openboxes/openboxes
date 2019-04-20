d45f935a5c1858e86c69f407c7dfc5acb275a950
Justin Miranda <jcm62@columbia.edu>
2019-04-20 13:08:51 -0500

    removed several unused resources

--- recreate_loadtest_db.sh
+++
@@ -1,1 +0,0 @@
-mysql -uroot -p -e'drop database openboxes_loadtest;create database openboxes_loadtest default charset utf8;grant all on openboxes_loadtest.* to openboxes@localhost identified by "openboxes";'
--- recreate_test_db.sh
+++
@@ -1,1 +0,0 @@
-mysql -uroot -p -e'drop database openboxes_test;create database openboxes_test default charset utf8;grant all on openboxes_test.* to openboxes@localhost identified by "openboxes";'
