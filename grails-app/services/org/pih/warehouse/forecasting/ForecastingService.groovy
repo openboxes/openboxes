e756b4e61b946f41dd7bdc9007758488aa997dae
Justin Miranda <jcm62@columbia.edu>
2019-06-25 23:29:12 -0500

    OBPIH-2123 Caught exceptions to prevent "view doesn't exist" error from being leaked to the user; also wrapped each method to see if forecasting has been enabled (should probably be implemented using AOP)

--- grails-app/services/org/pih/warehouse/forecasting/ForecastingService.groovy
+++ grails-app/services/org/pih/warehouse/forecasting/ForecastingService.groovy
@@ -20,102 +20,128 @@ class ForecastingService {
     boolean transactional = true
 
     def dataSource
+    def grailsApplication
 
 
 
     def getDemand(Location origin, Product product) {
 
-        def rows = getDemandDetails(origin, product)
-        def startDate = rows.min { it.date_requested }?.date_requested
-        def endDate = rows.max { it.date_requested }?.date_requested
-        def totalDemand = rows.sum { it.quantity_demand }?:0
-        def totalDays = (startDate && endDate) ? (endDate - startDate) : 1
-        def dailyDemand = (totalDemand && totalDays) ? (totalDemand / totalDays) : 0
-        def monthlyDemand = dailyDemand * 30
-        return [
-                totalDemand: totalDemand,
-                dateRange: [startDate: startDate, endDate: endDate],
-                totalDays: totalDays,
-                dailyDemand: dailyDemand,
-                monthlyDemand: monthlyDemand
-        ];
+        boolean forecastingEnabled = grailsApplication.config.openboxes.forecastingEnabled ?: false
+        if (forecastingEnabled) {
+            def rows = getDemandDetails(origin, product)
+            def startDate = rows.min { it.date_requested }?.date_requested
+            def endDate = rows.max { it.date_requested }?.date_requested
+            def totalDemand = rows.sum { it.quantity_demand } ?: 0
+            def totalDays = (startDate && endDate) ? (endDate - startDate) : 1
+            def dailyDemand = (totalDemand && totalDays) ? (totalDemand / totalDays) : 0
+            def monthlyDemand = dailyDemand * 30
+            return [
+                    dateRange    : [ startDate: startDate, endDate  : endDate ],
+                    totalDemand  : totalDemand,
+                    totalDays    : totalDays,
+                    dailyDemand  : dailyDemand,
+                    monthlyDemand: monthlyDemand
+            ];
+        }
     }
 
     def getDemandDetails(Location origin, Product product) {
-        String query = """
-            select 
-                request_status,
-                request_number,
-                date_requested,
-                origin_name,
-                destination_name,
-                product_code,
-                product_name,
-                quantity_requested,
-                quantity_canceled,
-                quantity_approved,
-                quantity_change_approved,
-                quantity_substitution_approved,
-                quantity_demand,
-                cancel_reason_code
-            FROM product_demand
-            WHERE product_id = :productId
-            AND origin_id = :originId
-        """
-        Sql sql = new Sql(dataSource)
-        List data = sql.rows(query, [productId: product.id, originId: origin.id])
+        List data = []
+        boolean forecastingEnabled = grailsApplication.config.openboxes.forecastingEnabled ?: false
+        if (forecastingEnabled) {
+            String query = """
+                select 
+                    request_status,
+                    request_number,
+                    date_requested,
+                    origin_name,
+                    destination_name,
+                    product_code,
+                    product_name,
+                    quantity_requested,
+                    quantity_canceled,
+                    quantity_approved,
+                    quantity_change_approved,
+                    quantity_substitution_approved,
+                    quantity_demand,
+                    cancel_reason_code
+                FROM product_demand
+                WHERE product_id = :productId
+                AND origin_id = :originId
+            """
+            Sql sql = new Sql(dataSource)
+            try {
+                data = sql.rows(query, [productId: product.id, originId: origin.id])
+
+            } catch (Exception e) {
+                log.error("Unable to execute query: " + e.message, e)
+            }
+        }
         return data
     }
 
 
     def getDemandSummary(Location origin, Product product) {
         List data = []
-        String query = """
-            select 
-                min(date_requested) as min_date_requested,
-                max(date_requested) as max_date_requested,
-                DATE_FORMAT(date_requested, '%b %y') as date_key,
-                month(date_requested) as request_month,
-                year(date_requested) as request_year,
-                sum(quantity_requested) as quantity_requested,
-                sum(quantity_canceled) as quantity_canceled,
-                sum(quantity_approved) as quantity_approved,
-                sum(quantity_change_approved) as quantity_change_approved,
-                sum(quantity_substitution_approved) as quantity_substitution_approved,
-                sum(quantity_demand) as quantity_demand
-            FROM product_demand
-            WHERE product_id = :productId
-            AND origin_id = :originId
-            GROUP BY request_month, request_year
-            ORDER BY request_year, request_month
-        """
-        Sql sql = new Sql(dataSource)
-        List rows = sql.rows(query, [productId: product.id, originId: origin.id])
-        if (rows) {
-            Timestamp startDate = rows.min { it.min_date_requested }?.min_date_requested
-            Timestamp endDate = rows.max { it.max_date_requested }?.max_date_requested
-            List allMonths = getMonths(startDate, endDate)
 
-            data = allMonths.collect { monthYear ->
-                def row = rows.find { it.request_year == monthYear.year && it.request_month == monthYear.month }
-                [
-                        dateKey          : "${monthYear?.month}/${monthYear?.year}",
-                        year             : monthYear?.year,
-                        month            : monthYear?.month,
-                        quantityRequested: row?.quantity_requested ?: 0,
-                        quantityCanceled : row?.quantity_canceled ?: 0,
-                        quantityApproved : row?.quantity_approved ?: 0,
-                        quantityDemand   : row?.quantity_demand ?: 0
-                ]
+        boolean forecastingEnabled = grailsApplication.config.openboxes.forecastingEnabled ?: false
+        if (forecastingEnabled) {
+            String query = """
+                select 
+                    min(date_requested) as min_date_requested,
+                    max(date_requested) as max_date_requested,
+                    DATE_FORMAT(date_requested, '%b %y') as date_key,
+                    month(date_requested) as request_month,
+                    year(date_requested) as request_year,
+                    sum(quantity_requested) as quantity_requested,
+                    sum(quantity_canceled) as quantity_canceled,
+                    sum(quantity_approved) as quantity_approved,
+                    sum(quantity_change_approved) as quantity_change_approved,
+                    sum(quantity_substitution_approved) as quantity_substitution_approved,
+                    sum(quantity_demand) as quantity_demand
+                FROM product_demand
+                WHERE product_id = :productId
+                AND origin_id = :originId
+                GROUP BY request_month, request_year
+                ORDER BY request_year, request_month
+            """
+            Sql sql = new Sql(dataSource)
+            List rows = []
+
+            try {
+                rows = sql.rows(query, [productId: product.id, originId: origin.id])
+            } catch (Exception e) {
+                log.error("Unable to execute query: " + e.message, e)
             }
-            log.info "data: ${data}"
+
+            if (rows) {
+                Timestamp startDate = rows.min { it.min_date_requested }?.min_date_requested
+                Timestamp endDate = rows.max { it.max_date_requested }?.max_date_requested
+                List allMonths = getMonths(startDate, endDate)
+
+                data = allMonths.collect { monthYear ->
+                    def row = rows.find { it.request_year == monthYear.year && it.request_month == monthYear.month }
+                    [
+                            dateKey          : "${monthYear?.month}/${monthYear?.year}",
+                            year             : monthYear?.year,
+                            month            : monthYear?.month,
+                            quantityRequested: row?.quantity_requested ?: 0,
+                            quantityCanceled : row?.quantity_canceled ?: 0,
+                            quantityApproved : row?.quantity_approved ?: 0,
+                            quantityDemand   : row?.quantity_demand ?: 0
+                    ]
+                }
+                log.info "data: ${data}"
+            }
         }
         return data
     }
 
 
     def getMonths(Date startDate, Date endDate) {
-        return (startDate..endDate).collect { [ year: it[Calendar.YEAR], month: it[Calendar.MONTH]+1 ] }.unique()
+        return (startDate..endDate).collect {
+            [ year: it[Calendar.YEAR], month: it[Calendar.MONTH]+1 ]
+        }.unique()
     }
 
 }
