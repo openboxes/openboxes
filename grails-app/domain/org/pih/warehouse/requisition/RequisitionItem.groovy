ce58e24e2148d2cb7a75feda4b079a7d7208b028
dominika <doniahatala@gmail.com>
2019-09-24 13:27:09 +0200

    OBPIH-2387 Fix wrong requisiton item's status code on edit page after updating item on add items page

--- grails-app/domain/org/pih/warehouse/requisition/RequisitionItem.groovy
+++ grails-app/domain/org/pih/warehouse/requisition/RequisitionItem.groovy
@@ -444,8 +444,7 @@ class RequisitionItem implements Comparable<RequisitionItem>, Serializable {
      * @return true if the requisition is no longer in the reviewing stage and there are no changes
      */
     def isApproved() {
-        //return requisition?.status > RequisitionStatus.REVIEWING && !isChanged()
-        return quantityApproved > 0
+        return quantityApproved > 0 && !isChanged()
     }
 
     def isPending() {
