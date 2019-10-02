6acafc94f6134c93eb1e270b12120ac9c24e1b9b
Walkowiak <awalkowiak@soldevelo.com>
2019-10-02 17:14:10 +0200

    OBGM-22: Replace org.codehaus.groovy.grails.commons.ConfigurationHolder with grails.util.Holders

--- grails-app/services/org/pih/warehouse/inventory/InventorySnapshotService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/InventorySnapshotService.groovy
@@ -12,7 +12,7 @@ package org.pih.warehouse.inventory
 import groovy.sql.Sql
 import groovyx.gpars.GParsPool
 import org.apache.commons.lang.StringEscapeUtils
-import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import grails.util.Holders
 import org.hibernate.Criteria
 import org.pih.warehouse.api.AvailableItem
 import org.pih.warehouse.core.Location
@@ -173,7 +173,7 @@ class InventorySnapshotService {
 
     def saveInventorySnapshots(Date date, Location location, List binLocations) {
         def startTime = System.currentTimeMillis()
-        def batchSize = ConfigurationHolder.config.openboxes.inventorySnapshot.batchSize ?: 1000
+        def batchSize = Holders.getConfig().getProperty("openboxes.inventorySnapshot.batchSize") ?: 1000
         def sql = new Sql(dataSource)
 
         try {
