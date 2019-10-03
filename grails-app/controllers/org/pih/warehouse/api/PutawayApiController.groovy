4fd50db1d112f355f9e5e83e9e8fd97914414b65
Justin Miranda <jcm62@columbia.edu>
2019-10-02 23:31:10 -0500

    Fixed imports related to the rendering plugin

--- grails-app/controllers/org/pih/warehouse/api/PutawayApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/PutawayApiController.groovy
@@ -10,6 +10,7 @@
 package org.pih.warehouse.api
 
 import grails.converters.JSON
+import grails.plugins.rendering.pdf.PdfRenderingService
 import org.grails.web.json.JSONObject
 import org.pih.warehouse.core.User
 import org.pih.warehouse.inventory.InventoryLevel
@@ -24,7 +25,7 @@ class PutawayApiController {
     def putawayService
     def inventoryService
     def identifierService
-    def pdfRenderingService
+    PdfRenderingService pdfRenderingService
 
     def list = {
         String locationId = params?.location?.id ?: session?.warehouse?.id
