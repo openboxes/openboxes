7e344ddfcc0ce1ffef4f7938c93ad5d4e03bd8b2
Justin Miranda <jcm62@columbia.edu>
2019-10-02 23:44:59 -0500

    Removed more springcache references

--- grails-app/controllers/org/pih/warehouse/user/UserController.groovy
+++ grails-app/controllers/org/pih/warehouse/user/UserController.groovy
@@ -9,7 +9,6 @@
  **/
 package org.pih.warehouse.user
 
-import grails.plugin.springcache.annotations.CacheFlush
 import grails.validation.ValidationException
 import org.pih.warehouse.core.*
 import org.pih.warehouse.core.Location
@@ -297,7 +296,7 @@ class UserController {
      * Updates the locale of the default user
      * Used by the locale selectors in the footer
      */
-    @CacheFlush(["megamenuCache"])
+    //@CacheFlush(["megamenuCache"])
     def updateAuthUserLocale = {
 
         log.info "update auth user locale " + params
