638e9cf0775ae467ce92f4d590cdb9f30c2fa776
Justin Miranda <jcm62@columbia.edu>
2019-10-07 13:41:10 -0500

    Converted filters to interceptors

---
+++ grails-app/controllers/org/pih/warehouse/SentryInterceptor.groovy
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2012 Partners In Health.  All rights reserved.
+ * The use and distribution terms for this software are covered by the
+ * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ * which can be found in the file epl-v10.html at the root of this distribution.
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ * You must not remove this notice, or any other, from this software.
+ **/
+package org.pih.warehouse
+
+
+import grails.util.Environment
+import grails.util.Holders
+import org.pih.warehouse.core.User
+
+class SentryInterceptor {
+    def ravenClient
+
+    public SentryInterceptor() {
+        matchAll()
+    }
+
+    boolean before() {
+        if (session.user) {
+
+            try {
+                def serverUrl = Holders.grailsApplication.config.grails.serverURL
+                def user = User.get(session.user.id)
+                def userData = [
+                        id         : user.id, is_authenticated: true,
+                        email      : user.email,
+                        username   : user.username,
+                        location   : session.warehouse ? session.warehouse.name : "No location",
+                        environment: Environment.current,
+                        timezone   : user.timezone,
+                        locale     : session?.user?.locale?.toString() ?: "No locale",
+                        sessionId  : session?.id,
+                        server     : serverUrl ?: "No server URL"
+
+                ]
+                //ravenClient.setUserData(userData)
+            } catch (Exception e) {
+                log.info("Unable to set the user data for sentry due to the following error " + e.message)
+            }
+        } else {
+            //ravenClient.setUserData([is_authenticated: false])
+        }
+        return true
+    }
+}
