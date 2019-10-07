dc6675c2b8fbe5e541c11b5da6db7dbc8c088064
Justin Miranda <jcm62@columbia.edu>
2019-10-07 16:14:08 -0500

    Fixed issues with LinkTagLib (probably needs a refactoring though)

--- grails-app/taglib/LinkTagLib.groovy
+++ grails-app/taglib/LinkTagLib.groovy
@@ -1,23 +1,26 @@
 import org.grails.plugins.web.taglib.ApplicationTagLib
+
 import util.ConfigHelper
+import org.pih.warehouse.RoleInterceptor
+import org.pih.warehouse.SecurityInterceptor
 
 class LinkTagLib extends ApplicationTagLib {
 
     static namespace = "g"
     def userService
 
-    def link = { attrs, body ->
+    Closure link = { attrs, body ->
 
         boolean disabled = attrs.disabled ?: false
         String disabledMessage = attrs.disabledMessage ?: 'Access denied'
         def actionName = attrs.action
         def controllerName = attrs.controller ?: ""
-        if (!SecurityFilters.actionsWithAuthUserNotRequired.contains(actionName)) {
+        if (!SecurityInterceptor.actionsWithAuthUserNotRequired.contains(actionName)) {
             def rule = ConfigHelper.findAccessRule(controllerName as String, actionName as String)
             def missRule = rule ? !userService.isUserInRole(session.user, rule?.accessRules?.minimumRequiredRole) : false
-            def missManager = RoleFilters.needManager(controllerName, actionName) && (RoleFilters.needRequestorOrManager(controllerName, actionName) ? !userService.isUserManager(session.user) && !userService.isUserRequestor(session.user) : !userService.isUserManager(session.user))
-            def missAdmin = RoleFilters.needAdmin(controllerName, actionName) && !userService.isUserAdmin(session.user)
-            def missSuperuser = RoleFilters.needSuperuser(controllerName, actionName) && !userService.isSuperuser(session.user)
+            def missManager = RoleInterceptor.needManager(controllerName, actionName) && (RoleInterceptor.needRequestorOrManager(controllerName, actionName) ? !userService.isUserManager(session.user) && !userService.isUserRequestor(session.user) : !userService.isUserManager(session.user))
+            def missAdmin = RoleInterceptor.needAdmin(controllerName, actionName) && !userService.isUserAdmin(session.user)
+            def missSuperuser = RoleInterceptor.needSuperuser(controllerName, actionName) && !userService.isSuperuser(session.user)
 
             // If user is not authorized to access link we just display the link body (text)
             if (missManager || missAdmin || missSuperuser || disabled || missRule) {
