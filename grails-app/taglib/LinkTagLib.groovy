0ee1387bd98546a0679f370a84789c99144c02d7
Justin Miranda <jcm62@columbia.edu>
2019-10-07 16:14:08 -0500

    Fixed issues with LinkTagLib (probably needs a refactoring though)

--- grails-app/taglib/LinkTagLib.groovy
+++ grails-app/taglib/LinkTagLib.groovy
@@ -1,20 +1,23 @@
 import org.grails.plugins.web.taglib.ApplicationTagLib
 
+import org.pih.warehouse.RoleInterceptor
+import org.pih.warehouse.SecurityInterceptor
+
 class LinkTagLib extends ApplicationTagLib {
 
     static namespace = "g"
     def userService
 
-    def link = { attrs, body ->
+    Closure link = { attrs, body ->
 
         boolean disabled = attrs.disabled ?: false
         String disabledMessage = attrs.disabledMessage ?: 'Access denied'
         def actionName = attrs.action
         def controllerName = attrs.controller ?: ""
-        if (!SecurityFilters.actionsWithAuthUserNotRequired.contains(actionName)) {
-            def missManager = RoleFilters.needManager(controllerName, actionName) && !userService.isUserManager(session.user)
-            def missAdmin = RoleFilters.needAdmin(controllerName, actionName) && !userService.isUserAdmin(session.user)
-            def missSuperuser = RoleFilters.needSuperuser(controllerName, actionName) && !userService.isSuperuser(session.user)
+        if (!SecurityInterceptor.actionsWithAuthUserNotRequired.contains(actionName)) {
+            def missManager = RoleInterceptor.needManager(controllerName, actionName) && !userService.isUserManager(session.user)
+            def missAdmin = RoleInterceptor.needAdmin(controllerName, actionName) && !userService.isUserAdmin(session.user)
+            def missSuperuser = RoleInterceptor.needSuperuser(controllerName, actionName) && !userService.isSuperuser(session.user)
 
             // If user is not authorized to access link we just display the link body (text)
             if (missManager || missAdmin || missSuperuser || disabled) {
