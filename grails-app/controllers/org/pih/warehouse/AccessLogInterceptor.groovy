3f17a3f8da0a74752dc17136e80ae56194cdfa6a
Justin Miranda <jcm62@columbia.edu>
2019-10-07 13:41:10 -0500

    Converted filters to interceptors

---
+++ grails-app/controllers/org/pih/warehouse/AccessLogInterceptor.groovy
@@ -0,0 +1,22 @@
+package org.pih.warehouse
+/**
+ * Copyright (c) 2012 Partners In Health.  All rights reserved.
+ * The use and distribution terms for this software are covered by the
+ * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ * which can be found in the file epl-v10.html at the root of this distribution.
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ * You must not remove this notice, or any other, from this software.
+ **/
+class AccessLogInterceptor {
+
+    public AccessLogInterceptor() {
+        matchAll()
+    }
+
+    boolean before() {
+        log.info("$controllerName.$actionName: [user:${session?.user?.username}, location:${session?.warehouse?.name}]")
+        log.debug("$controllerName.$actionName: ${params}")
+        return true;
+    }
+}
