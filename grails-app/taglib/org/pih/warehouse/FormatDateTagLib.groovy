c18da3cf90a815ffc74a9d8ce09dff5b08af4620
Justin Miranda <jcm62@columbia.edu>
2019-10-07 13:42:51 -0500

    Fixed references to codehaus package

--- grails-app/taglib/org/pih/warehouse/FormatDateTagLib.groovy
+++ grails-app/taglib/org/pih/warehouse/FormatDateTagLib.groovy
@@ -1,11 +1,13 @@
 package org.pih.warehouse
 
+import org.grails.plugins.web.taglib.FormatTagLib
+
 class FormatDateTagLib extends FormatTagLib {
 
     static namespace = "g"
 
-    def formatDate = { attrs, body ->
-        FormatTagLib formatTagLib = grailsApplication.mainContext.getBean('org.codehaus.groovy.grails.plugins.web.taglib.FormatTagLib')
+    Closure formatDate = { attrs, body ->
+        FormatTagLib formatTagLib = grailsApplication.mainContext.getBean('org.grails.plugins.web.taglib.FormatTagLib')
         if (session.timezone) {
             attrs.timeZone = session.timezone
         }
