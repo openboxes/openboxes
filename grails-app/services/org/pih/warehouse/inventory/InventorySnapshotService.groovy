51c2289c5371c9f313222f8f05d50d9b739a3d1d
Justin Miranda <jcm62@columbia.edu>
2019-10-10 13:10:44 -0500

    Suppress foreign key constraint violations during batch insert of inventory snapshot records

--- grails-app/services/org/pih/warehouse/inventory/InventorySnapshotService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/InventorySnapshotService.groovy
@@ -198,6 +198,7 @@ class InventorySnapshotService {
 
             // Execute inventory snapshot insert/update in batches
             sql.withTransaction {
+                sql.executeUpdate("SET foreign_key_checks = 0")
                 sql.withBatch(batchSize, insertStmt) { ps ->
                     binLocations.eachWithIndex { entry, index ->
                         def row = [
@@ -216,6 +217,7 @@ class InventorySnapshotService {
                         ps.addBatch(row)
                     }
                 }
+                sql.executeUpdate("SET foreign_key_checks = 1")
             }
             log.info "Saved ${binLocations?.size()} inventory snapshots for location ${location} on date ${date.format("MMM-dd-yyyy")} in ${System.currentTimeMillis() - startTime}ms"
 
