d18ba69c67b21fc67aa546dad93347122f1d5c86
Justin Miranda <jcm62@columbia.edu>
2019-10-13 23:11:15 -0500

    Removed unnecessary debug logging that was causing an exception

--- grails-app/controllers/org/pih/warehouse/shipping/ShipmentController.groovy
+++ grails-app/controllers/org/pih/warehouse/shipping/ShipmentController.groovy
@@ -130,7 +130,6 @@ class ShipmentController {
     }
 
     def update = {
-        log.info params
 
         def shipmentInstance = Shipment.get(params.id)
         if (shipmentInstance) {
@@ -269,7 +268,7 @@ class ShipmentController {
     }
 
     def editDetails = {
-        log.info params
+
         def shipmentInstance = Shipment.get(params.id)
         if (!shipmentInstance) {
             flash.message = "${warehouse.message(code: 'default.not.found.message', args: [warehouse.message(code: 'shipment.label', default: 'Shipment'), params.id])}"
@@ -745,8 +744,6 @@ class ShipmentController {
 
     def editContainer = {
 
-        log.info params
-
         def shipmentInstance = Shipment.get(params.shipmentId)
         def containerInstance = Container.get(params.containerId)
         if (containerInstance) {
@@ -819,7 +816,7 @@ class ShipmentController {
 
 
     def addDocument = {
-        log.info params
+
         def shipmentInstance = Shipment.get(params.id)
         def documentInstance = Document.get(params?.document?.id)
         if (!documentInstance) {
