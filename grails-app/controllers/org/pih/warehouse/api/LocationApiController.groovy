18067b249a85a263eb608bf7c09eb06f041dcc24
Walkowiak <awalkowiak@soldevelo.com>
2019-10-22 17:51:49 +0200

    OBGM-37: Fix import errors after rebasing to develop branch

--- grails-app/controllers/org/pih/warehouse/api/LocationApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/LocationApiController.groovy
@@ -22,6 +22,7 @@ import org.pih.warehouse.core.User
 import org.pih.warehouse.importer.ImportDataCommand
 import org.pih.warehouse.inventory.InventoryLevel
 import org.pih.warehouse.product.ProductAvailability
+import grails.core.GrailsApplication
 import org.springframework.web.multipart.MultipartFile
 import org.pih.warehouse.core.LocationStatus
 
@@ -29,9 +30,9 @@ class LocationApiController extends BaseDomainApiController {
 
     def locationService
     def userService
+    GrailsApplication grailsApplication
     def identifierService
     def inventoryService
-    def grailsApplication
     def documentService
 
     def read = {
