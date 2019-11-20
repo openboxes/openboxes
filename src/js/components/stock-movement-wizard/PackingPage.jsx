4241e90ae083adf2cc8933ca6f52545ae5792ed7
dominika <doniahatala@gmail.com>
2019-11-20 16:01:22 +0100

    OBPIH-2526 Enable arrow navigation on edit and pick page

--- src/js/components/stock-movement-wizard/PackingPage.jsx
+++ src/js/components/stock-movement-wizard/PackingPage.jsx
@@ -28,6 +28,7 @@ const showOnly = queryString.parse(window.location.search).type === 'REQUEST';
 const FIELDS = {
   packPageItems: {
     type: ArrayField,
+    arrowsNavigation: true,
     fields: {
       productCode: {
         type: LabelField,
