a46846e33e2110a6c3a143e9dca91a99b5f8e0ba
Walkowiak <awalkowiak@soldevelo.com>
2019-11-25 14:13:09 +0100

    OBGM-44: Fix events errors and finding users by role type in Location in Stock Movement

--- grails-app/domain/org/pih/warehouse/inventory/Transaction.groovy
+++ grails-app/domain/org/pih/warehouse/inventory/Transaction.groovy
@@ -13,11 +13,11 @@ import org.pih.warehouse.auth.AuthService
 import org.pih.warehouse.core.Constants
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.core.User
-import org.pih.warehouse.jobs.RefreshInventorySnapshotJob
 import org.pih.warehouse.order.Order
 import org.pih.warehouse.receiving.Receipt
 import org.pih.warehouse.requisition.Requisition
 import org.pih.warehouse.shipping.Shipment
+import grails.util.Holders
 
 /**
  *  Represents a unit of work completed within a single warehouse.  A
@@ -52,11 +52,11 @@ class Transaction implements Comparable, Serializable {
     }
 
     def publishSaveEvent = {
-        publishEvent(new TransactionEvent(this))
+        Holders.grailsApplication.mainContext.publishEvent(new TransactionEvent(this))
     }
 
     def publishDeleteEvent = {
-        publishEvent(new TransactionEvent(this, true))
+        Holders.grailsApplication.mainContext.publishEvent(new TransactionEvent(this, true))
     }
 
     // ID won't be available until after the record is inserted
