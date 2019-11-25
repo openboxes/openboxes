510aa8ea3c27a7d4bc30f8881fb0688f4082111a
Walkowiak <awalkowiak@soldevelo.com>
2019-11-25 14:13:09 +0100

    OBGM-44: Fix events errors and finding users by role type in Location in Stock Movement

--- grails-app/domain/org/pih/warehouse/product/Product.groovy
+++ grails-app/domain/org/pih/warehouse/product/Product.groovy
@@ -25,6 +25,7 @@ import org.pih.warehouse.inventory.TransactionEntry
 import org.pih.warehouse.shipping.ShipmentItem
 import org.pih.warehouse.core.Document
 import org.pih.warehouse.core.Location
+import grails.util.Holders
 
 
 /**
@@ -62,7 +63,7 @@ class Product implements Comparable, Serializable {
     }
 
     def publishPersistenceEvent = {
-        publishEvent(new InventorySnapshotEvent(this))
+        Holders.grailsApplication.mainContext.publishEvent(new InventorySnapshotEvent(this))
     }
 
     def afterInsert = publishPersistenceEvent
