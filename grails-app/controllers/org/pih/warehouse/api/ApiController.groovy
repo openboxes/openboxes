52fd93b3a6da3876f1613d61227246d0f6379e8e
Walkowiak <awalkowiak@soldevelo.com>
2019-12-20 14:13:55 +0100

    OBGM-37: Add minimum expiration date value in application config

--- grails-app/controllers/org/pih/warehouse/api/ApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/ApiController.groovy
@@ -134,7 +134,8 @@ class ApiController {
         def isPaginated = grailsApplication.config.openboxes.api.pagination.enabled
         def tablero = grailsApplication.config.openboxes.tablero
         DateFormat dateFormat = new SimpleDateFormat("MM/DD/YYYY");
-        String minimumExpirationDate = dateFormat.format(grailsApplication.config.openboxes.expirationDate.minValue)
+        String minValue = grailsApplication.getConfig().getProperty('openboxes.expirationDate.minValue')
+        String minimumExpirationDate = dateFormat.format(new Date(minValue))
         def logoLabel = grailsApplication.config.openboxes.logo.label
         def pageSize = grailsApplication.config.openboxes.api.pagination.pageSize
         def logoUrl = "/openboxes/location/viewLogo/${session.warehouse?.id}"
