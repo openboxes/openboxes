be9fa0ca142cb4d671725e1dabaf739940e23757
Walkowiak <awalkowiak@soldevelo.com>
2020-02-03 15:10:21 +0100

    OBGM-60: Add temporary PagedResultList from grails 1.3.9

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -10,7 +10,7 @@
 package org.pih.warehouse.inventory
 
 import grails.core.GrailsApplication
-import grails.orm.PagedResultList
+import org.pih.warehouse.PagedResultList
 import grails.validation.ValidationException
 import org.grails.web.json.JSONObject
 import org.hibernate.ObjectNotFoundException
