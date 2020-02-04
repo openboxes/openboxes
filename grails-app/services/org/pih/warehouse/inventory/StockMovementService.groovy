5946335fdec6daa9b8998498ed9f807db5fdf441
Walkowiak <awalkowiak@soldevelo.com>
2020-02-04 17:21:48 +0100

    OBGM-123: Fix deleting items on stock movement

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -9,6 +9,7 @@
  **/
 package org.pih.warehouse.inventory
 
+import grails.gorm.transactions.Transactional
 import grails.core.GrailsApplication
 import org.pih.warehouse.PagedResultList
 import grails.validation.ValidationException
@@ -59,6 +60,7 @@ import org.pih.warehouse.shipping.ShipmentStatusCode
 import org.pih.warehouse.shipping.ShipmentType
 import org.pih.warehouse.shipping.ShipmentWorkflow
 
+@Transactional
 class StockMovementService {
 
     def productService
