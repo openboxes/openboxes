fce331d369cb92058ab2645c3f1119b9d08f2ba1
Justin Miranda <jcm62@columbia.edu>
2020-02-04 23:32:05 -0600

    OBGM-85 Refactored bootstrap.groovy; postponed Quartz startup until after database migrations are executed

--- grails-app/init/org/pih/warehouse/BootStrap.groovy
+++ grails-app/init/org/pih/warehouse/BootStrap.groovy
@@ -13,8 +13,14 @@ package org.pih.warehouse
 import grails.converters.JSON
 import grails.core.GrailsApplication
 import grails.util.Environment
+import liquibase.Contexts
+import liquibase.LabelExpression
 import liquibase.Liquibase
+import liquibase.changelog.ChangeSet
+import liquibase.changelog.DatabaseChangeLog
 import liquibase.database.DatabaseFactory
+import liquibase.database.jvm.JdbcConnection
+import liquibase.resource.ClassLoaderResourceAccessor
 import org.pih.warehouse.api.AvailableItem
 import org.pih.warehouse.api.EditPage
 import org.pih.warehouse.api.EditPageItem
@@ -58,6 +64,7 @@ import org.pih.warehouse.shipping.ContainerType
 import org.pih.warehouse.shipping.Shipment
 import org.pih.warehouse.shipping.ShipmentItem
 import org.pih.warehouse.shipping.ShipmentType
+import org.quartz.Scheduler
 import util.LiquibaseUtil
 
 import javax.sql.DataSource
@@ -71,6 +78,85 @@ class BootStrap {
 
     def init = { servletContext ->
 
+        log.info("Registering JSON marshallers ...")
+        registerJsonMarshallers()
+
+        log.info("Executing database migrations ...")
+        executeDatabaseMigrations()
+
+        log.info("Starting Quartz scheduler ...")
+        Scheduler scheduler = grailsApplication.mainContext.getBean("quartzScheduler")
+        scheduler.start()
+
+        // Create uploads directory if it doesn't already exist
+        log.info("Creating uploads directory ...")
+        uploadService.findOrCreateUploadsDirectory()
+    }
+
+
+    def destroy = {
+
+    }
+
+    def executeDatabaseMigrations() {
+        Liquibase liquibase = null
+        try {
+
+            def connection = dataSource.getConnection()
+            if (connection == null) {
+                throw new RuntimeException("Connection could not be created.")
+            }
+
+            def database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(dataSource.connection))
+            database.setDefaultSchemaName(connection.catalog)
+            boolean isRunningMigrations = LiquibaseUtil.isRunningMigrations()
+            log.info("Liquibase running: " + isRunningMigrations)
+            log.info("Setting default schema to " + connection.catalog)
+            log.info("Product Version: " + database.databaseProductVersion)
+            log.info("Database Version: " + database.databaseMajorVersion + "." + database.databaseMinorVersion)
+
+            // Ensure the databasechangelog up-to-date in order to handle
+            // DatabaseException: Error executing SQL SELECT * FROM obnav.DATABASECHANGELOG
+            // ORDER BY DATEEXECUTED ASC, ORDEREXECUTED ASC: Unknown column 'ORDEREXECUTED' in 'order clause'
+            liquibase = new Liquibase(null as DatabaseChangeLog, new ClassLoaderResourceAccessor(), database)
+            liquibase.checkLiquibaseTables(false, null, new Contexts(), new LabelExpression())
+
+            //If nothing has been created yet, let's create all new database objects with the install scripts
+            List<ChangeSet> hasExecutedAnyChangesets = database.getRanChangeSetList()
+            if (!hasExecutedAnyChangesets) {
+                log.info("Has no executed changesets, creating schema from scratch")
+                liquibase = new Liquibase("install/changelog.xml", new ClassLoaderResourceAccessor(), database)
+                liquibase.update(null as Contexts, new LabelExpression());
+            }
+
+            // FIXME Not good that we'll need to update this on subsequent versions so this needs some more thought
+            List previousChangelogVersions = ["0.5.x", "0.6.x", "0.7.x", "0.8.x"]
+            def executedChangelogVersions = LiquibaseUtil.getExecutedChangelogVersions()
+            log.info ("changelogVersions: " + executedChangelogVersions)
+
+            // Check if the executed changelog versions include one of the previous versions
+            // and if so, then we need to keep running the old updates to catch up to 0.9.x
+            boolean hasExecutedAnyPreviousChangesets =
+                    executedChangelogVersions.any { previousChangelogVersions.contains(it.version) }
+            if (hasExecutedAnyPreviousChangesets) {
+                log.info("Has executed previous changelog version, running all updates")
+                liquibase = new Liquibase("upgrade/changelog.xml", new ClassLoaderResourceAccessor(), database)
+                liquibase.update(null as Contexts, new LabelExpression())
+            }
+
+            // And now we need to run changelogs from 0.9.x and beyond
+            log.info("Running latest updates")
+            liquibase = new Liquibase("changelog.groovy", new ClassLoaderResourceAccessor(), database)
+            liquibase.update(null as Contexts, new LabelExpression())
+        }
+        finally {
+            log.info("Closing database")
+            liquibase?.database?.close()
+        }
+        log.info("Finished running liquibase changelog(s)!")
+    }
+
+    def registerJsonMarshallers() {
         // Static data
         JSON.registerObjectMarshaller(ContainerType) { ContainerType containerType ->
             [
@@ -408,226 +494,6 @@ class BootStrap {
         JSON.registerObjectMarshaller(StocklistItem) { StocklistItem stocklistItem ->
             return stocklistItem.toJson()
         }
-
-
-        // ================================    Static Data    ============================================
-        //
-        // Use the 'demo' environment to create a database with 'static' and 'demo' data.  Then
-        // run the following:
-        //
-        // 		$ grails -Dgrails.env=demo run-app
-        //
-        // In another terminal, run through these commands to generate the appropriate
-        // changelog files for a new version of the data model
-        //
-        // 		$ grails db-diff-schema > grails-app/migrations/x.x.x/changelog-initial-schema.xml
-        // 		$ grails db-diff-index > grails-app/migrations/x.x.x/changelog-initial-indexes.xml
-        // 		$ grails db-diff-data > grails-app/migrations/x.x.x/changelog-initial-data.xml
-        //
-        // Migrating existing data to the new data model is still a work in progress, but you can
-        // use the previous versions changelogs.
-        //
-//        log.info("Running liquibase changelog(s) ...")
-//        Liquibase liquibase = null
-//        try {
-//
-//            def connection = dataSource.getConnection()
-//            if (connection == null) {
-//                throw new RuntimeException("Connection could not be created.")
-//            }
-//            def classLoader = getClass().classLoader
-//            def fileOpener = classLoader.loadClass("org.liquibase.grails.GrailsFileOpener").getConstructor().newInstance()
-//
-//            def database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(connection)
-//            boolean isRunningMigrations = LiquibaseUtil.isRunningMigrations()
-//            log.info("Liquibase running: " + isRunningMigrations)
-//            log.info("Setting default schema to " + connection.catalog)
-//            log.info("Product Version: " + database.databaseProductVersion)
-//            log.info("Database Version: " + database.databaseMajorVersion + "." + database.databaseMinorVersion)
-//            def ranChangeSets = database.getRanChangeSetList()
-//            database.setDefaultSchemaName(connection.catalog)
-//
-//            //If nothing has been created yet, let's create all new database objects with the install scripts
-//            if (!ranChangeSets) {
-//                liquibase = new Liquibase("install/install.xml", fileOpener, database)
-//                liquibase.update(null)
-//            }
-//
-//            // Run through the updates in the master changelog
-//            liquibase = new Liquibase("changelog.xml", fileOpener, database)
-//            liquibase.update(null)
-//        }
-//        finally {
-//            if (liquibase && liquibase.database) {
-//                liquibase.database.close()
-//            }
-//        }
-//        log.info("Finished running liquibase changelog(s)!")
-
-//        def enableFixtures = Boolean.valueOf(grailsApplication.config.openboxes.fixtures.enabled ?: true)
-//        log.info("Insert test fixtures?  " + enableFixtures)
-//        if (enableFixtures) {
-//            log.info("Inserting test fixtures ...")
-//            insertTestFixture()
-//        }
-
-        // Create uploads directory if it doesn't already exist
-        uploadService.findOrCreateUploadsDirectory()
     }
-
-
-    def destroy = {
-
-    }
-
-
-    static String TestFixure = "Test-Fixture"
-
-
-    def insertTestFixture() {
-        if (Environment.current != Environment.DEVELOPMENT && Environment.current != Environment.TEST && Environment.current.name != "loadtest")
-            return
-
-        log.info("Setup test fixture...")
-
-        createSupplierLocation()
-        def medicines = Category.findByName("Medicines")
-        def suppliers = Category.findByName("Supplies")
-
-        assert medicines != null
-        assert suppliers != null
-        def testData = [
-                ['expiration': new Date().plus(3), 'productGroup': 'PainKiller', 'quantity': 10000, 'product': new Product(category: medicines, name: "Advil 200mg", manufacturer: "ABC", productCode: "00001", manufacturerCode: "9001")]
-                , ['expiration': new Date().plus(20), 'productGroup': 'PainKiller', 'quantity': 10000, 'product': new Product(category: medicines, name: "Tylenol 325mg", manufacturer: "MedicalGait", productCode: "00002", manufacturerCode: "9002")]
-                , ['expiration': new Date().plus(120), 'productGroup': 'PainKiller', 'quantity': 10000, 'product': new Product(category: medicines, name: "Aspirin 20mg", manufacturer: "ABC", productCode: "00003", manufacturerCode: "9001")]
-                , ['expiration': new Date().plus(200), 'productGroup': 'PainKiller', 'quantity': 10000, 'product': new Product(category: medicines, name: "General Pain Reliever", manufacturer: "MedicalGait", productCode: "00004", manufacturerCode: "9002")]
-                , ['expiration': new Date().minus(1), 'productGroup': 'Iron', 'quantity': 10000, 'product': new Product(category: medicines, name: "Similac Advance low iron 400g", manufacturer: "ABC", productCode: "00005", manufacturerCode: "9001")]
-                , ['expiration': new Date().minus(30), 'productGroup': 'Iron', 'quantity': 10000, 'product': new Product(category: medicines, name: "Similac Advance + iron 365g", manufacturer: "MedicalGait", productCode: "00006", manufacturerCode: "9002")]
-                , ['expiration': new Date().plus(1000), 'productGroup': 'Laptop', 'quantity': 10000, 'product': new Product(category: suppliers, name: "MacBook Pro 8G", manufacturer: "Apple", productCode: "00007", manufacturerCode: "9003")]
-                , ['expiration': null, 'productGroup': 'Paper', 'quantity': 10000, 'product': new Product(category: suppliers, name: "Print Paper A4", manufacturer: "DSC", productCode: "00008", manufacturerCode: "9004")]
-        ]
-
-
-        if (Environment.current == Environment.TEST)
-            deleteTestFixture(testData)
-
-        createTestFixtureIfNotExist(testData)
-        if (Environment.current.name == "loadtest")
-            createLoadtestData()
-
-        log.info("Created test fixture.")
-    }
-
-    def createLoadtestData() {
-        def medicines = Category.findByName("Medicines")
-        def numberOfProducts = Integer.parseInt(System.getenv()["load"] ?: "5000")
-        if (Product.count() >= numberOfProducts) return
-        def nameSeeds = ["foo", "jing", "moon", "mountain", "evening", "smooth", "train", "paper", "sharp", "see", "ocean", "apple"]
-        def data = []
-        for (i in 0..numberOfProducts) {
-            def nameSeed = nameSeeds[i % nameSeeds.size()]
-            data.add(['expiration': new Date().plus(500), 'quantity': (i + 1) * 10, 'product': new Product(category: medicines, name: nameSeed + i, manufacturer: "ABC", productCode: "00001", manufacturerCode: "9001")])
-        }
-        createTestFixtureIfNotExist(data)
-        log.info("load test data created.")
-    }
-
-    def createSupplierLocation() {
-        def name = "Test Supplier"
-        if (Location.findByName(name)) return
-        def locationType = LocationType.get("4")
-        def supplierLocation = new Location(name: name, locationType: locationType)
-        supplierLocation.save(flush: true, failOnError: true)
-    }
-
-    def deleteTestFixture(List<Map<String, Object>> testData) {
-        deleteRequisitions()
-        testData.each { deleteProductAndInventoryItems(it) }
-        Transaction.findByComment(TestFixure).each { it.delete() }
-    }
-
-    def createTestFixtureIfNotExist(List<Map<String, Object>> testData) {
-        testData.each { addProductAndInventoryItemIfNotExist(it) }
-    }
-
-    private def deleteRequisitions() {
-        Picklist.list().each { it.delete(failOnError: true, flush: true) }
-        Requisition.list().each { it.delete(failOnError: true, flush: true) }
-    }
-
-    private def deleteProductAndInventoryItems(Map<String, Object> inventoryItemInfo) {
-        def product = Product.findByName(inventoryItemInfo.product.name)
-        if (!product) return
-        def shipmentItems = ShipmentItem.findAllByProduct(product)
-
-        for (shipmentItem in shipmentItems) {
-            shipmentItem.delete(failOnError: true, flush: true)
-        }
-
-        def inventoryItems = InventoryItem.findAllByProduct(product)
-        for (item in inventoryItems) {
-            for (entry in TransactionEntry.findAllByInventoryItem(item)) {
-                entry.delete(failOnError: true, flush: true)
-            }
-
-            item.delete(failOnError: true, flush: true)
-        }
-        product.delete(failOnError: true, flush: true)
-
-    }
-
-    private def addProductAndInventoryItemIfNotExist(Map<String, Object> inventoryItemInfo) {
-        def productGroup = null
-        if (inventoryItemInfo.productGroup) {
-            productGroup = ProductGroup.findByName(inventoryItemInfo.productGroup)
-            if (!productGroup) {
-                productGroup = new ProductGroup(name: inventoryItemInfo.productGroup)
-                productGroup.category = inventoryItemInfo.product.category
-                productGroup.save(failOnError: true, flush: true)
-            }
-        }
-        Product product = Product.findByName(inventoryItemInfo.product.name)
-
-        if (!product) {
-            log.info("creating product ${inventoryItemInfo.product.name}")
-            product = inventoryItemInfo.product
-            product.save(failOnError: true, flush: true)
-            if (inventoryItemInfo.productGroup) {
-                productGroup.addToProducts(product)
-                productGroup.save(failOnError: true, flush: true)
-                product.addToProductGroups(productGroup)
-                product.save(failOnError: true, flush: true)
-            }
-            addInventoryItem(product, inventoryItemInfo.expiration, inventoryItemInfo.quantity)
-        }
-    }
-
-
-    private def addInventoryItem(product, expirationDate, quantity) {
-
-        InventoryItem item = new InventoryItem()
-        item.product = product
-        item.lotNumber = "lot57"
-        item.expirationDate = expirationDate
-        item.save(failOnError: true, flush: true)
-
-        Location boston = Location.findByName("Boston Headquarters")
-        assert boston != null
-
-        Transaction transaction = new Transaction()
-        transaction.createdBy = User.get(2)
-        transaction.comment = TestFixure
-        transaction.transactionDate = new Date().minus(1)
-        transaction.inventory = boston.inventory
-        transaction.transactionType = TransactionType.get(Constants.PRODUCT_INVENTORY_TRANSACTION_TYPE_ID)
-
-        TransactionEntry transactionEntry = new TransactionEntry()
-        transactionEntry.quantity = quantity
-        transactionEntry.inventoryItem = item
-
-        transaction.addToTransactionEntries(transactionEntry)
-        transaction.save(failOnError: true, flush: true)
-    }
-
 
 }
