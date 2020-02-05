bc644a3244da85aaa9a625079c1a412b3853ddc8
Justin Miranda <jcm62@columbia.edu>
2020-02-04 23:45:08 -0600

    OBGM-37: Fix getting config property in RefreshDataJob (original commit by awalkowiak was lost during a rebase)

--- grails-app/jobs/org/pih/warehouse/jobs/RefreshDemandDataJob.groovy
+++ grails-app/jobs/org/pih/warehouse/jobs/RefreshDemandDataJob.groovy
@@ -1,23 +1,24 @@
 package org.pih.warehouse.jobs
 
-import grails.util.Holders
+import grails.core.GrailsApplication
 import org.quartz.DisallowConcurrentExecution
 import org.quartz.JobExecutionContext
 
 @DisallowConcurrentExecution
 class RefreshDemandDataJob {
 
+    GrailsApplication grailsApplication
     def reportService
 
     // Should never be triggered on a schedule - should only be triggered by persistence event listener
     static triggers = {
         cron name: 'refreshDemandDataJobCronTrigger',
-                cronExpression: Holders.grailsApplication.config.openboxes.jobs.refreshDemandDataJob.cronExpression
+                cronExpression: grailsApplication.getConfig().getProperty('openboxes.jobs.refreshDemandDataJob.cronExpression')
     }
 
     def execute(JobExecutionContext context) {
 
-        Boolean enabled = Holders.grailsApplication.config.openboxes.jobs.refreshDemandDataJob.enabled
+        Boolean enabled = grailsApplication.config.openboxes.jobs.refreshDemandDataJob.enabled
         if (enabled) {
             def startTime = System.currentTimeMillis()
             log.info("Refreshing demand data: " + context.mergedJobDataMap)
