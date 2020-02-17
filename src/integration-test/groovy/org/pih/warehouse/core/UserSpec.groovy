3b13c6327ff5b3d92c696321b6aa4f5a47b43c6e
Walkowiak <awalkowiak@soldevelo.com>
2020-02-17 14:43:46 +0100

    OBGM-29: Add integration and functional tests using grails 3 test framework

---
+++ src/integration-test/groovy/org/pih/warehouse/core/UserSpec.groovy
@@ -0,0 +1,29 @@
+package org.pih.warehouse.core
+
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
+import org.pih.warehouse.Application
+import spock.lang.Ignore
+import spock.lang.Specification
+
+@Integration(applicationClass = Application.class)
+@Rollback
+class UserSpec extends Specification {
+
+    def setupData() {
+        new User(username: "asd",
+                firstName: "Asd",
+                lastName: "Qwe",
+                password: "test123",
+                passwordConfirm: "test123").save(flush: true)
+    }
+
+    @Ignore("TODO: fix java.lang.IllegalStateException: No GORM implementations configured. Ensure GORM has been initialized correctly at...")
+    void "test user count"() {
+        given:
+        setupData()
+
+        expect:
+        User.count() == 1
+    }
+}
