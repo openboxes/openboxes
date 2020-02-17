4fb2ae74d7e52965e64d1e956117505233644810
Walkowiak <awalkowiak@soldevelo.com>
2020-02-17 14:41:43 +0100

    OBGM-29: Added few unit tests using grails 3 test framework

---
+++ src/test/groovy/unit/org/pih/warehouse/core/UserDomainSpec.groovy
@@ -0,0 +1,47 @@
+package unit.org.pih.warehouse.core
+
+import grails.test.mixin.Mock
+import org.pih.warehouse.core.User
+import spock.lang.Ignore
+import spock.lang.Specification
+
+@Mock(User)
+class UserDomainSpec extends Specification {
+
+    void "test validation on user with all required and valid data"() {
+        when:
+        def user = new User(
+                username: "asd",
+                firstName: "Asd",
+                lastName: "Qwe",
+                password: "test123")
+
+        then:
+        user.validate()
+    }
+
+    void "test validation on user with missing required data"() {
+        when:
+        def user = new User(
+                firstName: "Asd",
+                lastName: "Qwe",
+                password: "test123")
+
+        then:
+        !user.validate()
+    }
+
+    @Ignore("somehow passwordConfirm is null")
+    void "test validation on user with invalid data"() {
+        when:
+        def user = new User(
+                username: "asd",
+                firstName: "Asd",
+                lastName: "Qwe",
+                password: "PASSWORD123",
+                passwordConfirm: "WRONG_CONFIRM_PASSWORD")
+
+        then:
+        !user.validate()
+    }
+}
