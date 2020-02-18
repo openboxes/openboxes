37bf6c6c45e40641a1565858256fb197ca505a56
augusto-moraes <Augusto Moraes>
2020-02-18 15:43:54 +0100

    onDragStart debug with onMouseOn and distance

--- src/js/components/tablero/GraphCard.jsx
+++ src/js/components/tablero/GraphCard.jsx
@@ -1,94 +1,101 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Line, Bar, Doughnut, HorizontalBar } from 'react-chartjs-2';
-import { SortableElement, sortableHandle } from 'react-sortable-hoc';
-import LoadingCard from './LoadingCard';
-import './tablero.scss';
+import PropTypes from "prop-types";
+import React from "react";
+import { Line, Bar, Doughnut, HorizontalBar } from "react-chartjs-2";
+import { SortableElement, sortableHandle } from "react-sortable-hoc";
+import LoadingCard from "./LoadingCard";
+import "./tablero.scss";
 
-import { loadColors } from '../../../assets/dataFormat/dataLoading'
+import { loadColors } from "../../../assets/dataFormat/dataLoading";
 
 const Numbers = ({ data }) => (
-    <div className="gyrIndicator">
-        <div className="numberIndicator">
-            <div className="value">
-                <div className="circle green"></div> {data.green.value}
-            </div>
-            <div className="subtitle">{data.green.subtitle}</div>
-        </div>
-        <div className="numberIndicator">
-            <div className="value">
-                <div className="circle yellow"></div> {data.yellow.value}
-            </div>
-            <div className="subtitle">{data.yellow.subtitle}</div>
-        </div>
-        <div className="numberIndicator">
-            <div className="value">
-                <div className="circle red"></div> {data.red.value}
-            </div>
-            <div className="subtitle">{data.red.subtitle}</div>
-        </div>
+  <div className="gyrIndicator">
+    <div className="numberIndicator">
+      <div className="value">
+        <div className="circle green"></div> {data.green.value}
+      </div>
+      <div className="subtitle">{data.green.subtitle}</div>
     </div>
+    <div className="numberIndicator">
+      <div className="value">
+        <div className="circle yellow"></div> {data.yellow.value}
+      </div>
+      <div className="subtitle">{data.yellow.subtitle}</div>
+    </div>
+    <div className="numberIndicator">
+      <div className="value">
+        <div className="circle red"></div> {data.red.value}
+      </div>
+      <div className="subtitle">{data.red.subtitle}</div>
+    </div>
+  </div>
 );
 
 const ErrorCard = () => (
-    <div>
-        <i className="fa fa-repeat" />
-    </div>
+  <div>
+    <i className="fa fa-repeat" />
+  </div>
 );
 
+const DragHandle = sortableHandle(({ onDragStartHandle, cardTitle }) => (
+  <span
+    draggable
+    className="dragHandler"
+    onMouseDown={e => onDragStartHandle(e, cardTitle)}
+  >
+    ::
+  </span>
+));
+
 let graphClass = "graphCard";
-const DragHandle = sortableHandle(() => <span className="dragHandler">::</span>);
-
-const GraphCard = SortableElement(({
-    cardTitle, cardType, data,
-}) => {
+const GraphCard = SortableElement(
+  ({ cardTitle, cardType, data, onDragStartHandle }) => {
     let graph;
-    if (cardType === 'line') {
-        data['datasets'][0] = loadColors(data, "line");
-        graph = <Line data={data} />;
-        graphClass = "graphCard";
-    } else if (cardType === 'bar') {
-        data['datasets'][0] = loadColors(data, "bar");
-        graph = <Bar data={data} />;
-        graphClass = "graphCard";
-    } else if (cardType === 'doughnut') {
-        data['datasets'][0] = loadColors(data, "doughnut");
-        graph = <Doughnut data={data} />;
-        graphClass = "graphCard";
+    if (cardType === "line") {
+      data["datasets"][0] = loadColors(data, "line");
+      graph = <Line data={data} />;
+      graphClass = "graphCard";
+    } else if (cardType === "bar") {
+      data["datasets"][0] = loadColors(data, "bar");
+      graph = <Bar data={data} />;
+      graphClass = "graphCard";
+    } else if (cardType === "doughnut") {
+      data["datasets"][0] = loadColors(data, "doughnut");
+      graph = <Doughnut data={data} />;
+      graphClass = "graphCard";
+    } else if (cardType === "horizontalBar") {
+      data["datasets"][0] = loadColors(data, "horizontalBar");
+      graph = <HorizontalBar data={data} />;
+      graphClass = "graphCard";
+    } else if (cardType === "numbers") {
+      graph = <Numbers data={data} />;
+      graphClass = "graphCard";
+    } else if (cardType === "loading") {
+      graph = <LoadingCard />;
+      graphClass = "graphCard";
+    } else if (cardType === "error") {
+      graph = <ErrorCard />;
+      graphClass = "graphCard errorCard";
     }
-    else if (cardType === 'horizontalBar') {
-        data['datasets'][0] = loadColors(data, "horizontalBar");
-        graph = <HorizontalBar data={data} />
-    } else if (cardType === 'numbers') {
-        graph = <Numbers data={data} />;
-        graphClass = "graphCard";
-    } else if (cardType === 'loading') {
-        graph = <LoadingCard />;
-        graphClass = "graphCard";
-    } else if (cardType === 'error') {
-        graph = <ErrorCard />;
-        graphClass = "graphCard errorCard";
-    }
-
 
     return (
-        <div className={graphClass}>
-            <div className="headerCard">
-                <span className="titleCard"> {cardTitle} </span>
-                <DragHandle />
-            </div>
-            <div className="contentCard">
-                {graph}
-            </div>
+      <div className={graphClass}>
+        <div className="headerCard">
+          <span className="titleCard"> {cardTitle} </span>
+          <DragHandle
+            onDragStartHandle={onDragStartHandle}
+            cardTitle={cardTitle}
+          />
         </div>
-    )
-}
+        <div className="contentCard">{graph}</div>
+      </div>
+    );
+  }
 );
 
 export default GraphCard;
 
 GraphCard.propTypes = {
-    cardTitle: PropTypes.string.isRequired,
-    cardType: PropTypes.string.isRequired,
-    data: PropTypes.any.isRequired
+  cardTitle: PropTypes.string.isRequired,
+  cardType: PropTypes.string.isRequired,
+  data: PropTypes.any.isRequired
 };
--- src/js/components/tablero/Tablero.jsx
+++ src/js/components/tablero/Tablero.jsx
@@ -1,51 +1,63 @@
-import React, { Component } from 'react';
-import { defaults } from 'react-chartjs-2';
-import { connect } from 'react-redux';
-import { SortableContainer } from 'react-sortable-hoc';
-import 'react-table/react-table.css';
-import { addToIndicators, fetchIndicators, reorderIndicators } from '../../actions';
-import GraphCard from './GraphCard';
-import NumberCard from './NumberCard';
-import './tablero.scss';
+import React, { Component } from "react";
+import { defaults } from "react-chartjs-2";
+import { connect } from "react-redux";
+import { SortableContainer } from "react-sortable-hoc";
+import "react-table/react-table.css";
+import {
+  addToIndicators,
+  fetchIndicators,
+  reorderIndicators
+} from "../../actions";
+import GraphCard from "./GraphCard";
+import NumberCard from "./NumberCard";
+import "./tablero.scss";
 
 // Disable charts legends by default.
 defaults.global.legend = false;
 
+//Should be on JSON package with charts Data
 const numberData = [
   {
-    title: 'Bin Location Summary',
+    title: "Bin Location Summary",
     number: 2696,
-    subtitle: 'In stock',
-    id: Math.random()
-  }, {
-    title: 'Bin Location Summary',
-    number: 1082,
-    subtitle: 'Out of stock',
-    id: Math.random()
-  }, {
-    title: 'Stock Movements',
-    number: 468,
-    subtitle: 'Not shipped',
-    id: Math.random()
-  }, {
-    title: 'User Incomplete Tasks',
-    number: 246,
-    subtitle: 'Not shiped',
-    id: Math.random()
-  }, {
-    title: 'User Incomplete Tasks',
-    number: 188,
-    subtitle: 'Not completed',
-    id: Math.random()
-  }, {
-    title: 'Discrepancy',
-    number: 290,
-    subtitle: 'Items received',
+    subtitle: "In stock",
     id: Math.random()
   },
+  {
+    title: "Bin Location Summary",
+    number: 1082,
+    subtitle: "Out of stock",
+    id: Math.random()
+  },
+  {
+    title: "Stock Movements",
+    number: 468,
+    subtitle: "Not shipped",
+    id: Math.random()
+  },
+  {
+    title: "User Incomplete Tasks",
+    number: 246,
+    subtitle: "Not shiped",
+    id: Math.random()
+  },
+  {
+    title: "User Incomplete Tasks",
+    number: 188,
+    subtitle: "Not completed",
+    id: Math.random()
+  },
+  {
+    title: "Discrepancy",
+    number: 290,
+    subtitle: "Items received",
+    id: Math.random()
+  }
 ];
 
-const SortableCards = SortableContainer(({ data }) => (
+let hideArchive = true;
+
+const SortableCards = SortableContainer(({ data, onDragStartHandle }) => (
   <div className="cardComponent">
     {data.map((value, index) => (
       <GraphCard
@@ -53,7 +65,10 @@ const SortableCards = SortableContainer(({ data }) => (
         index={index}
         cardTitle={value.title}
         cardType={value.type}
-        data={value.data} />
+        data={value.data}
+        onDragStartHandle={onDragStartHandle}
+        onMouseDown={e => onDragStartHandle(e, cardTitle)}
+      />
     ))}
   </div>
 ));
@@ -66,8 +81,21 @@ const NumberCardsRow = ({ data }) => (
         index={index}
         cardTitle={value.title}
         cardNumber={value.number}
-        cardSubtitle={value.subtitle} />
+        cardSubtitle={value.subtitle}
+      />
     ))}
+  </div>
+);
+
+const ArchiveIndicator = ({ hideArchive, onDragOver, onDrop }) => (
+  <div
+    className={hideArchive ? "archiveDiv hideArchive" : "archiveDiv"}
+    onDragOver={e => onDragOver(e)}
+    onDrop={() => onDrop(e, "archived")}
+  >
+    <span>
+      Archive indicator <i className="fa fa-archive"></i>
+    </span>
   </div>
 );
 
@@ -86,20 +114,58 @@ class Tablero extends Component {
     this.props.fetchIndicators();
   }
 
+  handle = () => {
+    hideArchive = false;
+    //console.log(hideArchive);
+  };
+
+  onDragStart = (ev, id) => {
+    hideArchive = !hideArchive;
+    //console.log("dragstart: ", id);
+    //ev.dataTransfer.setData("text/plain", id);
+  };
+
+  onDragOver = ev => {
+    //console.log("dragOver: ", ev);
+    ev.preventDefault();
+  };
+
+  onDrop = (ev, cat) => {
+    let id = ev.dataTransfer.getData("text");
+
+    //console.log("Dropped:", id, cat);
+    hideArchive = !hideArchive;
+  };
+
   render() {
     return (
       <div className="cardsContainer">
         <NumberCardsRow data={numberData} />
-        <SortableCards data={this.props.indicatorsData} onSortEnd={this.props.reorderIndicators} axis="xy" useDragHandle />
+        <SortableCards
+          data={this.props.indicatorsData}
+          onSortEnd={this.props.reorderIndicators}
+          handle={this.handle}
+          axis="xy"
+          useDragHandle
+          onDragStartHandle={this.onDragStart}
+          distance={1}
+        />
+        <ArchiveIndicator hideArchive={hideArchive} />
+        <div className="unarchive">
+          <span>Unarchive indicator (2) </span>
+          <i className="fa fa-archive"></i>
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  indicatorsData: state.indicators.data,
+  indicatorsData: state.indicators.data
 });
 
 export default connect(mapStateToProps, {
-  fetchIndicators, addToIndicators, reorderIndicators,
+  fetchIndicators,
+  addToIndicators,
+  reorderIndicators
 })(Tablero);
