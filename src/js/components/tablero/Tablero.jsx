59abdb7a2578f0f30c4b1d9d3b192a3e18141747
augusto-moraes <Augusto Moraes>
2020-02-25 10:00:11 +0100

    getting data from api

--- src/js/components/tablero/Tablero.jsx
+++ src/js/components/tablero/Tablero.jsx
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import { defaults } from 'react-chartjs-2';
-import { connect } from 'react-redux';
-import { SortableContainer } from 'react-sortable-hoc';
-import 'react-table/react-table.css';
-import { addToIndicators, fetchIndicators, reorderIndicators } from '../../actions';
-import GraphCard from './GraphCard';
-import NumberCard from './NumberCard';
-import './tablero.scss';
-import { numberData } from '../../../assets/dataFormat/numberData'
+import React, { Component } from "react";
+import { defaults } from "react-chartjs-2";
+import { connect } from "react-redux";
+import { SortableContainer } from "react-sortable-hoc";
+import "react-table/react-table.css";
+import {
+  addToIndicators,
+  fetchIndicators,
+  reorderIndicators
+} from "../../actions";
+import GraphCard from "./GraphCard";
+import NumberCard from "./NumberCard";
+import "./tablero.scss";
+import apiClient from "../../utils/apiClient";
 
 // Disable charts legends by default.
 defaults.global.legend = false;
@@ -20,26 +24,31 @@ const SortableCards = SortableContainer(({ data }) => (
         index={index}
         cardTitle={value.title}
         cardType={value.type}
-        data={value.data} />
+        data={value.data}
+      />
     ))}
   </div>
 ));
 
 const NumberCardsRow = ({ data }) => (
-  <div className="cardComponent">
+  <div className={data.length ? "cardComponent" : "loading-numbers"}>
     {data.map((value, index) => (
       <NumberCard
         key={`item-${value.id}`}
         index={index}
         cardTitle={value.title}
         cardNumber={value.number}
-        cardSubtitle={value.subtitle} />
+        cardSubtitle={value.subtitle}
+      />
     ))}
   </div>
 );
 
 class Tablero extends Component {
   dataFetched = false;
+  state = {
+    numberData: []
+  };
 
   constructor(props) {
     super(props);
@@ -49,24 +58,39 @@ class Tablero extends Component {
     this.fetchData();
   }
 
+  fetchNumbersData() {
+    const url = "/openboxes/apitablero/getNumberData";
+    apiClient.get(url).then(res => {
+      this.setState({ numberData: res.data });
+    });
+  }
+
   fetchData() {
     this.props.fetchIndicators();
+    this.fetchNumbersData();
   }
 
   render() {
     return (
       <div className="cardsContainer">
-        <NumberCardsRow data={numberData} />
-        <SortableCards data={this.props.indicatorsData} onSortEnd={this.props.reorderIndicators} axis="xy" useDragHandle />
+        <NumberCardsRow data={this.state.numberData} />
+        <SortableCards
+          data={this.props.indicatorsData}
+          onSortEnd={this.props.reorderIndicators}
+          axis="xy"
+          useDragHandle
+        />
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  indicatorsData: state.indicators.data,
+  indicatorsData: state.indicators.data
 });
 
 export default connect(mapStateToProps, {
-  fetchIndicators, addToIndicators, reorderIndicators,
+  fetchIndicators,
+  addToIndicators,
+  reorderIndicators
 })(Tablero);
