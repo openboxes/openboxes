7696db87d834a463d618b7997c4a449167ec1ead
augusto-moraes <Augusto Moraes>
2020-03-10 10:59:40 +0100

    OBPIH-2676 solved
    # Conflicts:
    #	grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
    #	src/groovy/org/pih/warehouse/tablero/NumberData.groovy
    #	src/js/components/tablero/NumberCard.jsx

--- src/js/components/tablero/NumberCard.jsx
+++ src/js/components/tablero/NumberCard.jsx
@@ -6,9 +6,7 @@ function truncateString(value, length) {
   return (value.length > length) ? `${value.substr(0, length - 1)}...` : value;
 }
 
-const NumberCard = ({
-  cardTitle, cardNumber, cardSubtitle, cardLink,
-}) => {
+const NumberCard = ({ cardTitle, cardNumber, cardSubtitle, cardLink }) => {
   const card = (
     <div className="numberCard">
       <span className="titleCard"> {cardTitle} </span>
@@ -16,21 +14,27 @@ const NumberCard = ({
       <span className="subtitleCard"> {truncateString(cardSubtitle, 22)} </span>
     </div>
   );
+  const linkedCard = (
+    <a href={cardLink} className="numberCard" target="_blank">
+      <span className="titleCard"> {cardTitle} </span>
+      <span className="resultCard"> {cardNumber.toLocaleString()} </span>
+      <span className="subtitleCard"> {truncateString(cardSubtitle, 22)} </span>
+    </a>
+  );
 
   return (
-    cardLink ? <a href={cardLink}>{card}</a> : card
+    cardLink ? linkedCard : card
   );
 };
 
 export default NumberCard;
-// TO DELETE when data are received from the backend
-NumberCard.defaultProps = {
-  cardLink: '',
-};
-
 NumberCard.propTypes = {
   cardTitle: PropTypes.string.isRequired,
   cardNumber: PropTypes.number.isRequired,
-  cardSubtitle: PropTypes.string.isRequired,
-  cardLink: PropTypes.string,
+  cardSubtitle: PropTypes.string,
+  cardLink: PropTypes.string
+};
+
+NumberCard.defaultProps = {
+  cardSubtitle: '',
 };
