a9e39eaa85c3522ae27c2973a39d37ab19f1fb39
augusto-moraes <Augusto Moraes>
2020-03-10 11:34:08 +0100

    bin location summary calculation as described in the issue

--- grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
+++ grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
@@ -1,6 +1,7 @@
 package org.pih.warehouse.tableroapi
 
 import org.pih.warehouse.tablero.NumberData
+import org.pih.warehouse.inventory.InventorySnapshot
 
 class NumberDataService {
 
@@ -9,16 +10,20 @@ class NumberDataService {
     def dataService
 
     List<NumberData> getListNumberData(def location){
+    def tomorrow = new Date() + 1;
+    tomorrow.clearTime();
+
         def binLocations = inventorySnapshotService.getQuantityOnHandByBinLocation(location);
         def binLocationData = inventoryService.getBinLocationSummary(binLocations);
 
+    def binLocationsTotal = InventorySnapshot.executeQuery('select count(*) from InventorySnapshot i where i.location=:location and i.date = :tomorrow', ['location': location, 'tomorrow': tomorrow]);
         def shipments = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PARTIALLY_RECEIVED' or 'NOT_RECEIVED'");
         def pending = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PENDING'");
         def notCompleted = dataService.executeQuery("select count(*) from openboxes.order  where order.status != 'COMPLETED'");
         def discrepancy = dataService.executeQuery("select count(*) from receipt_item where quantity_shipped != quantity_received");
 
         List<NumberData> numberDataList = [
-            new NumberData("Bin Location Summary", binLocationData[0]["count"], binLocationData[0]["label"], 1, "/openboxes/report/showBinLocationReport?location.id="+ location.id +"&status=inStock"),
+            new NumberData("Bin Location Summary", binLocationsTotal[0], 'Total stock', 1, "/openboxes/report/showBinLocationReport?location.id="+ location.id),
             new NumberData("Bin Location Summary", binLocationData[1]["count"], binLocationData[1]["label"], 2, "/openboxes/report/showBinLocationReport?location.id="+ location.id +"&status=outOfStock"),
             new NumberData("Stock Movements", shipments[0][0], "Not shipped", 3),
             new NumberData("User Incomplete Tasks", pending[0][0], "Not shiped", 4),
