b455ad2d646622d2209ec0e771d73f33fe218fbe
Augusto Moraes <moraestf.augusto@gmail.com>
2020-03-18 10:40:01 +0100

    fixed query and url

--- grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
+++ grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
@@ -11,14 +11,14 @@ class NumberDataService {
         def tomorrow = new Date() + 1;
         tomorrow.clearTime();
 
-        def binLocations = InventorySnapshot.executeQuery('select count(*) from InventorySnapshot i where i.location=:location and i.date = :tomorrow', ['location': location, 'tomorrow': tomorrow]);
+        def binLocations = InventorySnapshot.executeQuery('select count(*) from InventorySnapshot i where i.location=:location and i.date = :tomorrow and i.quantityOnHand > 0', ['location': location, 'tomorrow': tomorrow]);
         def shipments = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PARTIALLY_RECEIVED' or 'NOT_RECEIVED'");
         def pending = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PENDING'");
         def notCompleted = dataService.executeQuery("select count(*) from openboxes.order  where order.status != 'COMPLETED'");
         def discrepancy = dataService.executeQuery("select count(*) from receipt_item where quantity_shipped != quantity_received");
 
         List<NumberData> numberDataList = [
-            new NumberData("Lot and Bin", binLocations[0], 'In stock', 1, "/openboxes/report/showBinLocationReport?location.id="+ location.id),
+            new NumberData("Lot and Bin", binLocations[0], 'In stock', 1, "/openboxes/report/showBinLocationReport?location.id=" + location.id + "&status=inStock"),
             new NumberData("Stock Movements", shipments[0][0], "Not shipped", 3),
             new NumberData("User Incomplete Tasks", pending[0][0], "Not shiped", 4),
             new NumberData("User Incomplete Tasks",notCompleted[0][0], "Not completed", 5),
