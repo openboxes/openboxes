fa26ab18811896fec33f3edc66005ee2bc5cdfcd
dominika <doniahatala@gmail.com>
2020-03-24 13:48:28 +0100

    OBPIH-2675 Change automatic recipient on stock movement from PO

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -1098,6 +1098,7 @@ class StockMovementService {
                 shipmentItem.product = orderItemCommand.orderItem.product
                 shipmentItem.inventoryItem = orderItemCommand.inventoryItem
                 shipmentItem.quantity = orderItemCommand.quantityToShip
+                shipmentItem.recipient = orderItemCommand.orderItem.recipient ?: order.orderedBy
                 shipment.addToShipmentItems(shipmentItem)
                 orderItem.addToShipmentItems(shipmentItem)
             }
