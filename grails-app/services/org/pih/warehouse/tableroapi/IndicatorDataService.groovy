64620d2d0189506d6ae4ae03ce3edd9894ef2fe2
Augusto Moraes <moraestf.augusto@gmail.com>
2020-03-27 16:40:40 +0100

    junk commit reset and code comments

--- grails-app/services/org/pih/warehouse/tableroapi/IndicatorDataService.groovy
+++ grails-app/services/org/pih/warehouse/tableroapi/IndicatorDataService.groovy
@@ -17,6 +17,7 @@ class IndicatorDataService {
     DataGraph getExpirationSummaryData(Location location, def params) {
         Integer querySize = params.querySize ? params.querySize.toInteger() - 1 : 5
 
+        // expirationSummary lists every expired item based on its month
         List expirationSummary = []
         List listLabels = []
         List expirationAlerts = dashboardService.getExpirationAlerts(location)
@@ -25,15 +26,19 @@ class IndicatorDataService {
 
         expirationAlerts.each {
             Integer daysCounter = 0
+            // We should count only items that will expire someday
             if(it.inventoryItem.expires != "never") {
+                // If an item is already inspired, we don't count it
                 if (it.daysToExpiry > 0) {
                     listLabels = []
+                    // For loop verifies if item expires in querySize coming months
                     for (int i=0; i<=querySize; i++) {
+                        // daysCounter += number of days of (i) month
                         daysCounter += date.plusMonths(i).dayOfMonth().getMaximumValue()
 
                         String monthLabel = date.plusMonths(i).toString("MMM", Locale.US)
                         listLabels.push(monthLabel)
-
+                        // if item expires in daysCounter incoming days, we count it
                         if (it.daysToExpiry <= daysCounter ) {
                             expirationSummary[i] =  expirationSummary[i] ? expirationSummary[i] + 1 : 1
                         } 
