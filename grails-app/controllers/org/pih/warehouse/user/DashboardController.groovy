648aaf70c706357844c2eb79c0367bcb39cb419d
Justin Miranda <jcm62@columbia.edu>
2020-03-27 22:03:08 -0500

    Fixed significant performance issue on the dashboard and megamenu

--- grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
+++ grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
@@ -180,7 +180,6 @@ class DashboardController {
         render results as JSON
     }
 
-    @Cacheable("megamenuCache")
     def megamenu = {
 
         def user = User.get(session?.user?.id)
@@ -227,8 +226,7 @@ class DashboardController {
                 outboundShipmentsCount: outboundShipmentsCount,
                 incomingOrders        : incomingOrders,
                 requisitionStatistics : requisitionStatistics,
-                quickCategories       : productService.getQuickCategories(),
-                tags                  : productService.getAllTags()
+                quickCategories       : productService.getQuickCategories()
         ]
     }
 
