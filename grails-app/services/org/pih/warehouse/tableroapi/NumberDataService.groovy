a103a7c662f9911afc84964b9295938727e57d33
Augusto Moraes <moraestf.augusto@gmail.com>
2020-03-31 16:42:17 +0200

    formating fix

--- grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
+++ grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
@@ -15,11 +15,11 @@ class NumberDataService {
         def binLocations = InventorySnapshot.executeQuery('select count(*) from InventorySnapshot i where i.location=:location and i.date = :tomorrow and i.quantityOnHand > 0', ['location': location, 'tomorrow': tomorrow]);
         def shipments = Requisition.executeQuery("""select count(*) from Requisition r where r.origin = :location and r.status <> 'ISSUED' and r.createdBy = :user""", 
         ['location': location, 'user': user]);
+        def inconpletePutaways = dataService.executeQuery("select count(*) from openboxes.order where order.order_type_code = 'TRANSFER_ORDER' AND order.status = 'PENDING' AND order.ordered_by_id = "+ user.id);
 
         def pending = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PENDING'");
-        def notCompleted = dataService.executeQuery("select count(*) from openboxes.order where order.status != 'COMPLETED'");
-
-        def inconpletePutaways = dataService.executeQuery("select count(*) from openboxes.order where order.order_type_code = 'TRANSFER_ORDER' AND order.status = 'PENDING' AND order.ordered_by_id = "+ user.id);
+        def notCompleted = dataService.executeQuery("select count(*) from openboxes.order  where order.status != 'COMPLETED'");
+        def discrepancy = dataService.executeQuery("select count(*) from receipt_item where quantity_shipped != quantity_received");
 
         List<NumberData> numberDataList = [
             new NumberData("Lot and Bin", binLocations[0], 'In stock', 1, "/openboxes/report/showBinLocationReport?location.id=" + location.id + "&status=inStock"),
@@ -27,6 +27,7 @@ class NumberDataService {
             new NumberData("Incomplete Putaways",inconpletePutaways[0][0], "of " + user.name, 3, "/openboxes/order/list/listForm?orderedById=" + user.id),
             new NumberData("User Incomplete Tasks", pending[0][0], "Not shiped", 4),
             new NumberData("User Incomplete Tasks",notCompleted[0][0], "Not completed", 5),
+            new NumberData("Discrepancy",discrepancy[0][0], "Items received", 6)
         ] as List<NumberData>
 
         return numberDataList;
