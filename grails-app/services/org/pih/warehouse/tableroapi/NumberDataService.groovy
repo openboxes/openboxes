31f6515e00efc57b1cf6ecc307e9c268b45c52c8
Augusto Moraes <moraestf.augusto@gmail.com>
2020-03-31 17:48:55 +0200

    code review

--- grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
+++ grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
@@ -1,5 +1,6 @@
 package org.pih.warehouse.tableroapi
 
+import org.pih.warehouse.order.Order
 import org.pih.warehouse.tablero.NumberData
 import org.pih.warehouse.requisition.Requisition
 import org.pih.warehouse.inventory.InventorySnapshot
@@ -15,7 +16,7 @@ class NumberDataService {
         def binLocations = InventorySnapshot.executeQuery('select count(*) from InventorySnapshot i where i.location=:location and i.date = :tomorrow and i.quantityOnHand > 0', ['location': location, 'tomorrow': tomorrow]);
         def shipments = Requisition.executeQuery("""select count(*) from Requisition r where r.origin = :location and r.status <> 'ISSUED' and r.createdBy = :user""", 
         ['location': location, 'user': user]);
-        def inconpletePutaways = dataService.executeQuery("select count(*) from openboxes.order where order.order_type_code = 'TRANSFER_ORDER' AND order.status = 'PENDING' AND order.ordered_by_id = "+ user.id);
+        def incompletePutaways = Order.executeQuery("select count(o.id) from Order o where o.orderTypeCode = 'TRANSFER_ORDER' AND o.status = 'PENDING' AND o.orderedBy = :user", ['user':user]);
 
         def pending = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PENDING'");
         def notCompleted = dataService.executeQuery("select count(*) from openboxes.order  where order.status != 'COMPLETED'");
@@ -23,9 +24,9 @@ class NumberDataService {
 
         List<NumberData> numberDataList = [
             new NumberData("Lot and Bin", binLocations[0], 'In stock', 1, "/openboxes/report/showBinLocationReport?location.id=" + location.id + "&status=inStock"),
-            new NumberData("Your shipments", shipments[0], "In Progress", 2, "/openboxes/stockMovement/list?receiptStatusCode=PENDING&origin.id=" + location.id + "&createdBy.id=" + user.id),
-            new NumberData("Incomplete Putaways",inconpletePutaways[0][0], "of " + user.name, 3, "/openboxes/order/list/listForm?orderedById=" + user.id),
-            new NumberData("User Incomplete Tasks", pending[0][0], "Not shiped", 4),
+            new NumberData("Your Shipments", shipments[0], "In Progress", 2, "/openboxes/stockMovement/list?receiptStatusCode=PENDING&origin.id=" + location.id + "&createdBy.id=" + user.id),
+            new NumberData("Your Putaways",incompletePutaways[0], "In Progress", 3, "/openboxes/order/list/listForm?orderedById=" + user.id),
+            new NumberData("User Incomplete Tasks", pending[0][0], "Not shipped", 4),
             new NumberData("User Incomplete Tasks",notCompleted[0][0], "Not completed", 5),
             new NumberData("Discrepancy",discrepancy[0][0], "Items received", 6)
         ] as List<NumberData>
