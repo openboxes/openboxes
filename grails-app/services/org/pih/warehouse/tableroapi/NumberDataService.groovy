c470cbf0d6b3687fcbe8b309d78e2f1378728227
Augusto Moraes <moraestf.augusto@gmail.com>
2020-03-31 16:37:48 +0200

    miss type correction

--- grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
+++ grails-app/services/org/pih/warehouse/tableroapi/NumberDataService.groovy
@@ -15,15 +15,16 @@ class NumberDataService {
         def binLocations = InventorySnapshot.executeQuery('select count(*) from InventorySnapshot i where i.location=:location and i.date = :tomorrow and i.quantityOnHand > 0', ['location': location, 'tomorrow': tomorrow]);
         def shipments = Requisition.executeQuery("""select count(*) from Requisition r where r.origin = :location and r.status <> 'ISSUED' and r.createdBy = :user""", 
         ['location': location, 'user': user]);
-        def inconpletePutaways = dataService.executeQuery("select count(*) from openboxes.order where order.order_type_code = 'TRANSFER_ORDER' AND order.status = 'PENDING' AND order.ordered_by_id = "+ user.id);
 
         def pending = dataService.executeQuery("select count(*) from shipment where shipment.current_status = 'PENDING'");
         def notCompleted = dataService.executeQuery("select count(*) from openboxes.order where order.status != 'COMPLETED'");
 
+        def inconpletePutaways = dataService.executeQuery("select count(*) from openboxes.order where order.order_type_code = 'TRANSFER_ORDER' AND order.status = 'PENDING' AND order.ordered_by_id = "+ user.id);
+
         List<NumberData> numberDataList = [
             new NumberData("Lot and Bin", binLocations[0], 'In stock', 1, "/openboxes/report/showBinLocationReport?location.id=" + location.id + "&status=inStock"),
             new NumberData("Your shipments", shipments[0], "In Progress", 2, "/openboxes/stockMovement/list?receiptStatusCode=PENDING&origin.id=" + location.id + "&createdBy.id=" + user.id),
-            new NumberData("Inconplete Putaways",inconpletePutaways[0][0], "of " + user.name, 3, "/openboxes/order/list/listForm?orderedById=" + user.id),
+            new NumberData("Incomplete Putaways",inconpletePutaways[0][0], "of " + user.name, 3, "/openboxes/order/list/listForm?orderedById=" + user.id),
             new NumberData("User Incomplete Tasks", pending[0][0], "Not shiped", 4),
             new NumberData("User Incomplete Tasks",notCompleted[0][0], "Not completed", 5),
         ] as List<NumberData>
