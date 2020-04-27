9f971bdd85b341bcc9afdec288be1c577d43e09b
Justin Miranda <jcm62@columbia.edu>
2020-04-26 21:33:55 -0500

    OBPIH-2831 Refactoring required based on pull request comments

--- grails-app/controllers/org/pih/warehouse/order/OrderController.groovy
+++ grails-app/controllers/org/pih/warehouse/order/OrderController.groovy
@@ -95,7 +95,7 @@ class OrderController {
                                     shipmentItem: shipmentItem,
                                     quantityMinimum: 0,
                                     quantityToShip: shipmentItem.quantity / orderItem?.quantityPerUom,
-                                    quantityMaximum: orderItem.quantityRemaining()
+                                    quantityMaximum: orderItem.quantityRemaining
 
                             )
                     command.shipOrderItems.add(shipOrderItem)
@@ -103,7 +103,7 @@ class OrderController {
             }
             // Or populate line items from quantity remaining on order item
             else {
-                def quantityRemaining = orderItem?.quantityRemaining()?:0
+                def quantityRemaining = orderItem?.quantityRemaining
                 ShipOrderItemCommand shipOrderItem =
                             new ShipOrderItemCommand(
                                     orderItem: orderItem,
@@ -134,9 +134,9 @@ class OrderController {
             order.orderItems.each { OrderItem orderItem ->
                 List shipOrderItems = shipOrderItemsByOrderItem.get(orderItem)
                 BigDecimal totalQuantityToShip = shipOrderItems.sum { it?.quantityToShip?:0 }
-                if (totalQuantityToShip > orderItem.quantityRemaining()) {
+                if (totalQuantityToShip > orderItem.quantityRemaining) {
                     orderItem.errors.reject("Sum of quantity to ship (${totalQuantityToShip}) " +
-                            "cannot be greater than remaining quantity (${orderItem.quantityRemaining()}) " +
+                            "cannot be greater than remaining quantity (${orderItem.quantityRemaining}) " +
                             "for order item '${orderItem.product.productCode} ${orderItem.product.name}'"
                     )
                     throw new ValidationException("Invalid order item", orderItem.errors)
@@ -594,6 +594,7 @@ class OrderController {
     }
 
     def saveOrderItem = {
+        log.info "params " + params
         Order order = Order.get(params.order.id)
         OrderItem orderItem = OrderItem.get(params.orderItem.id)
         if (!orderItem) {
