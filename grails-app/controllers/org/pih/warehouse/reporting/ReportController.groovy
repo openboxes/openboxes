4cf1f0bb568f0455f6c57d022aed9e2069c8a71d
Justin Miranda <jcm62@columbia.edu>
2020-05-02 13:06:20 -0500

    Fixed issue with the transaction report not rendering on initial load

--- grails-app/controllers/org/pih/warehouse/reporting/ReportController.groovy
+++ grails-app/controllers/org/pih/warehouse/reporting/ReportController.groovy
@@ -273,7 +273,7 @@ class ReportController {
                 command           : command,
                 locationKey       : locationKey,
                 transactionCount  : locationKey ? TransactionFact.countByLocationKey(locationKey) : 0,
-                productCount      : TransactionFact.countDistinctProducts(locationKey?.locationId).list(),
+                productCount      : TransactionFact.countDistinctProducts(locationKey?.locationId).get(),
                 minTransactionDate: TransactionFact.minTransactionDate(locationKey?.locationId).list(),
                 maxTransactionDate: TransactionFact.maxTransactionDate(locationKey?.locationId).list(),
                 previousFireTime  : previousFireTime,
