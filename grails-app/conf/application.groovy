3671a1070c6450f40a5655c81e622f7532162aee
Justin Miranda <jcm62@columbia.edu>
2020-05-04 13:51:14 -0500

    OBGM-216 Disable deep validation globally

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,3 +1,7 @@
 grails.gorm.default.constraints = {
     expirationDateConstraint(nullable:true, min: new Date("01/01/2000"))
 }
+
+grails.gorm.default.mapping = {
+    '*'(cascadeValidate: 'none')
+}
