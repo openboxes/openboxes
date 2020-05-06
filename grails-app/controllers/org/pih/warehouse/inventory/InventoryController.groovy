888eeaa970d461f4196b2d40f7fac292c8391168
Justin Miranda <jcm62@columbia.edu>
2020-05-06 13:11:12 -0500

    OBGM-221 Command object properties should be nullable by default

--- grails-app/controllers/org/pih/warehouse/inventory/InventoryController.groovy
+++ grails-app/controllers/org/pih/warehouse/inventory/InventoryController.groovy
@@ -20,6 +20,7 @@ import org.apache.commons.collections.list.LazyList
 import org.apache.commons.lang.StringEscapeUtils
 import org.pih.warehouse.core.ActivityCode
 import org.pih.warehouse.core.Constants
+import org.pih.warehouse.core.DefaultNullableCommand
 import org.pih.warehouse.core.Tag
 import org.pih.warehouse.core.User
 import org.pih.warehouse.importer.InventoryExcelImporter
@@ -1407,7 +1408,7 @@ class QuantityOnHandReportCommand implements Validateable {
     }
 }
 
-class ManageInventoryCommand implements Validateable {
+class ManageInventoryCommand extends DefaultNullableCommand {
 
     List<ManageInventoryEntryCommand> entries = LazyList.decorate(new ArrayList(), FactoryUtils.instantiateFactory(ManageInventoryEntryCommand.class))
     List inventoryItems = []
@@ -1416,7 +1417,7 @@ class ManageInventoryCommand implements Validateable {
     List tags = []
 }
 
-class ManageInventoryEntryCommand implements Validateable {
+class ManageInventoryEntryCommand extends DefaultNullableCommand {
     InventoryItem inventoryItem
     Integer quantity
 
