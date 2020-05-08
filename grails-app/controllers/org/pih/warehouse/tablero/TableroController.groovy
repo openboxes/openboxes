f9fa494da3083e4b823095aaf3e3d0f11a8771e2
Justin Miranda <jcm62@columbia.edu>
2020-05-07 22:15:29 -0500

    OBPIH-2881 Added config endpoint for the new dashboard feature

--- grails-app/controllers/org/pih/warehouse/tablero/TableroController.groovy
+++ grails-app/controllers/org/pih/warehouse/tablero/TableroController.groovy
@@ -1,10 +1,17 @@
 package org.pih.warehouse.tablero
 
+import grails.converters.JSON
+
 class TableroController {
 
-def index= {
-    
-    render(template: "/common/react")
-}
+    def index= {
+        render(template: "/common/react")
+    }
+
+    def config = {
+        render([
+                data: grailsApplication.config.openboxes.tablero,
+        ] as JSON)
+    }
 
 }
