2f732dbb995c1f6fa138c8765dd2f2fdc8489c9b
julienreliefapps <58730487+julienreliefapps@users.noreply.github.com>
2020-06-23 12:32:29 +0200

    OBPIH-2957 fix of the logoLabel null case issue (#1745)

--- src/js/components/Layout/Header.jsx
+++ src/js/components/Layout/Header.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Translate from '../../utils/Translate';
-import translations from '../../en';
 import GlobalSearch from '../GlobalSearch';
 import LocationChooser from '../location/LocationChooser';
 import apiClient from '../../utils/apiClient';
-
-const { dashboard } = translations.navbar;
 
 /** Logs out impersonated user and redirects to dashboard */
 function logoutImpersonatedUser() {
@@ -39,12 +36,12 @@ const Header = ({
     <div className="d-flex align-items-center justify-content-between flex-wrap">
       <div className="logo-header">
         <a
-          href={dashboard.link}
+          href="/openboxes"
           className="navbar-brand brand-name"
         >
           <img alt="Openboxes" src={`/openboxes/location/viewLogo/${currentLocationId}`} onError={(e) => { e.target.onerror = null; e.target.src = 'https://openboxes.com/img/logo_30.png'; }} />
         </a>
-        <span>{logoLabel} </span>
+        { logoLabel.trim() !== '' ? <span>{logoLabel} </span> : null }
       </div>
       <div className="d-flex flex-wrap">
         <GlobalSearch />
