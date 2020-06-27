1e9acac65e6c6401b20030ac65a0f6c8d3bbacf9
Justin Miranda <jcm62@columbia.edu>
2020-06-26 22:42:52 -0500

    OBGM-302 Consumption Report > Cannot invoke method minus() on null object

--- grails-app/controllers/org/pih/warehouse/reporting/ConsumptionController.groovy
+++ grails-app/controllers/org/pih/warehouse/reporting/ConsumptionController.groovy
@@ -483,8 +483,6 @@ class ShowConsumptionCommand implements Validateable {
     Boolean includeMonthlyBreakdown = Boolean.TRUE
     Boolean includeQuantityOnHand = Boolean.TRUE
 
-    def productDomain = new DefaultGrailsDomainClass(Product.class)
-
     def selectedProperties = LazyList.decorate(new ArrayList(), FactoryUtils.instantiateFactory(String.class))
 
     // Payload
@@ -496,11 +494,14 @@ class ShowConsumptionCommand implements Validateable {
     def onHandQuantityMap = new TreeMap()
     def transferOutMap = [:]
 
+    static transients = ["numberOfDays", "numberOfWeeks", "numberOfMonths"]
+
     static constraints = {
         fromLocations(nullable: false)
         toLocations(nullable: true)
         fromDate(nullable: true)
         toDate(nullable: true)
+        parametersHash(nullable: true)
     }
 
     Boolean hasParameterChanged() {
@@ -514,7 +515,10 @@ class ShowConsumptionCommand implements Validateable {
     }
 
     Integer getNumberOfDays() {
-        return (toDate - fromDate)
+        if (toDate && fromDate) {
+            return (toDate - fromDate)
+        }
+        return 0
     }
 
     Float getNumberOfWeeks() {
@@ -589,7 +593,6 @@ class ShowConsumptionRowCommand implements Validateable {
             return 0.0
         }
     }
-
 
     String transferOutLocations(List<Location> locations) {
         String transferOutLocations = ""
