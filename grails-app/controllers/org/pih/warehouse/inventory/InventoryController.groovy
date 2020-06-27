f5ce7ac2beba54d7b038712ad3f17d9d993f263a
Justin Miranda <jcm62@columbia.edu>
2020-06-26 22:11:00 -0500

    OBGM-236 Fixed data binding issue when creating a new transaction for a product

--- grails-app/controllers/org/pih/warehouse/inventory/InventoryController.groovy
+++ grails-app/controllers/org/pih/warehouse/inventory/InventoryController.groovy
@@ -469,10 +469,6 @@ class InventoryController {
         }
     }
 
-    def listTransactions() {
-        redirect(action: listAllTransactions)
-    }
-
     def listDailyTransactions() {
         def dateFormat = new SimpleDateFormat("dd/MM/yyyy")
         def dateSelected = (params.date) ? dateFormat.parse(params.date) : new Date()
@@ -827,8 +823,7 @@ class InventoryController {
 
     }
 
-
-    def listAllTransactions() {
+    def listTransactions() {
 
         Location location = Location.get(session.warehouse.id)
         def currentInventory = location.inventory
@@ -867,6 +862,9 @@ class InventoryController {
                                                  transactionCount       : transactions.totalCount, transactionTypeSelected: transactionType])
     }
 
+    def listAllTransactions() {
+        redirect(action: "listTransactions")
+    }
 
     def listPendingTransactions() {
         def transactions = Transaction.findAllByConfirmedOrConfirmedIsNull(Boolean.FALSE)
@@ -984,7 +982,7 @@ class InventoryController {
             transactionInstance?.dateConfirmed = new Date()
             flash.message = "${warehouse.message(code: 'inventory.transactionHasBeenConfirmed.message')}"
         }
-        redirect(action: "listAllTransactions")
+        redirect(action: "listTransactions")
     }
 
     def createInboundTransfer() {
@@ -1161,6 +1159,8 @@ class InventoryController {
         log.info("Saving debit transactions " + params)
         log.info("size: " + command?.transactionEntries?.size())
 
+        // Data binding not working properly for nested objects of command objects
+        command.transactionInstance = new Transaction(params.transactionInstance)
 
         // Get the products involved
         def productIds = params.list('product.id').collect { String.valueOf(it) }
@@ -1199,6 +1199,7 @@ class InventoryController {
                     transaction.save(failOnError: true)
                     flash.message = "Successfully saved transaction"
                     redirect(controller: "inventoryItem", action: "showStockCard", id: productIds[0])
+                    return
                 }
             } catch (ValidationException e) {
                 log.debug("caught validation exception " + e)
@@ -1228,8 +1229,8 @@ class InventoryController {
             command.warehouseInstance = warehouseInstance
             command.quantityMap = quantityMap
 
-            render(view: "createTransaction", model: [command: command])
         }
+        render(view: "createTransaction", model: [command: command])
     }
 
 
