379461cb20e6ef15b595635cf8c19a9826395183
Justin Miranda <jcm62@columbia.edu>
2020-06-30 15:59:55 -0500

    OBGM-146 Removed hard-coded openboxes reference from URLs

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -98,7 +98,7 @@ export function fetchSessionInfo() {
 }
 
 export function fetchMenuConfig() {
-  const url = '/openboxes/api/getMenuConfig';
+  const url = '/api/getMenuConfig';
   const request = apiClient.get(url);
 
   return {
@@ -375,7 +375,7 @@ export function reorderIndicators({ oldIndex, newIndex }, e, type) {
 
 export function fetchConfigAndData(locationId, config = 'personal', filterSelected) {
   return (dispatch) => {
-    apiClient.get('/openboxes/apitablero/config').then((res) => {
+    apiClient.get('/apitablero/config').then((res) => {
       dispatch({
         type: FETCH_CONFIG,
         payload: {
@@ -389,7 +389,7 @@ export function fetchConfigAndData(locationId, config = 'personal', filterSelect
 
 export function fetchConfig() {
   return (dispatch) => {
-    apiClient.get('/openboxes/apitablero/config').then((res) => {
+    apiClient.get('/apitablero/config').then((res) => {
       dispatch({
         type: FETCH_CONFIG,
         payload: {
