87199b593ab63e5b221b56043e5bc605598c7809
Justin Miranda <jcm62@columbia.edu>
2020-07-24 15:44:15 -0500

    OBGM-225 Person scaffolded views are empty due to missing domainSuffix

--- grails-app/controllers/org/pih/warehouse/core/PersonController.groovy
+++ grails-app/controllers/org/pih/warehouse/core/PersonController.groovy
@@ -15,10 +15,15 @@ import grails.gorm.transactions.Transactional
 class PersonController {
     def userService
 
-    static scaffold = Person
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
 
     def redirect() {
         redirect(action: "show", id: params.id)
+    }
+
+    def index() {
+        redirect(action: "list", params:params)
     }
 
 
@@ -26,7 +31,7 @@ class PersonController {
         def personInstanceList = []
         def personInstanceTotal = 0
 
-        params.max = Math.min(params.max ? params.int('max') : 15, 100)
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
 
         if (params.q) {
             String[] terms = ["%" + params.q + "%"]
@@ -39,6 +44,91 @@ class PersonController {
         }
 
         [personInstanceList: personInstanceList, personInstanceTotal: personInstanceTotal]
+    }
+
+    def create() {
+        def personInstance = new Person()
+        personInstance.properties = params
+        return [personInstance: personInstance]
+    }
+
+    def save() {
+        def personInstance = new Person(params)
+        if (personInstance.save(flush: true)) {
+            flash.message = "${warehouse.message(code: 'default.created.message', args: [warehouse.message(code: 'person.label', default: 'Person'), personInstance.id])}"
+            redirect(action: "list", id: personInstance.id)
+        }
+        else {
+            render(view: "create", model: [personInstance: personInstance])
+        }
+    }
+
+    def show() {
+        def personInstance = Person.get(params.id)
+        if (!personInstance) {
+            flash.message = "${warehouse.message(code: 'default.not.found.message', args: [warehouse.message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [personInstance: personInstance]
+        }
+    }
+
+    def edit() {
+        def personInstance = Person.get(params.id)
+        if (!personInstance) {
+            flash.message = "${warehouse.message(code: 'default.not.found.message', args: [warehouse.message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            return [personInstance: personInstance]
+        }
+    }
+
+    def update() {
+        def personInstance = Person.get(params.id)
+        if (personInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (personInstance.version > version) {
+
+                    personInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [warehouse.message(code: 'person.label', default: 'Person')] as Object[], "Another user has updated this Person while you were editing")
+                    render(view: "edit", model: [personInstance: personInstance])
+                    return
+                }
+            }
+            personInstance.properties = params
+            if (!personInstance.hasErrors() && personInstance.save(flush: true)) {
+                flash.message = "${warehouse.message(code: 'default.updated.message', args: [warehouse.message(code: 'person.label', default: 'Person'), personInstance.id])}"
+                redirect(action: "list", id: personInstance.id)
+            }
+            else {
+                render(view: "edit", model: [personInstance: personInstance])
+            }
+        }
+        else {
+            flash.message = "${warehouse.message(code: 'default.not.found.message', args: [warehouse.message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+
+    def delete() {
+        def personInstance = Person.get(params.id)
+        if (personInstance) {
+            try {
+                personInstance.delete(flush: true)
+                flash.message = "${warehouse.message(code: 'default.deleted.message', args: [warehouse.message(code: 'person.label', default: 'Person'), params.id])}"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "${warehouse.message(code: 'default.not.deleted.message', args: [warehouse.message(code: 'person.label', default: 'Person'), params.id])}"
+                redirect(action: "list", id: params.id)
+            }
+        }
+        else {
+            flash.message = "${warehouse.message(code: 'default.not.found.message', args: [warehouse.message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
     }
 
     def convertPersonToUser() {
