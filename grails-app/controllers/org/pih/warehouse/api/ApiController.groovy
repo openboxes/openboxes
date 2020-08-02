4d94dd3efc4642e04ad74d90b32c64d1892f9885
Justin Miranda <jcm62@columbia.edu>
2020-08-01 23:50:23 -0500

    Fixing references to taglib, holder, API and image URLs

--- grails-app/controllers/org/pih/warehouse/api/ApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/ApiController.groovy
@@ -102,7 +102,7 @@ class ApiController {
 
             localizationMode = [
                 "label"      : messageSource.getMessage('localization.disable.label', emptyArgs, 'Disable translation mode', locale),
-                "linkIcon"   : "${request.contextPath}/images/icons/silk/world_delete.png",
+                "linkIcon"   : "${request.contextPath}/static/images/icons/silk/world_delete.png",
                 "linkAction" : "${request.contextPath}/user/disableLocalizationMode",
                 "linkReactIcon" : "localization-mode",
             ]
@@ -111,7 +111,7 @@ class ApiController {
 
             localizationMode = [
                     "label"     : messageSource.getMessage('localization.enable.label', emptyArgs, 'Enable translation mode', locale),
-                    "linkIcon"  : "${request.contextPath}/images/icons/silk/world_add.png",
+                    "linkIcon"  : "${request.contextPath}/static/images/icons/silk/world_add.png",
                     "linkAction": "${request.contextPath}/user/enableLocalizationMode",
                     "linkReactIcon" : "localization-mode",
             ]
@@ -121,20 +121,20 @@ class ApiController {
                 "label"      : messageSource.getMessage('default.edit.label',
                         [messageSource.getMessage('user.profile.label', emptyArgs, 'Profile', locale)] as Object[],
                         'Enable translation mode', locale),
-                "linkIcon"   : "${request.contextPath}/images/icons/silk/user.png",
+                "linkIcon"   : "${request.contextPath}/static/images/icons/silk/user.png",
                 "linkAction" : "${request.contextPath}/user/edit/${session?.user?.id}",
                 "linkReactIcon" : "profile",
             ],
             localizationMode,
             [
                 "label"      : messageSource.getMessage('cache.flush.label', emptyArgs, 'Refresh caches', locale),
-                "linkIcon"   : "${request.contextPath}/images/icons/silk/database_wrench.png",
+                "linkIcon"   : "${request.contextPath}/static/images/icons/silk/database_wrench.png",
                 "linkAction" : "${request.contextPath}/dashboard/flushCache",
                 "linkReactIcon" : "flush-cache",
             ],
             [
                 "label"      : messageSource.getMessage('default.logout.label', emptyArgs, 'Logout', locale),
-                "linkIcon"   : "${request.contextPath}/images/icons/silk/door.png",
+                "linkIcon"   : "${request.contextPath}/static/images/icons/silk/door.png",
                 "linkAction" : "${request.contextPath}/auth/logout",
                 "linkReactIcon" : "logout",
             ]
--- grails-app/controllers/org/pih/warehouse/data/MigrationController.groovy
+++ grails-app/controllers/org/pih/warehouse/data/MigrationController.groovy
@@ -14,6 +14,7 @@ import grails.util.Holders
 import grails.gorm.transactions.Transactional
 import grails.validation.Validateable
 import grails.validation.ValidationException
+import org.grails.plugins.web.taglib.ValidationTagLib
 import org.hibernate.criterion.CriteriaSpecification
 import org.pih.warehouse.core.Constants
 import org.pih.warehouse.core.Location
@@ -92,7 +93,7 @@ class MigrationController {
     }
 
     def stockMovementsWithoutShipmentItems() {
-        def g = Holders.grailsApplication.mainContext.getBean( 'org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib' )
+        ValidationTagLib g = Holders.grailsApplication.mainContext.getBean(ValidationTagLib.class)
         def data = migrationService.stockMovementsWithoutShipmentItems
         // id, status, request_number, date_created, origin, requested, picked, shipped
         if ("count".equals(params.format)) {
@@ -234,7 +235,7 @@ class MigrationController {
 
 
     def receiptsWithoutTransaction() {
-        def g = ApplicationHolder.application.mainContext.getBean( 'org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib' )
+        def g = Holders.grailsApplication.mainContext.getBean(ApplicationTagLib.class)
         def data = migrationService.getReceiptsWithoutTransaction()
         if ("count".equals(params.format)) {
             render data.size()
