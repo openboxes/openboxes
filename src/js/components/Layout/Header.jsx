690677c27162161b89f21d8970a928eda27ec777
Justin Miranda <jcm62@columbia.edu>
2020-08-07 10:57:45 -0500

    Fixed eslint indent issue

--- src/js/components/Layout/Header.jsx
+++ src/js/components/Layout/Header.jsx
@@ -31,7 +31,7 @@ class Header extends Component {
 
     apiClient.post(url)
       .then(() => {
-      window.location = '/dashboard/index';
+        window.location = '/dashboard/index';
       });
   }
 
