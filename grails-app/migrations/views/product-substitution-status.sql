b4f995139c536728a3d772d8ab5da4f9d6f5512f
awalkowiak <awalkowiak@soldevelo.com>
2020-09-14 16:24:44 +0200

    Moved quantity on hand check into WHERE clause

--- grails-app/migrations/views/product-substitution-status.sql
+++ grails-app/migrations/views/product-substitution-status.sql
@@ -15,7 +15,7 @@ CREATE OR REPLACE VIEW product_substitution_status AS
 
         -- product_summary join (used for getting substitute product location and substitution product qoh)
         LEFT OUTER JOIN
-            product_summary AS p_s ON p_s.product_id = p_a.associated_product_id AND p_s.quantity_on_hand > 0
+            product_summary AS p_s ON p_s.product_id = p_a.associated_product_id
 
         -- inventory_item joins based on original and substitution products (used for original and substitution inventory items expiration date comparison)
         LEFT OUTER JOIN
@@ -23,7 +23,7 @@ CREATE OR REPLACE VIEW product_substitution_status AS
         LEFT OUTER JOIN
             inventory_item AS original_inventory_item ON original_inventory_item.product_id = p_a.product_id
 
-        WHERE p_a.code = 'SUBSTITUTE'
+        WHERE p_a.code = 'SUBSTITUTE' AND p_s.quantity_on_hand > 0
 
         GROUP BY product_id, associated_product_id, location_id) a
     WHERE original_min_date > substitution_min_date or substitution_quantity_on_hand > 0
