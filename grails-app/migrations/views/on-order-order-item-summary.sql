d72e6fe79665634fc043b5e7f18f4390e972fd94
dominika <doniahatala@gmail.com>
2020-09-30 17:43:37 +0200

    OBPIH-3256 Rewrite on order summary views

--- grails-app/migrations/views/on-order-order-item-summary.sql
+++ grails-app/migrations/views/on-order-order-item-summary.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE VIEW on_order_order_item_summary AS (
-SELECT a.product_code, a.name, a.destination_id, sum(a.quantity_ordered) - sum(a.quantity_shipped) as quantity_ordered_not_shipped FROM (
+SELECT a.product_code, a.name, a.destination_id, sum(a.quantity_ordered) - sum(a.quantity_shipped) as quantity_ordered_not_shipped , null as quantity_shipped_not_received FROM (
 SELECT
         product.product_code,
         product.name,
         order_item.id,
         `order`.destination_id,
-        sum(distinct case when `order`.status != 'PENDING' then order_item.quantity else 0 end) as quantity_ordered,
+        sum(distinct case when `order`.status != 'PENDING' then order_item.quantity * order_item.quantity_per_uom else 0 end) as quantity_ordered,
         sum(case when shipment_item.quantity then shipment_item.quantity else 0 end) as quantity_shipped,
         null as quantity_shipped_not_received
     FROM order_item
@@ -19,7 +19,7 @@ SELECT
         shipment_item ON order_shipment.shipment_item_id = shipment_item.id
     LEFT OUTER JOIN
         shipment ON shipment_item.shipment_id = shipment.id
-    WHERE `order`.order_type_code = 'PURCHASE_ORDER'
+    WHERE `order`.order_type_code = 'PURCHASE_ORDER' AND order_item.order_item_status_code != 'CANCELED'
     GROUP BY  product.product_code, order_item.id, product.name, `order`.destination_id ) a
 GROUP BY product_code, name, destination_id
 )
