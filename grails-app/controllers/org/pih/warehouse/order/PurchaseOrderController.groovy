29a60c564ab3f9e22139bd7576785a66b16933c0
Justin Miranda <jcm62@columbia.edu>
2020-12-09 15:59:25 -0600

    Fixed bug if purchase order has validation errors after save (e.g. duplicate PO number)

--- grails-app/controllers/org/pih/warehouse/order/PurchaseOrderController.groovy
+++ grails-app/controllers/org/pih/warehouse/order/PurchaseOrderController.groovy
@@ -67,6 +67,7 @@ class PurchaseOrderController {
 
         if (order.hasErrors()) {
             render(template: "enterOrderDetails", model: [order: order])
+            return
         }
 
         redirect(action: addItems, id: order.id)
