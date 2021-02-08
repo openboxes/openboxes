161c1de77a7dee53c58c3f41426ae9f3c54a96fd
Dominika <dhatala@soldevelo.com>
2021-02-08 14:07:27 +0100

    OBPIH-3547 Header redesign on stock movement and receiving

--- src/js/components/wizard/WizardTitle.jsx
+++ src/js/components/wizard/WizardTitle.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 const WizardTitle = props => (
   <div className="panel-heading movement-number">
-    {props.title.length ? (<span>{props.title}</span>) : null}
+    {props.title ?
+      (
+        <div>
+          {
+            _.map(props.title, element => (
+              <span style={{ color: element.color }}>
+                {element.text}
+                <span style={{ color: 'black' }}>
+                  {element.delimeter}
+                </span>
+              </span>
+            ))
+          }
+        </div>
+      )
+      : null}
     {props.additionalTitle}
   </div>
 );
@@ -12,7 +28,7 @@ export default WizardTitle;
 
 WizardTitle.propTypes = {
   /** Array of steps names */
-  title: PropTypes.string.isRequired,
+  title: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   additionalTitle: PropTypes.oneOf([PropTypes.string, PropTypes.func]),
 };
 
