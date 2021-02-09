45fae1a161c06c07409de264801684e00ad50798
Justin Miranda <jcm62@columbia.edu>
2021-02-08 22:58:31 -0600

    OBS-607 Added error logging to better understand why bin location import feature is not working

--- grails-app/services/org/pih/warehouse/core/LocationService.groovy
+++ grails-app/services/org/pih/warehouse/core/LocationService.groovy
@@ -363,18 +363,21 @@ class LocationService {
                         binLocation.locationNumber = it.name
                         binLocation.parentLocation = location
                         binLocation.locationType = defaultLocationType
-                        location.addToLocations(binLocation)
+                        if (binLocation.validate()) {
+                            location.addToLocations(binLocation)
+                        }
+                        log.info "Errors " + binLocation.errors
                     } else {
                         log.info "Bin location ${it.name} already exists"
                     }
                 }
                 location.save()
-
+                log.info "Errors " + location.errors
             } else {
                 throw new ValidationException("location.cannotImportEmptyBinLocations.message")
             }
         } catch (Exception e) {
-            log.error("Unable to bin locations due to exception: " + e.message, e)
+            log.error("Unable to save bin locations due to exception: " + e.message)
             throw new RuntimeException(e.message)
         }
         finally {
