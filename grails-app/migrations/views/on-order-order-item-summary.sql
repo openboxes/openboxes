de5ac4c71d5e081a8f9bcda1867023ee19d39747
Justin Miranda <jcm62@columbia.edu>
2021-02-13 00:39:13 -0600

    OBPIH-3487 Minor improvements to remove unnecessary columns, improve formatting, fix typos, return zeros instead of nulls

--- grails-app/migrations/views/on-order-order-item-summary.sql
+++ grails-app/migrations/views/on-order-order-item-summary.sql
@@ -1,8 +1,7 @@
 CREATE OR REPLACE VIEW on_order_order_item_summary AS (
-SELECT a.product_code, a.name, a.destination_id, sum(a.quantity_ordered) - sum(a.quantity_shipped) as quantity_ordered_not_shipped , null as quantity_shipped_not_received FROM (
+SELECT a.product_id, a.destination_id, sum(a.quantity_ordered) - sum(a.quantity_shipped) as quantity_ordered_not_shipped , null as quantity_shipped_not_received FROM (
 SELECT
-        product.product_code,
-        product.name,
+        product.id as product_id,
         order_item.id,
         `order`.destination_id,
         sum(distinct case when `order`.status != 'PENDING' then order_item.quantity * order_item.quantity_per_uom else 0 end) as quantity_ordered,
@@ -19,7 +18,8 @@ SELECT
         shipment_item ON order_shipment.shipment_item_id = shipment_item.id
     LEFT OUTER JOIN
         shipment ON shipment_item.shipment_id = shipment.id
-    WHERE `order`.order_type_code = 'PURCHASE_ORDER' AND order_item.order_item_status_code != 'CANCELED'
+    WHERE `order`.order_type_code = 'PURCHASE_ORDER'
+      AND order_item.order_item_status_code != 'CANCELED'
     GROUP BY  product.product_code, order_item.id, product.name, `order`.destination_id ) a
-GROUP BY product_code, name, destination_id
+GROUP BY product_id, destination_id
 )
--- grails-app/migrations/views/on-order-shipment-item-summary.sql
+++ grails-app/migrations/views/on-order-shipment-item-summary.sql
@@ -1,9 +1,8 @@
 CREATE OR REPLACE VIEW on_order_shipment_item_summary AS (
-SELECT c.product_code, c.name, c.destination_id, null as quantity_ordered_not_shipped, (sum(c.quantity_shipped) - (sum(c.quantity_received) + sum(c.quantity_canceled))) as quantity_shipped_not_received
+SELECT c.product_id, c.destination_id, null as quantity_ordered_not_shipped, (sum(c.quantity_shipped) - (sum(c.quantity_received) + sum(c.quantity_canceled))) as quantity_shipped_not_received
 FROM (
 SELECT
-	product.product_code,
-    product.name,
+    product.id as product_id,
     shipment_item.id,
     shipment.destination_id,
     sum(distinct shipment_item.quantity) as quantity_shipped,
@@ -26,7 +25,7 @@ FROM
 WHERE
     shipment.current_status IN (NULL, 'SHIPPED' , 'PARTIALLY_RECEIVED')
     AND location_type.location_type_code = 'DEPOT'
-GROUP BY product.product_code, product.name, shipment_item.id, shipment.destination_id) c
+GROUP BY product.id, shipment_item.id, shipment.destination_id) c
 WHERE c.quantity_received + c.quantity_canceled < c.quantity_shipped
-GROUP BY product_code, name, destination_id
+GROUP BY product_id, destination_id
 )
