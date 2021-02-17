d63228a3f7bcf6c0e6781169b70b06b9163cd3f3
Justin Miranda <jcm62@columbia.edu>
2021-02-16 23:38:27 -0600

    OBPIH-3487 Add QOO to product summary table (#2192)
    * OBPIH-3487 Improved performance of view by filtering on depot destinations
    
    * OBPIH-3487 Minor improvements to remove unnecessary columns, improve formatting, fix typos, return zeros instead of nulls
    
    * OBPIH-3487 Fixed on order summary view and reformatted related views
    
    * OBPIH-3487 Fixed product snapshot view used in on order summary report
    
    * OBPIH-3487 Added QOO to product summary domain; replace primary key with composite id

--- grails-app/migrations/views/on-order-shipment-item-summary.sql
+++ grails-app/migrations/views/on-order-shipment-item-summary.sql
@@ -1,27 +1,40 @@
-CREATE OR REPLACE VIEW on_order_shipment_item_summary AS (
-SELECT c.product_code, c.name, c.destination_id, null as quantity_ordered_not_shipped, (sum(c.quantity_shipped) - (sum(c.quantity_received) + sum(c.quantity_canceled))) as quantity_shipped_not_received
+CREATE OR REPLACE VIEW on_order_shipment_item_summary AS
+(
+SELECT c.product_id,
+       c.destination_id,
+       null      as quantity_ordered_not_shipped,
+       ifnull((sum(c.quantity_shipped) -
+               (sum(c.quantity_received) + sum(c.quantity_canceled))),
+              0) as quantity_shipped_not_received
 FROM (
-SELECT
-	product.product_code,
-    product.name,
-    shipment_item.id,
-    shipment.destination_id,
-    sum(distinct shipment_item.quantity) as quantity_shipped,
-	sum(case when receipt.receipt_status_code = 'RECEIVED' then receipt_item.quantity_received else 0 end) as quantity_received,
-	sum(case when receipt.receipt_status_code = 'RECEIVED' AND receipt_item.quantity_canceled then receipt_item.quantity_canceled else 0 end) as quantity_canceled
-FROM
-    shipment_item
-		JOIN
-	product on product.id = shipment_item.product_id
-        JOIN
-    shipment ON shipment.id = shipment_item.shipment_id
-        LEFT OUTER JOIN
-    receipt_item ON shipment_item.id = receipt_item.shipment_item_id AND shipment_item.product_id = receipt_item.product_id
-        LEFT OUTER JOIN
-    receipt ON receipt.id = receipt_item.receipt_id
-WHERE
-    shipment.current_status IN (NULL, 'SHIPPED' , 'PARTIALLY_RECEIVED')
-GROUP BY product.product_code, product.name, shipment_item.id, shipment.destination_id) c
+         SELECT product.id                           as product_id,
+                shipment_item.id,
+                shipment.destination_id,
+                sum(distinct shipment_item.quantity) as quantity_shipped,
+                sum(case
+                        when receipt.receipt_status_code = 'RECEIVED'
+                            then receipt_item.quantity_received
+                        else 0 end)                  as quantity_received,
+                sum(case
+                        when receipt.receipt_status_code = 'RECEIVED' AND
+                             receipt_item.quantity_canceled then receipt_item.quantity_canceled
+                        else 0 end)                  as quantity_canceled
+         FROM shipment_item
+                  JOIN
+              product on product.id = shipment_item.product_id
+                  JOIN
+              shipment ON shipment.id = shipment_item.shipment_id
+                  JOIN
+              location on location.id = shipment.destination_id
+                  JOIN
+              location_type on location.location_type_id = location_type.id
+                  LEFT OUTER JOIN
+              receipt_item ON shipment_item.id = receipt_item.shipment_item_id
+                  LEFT OUTER JOIN
+              receipt ON receipt.id = receipt_item.receipt_id
+         WHERE shipment.current_status IN (NULL, 'SHIPPED', 'PARTIALLY_RECEIVED')
+           AND location_type.location_type_code = 'DEPOT'
+         GROUP BY product.id, shipment_item.id, shipment.destination_id) c
 WHERE c.quantity_received + c.quantity_canceled < c.quantity_shipped
-GROUP BY product_code, name, destination_id
-)
+GROUP BY product_id, destination_id
+    )
--- grails-app/migrations/views/product-snapshot.sql
+++ grails-app/migrations/views/product-snapshot.sql
@@ -1,9 +1,7 @@
 CREATE OR REPLACE VIEW product_snapshot AS (
     SELECT location_id,
            product_id,
-           date,
-           SUM(quantity_on_hand) AS quantity_on_hand
-    FROM inventory_snapshot
-    WHERE date = (select max(date) from inventory_snapshot)
-    GROUP BY location_id, product_id, date
+           sum(quantity_on_hand) as quantity_on_hand
+    FROM product_availability
+    GROUP BY location_id, product_id
 )
