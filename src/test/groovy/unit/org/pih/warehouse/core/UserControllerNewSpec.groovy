ac3230fd70613afb9433304309547b8982d3c3c3
Justin Miranda <jcm62@columbia.edu>
2021-03-03 23:35:04 -0600

    OBGM-304 Fixed user controller unit test

--- src/test/groovy/unit/org/pih/warehouse/core/UserControllerNewSpec.groovy
+++ src/test/groovy/unit/org/pih/warehouse/core/UserControllerNewSpec.groovy
@@ -1,21 +1,14 @@
 package unit.org.pih.warehouse.core
 
-import grails.test.mixin.Mock
-import grails.test.mixin.TestFor
 import grails.testing.web.controllers.ControllerUnitTest
-import org.grails.testing.GrailsUnitTest
+import org.pih.warehouse.PagedResultList
 import org.pih.warehouse.PasswordCodec
-import org.pih.warehouse.core.Person
 import org.pih.warehouse.core.User
 import org.pih.warehouse.core.UserService
 import org.pih.warehouse.user.UserController
 import spock.lang.Specification
 
-//@Mock(User)
-@TestFor(UserController)
-@Mock([User, UserService])
-class UserControllerNewSpec extends Specification {
-    def stubMessager = new Expando()
+class UserControllerNewSpec extends Specification implements ControllerUnitTest<UserController> {
 
     def setup() {
         mockCodec(PasswordCodec)
@@ -34,7 +27,6 @@ class UserControllerNewSpec extends Specification {
 
     }
 
-// Failing with "No such property: totalCount for class: org.pih.warehouse.core.User"
     void "test list action"() {
 
         given:
@@ -45,14 +37,16 @@ class UserControllerNewSpec extends Specification {
                         password: "test123",
                         passwordConfirm: "test123")
         ]
+
+        PagedResultList results = new PagedResultList(users)
         controller.userService = Stub(UserService.class)
-        def listCriteria = [list: {Closure cls -> users}]
-        Person.metaClass.static.createCriteria = {listCriteria}
-        controller.userService.findUsers(_ as String, _ as Map) >> users
+        def listCriteria = [list: {Closure cls -> results}]
+        User.metaClass.static.createCriteria = {listCriteria}
+        controller.userService.findUsers(_ as String, _ as Map) >> results
 
         when: 'The list action is executed'
         controller.params.q = "asd"
-        controller.list()
+        def model = controller.list()
 
         then: 'modal should have userInstanceList and userInstanceTotal'
         model.userInstanceList
