b9faa35957fa1d0db01d6d3b850c5e4724f4623f
Walkowiak <awalkowiak@soldevelo.com>
2021-05-31 18:49:00 +0200

    OBPIH-3877 Generate source code from product code and vendor id

--- grails-app/services/org/pih/warehouse/data/ProductSupplierDataService.groovy
+++ grails-app/services/org/pih/warehouse/data/ProductSupplierDataService.groovy
@@ -10,6 +10,7 @@
 package org.pih.warehouse.data
 
 import groovy.sql.Sql
+import org.pih.warehouse.core.Constants
 import org.pih.warehouse.core.Organization
 import org.pih.warehouse.core.PreferenceType
 import org.pih.warehouse.core.ProductPrice
@@ -303,7 +304,7 @@ class ProductSupplierDataService {
     def createProductSupplierWithoutPackage(Map params) {
         Product product = Product.get(params.product.id)
         ProductSupplier productSupplier = new ProductSupplier()
-        productSupplier.code = params.sourceCode?:identifierService.generateProductSupplierIdentifier(product?.productCode)
+        productSupplier.code = params.sourceCode ?: "${product?.productCode ? product?.productCode + Constants.DEFAULT_NAME_SEPARATOR : ''}${params.supplier.id}"
         productSupplier.name = params.sourceName ?: product?.name
         productSupplier.supplier = Organization.get(params.supplier.id)
         productSupplier.supplierCode = params.supplierCode
