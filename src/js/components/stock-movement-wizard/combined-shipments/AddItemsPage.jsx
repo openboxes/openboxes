f20f349a969e4da599310c36b6f5030ef23eaad0
Walkowiak <awalkowiak@soldevelo.com>
2021-08-09 17:17:53 +0200

    OBAM-97 Add lot and expiry validation on inbound sm from PO (data entry)

--- src/js/components/stock-movement-wizard/combined-shipments/AddItemsPage.jsx
+++ src/js/components/stock-movement-wizard/combined-shipments/AddItemsPage.jsx
@@ -399,6 +399,18 @@ class AddItemsPage extends Component {
         item && item.quantityAvailable < _.toInteger(item.quantityRequested)) {
         errors.lineItems[key] = { quantityRequested: 'react.stockMovement.error.higherQuantity.label' };
       }
+      if (!_.isNil(item.product) && item.product.lotAndExpiryControl) {
+        if (!item.expirationDate && (_.isNil(item.lotNumber) || _.isEmpty(item.lotNumber))) {
+          errors.lineItems[key] = {
+            expirationDate: 'react.stockMovement.error.lotAndExpiryControl.label',
+            lotNumber: 'react.stockMovement.error.lotAndExpiryControl.label',
+          };
+        } else if (!item.expirationDate) {
+          errors.lineItems[key] = { expirationDate: 'react.stockMovement.error.lotAndExpiryControl.label' };
+        } else if (_.isNil(item.lotNumber) || _.isEmpty(item.lotNumber)) {
+          errors.lineItems[key] = { lotNumber: 'react.stockMovement.error.lotAndExpiryControl.label' };
+        }
+      }
     });
     return errors;
   }
