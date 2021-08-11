dfbb8e2a7e3f7c3411766e82823f3dfd63d359a8
Walkowiak <awalkowiak@soldevelo.com>
2021-08-11 12:19:48 +0200

    OBAM-99 Add lot and expiry validation on record stock

--- grails-app/services/org/pih/warehouse/inventory/InventoryService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/InventoryService.groovy
@@ -1305,6 +1305,13 @@ class InventoryService implements ApplicationContextAware {
                         row.error = true
                         return cmd
                     }
+
+                    if(cmd.product && cmd.product.lotAndExpiryControl && (!row.expirationDate || !row.lotNumber)) {
+                        cmd.errors.reject("inventoryItem.invalid", "Both lot number and expiry date are required for this product.")
+                        row.error = true
+                        return cmd
+                    }
+
                     // 1. Find an existing inventory item for the given lot number and product and description
                     def inventoryItem =
                             findInventoryItemByProductAndLotNumber(cmd.product, row.lotNumber)
