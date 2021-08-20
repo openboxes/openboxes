160314caf30397ad4c0645bffa4109b692a04323
npokharel <niraj.pokharel@gmail.com>
2021-08-20 22:31:22 +0545

    WIP: #OBKN-72 added POJOs for proof of delivery (POD)

---
+++ src/java/org/pih/warehouse/xml/pod/DocumentUpload.java
@@ -0,0 +1,50 @@
+package org.pih.warehouse.xml.pod;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name="DocumentUpload")
+@XmlType(propOrder = {"header", "action", "documentType", "uploadDetails"})
+public class DocumentUpload {
+    private Header header;
+    private String action;
+    private String documentType;
+    private UploadDetails uploadDetails;
+
+    @XmlElement(name = "Header")
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    @XmlElement(name = "Action")
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    @XmlElement(name = "DocumentType")
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    @XmlElement(name = "UploadDetails")
+    public UploadDetails getUploadDetails() {
+        return uploadDetails;
+    }
+
+    public void setUploadDetails(UploadDetails uploadDetails) {
+        this.uploadDetails = uploadDetails;
+    }
+}
