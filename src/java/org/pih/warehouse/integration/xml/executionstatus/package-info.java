ea899486483274470196b0549435c622624a1896
Niraj Kumar Pokharel <niraj.pokharel@gmail.com>
2021-08-25 09:32:11 +0545

    OBKN-67 Allow superuser to upload message for processing (#2603)
    * WIP: OBKN-67 Added file upload to sftp
    
    * WIP: OBKN-67 XSD Validator added, refactoring of package integration/xml
    
    * OBKN-67 Fixed minor issues with the message upload feature
    
    Co-authored-by: jmiranda <jcm62@columbia.edu>

--- src/java/org/pih/warehouse/integration/xml/executionstatus/package-info.java
+++ src/java/org/pih/warehouse/integration/xml/executionstatus/package-info.java
@@ -1,7 +1,7 @@
 @XmlSchema(
-        namespace = "http://www.etrucknow.com/edi/carrier/acceptance_status/v1",
+        namespace = "http://www.etrucknow.com/edi/carrier/trip_execution/v1",
         elementFormDefault = XmlNsForm.QUALIFIED)
-package org.pih.warehouse.xml.acceptancestatus;
+package org.pih.warehouse.integration.xml.executionstatus;
 
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
---
+++ src/java/org/pih/warehouse/integration/xml/pod/Header.java
@@ -0,0 +1,77 @@
+package org.pih.warehouse.integration.xml.pod;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "Header")
+@XmlType(propOrder={"version","username","password","sequenceNumber", "sourceApp"})
+public class Header {
+
+    private String version;
+    private String username;
+    private String password;
+    private String sequenceNumber;
+    private String sourceApp;
+
+    public Header() {    }
+
+    public Header(String version, String username, String password, String sequenceNumber, String sourceApp) {
+        this.version = version;
+        this.username = username;
+        this.password = password;
+        this.sequenceNumber = sequenceNumber;
+        this.sourceApp = sourceApp;
+    }
+
+    public Header(String version, String sequenceNumber, String sourceApp) {
+        this.version = version;
+        this.sequenceNumber = sequenceNumber;
+        this.sourceApp = sourceApp;
+    }
+
+    @XmlElement(name = "Version")
+    String getVersion() {
+        return version;
+    }
+
+    void setVersion(String version) {
+        this.version = version;
+    }
+
+    @XmlElement(name = "Username")
+    String getUsername() {
+        return username;
+    }
+
+    void setUsername(String username) {
+        this.username = username;
+    }
+
+    @XmlElement(name = "Password")
+    String getPassword() {
+        return password;
+    }
+
+    void setPassword(String password) {
+        this.password = password;
+    }
+
+    @XmlElement(name = "SequenceNumber")
+    String getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    void setSequenceNumber(String sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    @XmlElement(name = "SourceApp")
+    String getSourceApp() {
+        return sourceApp;
+    }
+
+    void setSourceApp(String sourceApp) {
+        this.sourceApp = sourceApp;
+    }
+}
