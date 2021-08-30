b3c9deec4cd605f01bb418b6699c0270a4468324
Niraj Kumar Pokharel <niraj.pokharel@gmail.com>
2021-08-31 04:10:28 +0545

    OBKN-26 Allow an OC stakeholder to bulk import inbound orders (#2613)
    * WIP: OBKN-26 Bulk import Inbound / Outbound Orders
    
    * OBKN-26 Minor fixes
    
    Co-authored-by: jmiranda <jcm62@columbia.edu>

--- grails-app/services/org/pih/warehouse/data/InboundStockMovementDataService.groovy
+++ grails-app/services/org/pih/warehouse/data/InboundStockMovementDataService.groovy
@@ -1,15 +1,16 @@
 /**
- * Copyright (c) 2012 Partners In Health.  All rights reserved.
- * The use and distribution terms for this software are covered by the
- * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
- * which can be found in the file epl-v10.html at the root of this distribution.
- * By using this software in any fashion, you are agreeing to be bound by
- * the terms of this license.
- * You must not remove this notice, or any other, from this software.
- **/
+* Copyright (c) 2012 Partners In Health.  All rights reserved.
+* The use and distribution terms for this software are covered by the
+* Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+* which can be found in the file epl-v10.html at the root of this distribution.
+* By using this software in any fashion, you are agreeing to be bound by
+* the terms of this license.
+* You must not remove this notice, or any other, from this software.
+**/ 
 package org.pih.warehouse.data
 
 import org.joda.time.LocalDate
+import org.pih.warehouse.auth.AuthService
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.importer.ImportDataCommand
 import org.pih.warehouse.product.Product
@@ -17,9 +18,8 @@ import org.pih.warehouse.requisition.Requisition
 import org.pih.warehouse.requisition.RequisitionItem
 import org.pih.warehouse.requisition.RequisitionStatus
 import org.springframework.validation.BeanPropertyBindingResult
-import org.pih.warehouse.auth.AuthService
 
-class OutboundStockMovementDataService {
+class InboundStockMovementDataService {
 
     Boolean validateData(ImportDataCommand command) {
         log.info "Validate data " + command.filename
@@ -27,12 +27,10 @@ class OutboundStockMovementDataService {
             if (!params?.origin) {
                 throw new IllegalArgumentException("Row ${index + 1}: Origin is required")
             }
-            if (!params?.destination) {
-                throw new IllegalArgumentException("Row ${index + 1}: Destination is required")
-            }
             if (!params?.quantity) {
                 throw new IllegalArgumentException("Row ${index + 1}: Requested Quantity is required")
             }
+            params.destination = command.location.id
             RequisitionItem requisitionItem = buildRequisitionItem(params)
             if (!requisitionItem.validate()) {
                 requisitionItem.errors.each { BeanPropertyBindingResult error ->
@@ -45,13 +43,13 @@ class OutboundStockMovementDataService {
     void importData(ImportDataCommand command) {
         log.info "Import data " + command.filename
         command.data.eachWithIndex {params, index ->
+            params.destination = command.location.id
             RequisitionItem requisitionItem = buildRequisitionItem(params)
             if(requisitionItem.validate()){
                 requisitionItem.save(failOnError: true)
             }
         }
     }
-
 
     RequisitionItem buildRequisitionItem(Map params) {
         String productCode = params.productCode
@@ -86,7 +84,7 @@ class OutboundStockMovementDataService {
             }
             requisition.origin = origin
 
-            Location destination = Location.findByLocationNumber(params.destination)
+            Location destination = Location.get(params.destination)
             if (!destination) {
                 throw new IllegalArgumentException("Location not found for destination ${params.destination}")
             }
