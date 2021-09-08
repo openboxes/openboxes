7788d696b3e35cebaf79923cf7a72b51e672f265
Justin Miranda <jcm62@columbia.edu>
2021-09-07 21:58:40 -0500

    OBKN-71 Added ability to upload delivery order message to sftp server; also added event handler for stock movement state transitions (#2632)

---
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementStatusEventService.groovy
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2012 Partners In Health.  All rights reserved.
+ * The use and distribution terms for this software are covered by the
+ * Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ * which can be found in the file epl-v10.html at the root of this distribution.
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ * You must not remove this notice, or any other, from this software.
+ **/
+package org.pih.warehouse.inventory
+
+import org.pih.warehouse.core.RoleType
+import org.pih.warehouse.shipping.Shipment
+import org.pih.warehouse.shipping.ShipmentStatusCode
+import org.pih.warehouse.shipping.ShipmentStatusTransitionEvent
+import org.springframework.context.ApplicationListener
+
+class StockMovementStatusEventService implements ApplicationListener<StockMovementStatusCode> {
+
+    boolean transactional = true
+
+    def tmsIntegrationService
+
+    void onApplicationEvent(StockMovementStatusCode event) {
+        log.info "Application event ${event.source} has been published"
+        if (event.stockMovementStatusCode == StockMovementStatusCode.PICKING) {
+            tmsIntegrationService.sendDeliveryOrder(event.source)
+        }
+    }
+}
