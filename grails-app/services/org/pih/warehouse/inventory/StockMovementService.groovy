cd8dcc6d8fe300f3b5cc6726d621a9864e68cf35
jmiranda <jcm62@columbia.edu>
2021-09-13 14:40:01 -0500

    OBKN-171 Fixed validation logic when creating an outbound stock movement

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -2021,8 +2021,12 @@ class StockMovementService {
 
         shipment.name = stockMovement.generateName()
 
-        def quantitiesValid = shipmentService.validateShipment(shipment)
-        if (shipment.hasErrors() || !quantitiesValid || !shipment.save(flush: true)) {
+        // Validate shipment and validate shipment items should only add to the shipment's errors
+        // object or throw a validation exception
+        shipmentService.validateShipment(shipment)
+
+        //  Check if the shipment has errors
+        if (shipment.hasErrors() || !shipment.save(flush: true)) {
             throw new ValidationException("Invalid shipment", shipment.errors)
         }
 
