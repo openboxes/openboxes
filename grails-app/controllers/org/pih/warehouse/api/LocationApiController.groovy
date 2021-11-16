4cbe1ebafc87a1d938dc2d9fe5331e95d3643da5
chetan <chetan.maharishi@gmail.com>
2021-11-16 21:35:48 +0530

    OBKN-99 Allow a warehouse user to print barcode labels for a bin location

--- grails-app/controllers/org/pih/warehouse/api/LocationApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/LocationApiController.groovy
@@ -9,8 +9,12 @@
  **/
 package org.pih.warehouse.api
 
+import fr.w3blog.zpl.utils.ZebraUtils
 import grails.converters.JSON
+import groovyx.net.http.HTTPBuilder
 import org.hibernate.Criteria
+import org.hibernate.ObjectNotFoundException
+import org.pih.warehouse.core.Document
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.core.User
 import org.pih.warehouse.product.ProductAvailability
@@ -20,6 +24,8 @@ class LocationApiController extends BaseDomainApiController {
     def locationService
     def userService
     def grailsApplication
+    def templateService
+
 
     def list = {
 
@@ -59,5 +65,44 @@ class LocationApiController extends BaseDomainApiController {
         }
         render ([data:data] as JSON)
 
+    }
+
+    def renderLabel = {
+        Location location = locationService.getLocation(params.id)
+        Document document = Document.get(params.documentId?:"ff8081817c7d2003017c7d21d20a0002")  // FIXME get ids from params or session
+        if (!document) {
+            throw new ObjectNotFoundException(params.documentId, Document.class.simpleName)
+        }
+        Map model = [location: location]
+        String body = templateService.renderTemplate(document, model)
+        String renderApiUrl = grailsApplication.config.openboxes.barcode.labelaryApi.url
+        def http = new HTTPBuilder(renderApiUrl)
+        def html = http.post(body: body)
+        response.contentType = "image/png"
+        response.outputStream << html
+    }
+
+    def printLabel = {
+        try {
+            Location location = locationService.getLocation(params.id)
+
+            // FIXME OBKN-98 Temporarily default to first barcode template if documentId is not provided
+            Document document = (params.documentId) ?
+                    Document.get(params.documentId) :
+                    Document.get("ff8081817c7d2003017c7d21d20a0002") // FIXME get ids from params or session
+            if (!document) {
+                throw new ObjectNotFoundException(params.documentId, Document.class.simpleName)
+            }
+            Map model = [location: location]
+            String renderedContent = templateService.renderTemplate(document, model)
+            String ipAddress = grailsApplication.config.openboxes.barcode.printer.ipAddress
+            Integer port = grailsApplication.config.openboxes.barcode.printer.port
+            log.info "Printing ${renderedContent} to ${ipAddress}:${port}"
+            ZebraUtils.printZpl(renderedContent, ipAddress, port)
+            render([data: "Location label has been printed to ${ipAddress}:${port}"] as JSON)
+            return
+        } catch (Exception e) {
+            render([errorCode: 500, cause: e?.class, errorMessage: e?.message] as JSON)
+        }
     }
 }
