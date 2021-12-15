29ca94f00b952125ab58a71fe8ef14da19ebd4b3
MagdaToczek <kocik.magdalena@gmail.com>
2021-12-15 10:09:58 +0100

    OBPIH-4197 Fixed possibility to change location for user without default location

--- grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
+++ grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
@@ -217,7 +217,7 @@ class DashboardController {
     def changeLocation = {
         User user = User.get(session.user.id)
         Map loginLocationsMap = locationService.getLoginLocationsMap(user, null)
-        List savedLocations = [user?.warehouse, session?.warehouse].unique()
+        List savedLocations = [user?.warehouse, session?.warehouse].unique() - null
         render(template: "loginLocations", model: [savedLocations: savedLocations, loginLocationsMap: loginLocationsMap])
     }
 
