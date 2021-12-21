d887ac87543cd10d9f4a968c6b633047b941cdf7
Matthew Pearson <mpearson@pih.org>
2021-12-21 10:13:44 -0800

    * use swagger-ui-dist

--- src/js/openapi/swagger_ui.js
+++ src/js/openapi/swagger_ui.js
@@ -1,5 +1,5 @@
 import SwaggerUI from 'swagger-ui-dist'
-import 'swagger-ui/dist/swagger-ui.css';
+import 'swagger-ui-dist/swagger-ui.css';
 
 const spec = require('../../../grails-app/conf/openapi.yaml');
 
