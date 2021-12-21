ee5923d55b61f9d70e79c6a8ebd40bb9091823e5
Matthew Pearson <mpearson@pih.org>
2021-12-21 09:51:44 -0800

    OBDS-73 Try setting up Swagger UI.

---
+++ src/js/openapi/swagger_ui.js
@@ -0,0 +1,15 @@
+import SwaggerUI from 'swagger-ui-dist'
+import 'swagger-ui/dist/swagger-ui.css';
+
+const spec = require('../../../grails-app/conf/openapi.yaml');
+
+const ui = SwaggerUI({
+    spec,
+    dom_id: '#swagger',
+});
+
+ui.initOAuth({
+    appName: "Swagger UI Webpack Demo",
+    // See https://demo.identityserver.io/ for configuration details.
+    clientId: 'implicit'
+});
