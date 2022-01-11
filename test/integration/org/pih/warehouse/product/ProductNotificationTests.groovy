5ed10b962aff3dc0229566613275e14085765f3e
chetanmaharishi <77116134+chetanmaharishi@users.noreply.github.com>
2022-01-12 00:55:49 +0530

    OBDX-7 Fixed a few bugs and added integration tests (#2901)
    * OBDX-7-bugfixes-and-integration-tests
    Fixed bug for creating product and product attributes from SNS
    
    * OBDX-fix-security-filter
    some optimisation as requested by justin on pr..

---
+++ test/integration/org/pih/warehouse/product/ProductNotificationTests.groovy
@@ -0,0 +1,94 @@
+package org.pih.warehouse.product
+
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+import org.junit.Test
+
+class ProductNotificationTests  extends GroovyTestCase {
+
+    def productService
+
+    @Test
+    void testSaveProductFromJson(){
+        String productJsonString = """{
+            "locationNumber": "SOF00001",
+            "products": [{
+                "id": "142",
+                "productCode": "SOF00001-SKU2",
+                "name": "Test name",
+                "pricePerUnit": 150.0,
+                "description": "Test description",
+                "unitOfMeasure": null,
+                "brandName": null,
+                "category": "FMCG",
+                "attributes": [{
+                    "code": "vat",
+                    "value": "20.0"
+                }, {
+                    "code": "country",
+                    "value": "Bulgaria"
+                }, {
+                    "code": "principalCompany",
+                    "value": "Test principal company"
+                }, {
+                    "code": "strength",
+                    "value": ""
+                }, {
+                    "code": "packSize",
+                    "value": "Pack size"
+                }, {
+                    "code": "productForm",
+                    "value": "Form of product"
+                }, {
+                    "code": "size",
+                    "value": "Size"
+                }, {
+                    "code": "color",
+                    "value": "Color"
+                }, {
+                    "code": "weight",
+                    "value": "Weight"
+                }, {
+                    "code": "packageInformation",
+                    "value": "Package information"
+                }, {
+                    "code": "dimensions",
+                    "value": "Dimensions"
+                }, {
+                    "code": "barcode",
+                    "value": "Test barcode"
+                }, {
+                    "code": "category",
+                    "value": "Acne & Sensitive Skin"
+                }, {
+                    "code": "subcategory",
+                    "value": "Subcategory 1"
+                }, {
+                    "code": "ingredients",
+                    "value": ""
+                }],
+                "documents": [{
+                    "fileUri": "link2.com"
+                }, {
+                    "fileUri": "link1.com"
+                }, {
+                    "fileUri": "videolink.com"
+                }],
+                "components": []
+            }]
+        }"""
+
+        def category = productService.findOrCreateCategory("FMCG")
+        JSONObject productsJson = new JSONObject(productJsonString)
+        JSONArray products = productsJson.getJSONArray("products")
+        for (int i = 0; i < products.length(); i++) {
+            JSONObject productJson = products.getJSONObject(i)
+            Product product = productService.createFromJson(productJson)
+            product = productService.findProduct(productJson.getString("id"))
+            product.refresh()
+            assert product.name == "Test name"
+            assert product.attributes?.size() == 15
+            assert product.documents?.size() == 3
+        }
+    }
+}
