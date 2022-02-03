3042fd6de0062ca4831a3d025fdea9087a771754
jmiranda <jcm62@columbia.edu>
2022-02-03 12:50:06 -0600

    OBDX-40 Should only send product availability messages for products that are managed externally

--- grails-app/services/org/pih/warehouse/report/NotificationService.groovy
+++ grails-app/services/org/pih/warehouse/report/NotificationService.groovy
@@ -260,6 +260,7 @@ class NotificationService {
     }
 
     Boolean publish(String topicArn, String message, String subject){
+        log.info "Publishing message to topic ${topicArn}: ${message}"
         PublishRequest publishRequest = new PublishRequest(topicArn, message, subject)
         def result = amazonSnsClient.publish(publishRequest)
         log.info "result::${result?.dump()}"
@@ -309,40 +310,44 @@ class NotificationService {
         Map<Product, Integer> quantityMap = productAvailabilityService.getQuantityOnHandByProduct(location, products)
         Map<Product, Integer> quantityAvailableMap = productAvailabilityService.getQuantityAvailableToPromiseByProduct(location, products)
         List availableItems = productAvailabilityService.getAvailableItems(location, products)
-        Map<Product, Map> quantityExpiringForProducts = productAvailabilityService.quantityExpiringFroProducts(location, products)
-        sendProductAvailabilityMessages(location, products, quantityMap, quantityAvailableMap, availableItems, quantityExpiringForProducts)
+        sendProductAvailabilityMessages(location, products, quantityMap, quantityAvailableMap, availableItems)
     }
 
     void sendProductAvailabilityMessages(Location location, List<Product> products, Map<Product, Integer> quantityMap,
-                                         Map<Product, Integer> quantityAvailableMap, List availableItems,
-                                         Map<Product, Map> quantityExpiringForProducts){
+                                         Map<Product, Integer> quantityAvailableMap, List availableItems) {
         String TOPIC_ARN = grailsApplication.config.awssdk.sns.productAvailability
         Attribute externalIdAttribute = productService.findExternalProductIdAttribute()
         JSONArray jsonArray = new JSONArray()
-        products?.each {Product product ->
-            JSONObject jsonObject = new JSONObject()
-            String externalId = product.attributes.find{it.attribute == externalIdAttribute}?.value
-            List productAvailableItems = availableItems.findAll {it.product == product}
-            def productAvailableItem = productAvailableItems?.findAll {
-                it.inventoryItem.expirationDate != null
+        products?.each { Product product ->
+
+            // We only want to send product availability for products that are managed in an external system
+            if (product.isExternalProduct) {
+                JSONObject jsonObject = new JSONObject()
+
+                String externalId = product.attributes.find { it.attribute == externalIdAttribute }?.value
+
+                def expiringAvailableItems = availableItems?.findAll { it.product == product && it.inventoryItem.expirationDate }
+                def minExpirationDate = expiringAvailableItems.collect {
+                    it?.inventoryItem?.expirationDate
+                }?.min()
+                def quantityExpiring = expiringAvailableItems.findAll {
+                    it?.inventoryItem?.expirationDate <= minExpirationDate
+                }.sum { it.quantityOnHand }
+                jsonObject.put("productId", externalId)
+                jsonObject.put("productCode", product?.productCode)
+                jsonObject.put("locationNumber", location?.locationNumber)
+                jsonObject.put("quantityOnHand", quantityMap[product])
+                jsonObject.put("quantityAvailable", quantityAvailableMap[product])
+                jsonObject.put("earliestExpirationDate", minExpirationDate.format("yyyy-MM-dd"))
+                jsonObject.put("quantityExpiring", quantityExpiring ?: 0)
+                log.info "JSONObject:${jsonObject.toString(2)}, for product:${product}"
+                jsonArray.put(jsonObject)
             }
-            Map quantityExpiringForProduct = quantityExpiringForProducts.get(product)
-            jsonObject.put("productId", externalId)
-            jsonObject.put("productCode", product?.productCode)
-            jsonObject.put("locationNumber", location?.locationNumber)
-            jsonObject.put("quantityOnHand", quantityMap[product])
-            jsonObject.put("quantityAvailable", quantityAvailableMap[product])
-            jsonObject.put("earliestExpirationDate", productAvailableItem?.collect {
-                it.inventoryItem?.expirationDate
-            }?.min()?.format("yyyy-MM-dd"))
-            jsonObject.put("quantityExpiring", quantityExpiringForProduct ? quantityExpiringForProduct.get("expiringQuantity") : 0)
-            jsonObject.put("quantityExpiringV2", productAvailableItem?.collect {
-                it.quantityOnHand
-            }?.min()?:0)
-            log.info "JSONObject:${jsonObject.toString(2)}, for product:${product}"
-            jsonArray.put(jsonObject)
         }
-        publish(TOPIC_ARN, jsonArray.toString(), "Product Availability")
+
+        if (!jsonArray.empty) {
+            publish(TOPIC_ARN, jsonArray.toString(), "Product Availability")
+        }
     }
 
 
