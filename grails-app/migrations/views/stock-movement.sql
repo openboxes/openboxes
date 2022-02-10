4c71dafe2ac4e419b412d3b28855d1a368d83752
Matthew Pearson <mpearson@pih.org>
2022-02-10 11:16:16 -0800

    OBPIH-4284 Simplify view by removing tracking number and line item count.

--- grails-app/migrations/views/stock-movement.sql
+++ grails-app/migrations/views/stock-movement.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE VIEW stock_movement AS
            r.requested_by_id,
            r.created_by_id,
            r.updated_by_id,
-           coalesce(ri.item_count, 0) as line_item_count,
+           0 as line_item_count,
            s.shipment_type_id,
            s.expected_shipping_date as date_shipped,
            s.expected_delivery_date,
@@ -20,7 +20,7 @@ CREATE OR REPLACE VIEW stock_movement AS
            s.additional_information as comments,
            s.id as shipment_id,
            s.current_status as shipment_status,
-           rn.identifier as tracking_number,
+           null as tracking_number,
            CASE
                WHEN r.status is null THEN 'REQUESTING'
                WHEN r.status = 'EDITING' THEN 'REQUESTING'
@@ -39,16 +39,6 @@ CREATE OR REPLACE VIEW stock_movement AS
            'STOCK_MOVEMENT' as stock_movement_type
     from requisition r
         left join shipment s on s.requisition_id = r.id
-        left join (
-            select
-                   requisition_id,
-                   count(*) as item_count
-            from requisition_item
-            group by requisition_id
-            ) ri on ri.requisition_id = r.id
-
-        left join shipment_reference_number sr on s.id = sr.shipment_reference_numbers_id
-        left join reference_number rn on rn.id = sr.reference_number_id and rn.reference_number_type_id = "10"
     where r.is_template is false
 
 union all
@@ -66,7 +56,7 @@ union all
            o.ordered_by_id as requested_by_id,
            o.created_by_id,
            o.updated_by_id,
-           count(oi.id) as line_item_count,
+           0 as line_item_count,
            s.shipment_type_id,
            s.expected_shipping_date as date_shipped,
            s.expected_delivery_date,
@@ -74,7 +64,7 @@ union all
            s.additional_information as comments,
            s.id as shipment_id,
            s.current_status as shipment_status,
-           rn.identifier as tracking_number,
+           0 as tracking_number,
            null as status_code,
            null as requisition_id,
            null as status,
@@ -89,9 +79,6 @@ union all
          left join order_shipment os on os.order_item_id = oi.id
          left join shipment_item si on si.id = os.shipment_item_id
          left join shipment s on s.id = si.shipment_id
-
-         left join shipment_reference_number sr on s.id = sr.shipment_reference_numbers_id
-         left join reference_number rn on rn.id = sr.reference_number_id and rn.reference_number_type_id = "10"
 
     where order_type_id = 'RETURN_ORDER'
     group by o.id, s.id, tracking_number;
