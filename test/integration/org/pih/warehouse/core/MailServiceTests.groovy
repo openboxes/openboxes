4bc396742bbcc8012f3ea1144c14707999f9d264
Matthew Pearson <mpearson@pih.org>
2022-02-23 12:52:56 -0800

    OBPIH-4250 Add email tests for invalid credentials and quota violations.

--- test/integration/org/pih/warehouse/core/MailServiceTests.groovy
+++ test/integration/org/pih/warehouse/core/MailServiceTests.groovy
@@ -1,16 +1,19 @@
 package org.pih.warehouse.core
 
+import com.icegreen.greenmail.user.GreenMailUser
 import com.icegreen.greenmail.util.GreenMail
 import com.icegreen.greenmail.util.GreenMailUtil
 import com.icegreen.greenmail.util.ServerSetupTest
+import com.sun.mail.imap.IMAPStore
 import grails.test.GrailsUnitTestCase
 import org.junit.Test
 
 import javax.mail.Message
+import javax.mail.Quota
 
 class MailServiceTests extends GrailsUnitTestCase {
 
-	def mailService
+	MailService mailService
 	def grailsApplication
 
 	static GreenMail testSmtpService
@@ -33,9 +36,10 @@ class MailServiceTests extends GrailsUnitTestCase {
 	}
 
 	@Test
-	void sendHtmlMail_shouldHandleAccentedCharactersCorrectly() {
+	void sendHtmlMailShouldHandleAccentedCharactersCorrectly() {
 		def sent = mailService.sendHtmlMail("sübĵéçt", "This Is Spın̈al Tap", "anybody@anywhere.com", null, true)
 		assertTrue(sent)
+		testSmtpService.waitForIncomingEmail(10000, 1)
 		Message[] messages = testSmtpService.receivedMessages
 		assertEquals(1, messages.length)
 		assertEquals("[OpenBoxes] sübĵéçt", messages[0].subject)
@@ -43,19 +47,79 @@ class MailServiceTests extends GrailsUnitTestCase {
 	}
 
 	@Test
-	void sendHtmlMail_shouldNotRaiseExceptionIfSmtpServerIsDown() {
+	void sendHtmlMailShouldNotRaiseExceptionIfSmtpServerIsDown() {
 		testSmtpService.stop()
 		def sent = mailService.sendHtmlMail("undeliverable", "this message will not send", "anybody@anywhere.com", null, true)
 		assertFalse(sent)
 	}
 
 	@Test
-	void sendMail_shouldNotRaiseExceptionIfSmtpServerIsDown() {
+	void sendMailShouldNotRaiseExceptionIfSmtpServerIsDown() {
+		Boolean oldEnabled = grailsApplication.config.grails.mail.enabled
 		grailsApplication.config.grails.mail.enabled = true
-		def shouldSend = mailService.sendMail("deliverable", "this message will send", "anybody@anywhere.com")
-		assertTrue(shouldSend)
-		testSmtpService.stop()
-		def shouldNotSend = mailService.sendMail("undeliverable", "this message will not send", "anybody@anywhere.com")
-		assertFalse(shouldNotSend)
+
+		try {
+			def shouldSend = mailService.sendMail("deliverable", "this message will send", "anybody@anywhere.com")
+			assertTrue(shouldSend)
+			testSmtpService.stop()
+			def shouldNotSend = mailService.sendMail("undeliverable", "this message will not send", "anybody@anywhere.com")
+			assertFalse(shouldNotSend)
+		} finally {
+			grailsApplication.config.grails.mail.enabled = oldEnabled
+		}
+	}
+
+	@Test
+	void sendHtmlMailShouldNotRaiseExceptionIfCredentialsAreWrong() {
+		String oldUsername = grailsApplication.config.grails.mail.username
+		grailsApplication.config.grails.mail.username = "wrong_" + grailsApplication.config.grails.mail.username
+		String oldPassword = grailsApplication.config.grails.mail.password
+		grailsApplication.config.grails.mail.password = "wrong_" + grailsApplication.config.grails.mail.password
+
+		try {
+			GreenMailUser user = testSmtpService.setUser(
+				grailsApplication.config.grails.mail.from,
+				"right_" + grailsApplication.config.grails.mail.username,
+				"right_" + grailsApplication.config.grails.mail.password
+			)
+			def sent = mailService.sendHtmlMail("unauthorized", "this message will not send", "anybody@anywhere.com", null, true)
+			assertFalse(sent)
+		} finally {
+			grailsApplication.config.grails.mail.username = oldUsername
+			grailsApplication.config.grails.mail.password = oldPassword
+		}
+	}
+
+	@Test
+	void sendMailShouldNotRaiseExceptionIfSmtpServerIsFull() {
+		Boolean oldEnabled = grailsApplication.config.grails.mail.enabled
+		grailsApplication.config.grails.mail.enabled = true
+
+		try {
+			testSmtpService.setQuotaSupported(true)
+			GreenMailUser recipient = testSmtpService.setUser(
+				"anybody@anywhere.com",
+				"recipient"
+			)
+
+			IMAPStore store = testSmtpService.imap.createStore()
+			store.connect(
+				"anybody@anywhere.com",
+				"recipient"
+			)
+
+			Quota testQuota = new Quota("INBOX")
+			testQuota.setResourceLimit("MESSAGES", 1)
+
+			store.quota = testQuota
+			GreenMailUtil.setQuota(recipient, testQuota)
+
+			def shouldSend = mailService.sendMail("under_quota", "this message will send", "anybody@anywhere.com")
+			assertTrue(shouldSend)
+			def shouldAlsoSend = mailService.sendMail("over_quota", "this message will send but not arrive", "anybody@anywhere.com")
+			assertTrue(shouldAlsoSend)
+		} finally {
+			grailsApplication.config.grails.mail.enabled = oldEnabled
+		}
 	}
 }
