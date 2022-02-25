da45043674642f8f6107a18920e3a43daad6dc8a
jmiranda <jcm62@columbia.edu>
2022-02-24 23:21:17 -0600

    OBKN-547 Refactor outbound order import to use stock movement API

--- grails-app/services/org/pih/warehouse/core/UserService.groovy
+++ grails-app/services/org/pih/warehouse/core/UserService.groovy
@@ -185,15 +185,6 @@ class UserService {
         return false
     }
 
-    Boolean hasRoleCustomer(User u) {
-        if (u) {
-            def user = User.get(u.id)
-            def roleTypes = [RoleType.ROLE_CUSTOMER]
-            return getEffectiveRoles(user).any { Role role -> roleTypes.contains(role.roleType) }
-        }
-        return false
-    }
-
     Boolean canEditUserRoles(User currentUser, User otherUser) {
         def location = AuthService.currentLocation.get()
         return isSuperuser(currentUser) || (currentUser.getHighestRole(location) >= otherUser.getHighestRole(location))
