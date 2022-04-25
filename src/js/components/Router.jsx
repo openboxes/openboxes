6b3179edef2cc4a91c09449a69f5e16041451855
Darek Rodzewicz <DRodzewicz@gmail.com>
2022-04-25 11:21:32 +0200

    OBPIH-4380 Implement Helpscout beacon as support button in top right corner of header

--- src/js/components/Router.jsx
+++ src/js/components/Router.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-import { HelpScout, LiveChatLoaderProvider } from 'react-live-chat-loader';
 import Loadable from 'react-loadable';
 import { connect } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -117,24 +116,6 @@ const AsyncResetInstancePage = Loadable({
   loading: Loading,
 });
 
-function displayHelpScoutBeacon(key, color, isEnabled) {
-  if (isEnabled) {
-    return (
-      <LiveChatLoaderProvider provider="helpScout" providerKey={key}>
-        <HelpScout
-          color={color}
-          horizontalPosition="right"
-          icon="question"
-          idlePeriod="0"
-          zIndex="1050"
-        />
-      </LiveChatLoaderProvider>
-    );
-  }
-
-  return null;
-}
-
 const Router = props => (
   <div>
     <BrowserRouter>
@@ -167,7 +148,6 @@ const Router = props => (
         <MainLayoutRoute path="/**/" component={AsyncDashboard} />
       </Switch>
     </BrowserRouter>
-    {displayHelpScoutBeacon(props.helpScoutKey, props.helpScoutColor, props.isHelpScoutEnabled)}
     <div className="spinner-container">
       <ClimbingBoxLoader
         color="#0c769e"
@@ -187,23 +167,11 @@ const Router = props => (
 );
 
 const mapStateToProps = state => ({
-  helpScoutColor: state.session.helpScoutColor,
-  helpScoutKey: state.session.helpScoutKey,
-  isHelpScoutEnabled: state.session.isHelpScoutEnabled,
   spinner: state.spinner.show,
 });
 
 export default connect(mapStateToProps, {})(Router);
 
 Router.propTypes = {
-  helpScoutColor: PropTypes.string,
-  helpScoutKey: PropTypes.string,
-  isHelpScoutEnabled: PropTypes.bool,
   spinner: PropTypes.bool.isRequired,
-};
-
-Router.defaultProps = {
-  helpScoutColor: '',
-  helpScoutKey: '',
-  isHelpScoutEnabled: false,
 };
