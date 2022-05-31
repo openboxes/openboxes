a64e5cda380e358656c46f97590d8bbc1a38f93c
Matthew Pearson <mpearson@pih.org>
2022-05-31 16:04:42 -0700

    OBPIH-4202 Update dev npm dependencies and re-lock.

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -378,7 +378,7 @@ export function reorderIndicators({ oldIndex, newIndex }, e, type) {
   };
 }
 
-export function fetchConfigAndData(locationId, config = 'personal', userId, filterSelected) {
+export function fetchConfigAndData(locationId, config, userId, filterSelected) {
   return (dispatch) => {
     apiClient.get('/openboxes/api/dashboard/config').then((res) => {
       dispatch({
