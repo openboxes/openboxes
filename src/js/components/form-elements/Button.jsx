e24b43bd2ccaaa17bb3f2bbe4fe90e357e9faed1
Darek Rodzewicz <DRodzewicz@gmail.com>
2022-09-22 12:21:39 +0200

    OBPIH-4773 Refactor button component

--- src/js/components/form-elements/Button.jsx
+++ src/js/components/form-elements/Button.jsx
@@ -6,31 +6,29 @@ import Translate from 'utils/Translate';
 
 const Button = ({
   label, defaultLabel, disabled, variant, type, onClickAction, EndIcon,
-}) => (
-  <button
-    className={`${variant}-button d-flex justify-content-around align-items-center ${disabled && `${variant}-button-disabled`} ${variant === 'dropdown' && 'dropdown-toggle'}`}
-    disabled={disabled}
-    type={type}
-    onClick={onClickAction}
-    data-toggle={variant === 'dropdown' && 'dropdown'}
-    aria-haspopup={variant === 'dropdown' && 'true'}
-    aria-expanded={variant === 'dropdown' && 'false'}
-  >
-    {EndIcon ? (
-      <React.Fragment>
-        <div className="px-1">
-          <Translate id={label} defaultMessage={defaultLabel} />
-        </div>
-        <div className="px-1">
-          {EndIcon}
-        </div>
-      </React.Fragment>
-    ) :
-      <Translate id={label} defaultMessage={defaultLabel} />
-    }
+}) => {
+  const buttonClass = 'd-flex justify-content-around align-items-center gap-8';
+  const variantClass = `${variant}-button`;
+  const disabledClass = `${disabled ? `${variant}-button-disabled` : ''}`;
+  const dropDownClass = `${variant === 'dropdown' ? 'dropdown-toggle' : ''}`;
 
-  </button>
-);
+  return (
+    <button
+      className={[variantClass, buttonClass, disabledClass, dropDownClass].join(' ')}
+      disabled={disabled}
+      type={type}
+      onClick={onClickAction}
+      data-toggle={variant === 'dropdown' && 'dropdown'}
+      aria-haspopup={variant === 'dropdown' && 'true'}
+      aria-expanded={variant === 'dropdown' && 'false'}
+    >
+      <React.Fragment>
+        <Translate id={label} defaultMessage={defaultLabel} />
+        {EndIcon && EndIcon}
+      </React.Fragment>
+    </button>
+  );
+};
 
 export default Button;
 
