9eab19817fc58bb15239324b6a84fb2619962eca
Darek Rodzewicz <DRodzewicz@gmail.com>
2022-10-17 17:36:33 +0200

    OBPIH-4910 Check for searchable no stock activity for global search

--- grails-app/controllers/org/pih/warehouse/JsonController.groovy
+++ grails-app/controllers/org/pih/warehouse/JsonController.groovy
@@ -1064,7 +1064,7 @@ class JsonController {
         def json = items.collect { Product product ->
             def quantity = quantityMap[product.id] ?: 0
 
-            if (product.productType) {
+            if (product.productType && !product.productType.supportedActivities?.contains(ProductActivityCode.SEARCHABLE_NO_STOCK)) {
                 if (!product.productType.supportedActivities?.contains(ProductActivityCode.SEARCHABLE)) {
                     return
                 } else if (quantity == 0) {
