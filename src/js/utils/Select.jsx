8d5e046b7ddbbaf7414d13ab1a841e1451155026
Artur Walkowiak <awalkowiak@soldevelo.com>
2022-10-19 15:30:36 +0200

    OBPIH-4922 Fix select dropdown on components using utils/Select

--- src/js/utils/Select.jsx
+++ src/js/utils/Select.jsx
@@ -2,10 +2,87 @@ import React, { Component } from 'react';
 
 import _ from 'lodash';
 import PropTypes from 'prop-types';
+import { Overlay } from 'react-overlays';
 import ReactSelect, { Async, components } from 'react-select';
 import { Tooltip } from 'react-tippy';
 
 import 'react-tippy/dist/tippy.css';
+
+const Dropdown = ({ children, style, width }) => (
+  <div
+    style={{
+      ...style, position: 'absolute', zIndex: 9999, width, backgroundColor: 'white', border: '1px solid hsl(0deg 0% 80%)',
+    }}
+  >
+    {children}
+  </div>
+);
+
+Dropdown.propTypes = {
+  children: PropTypes.element.isRequired,
+  style: PropTypes.shape({}).isRequired,
+  width: PropTypes.string.isRequired,
+};
+
+const Menu = (props) => {
+  const target = document.getElementById(`${props.selectProps.id}-container`);
+  return (
+    <Overlay
+      show
+      placement="bottom"
+      target={target}
+      container={document.getElementById('root')}
+    >
+      <Dropdown width={target.offsetWidth.toString()}>
+        <div className="custom-option" {...props.innerProps}>
+          {props.selectProps.createNewFromModal &&
+            <div
+              className="add-new-button"
+              onClick={props.selectProps.newOptionModalOpen}
+              onKeyPress={props.selectProps.newOptionModalOpen}
+              role="button"
+              tabIndex={0}
+            >
+              <span><i className="fa fa-plus pr-2" />{props.selectProps.createNewFromModalLabel}</span>
+            </div>
+          }
+          {props.children}
+        </div>
+      </Dropdown>
+    </Overlay>
+  );
+};
+
+Menu.propTypes = {
+  children: PropTypes.element.isRequired,
+  innerProps: PropTypes.shape({}).isRequired,
+  selectProps: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    createNewFromModal: PropTypes.bool.isRequired,
+    newOptionModalOpen: PropTypes.func.isRequired,
+    createNewFromModalLabel: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+
+const Option = props => (
+  <components.Option {...props}>
+    {props.selectProps.optionRenderer ? (
+      props.selectProps.optionRenderer(props.data)
+    ) : (
+      <div>{props.data.label}</div>
+    )}
+  </components.Option>
+);
+
+Option.propTypes = {
+  data: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+  }).isRequired,
+  selectProps: PropTypes.shape({
+    optionRenderer: PropTypes.func,
+  }).isRequired,
+};
 
 class Select extends Component {
   constructor(props) {
@@ -156,6 +233,8 @@ class Select extends Component {
             onChange={this.handleChange}
             components={{
               ...customSelectComponents,
+              Menu: customSelectComponents.Menu ?? Menu,
+              Option: customSelectComponents.Option ?? Option,
               SingleValue,
             }}
             ref={fieldRef}
