08110c4c6e36832ae503f7318411ab285807e4b4
Artur Walkowiak <awalkowiak@soldevelo.com>
2022-11-14 18:36:17 +0100

    Add comment to the supported activity util function

--- src/js/utils/supportedActivitiesUtils.js
+++ src/js/utils/supportedActivitiesUtils.js
@@ -1,5 +1,9 @@
 import ActivityCode from 'consts/activityCode';
 
+/**
+ * Request from Ward = request from any location (not only ward),
+ * that does not support MANAGE INVENTORY activity but, it supports SUBMIT REQUEST
+ * */
 const isRequestFromWard = (currentLocationId, destinationId, supportedActivities) =>
   currentLocationId === destinationId &&
   !supportedActivities.includes(ActivityCode.MANAGE_INVENTORY) &&
