aade6527c380737ae58e51df76529847694f6dd7
kchelstowski <kchelstowski@soldevelo.com>
2022-12-15 16:11:11 +0100

    OBPIH-5172 LocalizationModeInfo indicator component

---
+++ src/js/components/Layout/LocalizationModeInfo.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { RiGlobalLine, RiLogoutBoxRLine } from 'react-icons/all';
+
+import Button from 'components/form-elements/Button';
+import Translate from 'utils/Translate';
+
+const LocalizationModeInfo = () => (
+  <div className="header-indicator-box d-flex justify-content-between align-items-center">
+    <div className="info d-flex align-items-center">
+      <RiGlobalLine />
+      <span>
+        <Translate id="react.default.localizationModeActive.label" defaultMessage="The localization mode is active" />
+      </span>
+    </div>
+    <a href="/openboxes/user/disableLocalizationMode">
+      <Button
+        defaultLabel="Disable Localization Mode"
+        label="react.default.disableLocalizationMode.label"
+        StartIcon={<RiLogoutBoxRLine />}
+      />
+    </a>
+  </div>
+);
+
+
+export default LocalizationModeInfo;
