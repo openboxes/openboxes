945cbac049ea92bb45742728d1b0c7f718684f63
Alan Nadolny <alannadolnyy@gmail.com>
2022-12-31 16:39:05 +0100

    OBPIH-5267 add service for outbounds

--- src/js/api/urls.js
+++ src/js/api/urls.js
@@ -2,3 +2,8 @@
 export const PURCHASE_ORDER_API = '/openboxes/api/purchaseOrders';
 export const PURCHASE_ORDER_DELETE = id => `${PURCHASE_ORDER_API}/${id}`;
 export const PURCHASE_ORDER_ROLLBACK_ORDER = id => `${PURCHASE_ORDER_API}/${id}/rollback`;
+
+// STOCK_MOVEMENT
+
+export const STOCK_MOVEMENT_API = '/openboxes/api/stockMovements';
+export const STOCK_MOVEMENT_DELETE = id => `${STOCK_MOVEMENT_API}/${id}`;
