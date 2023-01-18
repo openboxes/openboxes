5d83cf45b66192332adb15990e011a0a81f45fee
kchelstowski <kchelstowski@soldevelo.com>
2023-01-18 20:02:37 +0100

    OBPIH-5307 Remove pagination from browse inventory list page

--- grails-app/migrations/views/browse-inventory.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE VIEW browse_inventory AS
-SELECT product.id as product_id, SUM(quantity_on_hand) as quantity_on_hand, location_id  FROM product
-LEFT OUTER JOIN product_availability ON product_id = product.id
-GROUP BY product.id, location_id
