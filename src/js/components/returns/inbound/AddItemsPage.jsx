aea4f41eedb80cfcb3cc5227963f78d4302ae0a8
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2023-02-03 14:00:42 +0100

    OBPIH-4949 Disable next button when no items added to all workflows  (#3823)
    * OBPIH-4949 Disabling button when no items added in inbound, inbound return, outbound return, requests workflows
    
    * OBPIH-4949 remove redundant statement
    
    * OBPIH-4949 fix indentation

--- src/js/components/returns/inbound/AddItemsPage.jsx
+++ src/js/components/returns/inbound/AddItemsPage.jsx
@@ -504,7 +504,9 @@ class AddItemsPage extends Component {
                     }
                   }}
                   className="btn btn-outline-primary btn-form float-right btn-xs"
-                  disabled={!_.some(values.returnItems, item => !_.isEmpty(item))}
+                  disabled={
+                    !_.some(values.returnItems, item => item.product && _.parseInt(item.quantity))
+                  }
                 ><Translate id="react.default.button.next.label" defaultMessage="Next" />
                 </button>
               </div>
