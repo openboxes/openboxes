0c2422c3319f3154c7a6bb84893ff46ce0f37c0b
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2023-02-16 18:32:33 +0100

    OBPIH-5411 See product name translation based on "product locale" (Scope: outbounds & requests) fixes after QA (#3847)
    * OBPIH-5411 add translation for originalItem in substitution modal
    
    * OBPIH-5411 fix adding custom substitution in substitution modal
    
    * OBPIH-5411 remove unused variable
    
    * OBPIH-5411 fix after review

--- src/js/components/stock-movement-wizard/modals/SubstitutionsModal.jsx
+++ src/js/components/stock-movement-wizard/modals/SubstitutionsModal.jsx
@@ -71,8 +71,9 @@ const FIELDS = {
         type: ProductSelectField,
         label: 'react.stockMovement.product.label',
         defaultMessage: 'Product',
-        headerAlign: 'left',
-        flexWidth: '6',
+        getDynamicAttr: ({ debouncedAvailableItemsFetch }) => ({
+          loadOptions: debouncedAvailableItemsFetch,
+        }),
       },
       'product.minExpirationDate': {
         type: LabelField,
@@ -149,7 +150,7 @@ class SubstitutionsModal extends Component {
     this.onSave = this.onSave.bind(this);
     this.validate = this.validate.bind(this);
 
-    this.debouncedProductsFetch = debounceAvailableItemsFetch(
+    this.debouncedAvailableItemsFetch = debounceAvailableItemsFetch(
       this.props.debounceTime,
       this.props.minSearchLength,
     );
@@ -222,7 +223,7 @@ class SubstitutionsModal extends Component {
             ...val,
             disabled: true,
             product: {
-              label: `${val.productCode} - ${val.productName}`,
+              label: `${val.productCode} - ${val.product.translatedName ?? val.productName}`,
               id: `${val.productId}`,
               productCode: `${val.productCode}`,
               name: `${val.productName}`,
@@ -236,17 +237,21 @@ class SubstitutionsModal extends Component {
         let originalItem = null;
 
         if (_.toInteger(this.state.attr.lineItem.quantityAvailable) > 0) {
+          const {
+            productCode, product, productName, minExpirationDate, quantityAvailable,
+          } = this.state.attr.lineItem;
+
           originalItem = {
             ...this.state.attr.lineItem,
             originalItem: true,
             product: {
-              label: `${this.state.attr.lineItem.productCode} - ${this.state.attr.lineItem.productName}`,
-              id: `${this.state.attr.lineItem.product.id}`,
-              productCode: `${this.state.attr.lineItem.productCode}`,
-              name: `${this.state.attr.lineItem.productName}`,
-              translatedName: this.state.attr.lineItem.product.translatedName,
-              minExpirationDate: this.state.attr.lineItem.minExpirationDate,
-              quantityAvailable: this.state.attr.lineItem.quantityAvailable,
+              label: `${productCode} - ${product.translatedName ?? productName}`,
+              id: `${product.id}`,
+              productCode: `${productCode}`,
+              name: `${productName}`,
+              translatedName: product.translatedName,
+              minExpirationDate,
+              quantityAvailable,
             },
           };
           substitutions = [
@@ -331,7 +336,7 @@ class SubstitutionsModal extends Component {
           defaultEmptyDataMessage: 'There are no substitutions available',
           reasonCodes: this.state.attr.reasonCodes,
           originalItem: this.state.originalItem,
-          debouncedProductsFetch: this.debouncedProductsFetch,
+          debouncedAvailableItemsFetch: this.debouncedAvailableItemsFetch,
         }}
         renderBodyWithValues={this.calculateSelected}
       >
