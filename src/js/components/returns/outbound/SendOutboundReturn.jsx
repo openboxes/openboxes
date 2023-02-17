684d829c864428ac558e50dff699a9e31a8be1f1
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2023-02-17 18:22:22 +0100

    OBPIH-5086 French labels are visible on shipment type field after using save button in returns  (#3853)
    * OBPIH-5086 fix french labels appearing after save
    
    * OBPIH-5086 use splitting function from utils
    
    * OBPIH-5086 use splitting function from utils - inbound
    
    * OBPIH-5086 change name to label in outbound return

--- src/js/components/returns/outbound/SendOutboundReturn.jsx
+++ src/js/components/returns/outbound/SendOutboundReturn.jsx
@@ -227,14 +227,9 @@ class SendMovementPage extends Component {
 
     return apiClient.get(url)
       .then((response) => {
-        const shipmentTypes = _.map(response.data.data, (type) => {
-          const [en, fr] = _.split(type.name, '|fr:');
-          return {
-            ...type,
-            label: this.props.locale === 'fr' && fr ? fr : en,
-          };
-        });
-
+        const shipmentTypes = _.map(response.data.data, type => ({
+          ...type, label: splitTranslation(type.name, this.props.locale),
+        }));
         this.setState({ shipmentTypes }, () => this.props.hideSpinner());
       })
       .catch(() => this.props.hideSpinner());
@@ -363,7 +358,14 @@ class SendMovementPage extends Component {
   save(values) {
     this.saveValues(values)
       .then((resp) => {
-        const outboundReturn = parseResponse(resp.data.data);
+        const { data } = resp.data;
+        const outboundReturn = {
+          ...parseResponse(data),
+          shipmentType: {
+            ...data.shipmentType,
+            label: splitTranslation(data.shipmentType.name, this.props.locale),
+          },
+        };
         const picklistItems = _.flatten(_.map(outboundReturn.stockTransferItems, 'picklistItems'));
         this.setState({
           values: {
