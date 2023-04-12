94839ab19af480f2311b1e9acf5566323a844c4e
kchelstowski <kchelstowski@soldevelo.com>
2023-04-12 22:46:46 +0200

    OBPIH-5591 Add generic logic for the new features bars consts and add config for the autosave bar

---
+++ src/js/consts/newFeatures.js
@@ -0,0 +1,18 @@
+export const NewFeature = {
+  AUTOSAVE: 'AUTOSAVE',
+};
+
+export const NewFeatureBarConfigs = {
+  [NewFeature.AUTOSAVE]: {
+    name: NewFeature.AUTOSAVE,
+    versionLabel: {
+      label: 'react.newFeature.autosave.version.beta.label',
+      defaultLabel: 'BETA',
+    },
+    title: {
+      label: 'react.newFeature.autosave.title.label',
+      defaultLabel: 'New autosave feature is here',
+    },
+  },
+};
+
