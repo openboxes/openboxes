d413608652a58f387feb24843b20520b3f35dfe5
kchelstowski <kchelstowski@soldevelo.com>
2023-04-13 22:09:22 +0200

    OBPIH-5592 Add generic redux action to open/hide info bar read more modal

--- src/js/reducers/infoBarReducer.jsx
+++ src/js/reducers/infoBarReducer.jsx
@@ -2,7 +2,9 @@ import {
   ADD_INFO_BAR,
   CLOSE_INFO_BAR,
   HIDE_INFO_BAR,
+  HIDE_INFO_BAR_MODAL,
   SHOW_INFO_BAR,
+  SHOW_INFO_BAR_MODAL,
 } from 'actions/types';
 
 const initialState = {
@@ -52,6 +54,24 @@ export default function infoBarReducer(state = initialState, action) {
           bars: state.bars,
           name: action.payload.name,
           propertyToChange: { show: true },
+        }),
+      };
+    case SHOW_INFO_BAR_MODAL:
+      return {
+        ...state,
+        bars: editProperty({
+          bars: state.bars,
+          name: action.payload.name,
+          propertyToChange: { isModalOpen: true },
+        }),
+      };
+    case HIDE_INFO_BAR_MODAL:
+      return {
+        ...state,
+        bars: editProperty({
+          bars: state.bars,
+          name: action.payload.name,
+          propertyToChange: { isModalOpen: false },
         }),
       };
     default:
