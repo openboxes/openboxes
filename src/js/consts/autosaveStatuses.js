fb3e4e47226fceac994fcd6ed911412c68a8cc8f
kchelstowski <kchelstowski@soldevelo.com>
2023-04-13 22:14:33 +0200

    OBPIH-5592 Add consts for static image urls and autosave statuses

---
+++ src/js/consts/autosaveStatuses.js
@@ -0,0 +1,11 @@
+export const AutosaveStatus = {
+  SAVED: 'saved',
+  DRAFT: 'draft',
+  ERROR: 'error',
+};
+
+export const AutosaveStatusDescription = {
+  SAVED: 'the line item is completed and saved.',
+  DRAFT: 'the line item is not saved. Required fields must be filled in.',
+  ERROR: 'the line item is not saved. Correct the errors.',
+};
