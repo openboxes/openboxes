7e6e0a7fab85f41334d5907df0c7fc03e6b291c4
kchelstowski <kchelstowski@soldevelo.com>
2023-04-28 14:53:05 +0200

    OBPIH-5621 Fix circular reference when an Organization has a defaultLocation and this location calls organization in the toJson() method

--- grails-app/domain/org/pih/warehouse/core/Location.groovy
+++ grails-app/domain/org/pih/warehouse/core/Location.groovy
@@ -362,41 +362,25 @@ class Location implements Comparable<Location>, java.io.Serializable {
         ]
     }
 
-    Map toJson(LocationTypeCode locationTypeCode) {
+    Map toJson(locationTypeCode) {
+        def json = [
+                id: id,
+                name: name,
+                locationNumber: locationNumber,
+                active: active,
+                locationType: locationType,
+                locationTypeCode: locationType?.locationTypeCode?.name(),
+        ]
         switch (locationTypeCode) {
-            case LocationTypeCode.DEPOT:
-                return [
-                        id: id,
-                        name: name,
-                        locationNumber: locationNumber,
-                        locationType: locationType,
-                        locationTypeCode: locationType?.locationTypeCode?.name(),
-                        active: active
-                ]
             case LocationTypeCode.INTERNAL:
             case LocationTypeCode.BIN_LOCATION:
-                return [
-                        id: id,
-                        name: name,
-                        locationNumber: locationNumber,
-                        locationType: locationType?.name,
-                        locationTypeCode: locationType?.locationTypeCode?.name(),
+               json += [
                         zoneId: zone?.id,
                         zoneName: zone?.name,
                         active: active
                 ]
-            case LocationTypeCode.ZONE:
-                return [
-                        id: id,
-                        name: name,
-                        locationNumber: locationNumber,
-                        locationType: locationType?.name,
-                        locationTypeCode: locationType?.locationTypeCode?.name(),
-                        active: active
-                ]
-            default:
-                return toJson()
         }
+        return json
     }
 
     static Map toJson(Location location) {
