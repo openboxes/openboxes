1b699f8826d88ab7966e625477e84da833ae50ca
Artur Walkowiak <awalkowiak@soldevelo.com>
2023-06-01 12:28:22 +0200

    OBPIH-5672 Show quantity issued only for approved items (#4076)
    * OBPIH-5672 Show quantity issued only for approved items
    
    * OBPIH-5672 Improvements after review

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -799,11 +799,12 @@ class StockMovementService {
                 ]
             } else if (!template || (template && template.replenishmentTypeCode == ReplenishmentTypeCode.PULL)) {
                 def demand
-                // if request FROM Ward then pull demand from origin to ward
-                if (requisition?.destination?.isWard()) {
+                // if request FROM downstream consumer (location without managed inventory but supporting submitting requests),
+                // then pull demand from origin to that location
+                if (requisition?.destination?.isDownstreamConsumer()) {
                     demand = forecastingService.getDemand(requisition.origin, requisition.destination, stockMovementItem.product)
                 } else {
-                    // if request is NOT FROM Ward, then pull demand outgoing FROM destination to all other locations
+                    // if request is NOT FROM downstream consumer, then pull demand outgoing FROM destination to all other locations
                     demand = forecastingService.getDemand(requisition.destination, null, stockMovementItem.product)
                 }
                 return [
