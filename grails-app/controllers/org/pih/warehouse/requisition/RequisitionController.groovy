be162120c637c7ae54999c4a17debca84493e493
Artur Walkowiak <awalkowiak@soldevelo.com>
2023-06-02 16:23:46 +0200

    OBPIH-5725 Remove unused methods from RequisitionController

--- grails-app/controllers/org/pih/warehouse/requisition/RequisitionController.groovy
+++ grails-app/controllers/org/pih/warehouse/requisition/RequisitionController.groovy
@@ -573,30 +573,6 @@ class RequisitionController {
         redirect(action: "pick", id: command?.requisition?.id)
     }
 
-
-    private List<Location> getDepots() {
-        Location.list().findAll { location -> location.id != session.warehouse.id && location.isWarehouse() }.sort {
-            it.name
-        }
-    }
-
-    private List<Location> getWardsPharmacies() {
-        def current = Location.get(session.warehouse.id)
-        def locations = []
-        if (current) {
-            if (current?.locationGroup == null) {
-                locations = Location.list().findAll { location -> location.isWardOrPharmacy() }.sort {
-                    it.name
-                }
-            } else {
-                locations = Location.list().findAll { location -> location.locationGroup?.id == current.locationGroup?.id }.findAll { location -> location.isWardOrPharmacy() }.sort {
-                    it.name
-                }
-            }
-        }
-        return locations
-    }
-
     /**
      * Generate the name of the requisition.
      *
