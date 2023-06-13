d8f75e25f6be0734ed21a9a82bf58b000474ff7f
Alan Nadolny <alannadolnyy@gmail.com>
2023-06-13 10:11:54 +0200

    OBGM-417 Move saving items method from CombinedShipmentItemService to CombinedShipmentService

--- grails-app/controllers/org/pih/warehouse/api/CombinedShipmentItemApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/CombinedShipmentItemApiController.groovy
@@ -27,7 +27,6 @@ class CombinedShipmentItemApiController {
 
     def orderService
     def combinedShipmentService
-    def combinedShipmentItemService
 
     def getProductsInOrders() {
         def minLength = grailsApplication.config.openboxes.typeahead.minLength
@@ -104,7 +103,7 @@ class CombinedShipmentItemApiController {
             render(status: 400, text: "Shipment not found")
             return
         }
-        combinedShipmentItemService.addItemsToShipment(shipment, itemsToAdd)
+        combinedShipmentService.saveItemsToShipment(shipment, itemsToAdd)
         render([data: shipment] as JSON)
     }
 
