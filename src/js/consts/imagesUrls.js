ef89ac76b53b2a4f35c1ed0d59317ac1df89a351
Alan Nadolny <alannadolnyy@gmail.com>
2023-08-03 17:25:29 +0200

    OBGM-660 Move image to webapp/images and change url of it in imagesUrl

--- src/js/consts/imagesUrls.js
+++ src/js/consts/imagesUrls.js
@@ -1,5 +1,5 @@
 const ImageUrl = {
-  AUTOSAVE_FEATURE_BAR_CONTENT: '/openboxes/images/autosave_feature_modal_content.png',
+  AUTOSAVE_FEATURE_BAR_CONTENT: '/openboxes/static/images/autosave_feature_modal_content.png',
 };
 
 export default ImageUrl;
