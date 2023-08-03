13e0b3a127df043955bcf64459f9d5e9627ea263
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2023-08-03 17:49:44 +0200

    OBGM-623 Unable to unselect use default settings for locations  (#4205)
    * OBGM-623 Add marking supportedActivities as a dirty for triggering save
    
    * OBGM-623 Refactor assigning supportedActivities

--- grails-app/controllers/org/pih/warehouse/core/LocationController.groovy
+++ grails-app/controllers/org/pih/warehouse/core/LocationController.groovy
@@ -83,11 +83,14 @@ class LocationController {
                 throw new IllegalArgumentException("The organization ${selectedOrganization?.name} is inactive, you can't assign it to the location")
             }
             // If none supported activities are chosen, assign "None"
-            // [""].empty would evaluate to false, so we want to filter out falsy values with findAll{it}
+            // [""].empty would evaluate to false, so we want to filter out falsy values with findAll{ it }
+            // In other case map supportedActivities to mark it as dirty for save,
+            // supportedActivities cannot be empty to avoid overriding ActivityCode.NONE
             List supportedActivities = params.list("supportedActivities").findAll{ it }
-            if (locationInstance?.id && supportedActivities?.empty) {
-                params.supportedActivities = [ActivityCode.NONE.id]
+            if (locationInstance?.id) {
+                params.supportedActivities = supportedActivities?.empty ? [ActivityCode.NONE.id] : supportedActivities
             }
+
             locationInstance.properties = params
 
             if (!locationInstance.id && !locationInstance.organization) {
