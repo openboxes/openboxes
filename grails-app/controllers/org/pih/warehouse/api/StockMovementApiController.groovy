e2eef60d4a5b61f75f0c8bf73123026b9b04c22b
Darek Rodzewicz <DRodzewicz@gmail.com>
2023-08-29 15:10:00 +0200

    OBGM-683 Remove Transactional annotation from StockMovementApiController

--- grails-app/controllers/org/pih/warehouse/api/StockMovementApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/StockMovementApiController.groovy
@@ -10,7 +10,6 @@
 package org.pih.warehouse.api
 
 import grails.converters.JSON
-import grails.gorm.transactions.Transactional
 import org.apache.commons.lang.math.NumberUtils
 import org.grails.web.json.JSONObject
 import org.pih.warehouse.core.ActivityCode
@@ -33,7 +32,7 @@ import org.pih.warehouse.requisition.RequisitionType
 import org.pih.warehouse.shipping.Shipment
 import org.pih.warehouse.shipping.ShipmentStatusCode
 
-@Transactional
+
 class StockMovementApiController {
 
     def dataService
