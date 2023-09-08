75af6bf4e8c10eddf503b84aefde1d7d5322c9e9
jmiranda <jcm62@columbia.edu>
2023-09-08 16:28:51 -0500

    WIP: Initial implementation of webhook event publisher

--- grails-app/services/org/pih/warehouse/shipping/ShipmentService.groovy
+++ grails-app/services/org/pih/warehouse/shipping/ShipmentService.groovy
@@ -1156,6 +1156,7 @@ class ShipmentService {
         else {
             throw new ValidationException("Failed to send shipment", shipmentInstance?.errors)
         }
+        webhookPublisherService.publishEvent(shipmentInstance)
 
         grailsApplication.mainContext.publishEvent(new ShipmentStatusTransitionEvent(shipmentInstance, ShipmentStatusCode.SHIPPED))
 
