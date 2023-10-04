acf5b1b5f11e07929729061b81f7e094a0490930
Darek Rodzewicz <DRodzewicz@gmail.com>
2023-10-04 12:54:15 +0200

    OBPIH-5814 Fix empty approvers breaking stockmovement list

--- src/js/components/stock-movement/outbound/StockMovementOutboundTable.jsx
+++ src/js/components/stock-movement/outbound/StockMovementOutboundTable.jsx
@@ -217,7 +217,7 @@ const StockMovementOutboundTable = ({
           {...row}
           tooltip
           defaultValue="None"
-          value={row.value.map(approver => approver.name).join(', ')}
+          value={row.value?.map(approver => approver.name)?.join(', ')}
         />),
     },
     {
