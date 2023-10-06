54b97fe29deaeeb29fc2dd4584151d8816d03192
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2023-10-06 10:43:26 +0200

    OBPIH-5873 Investigate empty date approved after approval action (#4315)
    * OBPIH-5873 Make status transition synchronous and pass status as an argument
    
    * OBPIH-5873 Fix passing status

--- src/groovy/org/pih/warehouse/requisition/RequisitionStatusTransitionEvent.groovy
+++ src/groovy/org/pih/warehouse/requisition/RequisitionStatusTransitionEvent.groovy
@@ -1,16 +1,12 @@
 package org.pih.warehouse.requisition
 
-import org.pih.warehouse.core.User
 import org.springframework.context.ApplicationEvent
 
 class RequisitionStatusTransitionEvent extends ApplicationEvent {
     Requisition requisition
 
-    User createdBy
-
-    RequisitionStatusTransitionEvent(Requisition requisition, User createdBy) {
+    RequisitionStatusTransitionEvent(Requisition requisition) {
         super(requisition)
         this.requisition = requisition
-        this.createdBy = createdBy
     }
 }
