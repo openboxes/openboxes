7748ad7cae5a74a140770ce8ce62a1aee90303f0
kchelstowski <kchelstowski@soldevelo.com>
2023-12-22 15:45:36 +0100

    OBPIH-6013 Change redirect of the product supplier list action, to render the react page

--- grails-app/controllers/org/pih/warehouse/product/ProductSupplierController.groovy
+++ grails-app/controllers/org/pih/warehouse/product/ProductSupplierController.groovy
@@ -34,30 +34,7 @@ class ProductSupplierController {
     }
 
     def list() {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        def productSuppliers = ProductSupplier.createCriteria().list(max: params.max, offset: params.offset) {
-            if (params.product?.id) {
-                eq('product.id', params.product.id)
-            }
-            if (params.supplierId) {
-                eq('supplier.id', params.supplierId)
-            }
-            if (params.manufacturerId) {
-                eq('manufacturer.id', params.manufacturerId)
-            }
-            if (params.q) {
-                or {
-                    ilike("productCode", "%" + params.q + "%")
-                    ilike("code", "%" + params.q + "%")
-                    ilike("name", "%" + params.q + "%")
-                    ilike("supplierCode", "%" + params.q + "%")
-                    ilike("supplierName", "%" + params.q + "%")
-                    ilike("manufacturerCode", "%" + params.q + "%")
-                    ilike("manufacturerName", "%" + params.q + "%")
-                }
-            }
-        }
-        [productSupplierInstanceList: productSuppliers, productSupplierInstanceTotal: productSuppliers.totalCount]
+        render(view: "/common/react")
     }
 
     def create() {
