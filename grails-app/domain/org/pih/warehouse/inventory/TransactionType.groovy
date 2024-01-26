e2f1b9930df1d82999aa0895697bb7d84f2b3c01
Alan Nadolny <alannadolnyy@gmail.com>
2024-01-26 11:03:43 +0100

    OBPIH-6035 Improve comment on isAdjustment func

--- grails-app/domain/org/pih/warehouse/inventory/TransactionType.groovy
+++ grails-app/domain/org/pih/warehouse/inventory/TransactionType.groovy
@@ -40,7 +40,13 @@ class TransactionType implements Serializable {
         return name.split(regex)[0] == transactionTypeName.split(regex)[0]
     }
 
-    // Checking whether the transaction type is adjustment for displaying the appropriate icon on the stock history page
+    // Checking whether the transaction type is adjustment for
+    // displaying the appropriate icon on the stock history page.
+    // To determine if a transaction is type of adjustment
+    // we have to check it's name using compareName,
+    // because it's saved as a "localized string", so it has
+    // to be split by localized string separator
+    // and then compared
     Boolean isAdjustment() {
         compareName(Constants.ADJUSTMENT)
     }
