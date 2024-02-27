b8867bbc43f517cd6eab5ae4aa5e2c2c334083ec
kchelstowski <kchelstowski@soldevelo.com>
2024-02-27 12:32:38 +0100

    OBPIH-6166 Add endpoint for create/edit product package with pricing

---
+++ grails-app/services/org/pih/warehouse/product/ProductPackageService.groovy
@@ -0,0 +1,62 @@
+package org.pih.warehouse.product
+
+import grails.gorm.transactions.Transactional
+import org.pih.warehouse.core.ProductPrice
+
+@Transactional
+class ProductPackageService {
+
+    ProductSupplier save(ProductPackageCommand command) {
+        setPackageData(command)
+        if (command.contractPricePrice) {
+            setContractPriceData(command.productSupplier, command.contractPricePrice, command.contractPriceValidUntil)
+        }
+        return command.productSupplier
+    }
+
+    private static void setPackageData(ProductPackageCommand command) {
+        ProductSupplier productSupplier = command.productSupplier
+        productSupplier.minOrderQuantity = command.minOrderQuantity
+
+        ProductPackage defaultProductPackage = productSupplier?.productPackages?.find {
+            it.uom == command.uom && it.quantity == command.productPackageQuantity
+        }
+
+        // If there is no product package with given unit of measure and of given quantity, create one
+        if (!defaultProductPackage) {
+            defaultProductPackage = new ProductPackage()
+            defaultProductPackage.name = "${command.uom?.code}/${command.productPackageQuantity}"
+            defaultProductPackage.description = "${command.uom?.name} of ${command.productPackageQuantity}"
+            defaultProductPackage.product = command.productSupplier?.product
+            defaultProductPackage.uom = command.uom
+            defaultProductPackage.quantity = command.productPackageQuantity
+            productSupplier.addToProductPackages(defaultProductPackage)
+            productSupplier.defaultProductPackage = defaultProductPackage
+        }
+        // If product package price is not provided, skip assigning pricing data
+        if (!command.productPackagePrice) {
+            return
+        }
+        // If product package price is provided and default product package doesn't have
+        // product price yet, create ProductPrice instance and assign it to the package
+        if (!defaultProductPackage.productPrice) {
+            ProductPrice productPrice = new ProductPrice(price: command.productPackagePrice)
+            defaultProductPackage.productPrice = productPrice
+            return
+        }
+        // If product package price is provided and the default product package already has price,
+        // update this price's data
+        defaultProductPackage.productPrice.price = command.productPackagePrice
+        defaultProductPackage.lastUpdated = new Date()
+    }
+
+    private static void setContractPriceData(ProductSupplier productSupplier, BigDecimal contractPricePrice, Date validUntil) {
+        if (!productSupplier.contractPrice) {
+            productSupplier.contractPrice = new ProductPrice()
+        }
+        productSupplier.contractPrice.price = contractPricePrice
+        if (validUntil) {
+            productSupplier.contractPrice.toDate = validUntil
+        }
+    }
+}
---
+++ src/main/groovy/org/pih/warehouse/product/ProductPackageCommand.groovy
@@ -0,0 +1,38 @@
+package org.pih.warehouse.product
+
+import grails.validation.Validateable
+import org.pih.warehouse.core.UnitOfMeasure
+import util.ConfigHelper
+
+class ProductPackageCommand implements Validateable {
+
+    ProductSupplier productSupplier
+
+    UnitOfMeasure uom
+
+    // Package size
+    Integer productPackageQuantity
+
+    BigDecimal minOrderQuantity
+
+    // Package price
+    BigDecimal productPackagePrice
+
+    BigDecimal contractPricePrice
+
+    Date contractPriceValidUntil
+
+    static constraints = {
+        productPackageQuantity(min: 0)
+        minOrderQuantity(nullable: true, min: 0.0)
+        productPackagePrice(nullable: true, min: 0.0)
+        contractPricePrice(nullable: true, min: 0.0)
+        contractPriceValidUntil(nullable: true, validator: { Date date ->
+            Date minDate = ConfigHelper.getMinimumExpirationDate()
+            if (date < minDate) {
+                return false
+            }
+            return true
+        })
+    }
+}
