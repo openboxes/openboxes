956449395e9e09f67ce584a2941b2a68c81d1ba5
Alan Nadolny <alannadolnyy@gmail.com>
2024-03-01 15:32:36 +0100

    OBPIH-6112 Add new section

--- src/js/api/services/ProductSupplierApi.js
+++ src/js/api/services/ProductSupplierApi.js
@@ -1,4 +1,5 @@
 import {
+  ATTRIBUTES,
   PREFERENCE_TYPE_OPTIONS,
   PRODUCT_SUPPLIER_BY_ID,
   RATING_TYPE_OPTIONS,
@@ -10,4 +11,5 @@ export default {
   getRatingTypeOptions: (config) => apiClient.get(RATING_TYPE_OPTIONS, config),
   deleteProductSupplier: (id) => apiClient.delete(PRODUCT_SUPPLIER_BY_ID(id)),
   getProductSupplier: (id) => apiClient.get(PRODUCT_SUPPLIER_BY_ID(id)),
+  getAttributes: (config) => apiClient.get(ATTRIBUTES, config),
 };
---
+++ src/js/components/productSupplier/create/sections/AttributesSection.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { Controller } from 'react-hook-form';
+
+import Section from 'components/Layout/v2/Section';
+import useProductSupplierAttributes from 'hooks/productSupplier/form/useProductSupplierAttributes';
+
+const AttributesSection = ({ control, errors }) => {
+  const { attributesWithInputTypes } = useProductSupplierAttributes();
+
+  return (
+    <Section title={{ label: 'react.productSupplier.section.attributes.title', defaultMessage: 'Attributes' }}>
+      <div className="attributes-section gap-8 mt-4">
+        {
+          attributesWithInputTypes.map(({ attribute, Input }) => {
+            const errorMessage = errors?.[attribute?.id]?.message;
+            return (
+              <Controller
+                name={`attributes.${attribute?.id}`}
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    title={{ defaultMessage: attribute?.name }}
+                    hasErrors={Boolean(errorMessage)}
+                    errorMessage={errorMessage}
+                    {...attribute}
+                    {...field}
+                  />
+                )}
+              />
+            );
+          })
+        }
+      </div>
+    </Section>
+  );
+};
+
+export default AttributesSection;
+
+AttributesSection.propTypes = {
+  control: PropTypes.shape({}).isRequired,
+  errors: PropTypes.objectOf(
+    PropTypes.shape({
+      message: PropTypes.string,
+    }),
+  ),
+};
+
+AttributesSection.defaultProps = {
+  errors: {},
+};
