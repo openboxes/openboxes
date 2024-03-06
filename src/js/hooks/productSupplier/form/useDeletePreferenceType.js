b4a11ae068a637cd4e40ed5a737665c04a5f5f7c
Alan Nadolny <alannadolnyy@gmail.com>
2024-03-06 15:10:36 +0100

    OBPIH-6206 Adjust hook for deleting preference types within the table

--- src/js/hooks/productSupplier/form/useDeletePreferenceType.js
+++ src/js/hooks/productSupplier/form/useDeletePreferenceType.js
@@ -1,53 +1,80 @@
 import _ from 'lodash';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
+import { hideSpinner, showSpinner } from 'actions';
+import productSupplierApi from 'api/services/ProductSupplierApi';
 import confirmationModal from 'utils/confirmationModalUtils';
 
-const useDeletePreferenceType = ({ preferenceTypeData, reset }) => {
+const useDeletePreferenceType = ({
+  preferenceTypeData,
+  afterDelete,
+  isDefaultPreferenceType,
+  onCancel,
+}) => {
+  const dispatch = useDispatch();
+
   const isPreferenceTypeEmpty = !_.some(
     Object.values(_.omit(preferenceTypeData || {}, 'id')),
   );
-
-  const defaultPreferenceType = {
-    bidName: '',
-    validityStartDate: '',
-    validityEndDate: '',
-    preferenceType: '',
-  };
 
   const modalLabels = {
     title: {
       label: 'react.productSupplier.deleteConfirmation.title.label',
       default: 'Are you sure?',
     },
-    content: {
-      label: 'react.productSupplier.defaultPreferenceDeleteConfirmation.label',
-      default: 'Are you sure you want to delete the default preference for this source?',
-    },
+    content: isDefaultPreferenceType
+      ? {
+        label: 'react.productSupplier.defaultPreferenceDeleteConfirmation.label',
+        default: 'Are you sure you want to delete the default preference for this source?',
+      }
+      : {
+        label: 'react.productSupplier.preferenceTypeVariationsDeleteConfirmation.label',
+        default: 'Are you sure you want to delete this preference variation?',
+      },
   };
 
-  const deleteConfirmationModalButtons = () => (onClose) => ([
+  const deletePreferenceType = async (onClose) => {
+    dispatch(showSpinner());
+    try {
+      if (preferenceTypeData?.id) {
+        await productSupplierApi.deleteProductSupplierPreference(preferenceTypeData?.id);
+      }
+      afterDelete?.();
+    } finally {
+      onClose?.();
+      dispatch(hideSpinner());
+    }
+  };
+
+  const deleteConfirmationModalButtons = (onClose) => ([
     {
       variant: 'transparent',
       defaultLabel: 'Cancel',
       label: 'react.productSupplier.deleteConfirmation.cancel.label',
-      onClick: onClose,
+      onClick: () => {
+        onClose();
+        onCancel?.();
+      },
     },
     {
       variant: 'danger',
       defaultLabel: 'Confirm',
       label: 'react.productSupplier.deleteConfirmation.delete.label',
-      onClick: () => {
-        reset({ defaultPreferenceType });
-        onClose();
-      },
+      onClick: () => deletePreferenceType(onClose),
     },
   ]);
 
-  const openConfirmationModal = (productSupplierId) => {
-    confirmationModal({
-      buttons: deleteConfirmationModalButtons(productSupplierId),
-      ...modalLabels,
-    });
+  const openConfirmationModal = () => {
+    if (preferenceTypeData?.id) {
+      confirmationModal({
+        buttons: deleteConfirmationModalButtons,
+        ...modalLabels,
+      });
+      return;
+    }
+
+    deletePreferenceType();
   };
 
   return {
@@ -57,3 +84,14 @@ const useDeletePreferenceType = ({ preferenceTypeData, reset }) => {
 };
 
 export default useDeletePreferenceType;
+
+useDeletePreferenceType.propTypes = {
+  preferenceTypeData: PropTypes.shape({}),
+  afterDelete: PropTypes.func.isRequired,
+  isDefaultPreferenceType: PropTypes.bool,
+};
+
+useDeletePreferenceType.defaultProps = {
+  preferenceTypeData: null,
+  isDefaultPreferenceType: false,
+};
