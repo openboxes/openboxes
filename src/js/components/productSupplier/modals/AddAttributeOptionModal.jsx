2945095a15ee6ba7ca2954a25e0f65da6597d039
Alan Nadolny <alannadolnyy@gmail.com>
2024-03-11 14:24:22 +0100

    OBPIH-6112 Add new modal

---
+++ src/js/components/productSupplier/modals/AddAttributeOptionModal.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+import PropTypes from 'prop-types';
+import { RiCloseFill } from 'react-icons/all';
+import Modal from 'react-modal';
+
+import Button from 'components/form-elements/Button';
+import TextInput from 'components/form-elements/v2/TextInput';
+import useTranslate from 'hooks/useTranslate';
+
+import '../styles.scss';
+
+const AddAttributeOptionModal = ({
+  isOpen,
+  close,
+  selectedAttribute,
+  setValue,
+}) => {
+  const [inputValue, setInputValue] = useState(null);
+  const translate = useTranslate();
+
+  const onChange = (e) => setInputValue(e.target.value);
+
+  const onClose = () => {
+    setInputValue(null);
+    close();
+  };
+
+  const setAttributeValue = (attribute, value) => () => {
+    setValue(`attributes.${selectedAttribute?.id}`, { id: attribute?.id, value, label: value });
+    setInputValue(null);
+    close();
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      className="modal-content w-50"
+      shouldCloseOnOverlayClick={false}
+    >
+      <div>
+        <div className="d-flex justify-content-between mb-3">
+          <h4>{selectedAttribute?.name}</h4>
+          <RiCloseFill
+            size="24px"
+            className="cursor-pointer"
+            role="button"
+            onClick={onClose}
+          />
+        </div>
+        <div>
+          <TextInput
+            required
+            title={{ id: '', defaultMessage: 'Other' }}
+            errorMessage={inputValue === '' && translate(
+              'react.productSupplier.validation.otherIsRequired',
+              'Other is required',
+            )}
+            value={inputValue}
+            onChange={onChange}
+          />
+        </div>
+        <div className="d-flex justify-content-end gap-8 mt-3">
+          <Button
+            variant="transparent"
+            defaultLabel="Cancel"
+            label=""
+            onClick={onClose}
+          />
+          <Button
+            disabled={!inputValue}
+            defaultLabel="Confirm"
+            label=""
+            onClick={setAttributeValue(selectedAttribute, inputValue)}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default AddAttributeOptionModal;
+
+AddAttributeOptionModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+  selectedAttribute: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }),
+  setValue: PropTypes.func.isRequired,
+};
+
+AddAttributeOptionModal.defaultProps = {
+  selectedAttribute: {},
+};
