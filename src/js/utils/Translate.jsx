ef4fa262dd998a68fef53eafd4a9149d94790f5f
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-03-12 17:16:13 +0100

    OBPIH-6126 Document translation util function hook and a component

--- src/js/utils/Translate.jsx
+++ src/js/utils/Translate.jsx
@@ -3,9 +3,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Translate } from 'react-localize-redux';
 
-export const translateWithDefaultMessage = translate => ((id, defaultMessage, data, options) =>
-  translate(id, data, { ...options, onMissingTranslation: () => (defaultMessage || id) }));
+/**
+ * @callback TranslationCallback
+ * @param {string} id - The translation code of the message to translate
+ * which is localed in messages.properties.
+ * @param {string} [defaultMessage] - The default message to use if the translation is missing.
+ * @param {Object} [data] - Optional data for variable replacements in dynamic translations.
+ * @param {Object} [options] - Additional options for the translation.
+ * @param {string} [options.language]  Optionally pass a language code to force
+ * Translate to render a specific language.
+ * @param {boolean} [options.renderInnerHtml] Override initialize renderInnerHtml option
+ * for translation.
+ * @returns {string} - The translated message.
+ */
 
+/**
+ * Creates a function that translates a message with a default if the translation is missing.
+ * @param {function} translate - The translation function provided by react-localize-redux.
+ * @returns {TranslationCallback} - The translated message function.
+ */
+export const translateWithDefaultMessage = translate => (
+
+  (id, defaultMessage, data, options) =>
+    translate(id, data, {
+      ...options,
+      onMissingTranslation: () => (defaultMessage || id),
+    })
+);
+
+/**
+ * A wrapper component for the Translate component that provides default message handling.
+ * @param {Object} props - The props for the TranslateWrapper component.
+ * @param {string} props.id - The ID of the message to translate.
+ * @param {string} props.defaultMessage - The default message to use if the translation is missing.
+ * @param {string} props.data - Optional data for variable replacements in dynamic translations.
+ * @returns {JSX.Element} - The Translate component with default message handling.
+ */
 const TranslateWrapper = ({
   id, defaultMessage, options = {}, ...props
 }) => (
