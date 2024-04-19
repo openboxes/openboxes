0ae7489529cefa188f2230f11d7e2df5036f8056
Alan Nadolny <alannadolnyy@gmail.com>
2024-04-19 14:49:41 +0200

    OBPIH-5813 Change statements and order of arguments in unit tests

--- src/test/groovy/unit/org/pih/warehouse/requisition/RequisitionItemSpec.groovy
+++ src/test/groovy/unit/org/pih/warehouse/requisition/RequisitionItemSpec.groovy
@@ -14,86 +14,86 @@ import spock.lang.Unroll
 class RequisitionItemSpec extends Specification implements DomainUnitTest<RequisitionItem> {
 
     void 'RequisitionItem.getDisplayStatus() should return: #requisitionItemStatus for requisition status #requisitionStatus'() {
-        when:
-        domain.requisition = new Requisition()
-        domain.requisition.status = requisitionStatus
+        given:
+            domain.requisition = new Requisition()
+            domain.requisition.status = requisitionStatus
 
-        then:
-        domain.displayStatus == requisitionItemStatus
+        expect:
+            domain.displayStatus == requisitionItemStatus
 
         where:
-        requisitionItemStatus          || requisitionStatus
-        RequisitionItemStatus.PENDING  || RequisitionStatus.PENDING_APPROVAL
-        RequisitionItemStatus.PENDING  || RequisitionStatus.PENDING
-        RequisitionItemStatus.CANCELED || RequisitionStatus.REJECTED
+            requisitionStatus                   || requisitionItemStatus
+            RequisitionStatus.PENDING_APPROVAL  || RequisitionItemStatus.PENDING
+            RequisitionStatus.PENDING           || RequisitionItemStatus.PENDING
+            RequisitionStatus.REJECTED          || RequisitionItemStatus.CANCELED
     }
 
     void 'RequisitionItem.getDisplayStatus() should return: #status when at least one child item is type of #childItemType and RequisitionItem.isSubstituted() should return: #isSubstituted'() {
-        when:
-        RequisitionItem item = new RequisitionItem()
-        item.requisitionItemType = childItemType
-        domain.requisition = new Requisition()
-        domain.requisition.status = RequisitionStatus.CHECKING
-        domain.addToRequisitionItems(item)
+        given:
+            RequisitionItem item = new RequisitionItem()
+            item.requisitionItemType = childItemType
+            domain.requisition = new Requisition()
+            domain.requisition.status = RequisitionStatus.CHECKING
+            domain.addToRequisitionItems(item)
 
-        then:
-        domain.isSubstituted() == isSubstituted
-        domain.displayStatus == status
+        expect:
+            domain.isSubstituted() == isSubstituted
+            domain.displayStatus == status
 
         where:
-        status                            || childItemType                    | isSubstituted
-        RequisitionItemStatus.SUBSTITUTED || RequisitionItemType.SUBSTITUTION | true
-        RequisitionItemStatus.PENDING     || null                             | false
+            childItemType                    || isSubstituted | status
+            RequisitionItemType.SUBSTITUTION || true          | RequisitionItemStatus.SUBSTITUTED
+            null                             || false         | RequisitionItemStatus.PENDING
     }
 
     void 'RequisitionItem.getDisplayStatus() should return: #status when qty is #quantity and the qty approved is #quantityApproved and RequisitionItem.isApproved() should return: #isApproved'() {
-        when:
-        domain.requisition = new Requisition()
-        domain.requisition.status = RequisitionStatus.CHECKING
-        domain.quantity = quantity
-        domain.quantityApproved = quantityApproved
+        given:
+            domain.requisition = new Requisition()
+            domain.requisition.status = RequisitionStatus.CHECKING
+            domain.quantity = quantity
+            domain.quantityApproved = quantityApproved
 
-        then:
-        domain.isApproved() == isApproved
-        domain.displayStatus == status
+        expect:
+            domain.isApproved() == isApproved
+            domain.displayStatus == status
 
         where:
-        status                         || quantity | quantityApproved | isApproved
-        RequisitionItemStatus.APPROVED || 2        | 2                | true
-        RequisitionItemStatus.PENDING  || 1        | 0                | false
+            quantity | quantityApproved || isApproved | status
+            2        | 2                || true | RequisitionItemStatus.APPROVED
+            1        | 0                || false | RequisitionItemStatus.PENDING
     }
 
     void 'RequisitionItem.getDisplayStatus() should return: #status when item has child item and the quantity cancelled is #quantityCancelled and RequisitionItem.isChanged() is #isChanged'() {
-        when:
-        domain.requisition = new Requisition()
-        domain.requisition.status = RequisitionStatus.CHECKING
-        domain.quantityCanceled = quantityCancelled
-        domain.modificationItem = new RequisitionItem()
+        given:
+            domain.requisition = new Requisition()
+            domain.requisition.status = RequisitionStatus.CHECKING
+            domain.quantityCanceled = quantityCancelled
+            domain.modificationItem = new RequisitionItem()
 
-        then:
-        domain.isChanged() == isChanged
-        domain.displayStatus == status
+        expect:
+            domain.isChanged() == isChanged
+            domain.displayStatus == status
 
         where:
-        status                        || quantityCancelled | isChanged
-        RequisitionItemStatus.CHANGED || 1                 | true
-        RequisitionItemStatus.PENDING || 0                 | false
+            quantityCancelled || isChanged | status
+            1                 || true      | RequisitionItemStatus.CHANGED
+            0                 || false     | RequisitionItemStatus.PENDING
     }
 
     void 'RequisitionItem.getDisplayStatus() should return: #status when item has quantity cancelled #quantityCancelled and quantity #quantity and RequisitionItem.isCancelled() is #isCancelled'() {
-        when:
+        given:
             domain.requisition = new Requisition()
             domain.requisition.status = RequisitionStatus.CHECKING
             domain.quantityCanceled = quantityCancelled
             domain.quantity = quantity
 
-        then:
+        expect:
             domain.isCanceled() == isCancelled
             domain.displayStatus == status
 
         where:
-            status                          || quantity | quantityCancelled | isCancelled
-            RequisitionItemStatus.CANCELED  || 1        | 1                 | true
-            RequisitionItemStatus.PENDING   || 2        | 1                 | false
+            quantity | quantityCancelled || isCancelled | status
+            1        | 1                 || true        | RequisitionItemStatus.CANCELED
+            2        | 1                 || false       | RequisitionItemStatus.PENDING
     }
 }
