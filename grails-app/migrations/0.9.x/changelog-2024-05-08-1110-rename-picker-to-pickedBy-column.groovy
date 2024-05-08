783339a304dab0c5312cf136ca5bab82bf57abd3
Alan Nadolny <alannadolnyy@gmail.com>
2024-05-08 12:51:19 +0200

    OBPIH-6327 Fixes after review

--- grails-app/migrations/0.9.x/changelog-2024-05-08-1110-rename-picker-to-pickedBy-column.groovy
+++ grails-app/migrations/0.9.x/changelog-2024-05-08-1110-rename-picker-to-pickedBy-column.groovy
@@ -2,11 +2,16 @@ databaseChangeLog = {
 
     changeSet(author: "anadolny", id: "080520241110-0", objectQuotingStrategy: "LEGACY") {
         preConditions(onError: "HALT", onFail: "MARK_RAN", onSqlOutput: "IGNORE") {
-            columnExists(columnName: "picker_id", tableName: "picklist_item")
+            and {
+                columnExists(columnName: "picker_id", tableName: "picklist_item")
+                not {
+                    columnExists(columnName: "picked_by_id", tableName: "picklist_item")
+                }
+            }
         }
 
         renameColumn(
-                newColumnName: "pickedBy_id",
+                newColumnName: "picked_by_id",
                 oldColumnName: "picker_id",
                 tableName: "picklist_item",
                 columnDataType: "CHAR(38)"
@@ -24,14 +29,14 @@ databaseChangeLog = {
     changeSet(author: "anadolny", id: "080520241110-2", objectQuotingStrategy: "LEGACY") {
         preConditions(onError: "HALT", onFail: "MARK_RAN", onSqlOutput: "IGNORE") {
             not {
-                foreignKeyConstraintExists(foreignKeyName: "fk_picklist_item_pickedBy_id_person")
+                foreignKeyConstraintExists(foreignKeyName: "fk_picklist_item_picked_by_id_person")
             }
         }
 
         addForeignKeyConstraint(
-                baseColumnNames: "pickedBy_id",
+                baseColumnNames: "picked_by_id",
                 baseTableName: "picklist_item",
-                constraintName: "fk_picklist_item_pickedBy_id_person",
+                constraintName: "fk_picklist_item_picked_by_id_person",
                 deferrable: "false",
                 initiallyDeferred: "false",
                 referencedColumnNames: "id",
