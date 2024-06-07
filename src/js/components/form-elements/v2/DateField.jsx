f0c593b89383b8a465e92cad1ad0423aa28e52b1
kchelstowski <kchelstowski@soldevelo.com>
2024-06-07 13:37:03 +0200

    OBPIH-6329 Add ability to use time in the redesigned DateField

--- src/js/components/form-elements/v2/DateField.jsx
+++ src/js/components/form-elements/v2/DateField.jsx
@@ -6,13 +6,13 @@ import DatePicker from 'react-datepicker';
 
 import DateFieldInput from 'components/form-elements/v2/DateFieldInput';
 import { DateFormat, TimeFormat } from 'consts/timeFormat';
+import useTranslate from 'hooks/useTranslate';
 import InputWrapper from 'wrappers/InputWrapper';
 import RootPortalWrapper from 'wrappers/RootPortalWrapper';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import 'components/form-elements/DateFilter/DateFilter.scss';
 import './style.scss';
-import useTranslate from 'hooks/useTranslate';
 
 const DateField = ({
   title,
@@ -25,16 +25,26 @@ const DateField = ({
   className,
   value,
   onChange,
+  showTimeSelect,
   ...fieldProps
 }) => {
   const translate = useTranslate();
   const onClear = () => onChange(null);
 
-  const onChangeHandler = date => onChange(date?.format(DateFormat.MMM_DD_YYYY));
+  const onChangeHandler = (date) => {
+    if (showTimeSelect) {
+      onChange(date?.format(DateFormat.MMM_DD_YYYY_HH_MM_SS));
+      return;
+    }
+    onChange(date?.format(DateFormat.MMM_DD_YYYY));
+  };
 
   const formatDate = (dateToFormat) => {
     if (!dateToFormat) {
       return null;
+    }
+    if (showTimeSelect) {
+      return moment(new Date(dateToFormat), DateFormat.MMM_DD_YYYY_HH_MM_SS);
     }
 
     return moment(new Date(dateToFormat), DateFormat.MMM_DD_YYYY);
@@ -57,10 +67,11 @@ const DateField = ({
     >
       <DatePicker
         {...fieldProps}
+        showTimeSelect={showTimeSelect}
         customInput={<DateFieldInput onClear={onClear} />}
         className={`form-element-input ${errorMessage ? 'has-errors' : ''} ${className}`}
         dropdownMode="scroll"
-        dateFormat={DateFormat.MMM_DD_YYYY}
+        dateFormat={showTimeSelect ? DateFormat.MMM_DD_YYYY_HH_MM_SS : DateFormat.MMM_DD_YYYY}
         timeFormat={TimeFormat.HH_MM}
         disabled={disabled}
         timeIntervals={15}
@@ -116,6 +127,7 @@ DateField.propTypes = {
   className: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
+  showTimeSelect: PropTypes.bool,
 };
 
 DateField.defaultProps = {
@@ -129,4 +141,5 @@ DateField.defaultProps = {
   className: '',
   value: null,
   onChange: () => {},
+  showTimeSelect: false,
 };
