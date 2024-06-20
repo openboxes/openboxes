303a666268a4289efda6a7bbbd3edcf40c4da168
Alan Nadolny <alannadolnyy@gmail.com>
2024-06-20 17:53:37 +0200

    OBPIH-6520 Add util and component for translating dates in React

--- src/js/utils/translation-utils.jsx
+++ src/js/utils/translation-utils.jsx
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+import moment from 'moment';
+import { getTranslate } from 'react-localize-redux';
 
 const splitTranslation = (data, locale) => {
   const [en, fr] = _.split(data, '|fr:');
   return locale === 'fr' && fr ? fr : en;
+};
+
+export const formatDate = (localize) => (date, formatName) => {
+  const localeCode = localize.languages.find(lang => lang.active)?.code;
+  const dateFormat = getTranslate(localize)(formatName);
+  return moment(date).locale(localeCode).format(dateFormat);
 };
 
 export default splitTranslation;
