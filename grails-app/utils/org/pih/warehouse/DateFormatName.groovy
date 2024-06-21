ca3ada3912cebf7ece6db001cc69c6c0923a2481
Alan Nadolny <alannadolnyy@gmail.com>
2024-06-21 16:50:31 +0200

    OBPIH-6520 Fix comment

--- grails-app/utils/org/pih/warehouse/DateFormatName.groovy
+++ grails-app/utils/org/pih/warehouse/DateFormatName.groovy
@@ -5,8 +5,8 @@ package org.pih.warehouse
 // 1. Add new format in message.properties file
 // 2. Add the newly created label to this enum
 // This enum is used in:
-// 1. G tag - <g:formatDate date={your date} formatName={id of enum property} />
-// 2. LocalizationUtil.formatDate(your date, id of enum property)
+// 1. G tag - <g:formatDate date={your date} formatName={enum property} />
+// 2. LocalizationUtil.formatDate(your date, enum property)
 
 enum DateFormatName {
     DATE_TIME('default.date.format'),
