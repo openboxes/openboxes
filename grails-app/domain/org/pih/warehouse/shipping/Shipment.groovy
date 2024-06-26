442c2e8a90c5f0a577eaa3c1a077e10afc48b034
Kacper Chełstowski <kchelstowski@soldevelo.com>
2024-06-26 12:32:35 +0200

    OBPIH-6447 Make the fallback for the requisition last updated, not sh… (#4695)

--- grails-app/domain/org/pih/warehouse/shipping/Shipment.groovy
+++ grails-app/domain/org/pih/warehouse/shipping/Shipment.groovy
@@ -417,7 +417,7 @@ class Shipment implements Comparable, Serializable {
         if (shippedEvent) {
             // For the e-requests we want to have the fallback to the user, who last updated the shipment
             if (requisition?.electronicType) {
-                return shippedEvent.createdBy ?: updatedBy
+                return shippedEvent.createdBy ?: requisition.updatedBy
             }
             // The fallback for createdBy is done, because there was a bug at Event level,
             // which didn't persist the event.createdBy, so for SMs from the past, we might not have the shippedEvent.createdBy
