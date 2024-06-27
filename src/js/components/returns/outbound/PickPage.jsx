2a5ee212b47a494d58310d0969d4a0b41498abf4
Alan Nadolny <alannadolnyy@gmail.com>
2024-06-27 17:11:16 +0200

    OBPIH-6438 Localize dates in outbound return workflow

--- src/js/components/returns/outbound/PickPage.jsx
+++ src/js/components/returns/outbound/PickPage.jsx
@@ -10,13 +10,14 @@ import { connect } from 'react-redux';
 import { hideSpinner, showSpinner } from 'actions';
 import ArrayField from 'components/form-elements/ArrayField';
 import LabelField from 'components/form-elements/LabelField';
+import DateFormat from 'consts/dateFormat';
 import apiClient, { parseResponse } from 'utils/apiClient';
 import { renderFormField } from 'utils/form-utils';
 import { formatProductDisplayName } from 'utils/form-values-utils';
 import Translate, { translateWithDefaultMessage } from 'utils/Translate';
+import { formatDate } from 'utils/translation-utils';
 
 import 'react-confirm-alert/src/react-confirm-alert.css';
-
 
 const FIELDS = {
   picklistItems: {
@@ -90,6 +91,9 @@ const FIELDS = {
         label: 'react.outboundReturn.expiry.label',
         defaultMessage: 'Expiry',
         flexWidth: '1',
+        getDynamicAttr: ({ formatLocalizedDate }) => ({
+          formatValue: (value) => formatLocalizedDate(value, DateFormat.COMMON),
+        }),
       },
       quantity: {
         type: LabelField,
@@ -201,6 +205,7 @@ class PickPage extends Component {
                   refetchOutboundReturn: this.fetchOutboundReturn,
                   locationId: this.props.locationId,
                   translate: this.props.translate,
+                  formatLocalizedDate: this.props.formatLocalizedDate,
                   values,
                 }))}
               </div>
@@ -228,6 +233,7 @@ class PickPage extends Component {
 const mapStateToProps = state => ({
   outboundReturnsTranslationsFetched: state.session.fetchedTranslations.outboundReturns,
   translate: translateWithDefaultMessage(getTranslate(state.localize)),
+  formatLocalizedDate: formatDate(state.localize),
 });
 
 export default connect(
@@ -251,4 +257,5 @@ PickPage.propTypes = {
   locationId: PropTypes.string.isRequired,
   outboundReturnsTranslationsFetched: PropTypes.bool.isRequired,
   translate: PropTypes.func.isRequired,
+  formatLocalizedDate: PropTypes.func.isRequired,
 };
