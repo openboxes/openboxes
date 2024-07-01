cd164b49d30012ec614f1388f5757112c053d2b5
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2024-07-01 11:25:02 +0200

    OBPIH-6520 Set up for date localization in DD/Month/YYYY format for Es and Mx locales (#4700)
    Co-authored-by: Artur Walkowiak <awalkowiak@soldevelo.com>

--- src/js/utils/translation-utils.jsx
+++ src/js/utils/translation-utils.jsx
@@ -1,8 +1,22 @@
 import _ from 'lodash';
+import moment from 'moment';
+import { getTranslate } from 'react-localize-redux';
 
 const splitTranslation = (data, locale) => {
   const [en, fr] = _.split(data, '|fr:');
   return locale === 'fr' && fr ? fr : en;
+};
+
+export const getDateFormat = (localize) => (formatName) =>
+  getTranslate(localize)(formatName);
+
+export const getLocaleCode = (localize) =>
+  localize.languages.find(lang => lang.active)?.code;
+
+export const formatDate = (localize) => (date, formatName) => {
+  const localeCode = getLocaleCode(localize);
+  const dateFormat = getTranslate(localize)(formatName);
+  return moment(date).locale(localeCode).format(dateFormat);
 };
 
 export default splitTranslation;
