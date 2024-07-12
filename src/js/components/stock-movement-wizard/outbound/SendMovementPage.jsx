cee2fe0ae8d4134150ba2c7ff04df6f7edbe7c3d
Kacper Chełstowski <kchelstowski@soldevelo.com>
2024-07-12 20:43:33 +0200

    OBPIH-6565 Fix ability to set time properly on ship date field in req… (#4738)

--- src/js/components/stock-movement-wizard/outbound/SendMovementPage.jsx
+++ src/js/components/stock-movement-wizard/outbound/SendMovementPage.jsx
@@ -94,6 +94,8 @@ const SHIPMENT_FIELDS = {
     attributes: {
       localizeDate: true,
       localizedDateFormat: DateFormat.DEFAULT,
+      // It's necessary for properly setting up the time part of the localized date format
+      dateFormat: 'MM/DD/YYYY HH:mm Z',
       required: true,
       showTimeSelect: true,
       autoComplete: 'off',
