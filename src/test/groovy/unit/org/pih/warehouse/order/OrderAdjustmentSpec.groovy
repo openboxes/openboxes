91032cf1a98cbd2a6f87c0ded009ff4a51fb89a5
Alan Nadolny <alannadolnyy@gmail.com>
2024-07-29 15:57:18 +0200

    OBPIH-6497 Add unit tests for order adjustments

---
+++ src/test/groovy/unit/org/pih/warehouse/order/OrderAdjustmentSpec.groovy
@@ -0,0 +1,48 @@
+package unit.org.pih.warehouse.order
+
+import grails.testing.gorm.DomainUnitTest
+import org.pih.warehouse.order.Order
+import org.pih.warehouse.order.OrderAdjustment
+import org.pih.warehouse.order.OrderStatus
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class OrderAdjustmentSpec extends Specification implements DomainUnitTest<OrderAdjustment> {
+    void 'OrderAdjustment.getIsInvoiceable() should return #isInvoiceable when isInvoiced: #isInvoiced and the status of order is: #orderStatus'() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getIsInvoiced() >> isInvoiced
+        }
+        orderAdjustment.order = Spy(Order)
+        orderAdjustment.order.status = orderStatus
+
+        expect:
+        orderAdjustment.isInvoiceable == isInvoiceable
+
+        where:
+        isInvoiced | orderStatus         || isInvoiceable
+        true       | OrderStatus.PENDING || false
+        false      | OrderStatus.PLACED  || true
+        true       | OrderStatus.PLACED  || false
+        false      | OrderStatus.PENDING || false
+    }
+
+    void 'OrderAdjustment.getIsEncumbered() should return: #isEncumbered when isInvoiced: #isInvoiced and hasPrepaymentInvoice: #hasPrepaymentInvoice'() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getIsInvoiced() >> isInvoiced
+            getHasPrepaymentInvoice() >> hasPrepaymentInvoice
+        }
+
+        expect:
+        orderAdjustment.isEncumbered == isEncumbered
+
+        where:
+        isInvoiced | hasPrepaymentInvoice || isEncumbered
+        true       | true                 || false
+        true       | false                || false
+        false      | true                 || true
+        false      | false                || false
+    }
+}
