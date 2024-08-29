9d68ab6b66aad3ca129a14d9ad61702be869e95e
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2024-08-29 14:43:49 +0200

    OBPIH-6642 Improve order of items on view Invoice page and Invoice tab on PO (#4809)

--- grails-app/domain/org/pih/warehouse/order/Order.groovy
+++ grails-app/domain/org/pih/warehouse/order/Order.groovy
@@ -412,11 +412,17 @@ class Order implements Serializable {
         return invoiceItems
     }
 
+    /**
+     * Function returning items so that prepayment invoices are at the bottom.
+     * Final invoices at the top: the newest submitted at the top, the oldest submitted at the bottom.
+     * Method used for displaying items on PO view page
+     */
     List<InvoiceItem> getSortedInvoiceItems() {
-        invoiceItems?.sort { a, b ->
+        return invoiceItems?.sort { a, b ->
+            b?.invoice?.dateCreated <=> a?.invoice?.dateCreated ?:
             a?.invoice?.invoiceNumber <=> b?.invoice?.invoiceNumber ?:
-                a?.product?.productCode <=> b?.product?.productCode ?:
-                    a.id <=> b.id
+                    a?.inverse <=> b?.inverse ?:
+                            a?.id <=> b?.id
         } ?: []
     }
 
