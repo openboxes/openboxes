859b1a4107e60a6a5a281ea8e4ea4d6f46be6967
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-10-02 14:09:07 +0200

    OBPIH-6693 Add test for derived status function on order adjustments

--- src/test/groovy/unit/org/pih/warehouse/order/OrderAdjustmentSpec.groovy
+++ src/test/groovy/unit/org/pih/warehouse/order/OrderAdjustmentSpec.groovy
@@ -1,6 +1,8 @@
 package unit.org.pih.warehouse.order
 
 import grails.testing.gorm.DomainUnitTest
+import org.pih.warehouse.invoice.InvoiceItem
+import org.pih.warehouse.invoice.OrderAdjustmentInvoiceStatus
 import org.pih.warehouse.order.Order
 import org.pih.warehouse.order.OrderAdjustment
 import org.pih.warehouse.order.OrderStatus
@@ -58,4 +60,98 @@ class OrderAdjustmentSpec extends Specification implements DomainUnitTest<OrderA
         true               | false      | 3                 || true
         true               | false      | 4                 || false
     }
+
+    void "derivedPaymentStatus should return NOT_INVOICED when postedPurchaseInvoiceItems is empty"() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getPostedPurchaseInvoiceItems() >> []
+            getTotalAdjustments() >> 100
+            getUnitPriceOnPostedInvoices() >> 100
+        }
+        orderAdjustment.canceled = false
+
+        expect:
+        orderAdjustment.derivedPaymentStatus == OrderAdjustmentInvoiceStatus.NOT_INVOICED
+    }
+
+    void "derivedPaymentStatus should return INVOICED when order is canceled or totalAdjustments is zero"() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getPostedPurchaseInvoiceItems() >> [
+                    Mock(InvoiceItem)
+            ]
+            getTotalAdjustments() >> totalAdjustments
+            getUnitPriceOnPostedInvoices() >> 100
+        }
+        orderAdjustment.canceled = canceled
+
+        expect: "The derived payment status is INVOICED"
+        orderAdjustment.derivedPaymentStatus == OrderAdjustmentInvoiceStatus.INVOICED
+
+        where:
+        totalAdjustments | canceled
+        0                | false
+        100              | true
+    }
+
+    void "derivedPaymentStatus should return NOT_INVOICED when unitPriceOnPostedInvoices is zero"() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getPostedPurchaseInvoiceItems() >> [
+                    Mock(InvoiceItem)
+            ]
+            getTotalAdjustments() >> 100
+            getUnitPriceOnPostedInvoices() >> 0
+        }
+        orderAdjustment.canceled = false
+
+        expect:
+        orderAdjustment.derivedPaymentStatus == OrderAdjustmentInvoiceStatus.NOT_INVOICED
+    }
+
+    void "derivedPaymentStatus should return INVOICED when fully invoiced"() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getPostedPurchaseInvoiceItems() >> [
+                    Mock(InvoiceItem)
+            ]
+            getTotalAdjustments() >> 100
+            getUnitPriceOnPostedInvoices() >> 100
+        }
+        orderAdjustment.canceled = false
+
+        expect:
+        orderAdjustment.derivedPaymentStatus == OrderAdjustmentInvoiceStatus.INVOICED
+    }
+
+    void "derivedPaymentStatus should return PARTIALLY_INVOICED when none of the other conditions apply"() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getPostedPurchaseInvoiceItems() >> [
+                    Mock(InvoiceItem)
+            ]
+            getTotalAdjustments() >> 100
+            getUnitPriceOnPostedInvoices() >> 50
+        }
+        orderAdjustment.canceled = false
+
+        expect:
+        orderAdjustment.derivedPaymentStatus == OrderAdjustmentInvoiceStatus.PARTIALLY_INVOICED
+    }
+
+    void "derivedPaymentStatus should return INVOICED when invoiced more than total adjustment"() {
+        given:
+        OrderAdjustment orderAdjustment = Spy(OrderAdjustment) {
+            getPostedPurchaseInvoiceItems() >> [
+                    Mock(InvoiceItem)
+            ]
+            getTotalAdjustments() >> 100
+            getUnitPriceOnPostedInvoices() >> 120
+        }
+        orderAdjustment.canceled = false
+
+        expect:
+        orderAdjustment.derivedPaymentStatus == OrderAdjustmentInvoiceStatus.INVOICED
+    }
+
 }
