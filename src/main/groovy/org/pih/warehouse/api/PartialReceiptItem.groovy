f47a61431ff139a7629d61508f572d1b1d35969a
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2024-10-18 15:37:32 +0200

    OBPIH-6226 Add PO UoM in the PO receipt workflow (#4899)

--- src/main/groovy/org/pih/warehouse/api/PartialReceiptItem.groovy
+++ src/main/groovy/org/pih/warehouse/api/PartialReceiptItem.groovy
@@ -71,7 +71,7 @@ class PartialReceiptItem {
     }
 
     String getDisplayUnitOfMeasure() {
-        return "${shipmentItem?.quantityPerUom?.toInteger()} ${shipmentItem?.unitOfMeasure}"
+        return "${quantityShipped} ${shipmentItem?.unitOfMeasure}"
     }
 
     Map toJson() {
