c87bcc8af4bfeeaa61893253e9ab43b2fc154701
Evan <evanwaterman@hotmail.com>
2024-11-13 13:24:35 -0800

    add comments and server setting for switching transaction name format

--- src/main/groovy/org/pih/warehouse/monitoring/GrailsHttpTransactionNameProvider.groovy
+++ src/main/groovy/org/pih/warehouse/monitoring/GrailsHttpTransactionNameProvider.groovy
@@ -1,23 +1,49 @@
 package org.pih.warehouse.monitoring
 
+import grails.util.Holders
 import io.sentry.protocol.TransactionNameSource
 import io.sentry.spring.tracing.TransactionNameProvider
 import javax.servlet.http.HttpServletRequest
 
 /**
  * Defines how Sentry transaction names for HTTP requests should be structured.
+ *
+ * If we ever add another HttpTransactionNameFormat, we should break this class up into one provider per format
+ * and move the switch statement into the SentryGrailsTracingFilter instead.
  */
 class GrailsHttpTransactionNameProvider implements TransactionNameProvider {
 
-    String provideTransactionName(HttpServletRequest var1) {
-        // If we wanted our logs to look more like HTTP Requests, we could have done the following instead, but
-        // we decided that "invoiceApi/list" is easier to debug than "GET /openboxes/api/invoices".
-        //return "${var1.method} ${var1.requestURI}"
-        return "${var1.getAttribute("org.grails.CONTROLLER_NAME_ATTRIBUTE")}/${var1.getAttribute("org.grails.ACTION_NAME_ATTRIBUTE")}"
+    String provideTransactionName(HttpServletRequest request) {
+        HttpTransactionNameFormat nameFormat = getTransactionNameFormat()
+        switch (nameFormat) {
+            // If we want HTTP Request style transaction names, such as "GET /openboxes/api/invoices"
+            case HttpTransactionNameFormat.URI:
+                return "${request.method} ${request.requestURI}"
+
+            // Will result in Grails Controller based transaction names, such as "invoiceApi/list"
+            case HttpTransactionNameFormat.CONTROLLER_METHOD:
+                return "${request.getAttribute("org.grails.CONTROLLER_NAME_ATTRIBUTE")}/${request.getAttribute("org.grails.ACTION_NAME_ATTRIBUTE")}"
+
+            default:
+                throw new IllegalArgumentException("Un-supported transaction name format: ${nameFormat}")
+        }
     }
 
     TransactionNameSource provideTransactionSource() {
-        // Do what SpringMvcTransactionNameProvider does, which is the provider that Sentry normally uses.
-        return TransactionNameSource.ROUTE
+        HttpTransactionNameFormat nameFormat = getTransactionNameFormat()
+        switch (nameFormat) {
+            case HttpTransactionNameFormat.URI:
+                return TransactionNameSource.ROUTE
+
+            case HttpTransactionNameFormat.CONTROLLER_METHOD:
+                return TransactionNameSource.COMPONENT
+
+            default:
+                throw new IllegalArgumentException("Un-supported transaction name format: ${nameFormat}")
+        }
+    }
+
+    private static HttpTransactionNameFormat getTransactionNameFormat() {
+        return HttpTransactionNameFormat.valueOf(Holders.config.getProperty('sentry.httpTransactionNameFormat'))
     }
 }
--- src/main/groovy/org/pih/warehouse/monitoring/SentryGrailsTracingFilter.groovy
+++ src/main/groovy/org/pih/warehouse/monitoring/SentryGrailsTracingFilter.groovy
@@ -7,7 +7,7 @@ import io.sentry.spring.tracing.SentryTracingFilter
 /**
  * Creates HTTP request transactions for Sentry performance tracing.
  *
- * Note that this bean is registered in grails-app/conf/spring/resource.groovy
+ * Note that this bean is registered in grails-app/conf/spring/resources.groovy
  *
  * Because Grails and Spring MVC have different approaches for mapping URIs to controllers, we need to generate
  * the transaction name ourselves for traces to display properly in Sentry.
