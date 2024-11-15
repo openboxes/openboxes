01c7faee33fa06a522337fe6abbb79f419024cfa
Evan <evanwaterman@hotmail.com>
2024-11-15 10:27:13 -0800

    use SENTRY_DSN_BACKEND when setting up sentry

--- grails-app/init/org/pih/warehouse/Application.groovy
+++ grails-app/init/org/pih/warehouse/Application.groovy
@@ -4,14 +4,13 @@ import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.boot.web.servlet.ServletComponentScan
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 
-// Adding the "ComponentScan" here makes the app search for Spring components (not just Grails ones), which
-// allows us to define beans outside of the "/grails-app" folder as long as those beans are still under the
-// "org.pih.warehouse" package. This is useful for things like defining Spring Components under the "/src" folder.
+// Searches for Spring components (not just Grails ones), which allows us to define beans outside of the "/grails-app"
+// folder as long as those beans are still under the "org.pih.warehouse" package. This is useful for things like
+// defining Spring Components under the "/src" folder.
 // https://tedvinke.wordpress.com/2017/04/04/grails-anti-pattern-everything-is-a-service/
-@Configuration
 @ComponentScan
+// Searches for components tagged with the "WebFilter", "WebListener", and "WebServlet" Servlet 3.0 annotations.
 @ServletComponentScan
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
