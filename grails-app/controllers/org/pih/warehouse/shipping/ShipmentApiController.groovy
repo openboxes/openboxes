ec5a8c0d7032d42bede28caa335eaa90441675b7
Jan <jdolkowski@soldevelo.com>
2024-12-17 15:55:36 +0100

    WMS-3 Add ShipmentApiController (#4980)

---
+++ grails-app/controllers/org/pih/warehouse/shipping/ShipmentApiController.groovy
@@ -0,0 +1,27 @@
+package org.pih.warehouse.shipping
+
+import grails.converters.JSON
+import org.pih.warehouse.api.BaseDomainApiController
+import org.pih.warehouse.core.Location
+import org.pih.warehouse.requisition.RequisitionStatus
+
+class ShipmentApiController extends BaseDomainApiController {
+
+    def shipmentService
+
+    def list = {
+        // MOBILE
+        Location origin = params.origin ? Location.get(params?.origin) : null
+        Location destination = params.destination ? Location.get(params?.destination) : null
+        ShipmentStatusCode shipmentStatusCode = params.shipmentStatusCode ? params.shipmentStatusCode as ShipmentStatusCode : null
+        List<RequisitionStatus> requisitionStatuses = params.list("requisitionStatus").collect { it as RequisitionStatus }
+        List<Shipment> shipments = shipmentService.getShipmentsByLocationAndRequisitionStatuses(origin, destination, shipmentStatusCode, requisitionStatuses)
+        render ([data: shipments] as JSON)
+    }
+
+    def read = {
+        // MOBILE
+        Shipment shipment = shipmentService.getShipment(params.id)
+        render ([data:shipment] as JSON)
+    }
+}
