0cd4ff7889509a953f2f2c487db6acfb4023d469
olewandowski1 <olewandowski@soldevelo.com>
2024-12-18 17:06:54 +0100

    WMS-15 use toJson method on Shipment, add attributes used by mobile

--- grails-app/init/org/pih/warehouse/BootStrap.groovy
+++ grails-app/init/org/pih/warehouse/BootStrap.groovy
@@ -466,35 +466,8 @@ class BootStrap {
             ]
         }
 
-
         JSON.registerObjectMarshaller(Shipment) { Shipment shipment ->
-            def containerList = []
-            def shipmentItemsByContainer = shipment?.shipmentItems?.groupBy { it.container }
-            shipmentItemsByContainer.each { container, shipmentItems ->
-                containerList << [id: container?.id, name: container?.name, type: container?.containerType?.name, shipmentItems: shipmentItems]
-            }
-            return [
-                id                  : shipment.id,
-                name                : shipment.name,
-                status              : shipment?.status?.code?.name(),
-                origin              : [
-                    id  : shipment.origin?.id,
-                    name: shipment?.origin?.name,
-                    type: shipment?.origin?.locationType?.locationTypeCode?.name()
-                ],
-                destination         : [
-                    id  : shipment?.destination?.id,
-                    name: shipment?.destination?.name,
-                    type: shipment?.destination?.locationType?.locationTypeCode?.name()
-
-                ],
-                expectedShippingDate: shipment.expectedShippingDate?.format("MM/dd/yyyy HH:mm XXX"),
-                actualShippingDate  : shipment.actualShippingDate?.format("MM/dd/yyyy HH:mm XXX"),
-                expectedDeliveryDate: shipment.expectedDeliveryDate?.format("MM/dd/yyyy HH:mm XXX"),
-                actualDeliveryDate  : shipment.actualDeliveryDate?.format("MM/dd/yyyy HH:mm XXX"),
-                shipmentItems       : shipment.shipmentItems,
-                containers          : containerList
-            ]
+            return shipment?.toJson()
         }
 
         JSON.registerObjectMarshaller(ShipmentItem) { ShipmentItem shipmentItem ->
