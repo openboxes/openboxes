2bf3b2f3459b4d3e0b5eaac61b462e09aaed3926
kchelstowski <kchelstowski@soldevelo.com>
2025-01-08 10:51:59 +0100

    OBPIH-6903 Rely on the facilityId provided in the URL instead of providing it in the payload

--- src/main/groovy/org/pih/warehouse/inventory/CycleCountRequestCommand.groovy
+++ src/main/groovy/org/pih/warehouse/inventory/CycleCountRequestCommand.groovy
@@ -3,6 +3,7 @@ package org.pih.warehouse.inventory
 import grails.validation.Validateable
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.product.Product
+import org.springframework.web.context.request.RequestContextHolder
 
 class CycleCountRequestCommand implements Validateable {
 
@@ -12,10 +13,15 @@ class CycleCountRequestCommand implements Validateable {
 
     Boolean blindCount
 
+    def beforeValidate() {
+        String locationId = RequestContextHolder.getRequestAttributes().params?.facilityId
+        facility = Location.findById(locationId)
+    }
+
     static constraints = {
         product(nullable: true, validator: { Product product, CycleCountRequestCommand obj ->
             // TODO: Most probably, this will have to be modified not to include completed cycle counts
-            CycleCountRequest cycleCountRequest = CycleCountRequest.findByProductAndFacility(product, obj.facility)
+            CycleCountRequest cycleCountRequest = CycleCountRequest.findByProductAndFacilityAndStatusNotInList(product, obj.facility, [CycleCountRequestStatus.COMPLETED, CycleCountRequestStatus.CANCELED])
             if (cycleCountRequest) {
                 return ['duplicateExists', product.productCode]
             }
