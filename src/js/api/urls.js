90b03914e1051004c95856f760881842872ff18e
Alan Nadolny <alannadolnyy@gmail.com>
2025-01-14 14:18:04 +0100

    OBPIH-6871 Add cycle count urls

--- src/js/api/urls.js
+++ src/js/api/urls.js
@@ -177,3 +177,7 @@ export const HANDLING_REQUIREMENTS_OPTIONS = `${API}/handlingRequirementsOptions
 
 // INTERNAL LOCATIONS
 export const INTERNAL_LOCATIONS = `${API}/internalLocations`;
+
+// CYCLE COUNT
+export const CYCLE_COUNT = (locationId) => `${API}/facilities/${locationId}`;
+export const CYCLE_COUNT_CANDIDATES = (locationId) => `${CYCLE_COUNT(locationId)}/cycle-count-candidates`;
