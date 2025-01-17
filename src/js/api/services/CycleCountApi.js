30e4fa5bd3c3f491dfde81be48eb8a98053b706f
Alan Nadolny <alannadolnyy@gmail.com>
2025-01-17 13:10:30 +0100

    OBPIH-6848 Add sending request for cycle count request creation

---
+++ src/js/api/services/CycleCountApi.js
@@ -0,0 +1,6 @@
+import { CYCLE_COUNT_REQUESTS } from 'api/urls';
+import apiClient from 'utils/apiClient';
+
+export default {
+  createRequest: (payload, locationId) => apiClient.post(CYCLE_COUNT_REQUESTS(locationId), payload),
+};
