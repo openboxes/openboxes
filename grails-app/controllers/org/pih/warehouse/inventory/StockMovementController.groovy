467d1895d981f4fe8c5077efaad5d062dfa50247
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-01-21 15:34:57 +0100

    OBPIH 6924 Create a wrapper component for the inbound workflow (#4998)

--- grails-app/controllers/org/pih/warehouse/inventory/StockMovementController.groovy
+++ grails-app/controllers/org/pih/warehouse/inventory/StockMovementController.groovy
@@ -78,6 +78,10 @@ class StockMovementController {
         render(view: "/common/react", params: params)
     }
 
+    def createInboundV2() {
+        render(view: "/common/react", params: params)
+    }
+
     def createRequest() {
         render(view: "/common/react", params: params)
     }
---
+++ src/js/components/stock-movement-wizard/inboundV2/sections/InboundV2AddItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Section from 'components/Layout/v2/Section';
+
+const InboundV2AddItems = () => {
+  console.log('InboundV2AddItems');
+  return (
+    <Section>
+      InboundV2AddItems
+    </Section>
+  );
+};
+
+export default InboundV2AddItems;
