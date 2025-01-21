f20d9960e4accdb8d27bc9c7001b3b64c7252028
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-01-21 15:34:57 +0100

    OBPIH 6924 Create a wrapper component for the inbound workflow (#4998)

---
+++ src/js/consts/MockInboundV2Title.js
@@ -0,0 +1,9 @@
+const mockInboundV2Title = [
+  { text: '445STA', color: '#000000', delimeter: ' - ' },
+  { text: 'Metropolitan Medical Services of', color: '#004d40', delimeter: ' to ' },
+  { text: 'Lisungwi Warehouse', color: '#01579b', delimeter: ', ' },
+  { text: '01/13/2025', color: '#4a148c', delimeter: ', ' },
+  { text: 'dsds', color: '#770838', delimeter: '' },
+];
+
+export default mockInboundV2Title;
