0106b6d21f6ddc3dff0de34c543ad007a41553c9
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-01-21 15:34:57 +0100

    OBPIH 6924 Create a wrapper component for the inbound workflow (#4998)

---
+++ src/js/components/stock-movement-wizard/inboundV2/sections/InboundV2Create.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Section from 'components/Layout/v2/Section';
+
+const InboundV2Create = () => {
+  console.log('InboundV2Create');
+  return (
+    <Section>
+      InboundV2Create
+    </Section>
+  );
+};
+
+export default InboundV2Create;
