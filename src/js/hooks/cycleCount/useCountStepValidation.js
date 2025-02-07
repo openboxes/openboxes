489f022b0298a51309b9bc009f4f45a7b8159de7
Alan Nadolny <alannadolnyy@gmail.com>
2025-02-07 14:32:10 +0100

    OBPIH-6894 Add hook for handling validation separately

---
+++ src/js/hooks/cycleCount/useCountStepValidation.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+import _ from 'lodash';
+import { z } from 'zod';
+
+import useTranslate from 'hooks/useTranslate';
+
+const useCountStepValidation = ({ tableData }) => {
+  const [validationErrors, setValidationErrors] = useState({});
+
+  const translate = useTranslate();
+
+  const checkLotNumberUniqueness = (data) => {
+    if (!data?.inventory?.lotNumber) {
+      return true;
+    }
+    const product = tableData.current.find((row) => row?.id === data.id)?.product?.productCode;
+    const table = _.groupBy(tableData.current, 'product.productCode')[product];
+    const groupedLotNumbers = _.groupBy(table, 'inventoryItem.lotNumber');
+    return groupedLotNumbers[data.inventoryItem?.lotNumber].length === 1;
+  };
+
+  const rowValidationSchema = z.object({
+    id: z
+      .string(),
+    quantityCounted: z
+      .number({
+        required_error: translate('react.cycleCount.requiredQuantityCounted', 'Quantity counted is required'),
+        invalid_type_error: translate('react.cycleCount.requiredQuantityCounted', 'Quantity counted is required'),
+      })
+      .gte(0),
+    inventoryItem: z.object({
+      expirationDate: z
+        .string()
+        .optional(),
+      lotNumber: z
+        .string()
+        .optional(),
+    }).optional(),
+    internalLocation: z.object({
+      id: z.string(),
+      name: z.string(),
+      label: z.string().optional(),
+    }).optional(),
+  }).refine((data) => data?.inventoryItem?.lotNumber || !data?.inventoryItem?.expirationDate, {
+    path: ['inventoryItem.lotNumber'],
+    message: translate('react.cycleCount.requiredLotNumber', 'Lot number is required'),
+  }).refine(checkLotNumberUniqueness, {
+    path: ['inventoryItem.lotNumber'],
+    message: translate('react.cycleCount.uniqueLotNumber', 'Lot number should be unique'),
+  });
+
+  const rowsValidationSchema = z.array(rowValidationSchema);
+
+  const triggerValidation = () => {
+    const errors = tableData.current.reduce((acc, cycleCount) => {
+      const parsedValidation = rowsValidationSchema.safeParse(cycleCount.cycleCountItems);
+      return {
+        ...acc,
+        [cycleCount.id]: {
+          errors: parsedValidation?.error?.format(),
+          success: parsedValidation.success,
+        },
+      };
+    }, {});
+
+    setValidationErrors(errors);
+    return _.every(Object.values(errors), (val) => val.success);
+  };
+
+  return {
+    validationErrors,
+    setValidationErrors,
+    triggerValidation,
+    rowValidationSchema,
+    rowsValidationSchema,
+  };
+};
+
+export default useCountStepValidation;
