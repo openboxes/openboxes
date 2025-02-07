428f004edbe60d39d779c637f97c017ccbc8ff54
Alan Nadolny <alannadolnyy@gmail.com>
2025-02-07 14:31:25 +0100

    OBPIH-6894 Remove unused files

--- src/js/hooks/useForceUpdate.js
+++
@@ -1,9 +0,0 @@
-import { useReducer } from 'react';
-
-const useForceUpdate = () => {
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
-
-  return forceUpdate;
-};
-
-export default useForceUpdate;
