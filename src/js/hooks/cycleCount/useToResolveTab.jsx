f9573989d8e1618430e805dcd6d45df69cb1240a
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2025-02-11 12:19:47 +0100

    OBPIH-6940 Adjust display of product name in cycle count tables to 2 rows only (to resolve tab) (#5036)

--- src/js/hooks/cycleCount/useToResolveTab.jsx
+++ src/js/hooks/cycleCount/useToResolveTab.jsx
@@ -158,10 +158,14 @@ const useToResolveTab = ({
       ),
       cell: ({ getValue, row }) => (
         <TableCell
+          tooltip
+          tooltipLabel={getValue()}
           link={INVENTORY_ITEM_URL.showStockCard(row.original.product.id)}
           className="rt-td multiline-cell"
         >
-          {getValue()}
+          <div className="limit-lines-2">
+            {getValue()}
+          </div>
         </TableCell>
       ),
     }),
