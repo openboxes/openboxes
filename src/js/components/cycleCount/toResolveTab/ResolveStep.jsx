cd0d054f4d73ddf0ce322d6fd1ed3e0ec230b6f9
Alan Nadolny <alannadolnyy@gmail.com>
2025-02-13 15:20:12 +0100

    OBPIH-6974 Add resolve step & header

---
+++ src/js/components/cycleCount/toResolveTab/ResolveStep.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import ResolveStepHeader from 'components/cycleCount/toResolveTab/ResolveStepHeader';
+import ResolveStepTable from 'components/cycleCount/toResolveTab/ResolveStepTable';
+import useResolveStep from 'hooks/cycleCount/useResolveStep';
+import PageWrapper from 'wrappers/PageWrapper';
+
+import 'components/cycleCount/cycleCount.scss';
+
+const ResolveStep = () => {
+  const {
+    tableData,
+    printRecountForm,
+    next,
+    tableMeta,
+    addEmptyRow,
+    removeRow,
+    assignRecountedBy,
+    getRecountedDate,
+    setRecountedDate,
+  } = useResolveStep();
+
+  return (
+    <PageWrapper>
+      <ResolveStepHeader printRecountForm={printRecountForm} next={next} />
+      {tableData
+        .map(({ cycleCountItems, id }) => (
+          <ResolveStepTable
+            key={id}
+            id={id}
+            product={cycleCountItems[0]?.product}
+            dateCounted={cycleCountItems[0]?.dateCounted}
+            dateRecounted={getRecountedDate(id)}
+            tableData={cycleCountItems}
+            tableMeta={tableMeta}
+            addEmptyRow={addEmptyRow}
+            removeRow={removeRow}
+            setRecountedDate={setRecountedDate(id)}
+            assignRecountedBy={assignRecountedBy}
+          />
+        ))}
+    </PageWrapper>
+  );
+};
+
+export default ResolveStep;
