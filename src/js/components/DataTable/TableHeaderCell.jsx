611a5c9c8f78486c0bead479537e5ceffeeaec2b
Sebastian Lib <sebastianlib04@gmail.com>
2025-02-26 16:31:01 +0100

    OBPIH-6960 fix eslint errors

--- src/js/components/DataTable/TableHeaderCell.jsx
+++ src/js/components/DataTable/TableHeaderCell.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import { RiArrowDownSFill, RiArrowUpSFill } from 'react-icons/ri';
+
 import CustomTooltip from 'wrappers/CustomTooltip';
 
 const TableHeaderCell = ({
