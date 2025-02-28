23643ae8fe7ede56493b9c7e1544b49683d2621a
jmiranda <jcm62@columbia.edu>
2025-02-27 22:47:46 -0600

    OBPIH-6939 Improved performance of views used to calculate last count date; restored cycle count status in cycle count session view

--- grails-app/migrations/views/product-count-history.sql
+++ grails-app/migrations/views/product-count-history.sql
@@ -1,28 +1,19 @@
 create or replace view product_count_history as
 (
 select
-    facility_id,
+    inventory_id,
     product_id,
-    max(date_counted) as date_counted,
-    max(date_received) as date_received
-from (select facility_id,
+    date_counted as date_counted
+from (
+select inventory_id,
              product_id,
-             date_counted as date_counted,
-             null         as date_received
+             date_counted as date_counted
       from product_cycle_count_history
       union
-      select facility_id,
+      select inventory_id,
              product_id,
-             date_counted,
-             null
+             date_counted
       from product_physical_count_history
---          union
---          select facility_id,
---                product_id,
---                null,
---                date_received
---          from product_receipt_history
      ) as inventory_level_summary_unions
-    group by facility_id, product_id
     )
 ;
--- grails-app/migrations/views/product-cycle-count-history.sql
+++ grails-app/migrations/views/product-cycle-count-history.sql
@@ -2,14 +2,12 @@
 -- tables. However, this can be renamed if necessary.
 create or replace view product_cycle_count_history as (
     select
-        location.id as facility_id,
-        inventory_item.product_id,
-        -- FIXME need to set this to something that's not an auditing field
-        max(cycle_count.date_created) as date_counted
+        location.inventory_id as inventory_id,
+        cycle_count_item.product_id as product_id,
+        max(cycle_count_item.date_counted) as date_counted
     from cycle_count
-    join cycle_count_item on cycle_count_item.cycle_count_id = cycle_count.id
-    join inventory_item on inventory_item.id = cycle_count_item.inventory_item_id = inventory_item.id
-    join location on location.inventory_id = cycle_count.facility_id
+        join cycle_count_request on cycle_count.id = cycle_count_request.cycle_count_id
+        join cycle_count_item on cycle_count_item.cycle_count_id = cycle_count.id
+        join location on location.id = cycle_count.facility_id
     where cycle_count.status = 'COMPLETED'
-    group by location.id, inventory_item.product_id
 );
