d0a87fc5b76253a1c432a20d5930c47b68541832
jmiranda <jcm62@columbia.edu>
2025-03-11 15:58:02 -0500

    OBPIH-7020 Removed temporary code used to materialize a view used to test performance

--- grails-app/migrations/views/cycle-count-frequency.sql
+++ grails-app/migrations/views/cycle-count-frequency.sql
@@ -11,20 +11,3 @@ create or replace view cycle_count_frequency as (
       select 'DEFAULT' as abc_class, NULL as facility_id, 365 as frequency
   ) cycle_count_frequency
 );
--- FIXME Move this to a proper database migration if it actually helps with performance.
-# DROP VIEW IF EXISTS cycle_count_frequency;
-# DROP TABLE IF EXISTS cycle_count_frequency;
-# CREATE TABLE cycle_count_frequency (
-#     id INT AUTO_INCREMENT PRIMARY KEY,
-#     abc_class VARCHAR(50),  -- e.g., 'A', 'B', 'C'
-#     frequency INT NOT NULL,        -- Count frequency in days
-#     INDEX abc_class_idx (abc_class)
-# );
-# INSERT INTO cycle_count_frequency (abc_class, frequency) VALUES
-#   ('A', 90),
-#   ('B', 180),
-#   ('C', 365),
-#   ('DEFAULT', 365);
-#
-# ALTER TABLE cycle_count_frequency ADD INDEX idx_cycle_count_frequency (abc_class, frequency);
-
