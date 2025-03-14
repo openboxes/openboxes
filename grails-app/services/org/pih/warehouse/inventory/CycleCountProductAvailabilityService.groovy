adf0ae75dc67cf7c836052ef13901d1b3c747aee
kchelstowski <kchelstowski@soldevelo.com>
2025-03-14 23:15:05 +0100

    OBPIH-7097 Fix after review

--- grails-app/services/org/pih/warehouse/inventory/CycleCountProductAvailabilityService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/CycleCountProductAvailabilityService.groovy
@@ -7,7 +7,6 @@ import org.pih.warehouse.api.AvailableItem
 import org.pih.warehouse.auth.AuthService
 import org.pih.warehouse.core.Location
 import org.pih.warehouse.core.Person
-import org.pih.warehouse.core.User
 import org.pih.warehouse.product.Product
 
 /**
@@ -118,11 +117,11 @@ class CycleCountProductAvailabilityService {
         // Find for an original item (countIndex 0) to see, if the original item is custom.
         // On the counting step we eventually create a custom row that on recount has custom flag as false,
         // to prevent from deleting such a row. (OBPIH-7097)
-        CycleCountItem correspondingOriginalItem = cycleCountItem.cycleCount.cycleCountItems.find {
-            it.inventoryItem == cycleCountItem.inventoryItem &&
-                    it.location == cycleCountItem.location &&
-                    it.countIndex == 0
-        }
+        CycleCountItem correspondingOriginalItem = cycleCountItem.cycleCount.getCycleCountItem(
+                cycleCountItem.product,
+                cycleCountItem.location,
+                cycleCountItem.inventoryItem,
+                0)
         // Otherwise, it does not exist, so we need to remove it from the count (unless it's a custom added row).
         // Custom added items should not get removed from the count, even if there's no available item. We trust
         // the user to manage items they manually added.
