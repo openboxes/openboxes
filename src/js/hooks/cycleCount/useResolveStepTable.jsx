2f45fbc09a95299cfcabcfe3f47d1aa17feaffb6
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-03-14 20:12:54 +0100

    OBPIH-7054 Expand "Root Cause" selector for better visibility (#5111)

--- src/js/hooks/cycleCount/useResolveStepTable.jsx
+++ src/js/hooks/cycleCount/useResolveStepTable.jsx
@@ -180,16 +180,22 @@ const useResolveStepTable = ({
           cycleCountColumn.ROOT_CAUSE,
           cycleCountColumn.COMMENT,
         ].includes(id);
+      const showStaticTooltip = [cycleCountColumn.ROOT_CAUSE, cycleCountColumn.COMMENT]
+        .includes(id);
       // We shouldn't allow users edit fetched data (quantityRecounted, rootCause and comment
       // field are editable)
       if (isFieldEditable || !isStepEditable) {
         return (
           <CustomTooltip
             content={getValueToDisplay(id, value)}
-            show={id === cycleCountColumn.COMMENT}
+            show={showStaticTooltip}
           >
-            <TableCell className="static-cell-count-step align-items-center limit-lines-3 text-break resolve-table-limit-lines">
-              {getValueToDisplay(id, value)}
+            <TableCell
+              className="static-cell-count-step align-items-center resolve-table-limit-lines"
+            >
+              <div className={showStaticTooltip ? 'limit-lines-1' : 'limit-lines-3 text-break'}>
+                {getValueToDisplay(id, value)}
+              </div>
             </TableCell>
           </CustomTooltip>
         );
@@ -277,15 +283,21 @@ const useResolveStepTable = ({
         addNewRow: () => addEmptyRow(productId, cycleCountId),
         isNewRow,
       });
-
+      const isAutoWidth = [cycleCountColumn.ROOT_CAUSE, cycleCountColumn.COMMENT].includes(id);
+      const showTooltip = [cycleCountColumn.ROOT_CAUSE].includes(id);
       return (
-        <TableCell className="rt-td rt-td-count-step pb-0">
+        <TableCell
+          className="rt-td rt-td-count-step pb-0"
+          customTooltip={showTooltip && getValueToDisplay(id, value)}
+          tooltipClassname="w-100"
+          tooltipLabel={getValueToDisplay(id, value)}
+        >
           <Component
             type={type}
             value={value}
             onChange={onChange}
             onBlur={onBlur}
-            className={`w-75 m-1 hide-arrows ${error && 'border border-danger input-has-error'}`}
+            className={`${isAutoWidth ? 'w-auto' : 'w-75'} m-1 hide-arrows ${error && 'border border-danger input-has-error'}`}
             showErrorBorder={error}
             onChangeRaw={onChangeRaw}
             hideErrorMessageWrapper
@@ -447,7 +459,7 @@ const useResolveStepTable = ({
         </TableHeaderCell>
       ), []),
       meta: {
-        flexWidth: 120,
+        flexWidth: 160,
       },
     }),
     columnHelper.accessor(cycleCountColumn.COMMENT, {
@@ -457,7 +469,7 @@ const useResolveStepTable = ({
         </TableHeaderCell>
       ), []),
       meta: {
-        flexWidth: 228,
+        flexWidth: 160,
         getCellContext: () => ({
           className: 'overflow-hidden',
         }),
@@ -491,7 +503,7 @@ const useResolveStepTable = ({
       ), []),
       meta: {
         flexWidth: 50,
-        hide: !isStepEditable,
+        hide: !tableData.some((row) => row.id?.includes('newRow')) || !isStepEditable,
       },
     }),
   ];
