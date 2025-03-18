166b55da28e7683e0e1ce9b97907f39ed34880e1
Sebastian Lib <sebastianlib04@gmail.com>
2025-03-18 09:56:14 +0100

    OBPIH-7038 Remove clearCycleCountData

--- grails-app/init/org/pih/warehouse/BootStrap.groovy
+++ grails-app/init/org/pih/warehouse/BootStrap.groovy
@@ -91,16 +91,6 @@ import org.pih.warehouse.shipping.Shipment
 import org.pih.warehouse.shipping.ShipmentItem
 import org.pih.warehouse.shipping.ShipmentType
 
-import grails.gorm.transactions.Transactional
-
-import org.pih.warehouse.inventory.CycleCountRequest
-import org.pih.warehouse.inventory.CycleCount
-import org.pih.warehouse.inventory.CycleCountItem
-import org.pih.warehouse.inventory.Transaction
-import org.pih.warehouse.inventory.TransactionEntry
-
-
-
 class BootStrap {
 
     UploadService uploadService
@@ -122,22 +112,6 @@ class BootStrap {
         uploadService.findOrCreateUploadsDirectory()
 
         refreshAnalyticsData()
-        clearCycleCountData()
-    }
-
-    @Transactional
-    void clearCycleCountData() {
-        List<Transaction> cycleCountTransactions = Transaction.createCriteria().list { isNotNull("cycleCount") }
-        List<TransactionEntry> cycleCountTransactionEntries = cycleCountTransactions.transactionEntries.flatten()
-
-        TransactionEntry.deleteAll(cycleCountTransactionEntries)
-        Transaction.deleteAll(cycleCountTransactions)
-
-        CycleCountItem.findAll().each { it.delete() }
-
-        CycleCountRequest.findAll().each { it.delete() }
-
-        CycleCount.findAll().each { it.delete() }
     }
 
     void refreshAnalyticsData() {
