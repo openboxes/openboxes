aab5dd47d9aff32cfcbc54f5e41d3b37b5cf21ad
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2025-03-20 15:57:02 +0100

    OBPIH-7100 Fix and improve deleting new lines on recount form  (#5145)

--- src/js/hooks/cycleCount/useResolveStepTable.jsx
+++ src/js/hooks/cycleCount/useResolveStepTable.jsx
@@ -536,7 +536,7 @@ const useResolveStepTable = ({
       ), []),
       meta: {
         flexWidth: 50,
-        hide: !tableData.some((row) => row.id?.includes('newRow')) || !isStepEditable,
+        hide: !tableData.some((row) => row.id?.includes('newRow') || row.custom) || !isStepEditable,
       },
     }),
   ];
