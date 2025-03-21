5196199eb81f1b4eb010765617bab998bd84f749
Sebastian Lib <sebastianlib04@gmail.com>
2025-03-21 08:48:50 +0100

    OBPIH-7099 Added disabled state to prevent multiple clicks

--- src/js/components/cycleCount/ConfirmStepHeader.jsx
+++ src/js/components/cycleCount/ConfirmStepHeader.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 
 import Button from 'components/form-elements/Button';
 
-const ConfirmStepHeader = ({ back, save }) => (
+const ConfirmStepHeader = ({ back, save }) => {
+  const [disabledSave, setDisabledSave] = useState(false);
 
-  <div className="confirm-step-header">
-    <Button
-      onClick={back}
-      label="react.default.button.back.label"
-      defaultLabel="Back"
-      variant="primary-outline"
-    />
-    <Button
-      onClick={save}
-      label="react.default.button.save.label"
-      defaultLabel="Save"
-      variant="primary"
-    />
-  </div>
-);
+  const handleSaveClick = () => {
+    setDisabledSave(true);
+    save();
+  };
+
+  return (
+    <div className="confirm-step-header">
+      <Button
+        onClick={back}
+        label="react.default.button.back.label"
+        defaultLabel="Back"
+        variant="primary-outline"
+      />
+      <Button
+        onClick={handleSaveClick}
+        label="react.default.button.save.label"
+        defaultLabel="Save"
+        variant="primary"
+        disabled={disabledSave}
+      />
+    </div>
+  );
+};
 
 export default ConfirmStepHeader;
 
