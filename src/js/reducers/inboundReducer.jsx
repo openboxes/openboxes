3e8b87c4a0ea7cb96db1f868f9585e07958f0aaa
Sebastian Lib <sebastianlib04@gmail.com>
2025-04-08 09:50:36 +0200

    OBPIH-7175 Add reducer improvements

--- src/js/reducers/inboundReducer.jsx
+++ src/js/reducers/inboundReducer.jsx
@@ -8,11 +8,14 @@ const initialState = {
 export default function inboundReducer(state = initialState, action) {
   switch (action.type) {
     case UPDATE_INBOUND_HEADER:
+      if (!action.payload) return state;
+
       return {
         ...state,
-        headerInfo: action.payload.headerInfo || state.headerInfo,
-        headerStatus: action.payload.headerStatus || state.headerStatus,
+        headerInfo: action.payload.headerInfo ?? state.headerInfo,
+        headerStatus: action.payload.headerStatus ?? state.headerStatus,
       };
+
     default:
       return state;
   }
