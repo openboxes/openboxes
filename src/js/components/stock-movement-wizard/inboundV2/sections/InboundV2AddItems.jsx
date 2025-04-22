b4015aa14d1cd8d5cf5a48942f75269f66b14a36
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-04-22 16:28:24 +0200

    OBPIH-7150 Arrow navigation in inbound add items pages (#5184)

--- src/js/components/stock-movement-wizard/inboundV2/sections/InboundV2AddItems.jsx
+++ src/js/components/stock-movement-wizard/inboundV2/sections/InboundV2AddItems.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import { useFieldArray, useWatch } from 'react-hook-form';
-import { BiExit, BiSave, IoCloseCircleOutline } from 'react-icons/all';
+import {
+  BiDownload,
+  BiExit,
+  BiRefresh,
+  BiSave,
+  BiUpload,
+  IoCloseCircleOutline,
+} from 'react-icons/all';
 import { RiAddLine } from 'react-icons/ri';
 
 import DataTable from 'components/DataTable/v2/DataTable';
@@ -25,6 +32,11 @@ const InboundV2AddItems = ({
   removeAll,
   saveAndExit,
   previousPage,
+  refreshFocusCounter,
+  resetFocus,
+  refresh,
+  importTemplate,
+  exportTemplate,
 }) => {
   const hasErrors = !!Object.keys(errors).length;
   const {
@@ -59,6 +71,8 @@ const InboundV2AddItems = ({
     updateTotalCount,
     removeAll,
     currentLineItems,
+    append,
+    refreshFocusCounter,
   });
 
   const defaultTableRow = {
@@ -82,6 +96,7 @@ const InboundV2AddItems = ({
     };
     append(newRow);
     resetScrollbar();
+    resetFocus();
   };
 
   return (
@@ -97,6 +112,41 @@ const InboundV2AddItems = ({
             />
 
             <div className="buttons-container">
+              <Button
+                onClick={() => {
+                  const fileInput = document.getElementById('csvInput');
+                  // Resetting fileInput.value to null ensures the onChange
+                  // event triggers even if the same file is selected again,
+                  // as browsers don't fire onChange for unchanged file inputs
+                  fileInput.value = null;
+                  fileInput?.click();
+                }}
+                StartIcon={<BiDownload className="icon" />}
+                defaultLabel="Import template"
+                label="react.default.button.importTemplate.label"
+                variant="primary-outline"
+              />
+              <input
+                id="csvInput"
+                type="file"
+                className="d-none"
+                onChange={importTemplate}
+                accept=".csv"
+              />
+              <Button
+                onClick={exportTemplate}
+                StartIcon={<BiUpload className="icon" />}
+                defaultLabel="Export template"
+                label="react.default.button.exportTemplate.label"
+                variant="primary-outline"
+              />
+              <Button
+                onClick={refresh}
+                StartIcon={<BiRefresh className="icon" />}
+                defaultLabel="Reload"
+                label="react.default.button.refresh.label"
+                variant="primary-outline"
+              />
               <Button
                 onClick={save}
                 StartIcon={<BiSave className="icon" />}
@@ -125,7 +175,6 @@ const InboundV2AddItems = ({
           <DataTable
             columns={columns}
             data={fields}
-            defaultPageSize={4}
             loading={loading}
             disablePagination
             emptyTableMessage={{
@@ -173,4 +222,9 @@ InboundV2AddItems.propTypes = {
   removeAll: PropTypes.func.isRequired,
   saveAndExit: PropTypes.func.isRequired,
   previousPage: PropTypes.func.isRequired,
+  refresh: PropTypes.func.isRequired,
+  importTemplate: PropTypes.func.isRequired,
+  exportTemplate: PropTypes.func.isRequired,
+  refreshFocusCounter: PropTypes.number.isRequired,
+  resetFocus: PropTypes.func.isRequired,
 };
