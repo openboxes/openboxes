3fa643851fc7f9ede53d63ef80b1d3c02573a5a1
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-04-24 21:44:22 +0200

    OBPIH-7175 Header improvements (#5231)

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -56,6 +56,7 @@ import {
   START_RESOLUTION,
   TOGGLE_USER_ACTION_MENU,
   TRANSLATIONS_FETCHED,
+  UPDATE_WORKFLOW_HEADER,
 } from 'actions/types';
 import cycleCountApi from 'api/services/CycleCountApi';
 import genericApi from 'api/services/GenericApi';
@@ -801,4 +802,13 @@ export const startResolution = (requestIds, locationId) => async (dispatch) => {
 export const eraseDraft = (tab) => ({
   type: ERASE_DRAFT,
   payload: tab,
+});
+
+export const updateWorkflowHeader = (headerInfo, headerStatus, workflowName) => ({
+  type: UPDATE_WORKFLOW_HEADER,
+  payload: {
+    headerInfo,
+    headerStatus,
+    workflowName,
+  },
 });
