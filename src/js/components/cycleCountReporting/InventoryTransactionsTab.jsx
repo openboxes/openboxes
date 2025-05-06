7992f5bc419dd0c7b409db3a22cced5f3d7f3b6c
Alan Nadolny <alannadolnyy@gmail.com>
2025-05-06 14:52:00 +0200

    OBPIH-7241 Create table component for inventory transactions report

--- src/js/components/cycleCountReporting/InventoryTransactionsTab.jsx
+++ src/js/components/cycleCountReporting/InventoryTransactionsTab.jsx
@@ -1,3 +1,72 @@
-const InventoryTransactionsTab = () => 'inventory transactions tab';
+import React, { useEffect } from 'react';
+
+import PropTypes from 'prop-types';
+import { RiDownload2Line } from 'react-icons/ri';
+
+import DataTable from 'components/DataTable/v2/DataTable';
+import Button from 'components/form-elements/Button';
+import useInventoryTransactionsTab from 'hooks/cycleCount/useInventoryTransactionsTab';
+
+const InventoryTransactionsTab = ({
+  filterParams,
+  tablePaginationProps,
+}) => {
+  const {
+    paginationProps,
+    setTotalCount,
+  } = tablePaginationProps;
+
+  const {
+    columns,
+    tableData,
+    loading,
+    emptyTableMessage,
+    exportData,
+    fetchData,
+  } = useInventoryTransactionsTab({
+    paginationProps,
+    filterParams,
+  });
+
+  useEffect(() => {
+    setTotalCount(tableData.totalCount);
+  }, [tableData.totalCount]);
+
+  return (
+    <div>
+      <div className="d-flex m-2 justify-content-end">
+        <button onClick={() => fetchData()} type="button">
+          load
+        </button>
+        <Button
+          onClick={exportData}
+          defaultLabel="Export"
+          label="react.default.button.export.label"
+          variant="secondary"
+          EndIcon={<RiDownload2Line />}
+        />
+      </div>
+      <DataTable
+        columns={columns}
+        data={tableData.data}
+        emptyTableMessage={emptyTableMessage}
+        loading={loading}
+        totalCount={tableData.totalCount}
+        filterParams={filterParams}
+        paginationProps={paginationProps}
+      />
+    </div>
+  );
+};
 
 export default InventoryTransactionsTab;
+
+InventoryTransactionsTab.propTypes = {
+  filterParams: PropTypes.shape({}).isRequired,
+  tablePaginationProps: PropTypes.shape({
+    paginationProps: PropTypes.shape({}).isRequired,
+    offset: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    setTotalCount: PropTypes.func.isRequired,
+  }).isRequired,
+};
