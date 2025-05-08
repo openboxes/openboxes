fb1aca77afc0a675fce50a8ad3d393bfd968ee81
Sebastian Lib <sebastianlib04@gmail.com>
2025-05-08 17:36:12 +0200

    OBPIH-7019 add eraseDraft only if there is no outdated products

--- src/js/hooks/cycleCount/useResolveStep.js
+++ src/js/hooks/cycleCount/useResolveStep.js
@@ -686,7 +686,6 @@ const useResolveStep = () => {
           outdatedProducts += 1;
         }
       }
-      dispatch(eraseDraft(TO_RESOLVE_TAB));
       if (outdatedProducts > 0) {
         dispatch({
           type: UPDATE_CYCLE_COUNT_IDS,
@@ -695,6 +694,7 @@ const useResolveStep = () => {
         openReviewProductsModal(outdatedProducts, cycleCountIdsForOutdatedProducts);
         return;
       }
+      dispatch(eraseDraft(TO_RESOLVE_TAB));
       history.push(CYCLE_COUNT.list(TO_RESOLVE_TAB));
     } finally {
       hide();
