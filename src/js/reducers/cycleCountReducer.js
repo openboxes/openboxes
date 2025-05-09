b7a12b0de7a48c12ff70cf0d98331e59716966b9
Alan Nadolny <alannadolnyy@gmail.com>
2025-05-09 17:11:23 +0200

    OBPIH-7139 Adjust reducers to handle drafts assigned to locations

--- src/js/reducers/cycleCountReducer.js
+++ src/js/reducers/cycleCountReducer.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import {
   ERASE_DRAFT,
   FETCH_BIN_LOCATIONS,
@@ -9,8 +11,8 @@ import {
 import { TO_COUNT_TAB, TO_RESOLVE_TAB } from 'consts/cycleCount';
 
 const initialState = {
-  requests: [],
-  cycleCounts: [],
+  requests: {},
+  cycleCounts: {},
   reasonCodes: [],
   binLocations: [],
 };
@@ -20,12 +22,18 @@ export default function cycleCountReducer(state = initialState, action) {
     case START_COUNT:
       return {
         ...state,
-        requests: action.payload,
+        requests: {
+          ...state.requests,
+          [action.payload.locationId]: action.payload.requests,
+        },
       };
     case START_RESOLUTION:
       return {
         ...state,
-        cycleCounts: action.payload,
+        cycleCounts: {
+          ...state.cycleCounts,
+          [action.payload.locationId]: action.payload.cycleCounts,
+        },
       };
     case FETCH_CYCLE_COUNT_REASON_CODES:
       return {
@@ -38,23 +46,26 @@ export default function cycleCountReducer(state = initialState, action) {
         binLocations: action.payload,
       };
     case ERASE_DRAFT:
-      if (action.payload === TO_COUNT_TAB) {
+      if (action.payload.tab === TO_COUNT_TAB) {
         return {
           ...state,
-          requests: [],
+          requests: _.omit(state.requests, action.payload.locationId),
         };
       }
-      if (action.payload === TO_RESOLVE_TAB) {
+      if (action.payload.tab === TO_RESOLVE_TAB) {
         return {
           ...state,
-          cycleCounts: [],
+          requests: _.omit(state.requests, action.payload.locationId),
         };
       }
       return state;
     case UPDATE_CYCLE_COUNT_IDS:
       return {
         ...state,
-        cycleCounts: action.payload,
+        cycleCounts: {
+          ...state.cycleCounts,
+          [action.payload.locationId]: action.payload,
+        },
       };
     default:
       return state;
