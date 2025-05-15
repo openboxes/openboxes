5c968db2adcc0bceff697854e8e71b5cbca78443
Oliver Lewandowski <109145288+olewandowski1@users.noreply.github.com>
2025-05-15 10:02:09 +0200

    OB-3223 do not duplicate putaway items (#5250)

--- grails-app/controllers/org/pih/warehouse/api/PutawayApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/PutawayApiController.groovy
@@ -153,22 +153,6 @@ class PutawayApiController {
 
         putaway.putawayAssignee = currentUser
 
-        // TODO: ensure this does not break completing putaway in web app
-        // Bind the putaway items
-        jsonObject.putawayItems.each { putawayItemMap ->
-            PutawayItem putawayItem = new PutawayItem()
-            bindData(putawayItem, putawayItemMap)
-
-            // Bind the split items
-            putawayItemMap.splitItems.each { splitItemMap ->
-                PutawayItem splitItem = new PutawayItem()
-                bindData(splitItem, splitItemMap)
-                putawayItem.splitItems.add(splitItem)
-            }
-
-            putaway.putawayItems.add(putawayItem)
-        }
-
         return putaway
     }
 
