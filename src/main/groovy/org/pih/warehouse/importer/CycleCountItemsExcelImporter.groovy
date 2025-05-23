65c70b5247a0e872898260944db424755cb984a7
Alan Nadolny <alannadolnyy@gmail.com>
2025-05-23 18:03:31 +0200

    OBPIH-7270 Add fetching products and locations while importing

--- src/main/groovy/org/pih/warehouse/importer/CycleCountItemsExcelImporter.groovy
+++ src/main/groovy/org/pih/warehouse/importer/CycleCountItemsExcelImporter.groovy
@@ -5,6 +5,8 @@ import org.grails.plugins.excelimport.AbstractExcelImporter
 import org.grails.plugins.excelimport.DefaultImportCellCollector
 import org.grails.plugins.excelimport.ExcelImportService
 import org.grails.plugins.excelimport.ExpectedPropertyType
+import org.pih.warehouse.core.Location
+import org.pih.warehouse.product.Product
 
 class CycleCountItemsExcelImporter extends AbstractExcelImporter implements DataImporter {
 
@@ -44,17 +46,43 @@ class CycleCountItemsExcelImporter extends AbstractExcelImporter implements Data
         super()
         read(fileName)
         excelImportService = Holders.grailsApplication.mainContext.getBean("excelImportService")
-
     }
 
     @Override
     List<Map> getData() {
-        excelImportService.columns(
+        List<Map> data = excelImportService.columns(
                 workbook,
                 COLUMN_MAP,
                 CELL_REPORTER,
                 PROPERTY_MAP
-        )
+        ) as List<Map>
+
+        // 1. Collect all uniques bin location names and product codes
+        List<String> locationNames = data*.binLocation.unique()
+        List<String> productCodes  = data*.productCode.unique()
+
+        // 2. Fetch all of the necessary data in single database call
+        Map<String, Location> locationMap = Location.findAllByNameInList(locationNames).collectEntries {
+            [it.name, it]
+        }
+        Map<String, Product> productMap = Product.findAllByProductCodeInList(productCodes).collectEntries {
+            [it.productCode, it]
+        }
+
+        // 3. Replace data from importer with the fetched data
+        data.each { row ->
+            row.binLocation = [
+                    id: locationMap[row.binLocation]?.id,
+                    name: locationMap[row.binLocation]?.name
+            ]
+            row.product = [
+                    id: productMap[row.productCode]?.id,
+                    name: productMap[row.productCode]?.name,
+                    productCode: productMap[row.productCode]?.productCode
+            ]
+        }
+
+        return data
     }
 
 
