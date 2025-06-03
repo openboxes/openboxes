a1b5b3bb685a07793351c1f9065898571bed3fcb
Alan Nadolny <alannadolnyy@gmail.com>
2025-06-03 19:18:11 +0200

    OBPIH-7194 Fix issue with not merging new lines on record stock

--- grails-app/services/org/pih/warehouse/inventory/RecordStockProductInventoryTransactionService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/RecordStockProductInventoryTransactionService.groovy
@@ -29,7 +29,21 @@ class RecordStockProductInventoryTransactionService extends ProductInventoryTran
     ) {
         String key = ProductAvailabilityService.constructAvailableItemKey(recordInventoryRowCommand.binLocation, inventoryItem)
         int quantityOnHand = availableItems.get(key)?.quantityOnHand ?: 0
-        int adjustmentQuantity = recordInventoryRowCommand.newQuantity - quantityOnHand
+        // When the row has old qty set to 0, it means that it is a new row.
+        // In that case, we should add the whole value from that row to
+        // the already existing corresponding line. If the quantity in
+        // already existing row is 5, and the new row has quantity 2,
+        // We should get one line with qty 7, and the transaction entry
+        // should contain quantity 2. In case we are editing already
+        // existing line (old quantity greater than 0), we should set the
+        // new value for that line. The adjustment entry should be a
+        // difference between the new value and the original value.
+        // When we had quantity 5, and then we changed it to 7,
+        // The row should have quantity 7, but the adjustment should
+        // have quantity 2.
+        int adjustmentQuantity = !recordInventoryRowCommand.oldQuantity
+            ? recordInventoryRowCommand.newQuantity
+            : recordInventoryRowCommand.newQuantity - quantityOnHand
         if (adjustmentQuantity == 0) {
             return null
         }
