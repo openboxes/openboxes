383bde5c5d2bd9840a69698df32f86fb44e07b33
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-06-16 10:49:09 +0200

    OBPIH-7046 Don't block count for 'In Progress' products (fix) (#5321)

--- src/js/hooks/cycleCount/useCycleCountProductAvailability.jsx
+++ src/js/hooks/cycleCount/useCycleCountProductAvailability.jsx
@@ -1,16 +1,17 @@
 import { ALL_PRODUCTS_TAB, TO_RESOLVE_TAB } from 'consts/cycleCount';
+import cycleCountCandidateStatus from 'consts/cycleCountCandidateStatus';
 import useQueryParams from 'hooks/useQueryParams';
 import { isCounting, isResolving } from 'utils/checkCycleCountStatus';
 
 const useCycleCountProductAvailability = (row) => {
   const { tab } = useQueryParams();
-
   if (isCounting(row.status) && tab === ALL_PRODUCTS_TAB) {
     return {
       isRowDisabled: true,
       label: 'react.cycleCount.status.counting.label',
       defaultMessage: 'Count started on the product: find the product on To Count tab',
       isFromOtherTab: true,
+      showCancelCheckbox: false,
     };
   }
 
@@ -20,15 +21,18 @@ const useCycleCountProductAvailability = (row) => {
       label: 'react.cycleCount.status.resolving.label',
       defaultMessage: 'Resolution started on the product: find the product on To Resolve tab',
       isFromOtherTab: true,
+      showCancelCheckbox: false,
     };
   }
 
-  if (row.quantityAllocated > 0 && tab === TO_RESOLVE_TAB) {
+  if (row.quantityAllocated > 0 && (tab === TO_RESOLVE_TAB
+      || row?.status === cycleCountCandidateStatus.COUNTING)) {
     return {
       isRowDisabled: true,
       label: 'react.cycleCount.pendingStockMovement.warning.label',
       defaultMessage: 'This product is in a pending stock movement. Check quantity input carefully',
       isFromOtherTab: false,
+      showCancelCheckbox: false,
     };
   }
 
@@ -38,6 +42,7 @@ const useCycleCountProductAvailability = (row) => {
       label: 'react.cycleCount.pendingStockMovement.info.label',
       defaultMessage: 'Cannot start count on this product with pending stock movement',
       isFromOtherTab: false,
+      showCancelCheckbox: true,
     };
   }
 
@@ -46,6 +51,7 @@ const useCycleCountProductAvailability = (row) => {
     label: null,
     defaultMessage: null,
     isFromOtherTab: false,
+    showCancelCheckbox: false,
   };
 };
 
