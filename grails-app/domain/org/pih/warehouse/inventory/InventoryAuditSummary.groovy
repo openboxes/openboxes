20b716b0f3048fdd60f435a5e8e44e3a2022b851
jmiranda <jcm62@columbia.edu>
2025-06-26 17:42:43 -0500

    OBPIH-7306 Implemented all column logic for inventory audit report

--- grails-app/domain/org/pih/warehouse/inventory/InventoryAuditSummary.groovy
+++ grails-app/domain/org/pih/warehouse/inventory/InventoryAuditSummary.groovy
@@ -7,10 +7,15 @@ class InventoryAuditSummary implements Serializable {
 
     Product product
     Location facility
-
     String abcClass
-
-    Integer quantityAdjusted
+    Integer quantityAdjusted = 0
+    Integer countAdjustments = 0
+    Integer countCycleCounts = 0
+    BigDecimal amountAdjusted = 0
+    Date lastCounted
+    Integer quantityDemanded = 0
+    Integer quantityOnHand = 0
+    BigDecimal amountOnHand = 0
 
     static mapping = {
         version false
@@ -33,17 +38,18 @@ class InventoryAuditSummary implements Serializable {
                         category   : product?.category?.name,
                         abcClass   : abcClass,
                         tags       : product.tags.collect { [id: it.id, name: it.tag] },
-                        catalogs   : product?.productCatalogs?.collect { [id: it.id, name: it.name] },
+                        catalogs   : product?.productCatalogs?.collect { [id: it.id, name: it.name] }
                 ],
-                lastCounted        : new Date(),
-                numberOfCounts     : 0,
+                lastCounted        : lastCounted,
+                pricePerUnit       : product?.pricePerUnit,
+                countCycleCounts   : countCycleCounts,
+                countAdjustments   : countAdjustments,
                 quantityAdjusted   : quantityAdjusted,
-                numberOfAdjustments: 0,
-                valueAdjusted      : 0,
-                monthsOfStockChange: 0,
-                quantityOnHand     : 0,
-                valueOnHand        : 0,
-
+                amountAdjusted     : amountAdjusted,
+                quantityDemanded   : quantityDemanded,
+                monthsOfStockChange: quantityDemanded > 0 ? (quantityAdjusted / quantityDemanded) : 0,
+                quantityOnHand     : quantityOnHand,
+                amountOnHand       : amountOnHand
         ]
     }
 }
