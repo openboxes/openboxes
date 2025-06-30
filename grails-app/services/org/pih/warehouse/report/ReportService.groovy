93493c40c482745977c658db2e5c64d30fb06ac7
Sebastian Lib <sebastianlib04@gmail.com>
2025-06-30 13:59:23 +0200

    OBPIH-7323 Add small correction

--- grails-app/services/org/pih/warehouse/report/ReportService.groovy
+++ grails-app/services/org/pih/warehouse/report/ReportService.groovy
@@ -1234,7 +1234,7 @@ class ReportService implements ApplicationContextAware {
     }
 
     Map getInventoryAccuracy(IndicatorApiCommand command) {
-        String hql = """
+        String query = """
         SELECT 
             SUM(CASE WHEN cps.hasVariance = false THEN 1 ELSE 0 END),
             COUNT(cps)
@@ -1243,7 +1243,7 @@ class ReportService implements ApplicationContextAware {
           AND cps.facility = :facility
     """
 
-        List<Object[]> result = CycleCountProductSummary.executeQuery(hql, [
+        List<Object[]> result = CycleCountProductSummary.executeQuery(query, [
                 startDate: command.startDate,
                 endDate  : command.endDate,
                 facility : command.facility
