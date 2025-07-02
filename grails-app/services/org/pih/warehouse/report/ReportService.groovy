867f789b442aa67965b0150ecbf79026da387819
Sebastian Lib <sebastianlib04@gmail.com>
2025-07-02 15:30:28 +0200

    OBPIH-7323 Add small corrections

--- grails-app/services/org/pih/warehouse/report/ReportService.groovy
+++ grails-app/services/org/pih/warehouse/report/ReportService.groovy
@@ -50,6 +50,7 @@ import org.pih.warehouse.order.OrderService
 import org.pih.warehouse.product.Category
 import org.pih.warehouse.product.Product
 import org.pih.warehouse.product.ProductCatalog
+import org.pih.warehouse.reporting.CycleCountProductSummary
 import org.pih.warehouse.reporting.DateDimension
 import org.pih.warehouse.LocalizationUtil
 import org.pih.warehouse.reporting.IndicatorApiCommand
