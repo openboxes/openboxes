f78edfd174f9157d9ac3e36ea817d75c2d015a4f
druchniewicz <51370022+druchniewicz@users.noreply.github.com>
2025-07-10 15:51:09 +0200

    OB-3254 Implemented logic for auto receiving (#5351)
    Co-authored-by: olewandowski1 <olewandowski@soldevelo.com>

--- grails-app/services/org/pih/warehouse/shipping/ShipmentStatusTransitionEventService.groovy
+++ grails-app/services/org/pih/warehouse/shipping/ShipmentStatusTransitionEventService.groovy
@@ -9,20 +9,31 @@
  **/
 package org.pih.warehouse.shipping
 
+import org.pih.warehouse.core.ActivityCode
+import grails.core.GrailsApplication
 import grails.gorm.transactions.Transactional
 import org.pih.warehouse.core.RoleType
-import org.pih.warehouse.shipping.ShipmentStatusTransitionEvent
 import org.springframework.context.ApplicationListener
 
 @Transactional
 class ShipmentStatusTransitionEventService implements ApplicationListener<ShipmentStatusTransitionEvent> {
 
+    GrailsApplication grailsApplication
     def notificationService
     def webhookPublisherService
 
     void onApplicationEvent(ShipmentStatusTransitionEvent event) {
         log.info "Application event ${event} has been published!"
         Shipment shipment = Shipment.get(event?.source?.id)
+
+        if (!shipment) {
+            log.warn("Shipment not found; skipping notification.")
+            return
+        }
+
+        def originNotificationsEnabled = shipment.origin.supports(ActivityCode.ENABLE_NOTIFICATIONS) ?: false
+        def destinationNotificationsEnabled = shipment.destination.supports(ActivityCode.ENABLE_NOTIFICATIONS) ?: false
+
         log.info "Shipment ${shipment?.shipmentNumber} from ${shipment?.origin} to ${shipment.destination}"
         List outboundCreatedRoleTypes = [RoleType.ROLE_SHIPMENT_NOTIFICATION, RoleType.ROLE_SHIPMENT_OUTBOUND_CREATED_NOTIFICATION]
         List inboundCreatedRoleTypes = [RoleType.ROLE_SHIPMENT_NOTIFICATION, RoleType.ROLE_SHIPMENT_INBOUND_CREATED_NOTIFICATION]
@@ -32,25 +43,31 @@ class ShipmentStatusTransitionEventService implements ApplicationListener<Shipme
         List outboundReceivedRoleTypes = [RoleType.ROLE_SHIPMENT_NOTIFICATION, RoleType.ROLE_SHIPMENT_OUTBOUND_RECEIVED_NOTIFICATION]
 
         if (event.shipmentStatusCode == ShipmentStatusCode.CREATED) {
-            notificationService.sendShipmentCreatedNotification(shipment, shipment.origin, outboundCreatedRoleTypes)
-            notificationService.sendShipmentCreatedNotification(shipment, shipment.destination, inboundCreatedRoleTypes)
-        }
-        else if(event.shipmentStatusCode == ShipmentStatusCode.SHIPPED) {
-            notificationService.sendShipmentIssuedNotification(shipment, shipment.origin, outboundShippedRoleTypes)
-            notificationService.sendShipmentIssuedNotification(shipment, shipment.destination, inboundShippedRoleTypes)
+            if (originNotificationsEnabled) {
+                notificationService.sendShipmentCreatedNotification(shipment, shipment.origin, outboundCreatedRoleTypes)
+            }
+            if (destinationNotificationsEnabled) {
+                notificationService.sendShipmentCreatedNotification(shipment, shipment.destination, inboundCreatedRoleTypes)
+            }
+        } else if (event.shipmentStatusCode == ShipmentStatusCode.SHIPPED) {
+            if (originNotificationsEnabled) {
+                notificationService.sendShipmentIssuedNotification(shipment, shipment.origin, outboundShippedRoleTypes)
+            }
+            if (destinationNotificationsEnabled) {
+                notificationService.sendShipmentIssuedNotification(shipment, shipment.destination, inboundShippedRoleTypes)
+            }
             notificationService.sendShipmentItemsShippedNotification(shipment)
-
             // Temporarily hard-code publishing webhook events for shipped events
             webhookPublisherService.publishShippedEvent(shipment)
-        }
-        else if (event.shipmentStatusCode in [ShipmentStatusCode.RECEIVED, ShipmentStatusCode.PARTIALLY_RECEIVED]) {
-            notificationService.sendShipmentReceiptNotification(shipment, shipment.origin, outboundReceivedRoleTypes)
-            notificationService.sendShipmentReceiptNotification(shipment, shipment.destination, inboundReceivedRoleTypes)
-
+        } else if (event.shipmentStatusCode in [ShipmentStatusCode.RECEIVED, ShipmentStatusCode.PARTIALLY_RECEIVED]) {
+            if (originNotificationsEnabled) {
+                notificationService.sendShipmentReceiptNotification(shipment, shipment.origin, outboundReceivedRoleTypes)
+            }
+            if (destinationNotificationsEnabled) {
+                notificationService.sendShipmentReceiptNotification(shipment, shipment.destination, inboundReceivedRoleTypes)
+            }
             // Send notification email to recipients on completed receipt
             notificationService.sendReceiptNotifications(event?.partialReceipt)
         }
-
     }
-
 }
