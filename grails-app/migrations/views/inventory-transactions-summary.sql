28aeb9be2940e2aff51bd1283a300755f039fff9
kchelstowski <kchelstowski@soldevelo.com>
2025-08-05 15:42:04 +0200

    OBPIH-7313 Fix inventory transactions report crashing MariaDB

--- grails-app/migrations/views/inventory-transactions-summary.sql
+++ grails-app/migrations/views/inventory-transactions-summary.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW inventory_transactions_summary AS
        WITH product_inventory_summary AS (
            SELECT
               inventory_item.product_id as product_id,
+              product.product_code as product_code,
               facility.id as facility_id,
               transaction.id as transaction_id,
               transaction.transaction_date as baseline_transaction_date,
@@ -11,6 +12,7 @@ CREATE OR REPLACE VIEW inventory_transactions_summary AS
             JOIN transaction_type ON transaction.transaction_type_id = transaction_type.id
             JOIN location facility ON transaction.inventory_id = facility.inventory_id
             JOIN inventory_item ON inventory_item.id = transaction_entry.inventory_item_id
+            JOIN product ON product.id = inventory_item.product_id
             WHERE transaction_type.id = '12' -- baseline inventory transaction
             GROUP BY transaction.id, inventory_item.product_id
     ),
@@ -18,12 +20,14 @@ CREATE OR REPLACE VIEW inventory_transactions_summary AS
     quantity_sum_for_adjustment AS (
        SELECT ii.product_id as product_id,
               t.transaction_date as transaction_date,
+              product.product_code as product_code,
               t.inventory_id as inventory_id,
               t.id as transaction_id,
               SUM(te.quantity) as quantity_sum
            FROM transaction t
                     JOIN transaction_entry te ON te.transaction_id = t.id
                     JOIN inventory_item ii ON ii.id = te.inventory_item_id
+                    JOIN product ON product.id = ii.product_id
            WHERE t.transaction_type_id = '3'
            GROUP BY t.id, ii.product_id
        )
@@ -31,7 +35,7 @@ CREATE OR REPLACE VIEW inventory_transactions_summary AS
 SELECT
     CRC32(CONCAT(transaction.id, inventory_item.product_id, facility.id)) as id,
     transaction.id AS transaction_id,
-    inventory_item.product_id as product_id,
+    product.id as product_id,
     facility.id as facility_id,
     -- If product inventory summary id is null, it means, that the transaction we query against is a single adjustment
     -- to calculate quantity before for such adjustment (since it is not associated with a baseline transaction),
@@ -47,13 +51,13 @@ SELECT
                   -- 2) we don't take SUM, but MAX not to double the result
                   SELECT MAX(quantity_balance)
                   FROM product_inventory_summary pis
-                  WHERE pis.product_id = inventory_item.product_id
+                  WHERE pis.product_code = product.product_code
                     AND pis.facility_id = facility.id
                     -- Find the latest baseline before the calculated adjustment
                     AND pis.baseline_transaction_date = (
                       SELECT MAX(pis2.baseline_transaction_date)
                       FROM product_inventory_summary pis2
-                      WHERE pis2.product_id = inventory_item.product_id
+                      WHERE pis2.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
                         AND pis2.facility_id = facility.id
                         AND pis2.baseline_transaction_date < transaction.transaction_date
                     )
@@ -63,13 +67,13 @@ SELECT
                  -- Sum the quantity of all adjustments between the calculated adjustment and the latest baseline
                  SELECT SUM(quantity_sum)
                  FROM quantity_sum_for_adjustment
-                 WHERE quantity_sum_for_adjustment.product_id = inventory_item.product_id
+                 WHERE quantity_sum_for_adjustment.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
                  AND quantity_sum_for_adjustment.inventory_id = transaction.inventory_id
                  -- Condition to find the date range between the latest baseline and the current calculated adjustment
                  AND quantity_sum_for_adjustment.transaction_date > (
                     SELECT MAX(baseline_transaction_date)
                     FROM product_inventory_summary
-                    WHERE product_inventory_summary.product_id = inventory_item.product_id
+                    WHERE product_inventory_summary.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
                       AND product_inventory_summary.facility_id = facility.id
                       AND product_inventory_summary.baseline_transaction_date < transaction.transaction_date
                  )
@@ -83,13 +87,12 @@ SELECT
     COALESCE((
          SELECT MAX(quantity_balance)
          FROM product_inventory_summary pis
-         WHERE pis.product_id = inventory_item.product_id
-           AND pis.facility_id = facility.id
+         WHERE pis.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
            AND pis.facility_id = facility.id
            AND pis.baseline_transaction_date = (
              SELECT MAX(pis2.baseline_transaction_date)
              FROM product_inventory_summary pis2
-             WHERE pis2.product_id = inventory_item.product_id
+             WHERE pis2.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
                AND pis2.facility_id = facility.id
                AND pis2.baseline_transaction_date < transaction.transaction_date
            )
@@ -98,12 +101,12 @@ SELECT
     COALESCE((
         SELECT SUM(quantity_sum)
         FROM quantity_sum_for_adjustment
-        WHERE quantity_sum_for_adjustment.product_id = inventory_item.product_id
+        WHERE quantity_sum_for_adjustment.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
           AND quantity_sum_for_adjustment.inventory_id = transaction.inventory_id
           AND quantity_sum_for_adjustment.transaction_date > (
                 SELECT MAX(baseline_transaction_date)
                 FROM product_inventory_summary
-                WHERE product_inventory_summary.product_id = inventory_item.product_id
+                WHERE product_inventory_summary.product_code = product.product_code -- comparing the product_code instead of inventory_item.product_id is intentional to avoid MariaDB bug when accessing the inventory_item.product_id with the table prefix (OBS-1901)
                   AND product_inventory_summary.facility_id = facility.id
                   AND product_inventory_summary.baseline_transaction_date < transaction.transaction_date
           )
