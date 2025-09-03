743c723ec88743427fe702d54b7bc42d3648e6b0
Alan Nadolny <83239466+alannadolny@users.noreply.github.com>
2025-09-03 10:40:24 +0200

    OBPIH-7465 Record stock not creating the correct adjustment when backdated (#5473)

--- grails-app/services/org/pih/warehouse/inventory/RecordStockProductInventoryTransactionService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/RecordStockProductInventoryTransactionService.groovy
@@ -28,22 +28,18 @@ class RecordStockProductInventoryTransactionService extends ProductInventoryTran
             Map<String, AvailableItem> availableItems
     ) {
         String key = ProductAvailabilityService.constructAvailableItemKey(recordInventoryRowCommand.binLocation, inventoryItem)
-        int quantityOnHand = availableItems.get(key)?.quantityOnHand ?: 0
-        // When the row has old qty set to 0, it means that it is a new row.
-        // In that case, we should add the whole value from that row to
-        // the already existing corresponding line. If the quantity in
-        // already existing row is 5, and the new row has quantity 2,
-        // We should get one line with qty 7, and the transaction entry
-        // should contain quantity 2. In case we are editing already
-        // existing line (old quantity greater than 0), we should set the
-        // new value for that line. The adjustment entry should be a
-        // difference between the new value and the original value.
-        // When we had quantity 5, and then we changed it to 7,
-        // The row should have quantity 7, but the adjustment should
-        // have quantity 2.
-        int adjustmentQuantity = !recordInventoryRowCommand.oldQuantity
-            ? recordInventoryRowCommand.newQuantity
-            : recordInventoryRowCommand.newQuantity - quantityOnHand
+        Integer quantityOnHand = availableItems.get(key)?.quantityOnHand
+        // When the row doesn't already have an existing quantity on hand on the date of creation, it means that it is a new
+        // row. In that case, we should add the whole value from that row to the already existing corresponding line. If
+        //The quantity in the already existing row is 5, and the new row has a quantity of 2. We should get one line with qty 7,
+        // and the transaction entry should contain quantity 2. In case we are editing an already existing line (old quantity
+        // greater than 0), we should set the new value for that line. The adjustment entry should be the difference between
+        // the new value and the original value. When we had quantity 5, and then we changed it to 7, the row should have
+        // quantity 7, but the adjustment should have quantity 2.
+        int adjustmentQuantity = quantityOnHand == null
+                ? recordInventoryRowCommand.newQuantity
+                : recordInventoryRowCommand.newQuantity - quantityOnHand
+
         if (adjustmentQuantity == 0) {
             return null
         }
