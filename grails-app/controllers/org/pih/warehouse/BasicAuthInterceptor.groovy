a9fa6c4dcb18eea9f7675cc050ee5331c9e355b0
jmiranda <jcm62@columbia.edu>
2025-09-05 17:34:52 -0500

    OB-3247 Added configuration to enable/disable the basic authentication interceptor

--- grails-app/controllers/org/pih/warehouse/BasicAuthInterceptor.groovy
+++ grails-app/controllers/org/pih/warehouse/BasicAuthInterceptor.groovy
@@ -1,18 +1,25 @@
 package org.pih.warehouse
 
+import grails.core.GrailsApplication
 import org.pih.warehouse.core.User
 import org.pih.warehouse.core.UserService
 
 class BasicAuthInterceptor {
 
     UserService userService
+    GrailsApplication grailsApplication
 
     BasicAuthInterceptor() {
         // TODO Need to make sure there are no requests to the API outside the scope of the /api path
+        //  for now these requests can be authenticated using the cookie auth.
         match(uri: '/api/**')
     }
 
     boolean before() {
+
+        // Check if the basic auth interceptor is enabled
+        Boolean enabled = grailsApplication.config.getProperty('openboxes.interceptor.basicAuth.enabled', Boolean, false)
+        if (!enabled) return true
 
         // User was already authenticated
         if (session.user) {
@@ -20,24 +27,22 @@ class BasicAuthInterceptor {
             return true
         }
 
-        else {
+        // If there's an auth header, we'll attempt to authenticate
+        def authHeader = request.getHeader('Authorization')
+        if (authHeader) {
+            // Authenticate user credentials
+            def (username, password) = decodeBasicAuth(authHeader)
+            Boolean authenticated = userService.authenticate(username, password)
 
-            // If there's an auth header, we'll attempt to authenticate
-            def authHeader = request.getHeader('Authorization')
-            if (authHeader) {
-                // Authenticate user credentials
-                def (userIdentifier, password) = decodeBasicAuth(authHeader)
-                Boolean authenticated = userService.authenticate(userIdentifier, password)
-
-                // FIXME This is required because authentication does not return the user.
-                //  It might make more sense to return the user as part of the auth process.
-                def user = User.findByUsernameOrEmail(userIdentifier, userIdentifier)
-                if (!user?.active || !authenticated) {
-                    response.sendError(401)
-                    return false
-                }
-                setUser(user)
+            // FIXME This is required because authentication does not return the user.
+            //  It might make more sense to return the user as part of the auth process.
+            def user = User.findByUsernameOrEmail(username, username)
+            if (!user?.active || !authenticated) {
+                response.sendError(401)
+                return false
             }
+            // Initialize the user session
+            initializeUserSession(user)
         }
         return true
     }
@@ -49,7 +54,7 @@ class BasicAuthInterceptor {
         return decoded.split(":", 2)
     }
 
-    private void setUser(User user) {
+    private void initializeUserSession(User user) {
         session.user = user
         // Set saved location, but don't override the session warehouse if it's already set
         if (user?.warehouse && user?.rememberLastLocation && !session.warehouse) {
