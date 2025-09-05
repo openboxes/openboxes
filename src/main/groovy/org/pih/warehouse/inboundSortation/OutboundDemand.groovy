43d3054a278a74b4fdbe5654175b4495b24628a5
druchniewicz <51370022+druchniewicz@users.noreply.github.com>
2025-09-05 23:33:00 +0200

    OBLS-129 Creation of automatic putaway after auto-receiving (#5469)

---
+++ src/main/groovy/org/pih/warehouse/inboundSortation/OutboundDemand.groovy
@@ -0,0 +1,9 @@
+package org.pih.warehouse.inboundSortation
+
+import org.pih.warehouse.product.Product
+
+class OutboundDemand {
+    Product product
+    DemandTypeCode demandTypeCode
+    Integer quantity
+}
