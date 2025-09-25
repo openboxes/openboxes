e6f9e613acae47a558624e3f9d6514fc1ac6338e
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-09-25 15:58:56 +0200

    OBPIH-7509 Add Read-only summary table (#5510)

--- src/js/consts/inboundColumns.js
+++ src/js/consts/inboundColumns.js
@@ -7,6 +7,12 @@ const columns = {
   QUANTITY_REQUESTED: 'quantityRequested',
   RECIPIENT: 'recipient',
   DELETE: 'delete',
+  PRODUCT_CODE: 'productCode',
+  PRODUCT_NAME: 'product.name',
+  INVENTORY_ITEM_LOT_NUMBER: 'inventoryItem.lotNumber',
+  INVENTORY_ITEM_EXPIRATION_DATE: 'inventoryItem.expirationDate',
+  QUANTITY_PICKED: 'quantityRequested',
+  RECIPIENT_NAME: 'recipient.name',
 };
 
 export default columns;
