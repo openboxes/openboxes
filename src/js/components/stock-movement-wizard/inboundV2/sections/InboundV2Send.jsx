cb361d4e9474113e51bf6b031fea3d4d86352ffe
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-09-25 15:58:56 +0200

    OBPIH-7509 Add Read-only summary table (#5510)

--- src/js/components/stock-movement-wizard/inboundV2/sections/InboundV2Send.jsx
+++ src/js/components/stock-movement-wizard/inboundV2/sections/InboundV2Send.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import InboundSendForm from 'components/stock-movement-wizard/inboundV2/sections/InboundSendForm';
+import InboundSendTable from 'components/stock-movement-wizard/inboundV2/sections/InboundSendTable';
 
 const InboundV2Send = ({ previous }) => (
-  <div>
+  <div className="mb-4">
     <InboundSendForm previous={previous} />
+    <InboundSendTable />
   </div>
 );
 
