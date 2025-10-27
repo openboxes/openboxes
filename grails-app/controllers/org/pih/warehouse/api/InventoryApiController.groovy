c7ab3d4e787e1dfd05c865d50032f80de8b37582
Kacper Chełstowski <kchelstowski@soldevelo.com>
2025-10-27 10:49:29 +0100

    OBPIH-7542 Bring back fallback to JSON response if not format provided

--- grails-app/controllers/org/pih/warehouse/api/InventoryApiController.groovy
+++ grails-app/controllers/org/pih/warehouse/api/InventoryApiController.groovy
@@ -46,7 +46,7 @@ class InventoryApiController {
         List<ReorderReportItemDto> reorderReport = dashboardService.getReorderReport(command)
 
         withFormat {
-            "*" {
+            "csv" {
                 String csv = dashboardService.getReorderReportCsv(reorderReport)
                 response.contentType = "text/csv"
                 String filename = "Reorder report - ${AuthService.currentLocation?.name}.csv"
@@ -54,7 +54,9 @@ class InventoryApiController {
                 render(csv)
                 return
             }
-
+            "*" {
+                render([data: reorderReport] as JSON)
+            }
         }
     }
 }
