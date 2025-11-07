1d06a914a966572e8ad5de2ea48edca7de52de93
Evan <evanwaterman@hotmail.com>
2025-11-07 10:35:28 -0800

    test refactors p4

--- src/test/groovy/unit/org/pih/warehouse/product/ProductSpec.groovy
+++ src/test/groovy/unit/org/pih/warehouse/product/ProductSpec.groovy
@@ -5,16 +5,18 @@ import java.time.Instant
 import org.grails.plugins.web.taglib.ApplicationTagLib
 import spock.lang.Specification
 
+import org.pih.warehouse.core.Location
+import org.pih.warehouse.inventory.Inventory
+import org.pih.warehouse.inventory.InventoryLevel
 import org.pih.warehouse.product.Category
 import org.pih.warehouse.product.Product
 import org.pih.warehouse.product.ProductAssociation
 import org.pih.warehouse.product.ProductAssociationTypeCode
-import org.pih.warehouse.product.ProductGroup
 
 class ProductSpec extends Specification implements DomainUnitTest<Product> {
 
     void setupSpec() {
-        mockDomains(Product, ProductAssociation, ProductGroup)
+        mockDomains(Product, ProductAssociation, Inventory, InventoryLevel)
 
         ApplicationTagLib gStub = Stub(ApplicationTagLib)
         Product.metaClass.getApplicationTagLib = { -> gStub }
@@ -72,5 +74,67 @@ class ProductSpec extends Specification implements DomainUnitTest<Product> {
         assert alternativeProducts.size() == 2
         assert alternativeProducts.contains(p1)
         assert alternativeProducts.contains(p2)
+    }
+
+    void 'getInventoryLevel should return expected inventory level'() {
+        given:
+        Inventory inventory = new Inventory().save(validate:false)
+        Location location = new Location(
+                inventory: inventory
+        ).save(validate: false)
+        Product product = new Product().save(validate: false)
+        InventoryLevel inventoryLevel = new InventoryLevel(
+                product: product,
+                inventory: inventory,
+        ).save(validate: false)
+
+        expect:
+        product.getInventoryLevel(location.id) == inventoryLevel
+    }
+
+    void 'getBinLocation should return the bin location of the associated inventory level'() {
+        given:
+        Inventory inventory = new Inventory().save(validate:false)
+        Location location = new Location(
+                inventory: inventory
+        ).save(validate: false)
+        Product product = new Product().save(validate: false)
+        InventoryLevel inventoryLevel = new InventoryLevel(
+                product: product,
+                inventory: inventory,
+                binLocation: "TEST BIN"
+        ).save(validate: false)
+
+        expect:
+        product.getBinLocation(location.id) == "TEST BIN"
+    }
+
+    void 'getBinLocation should return null when there is no matching inventory level'() {
+        given:
+        Inventory inventory = new Inventory().save(validate:false)
+        Location location = new Location(
+                inventory: inventory
+        ).save(validate: false)
+        Product product = new Product().save(validate: false)
+
+        expect:
+        product.getBinLocation(location.id) == null
+    }
+
+    void 'getBinLocation should return null when the bin location in the inventory level is null'() {
+        given:
+        Inventory inventory = new Inventory().save(validate:false)
+        Location location = new Location(
+                inventory: inventory
+        ).save(validate: false)
+        Product product = new Product().save(validate: false)
+        InventoryLevel inventoryLevel = new InventoryLevel(
+                product: product,
+                inventory: inventory,
+                binLocation: null
+        ).save(validate: false)
+
+        expect:
+        product.getBinLocation(location.id) == null
     }
 }
