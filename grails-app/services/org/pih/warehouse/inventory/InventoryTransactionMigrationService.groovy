b3632f1ab1d4049cdbf49ae4f4f32d9286cbc94c
Evan <evanwaterman@hotmail.com>
2025-11-14 13:15:57 -0800

    Update InventoryTransactionMigrationService.groovy

--- grails-app/services/org/pih/warehouse/inventory/InventoryTransactionMigrationService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/InventoryTransactionMigrationService.groovy
@@ -2,16 +2,15 @@ package org.pih.warehouse.inventory
 
 import grails.gorm.transactions.Transactional
 
-import org.pih.warehouse.importer.ImportDataCommand
-
 /**
  * Responsible for creating baseline transactions when migrating old INVENTORY transactions.
+ * (Not to be confused with the PRODUCT_INVENTORY migration done by ProductInventoryTransactionMigrationService.)
  */
 @Transactional
-class InventoryTransactionMigrationService extends ProductInventoryTransactionService<ImportDataCommand> {
+class InventoryTransactionMigrationService extends ProductInventoryTransactionService<Object> {
 
     @Override
-    void setSourceObject(Transaction transaction, ImportDataCommand sourceObject) {
+    void setSourceObject(Transaction transaction, Object sourceObject) {
         // Transaction migration has no source object.
     }
 }
