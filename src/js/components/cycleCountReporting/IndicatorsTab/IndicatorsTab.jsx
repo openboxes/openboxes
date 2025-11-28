a7b118be38755f5c3ed2e557e7ec25f925c30b9c
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-11-28 15:04:40 +0100

    OBPIH-7583 Fix preserve selected dates across inventory reports (#5645)

--- src/js/components/cycleCountReporting/IndicatorsTab/IndicatorsTab.jsx
+++ src/js/components/cycleCountReporting/IndicatorsTab/IndicatorsTab.jsx
@@ -10,15 +10,18 @@ import 'components/cycleCountReporting/cycleCountReporting.scss';
 
 const IndicatorsTab = ({
   filterParams,
-  tablePaginationProps,
+  shouldFetch,
+  setShouldFetch,
+  filtersInitialized,
 }) => {
-  const { serializedParams } = tablePaginationProps;
   const {
     loading,
     tiles,
   } = useIndicatorsTab({
     filterParams,
-    serializedParams,
+    shouldFetch,
+    setShouldFetch,
+    filtersInitialized,
   });
 
   return (
@@ -32,11 +35,7 @@ export default IndicatorsTab;
 
 IndicatorsTab.propTypes = {
   filterParams: PropTypes.shape({}).isRequired,
-  tablePaginationProps: PropTypes.shape({
-    paginationProps: PropTypes.shape({}).isRequired,
-    offset: PropTypes.number.isRequired,
-    pageSize: PropTypes.number.isRequired,
-    setTotalCount: PropTypes.func.isRequired,
-    serializedParams: PropTypes.number.isRequired,
-  }).isRequired,
+  shouldFetch: PropTypes.bool.isRequired,
+  setShouldFetch: PropTypes.func.isRequired,
+  filtersInitialized: PropTypes.bool.isRequired,
 };
