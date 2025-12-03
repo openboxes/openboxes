c0ee8bcd269ecee84a1fc17683f0c1d4e3ae891f
Sebastian Lib <120328147+SebastianLib@users.noreply.github.com>
2025-12-03 15:36:08 +0100

    OBPIH-7338 Add Import Recount Button And display errors (#5649)

--- src/js/components/cycleCount/toResolveTab/ResolveStepHeader.jsx
+++ src/js/components/cycleCount/toResolveTab/ResolveStepHeader.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import { RiPrinterLine, RiSave2Line } from 'react-icons/ri';
 
 import Button from 'components/form-elements/Button';
+import ButtonFileSelect from 'components/form-elements/v2/ButtonFileSelect';
 import { CYCLE_COUNT } from 'consts/applicationUrls';
 import { TO_RESOLVE_TAB } from 'consts/cycleCount';
 import FileFormat from 'consts/fileFormat';
 import useTranslate from 'hooks/useTranslate';
+import AlertMessage from 'utils/AlertMessage';
 import RedirectButton from 'utils/RedirectButton';
 import Translate from 'utils/Translate';
 import CustomTooltip from 'wrappers/CustomTooltip';
@@ -20,79 +22,93 @@ const ResolveStepHeader = ({
   isFormDisabled,
   sortByProductName,
   setSortByProductName,
+  importItems,
+  importErrors,
 }) => {
   const translate = useTranslate();
 
   return (
-    <div className="d-flex justify-content-sm-between align-items-end">
-      <RedirectButton
-        label="react.cycleCount.redirectToResolveTab.label"
-        defaultMessage="Back to Resolve tab"
-        redirectTo={CYCLE_COUNT.list(TO_RESOLVE_TAB)}
-        className="pt-5"
-      />
-      <div className="d-flex gap-8">
-        <div className="btn-group">
-          <Button
-            isDropdown
-            defaultLabel="Print recount form"
-            label="react.cycleCount.printRecountForm.label"
+    <div>
+      <div className="d-flex justify-content-sm-between align-items-end">
+        <RedirectButton
+          label="react.cycleCount.redirectToResolveTab.label"
+          defaultMessage="Back to Resolve tab"
+          redirectTo={CYCLE_COUNT.list(TO_RESOLVE_TAB)}
+          className="pt-5"
+        />
+        <div className="d-flex gap-8">
+          <div className="btn-group">
+            <Button
+              isDropdown
+              defaultLabel="Print recount form"
+              label="react.cycleCount.printRecountForm.label"
+              variant="primary-outline"
+              StartIcon={<RiPrinterLine size={18} />}
+              disabled={isFormDisabled}
+            />
+            <div className="dropdown-menu dropdown-menu-right nav-item padding-8" aria-labelledby="dropdownMenuButton">
+              <a href="#" className="dropdown-item" onClick={() => printRecountForm(FileFormat.PDF)} role="button" aria-disabled={isFormDisabled}>
+                <Translate
+                  id="react.cycleCount.printRecountFormPdf.label"
+                  defaultMessage="Print Recount Form PDF"
+                />
+              </a>
+              <a href="#" className="dropdown-item" onClick={() => printRecountForm(FileFormat.XLS)} role="button" aria-disabled={isFormDisabled}>
+                <Translate
+                  id="react.cycleCount.exportRecountSheet.label"
+                  defaultMessage="Export Recount Sheet"
+                />
+              </a>
+            </div>
+          </div>
+          <ButtonFileSelect
+            onFileUpload={importItems}
+            defaultLabel="Import Recount"
+            label="react.cycleCount.importRecount.label"
+            allowedExtensions={[FileFormat.XLS]}
+            disabled={isFormDisabled}
             variant="primary-outline"
-            StartIcon={<RiPrinterLine size={18} />}
+            className="no-transition"
+          />
+          <Button
+            onClick={() => setSortByProductName((prev) => !prev)}
+            label="react.cycleCount.sortAlphabetically.label"
+            defaultLabel="Sort alphabetically"
+            variant={sortByProductName ? 'secondary' : 'primary-outline'}
             disabled={isFormDisabled}
           />
-          <div className="dropdown-menu dropdown-menu-right nav-item padding-8" aria-labelledby="dropdownMenuButton">
-            <a href="#" className="dropdown-item" onClick={() => printRecountForm(FileFormat.PDF)} role="button" aria-disabled={isFormDisabled}>
-              <Translate
-                id="react.cycleCount.printRecountFormPdf.label"
-                defaultMessage="Print Recount Form PDF"
-              />
-            </a>
-            <a href="#" className="dropdown-item" onClick={() => printRecountForm(FileFormat.XLS)} role="button" aria-disabled={isFormDisabled}>
-              <Translate
-                id="react.cycleCount.exportRecountSheet.label"
-                defaultMessage="Export Recount Sheet"
-              />
-            </a>
-          </div>
-        </div>
-        <Button
-          onClick={() => setSortByProductName((prev) => !prev)}
-          label="react.cycleCount.sortAlphabetically.label"
-          defaultLabel="Sort alphabetically"
-          variant={sortByProductName ? 'secondary' : 'primary-outline'}
-          disabled={isFormDisabled}
-        />
-        <CustomTooltip
-          content={translate(
-            'react.cycleCount.table.refreshQuantitiesTooltip.label',
-            'Fix missing or transaction error on this product and Refresh to see updated discrepancies.',
-          )}
-        >
+          <CustomTooltip
+            content={translate(
+              'react.cycleCount.table.refreshQuantitiesTooltip.label',
+              'Fix missing or transaction error on this product and Refresh to see updated discrepancies.',
+            )}
+          >
+            <Button
+              onClick={() => refreshCountItems()}
+              label="react.default.button.refresh.label"
+              defaultLabel="Reload"
+              variant="primary"
+              disabled={isFormDisabled}
+            />
+          </CustomTooltip>
           <Button
-            onClick={() => refreshCountItems()}
-            label="react.default.button.refresh.label"
-            defaultLabel="Reload"
+            onClick={save}
+            label="react.cycleCount.save.label"
+            defaultLabel="Save progress"
+            variant="primary"
+            StartIcon={<RiSave2Line size={18} />}
+            disabled={isFormDisabled}
+          />
+          <Button
+            onClick={next}
+            label="react.default.button.next.label"
+            defaultLabel="Next"
             variant="primary"
             disabled={isFormDisabled}
           />
-        </CustomTooltip>
-        <Button
-          onClick={save}
-          label="react.cycleCount.save.label"
-          defaultLabel="Save progress"
-          variant="primary"
-          StartIcon={<RiSave2Line size={18} />}
-          disabled={isFormDisabled}
-        />
-        <Button
-          onClick={next}
-          label="react.default.button.next.label"
-          defaultLabel="Next"
-          variant="primary"
-          disabled={isFormDisabled}
-        />
+        </div>
       </div>
+      <AlertMessage show={importErrors.length > 0} message={importErrors} danger />
     </div>
   );
 };
@@ -107,4 +123,6 @@ ResolveStepHeader.propTypes = {
   isFormDisabled: PropTypes.bool.isRequired,
   sortByProductName: PropTypes.bool.isRequired,
   setSortByProductName: PropTypes.func.isRequired,
+  importItems: PropTypes.func.isRequired,
+  importErrors: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
---
+++ src/js/hooks/cycleCount/useResolveStepImport.js
@@ -0,0 +1,42 @@
+import { useCallback, useState } from 'react';
+
+import { useDispatch, useStore } from 'react-redux';
+
+import useSpinner from 'hooks/useSpinner';
+import { importCycleCounts } from 'utils/cycleCountUtils';
+
+const useResolveStepImport = (currentLocationId, locale) => {
+  const [importErrors, setImportErrors] = useState([]);
+  const dispatch = useDispatch();
+  const store = useStore();
+  const { show, hide } = useSpinner();
+
+  // This function should be changed in the next ticket OBPIH-7338,
+  // because I copied this from count step and this is not working properly
+  const importItems = useCallback(async (importFile) => {
+    try {
+      show();
+      const state = store.getState();
+      const currentCycleCountEntities = Object.values(state.countWorkflow.entities);
+
+      const importAction = await importCycleCounts({
+        importFile: importFile[0],
+        locationId: currentLocationId,
+        currentCycleCountEntities,
+        setImportErrors,
+        locale,
+      });
+
+      dispatch(importAction);
+    } finally {
+      hide();
+    }
+  }, [currentLocationId, locale, store]);
+
+  return {
+    importErrors,
+    importItems,
+  };
+};
+
+export default useResolveStepImport;
