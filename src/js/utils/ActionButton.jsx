ba33a7ea890312a2e4f42e6e1ac4ea12dc8e478c
Alan Nadolny <alannadolnyy@gmail.com>
2025-12-04 14:05:33 +0100

    OBPIH-7335 Create action button

---
+++ src/js/utils/ActionButton.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { RiDownload2Line } from 'react-icons/ri';
+
+import Button from 'components/form-elements/Button';
+import Translate from 'utils/Translate';
+
+const ActionButton = ({
+  actions,
+  disabled,
+  buttonLabel,
+  buttonDefaultLabel,
+}) => (
+  <>
+    <Button
+      isDropdown
+      disabled={disabled}
+      defaultLabel={buttonDefaultLabel}
+      label={buttonLabel}
+      variant="secondary"
+      EndIcon={<RiDownload2Line />}
+    />
+    <div
+      className="dropdown-menu dropdown-menu-right nav-item padding-8"
+      aria-labelledby="dropdownMenuButton"
+    >
+      {actions.map((action) => (
+        <a
+          href="#"
+          className="dropdown-item"
+          onClick={action.onClick}
+          role="button"
+          tabIndex={0}
+        >
+          <Translate
+            id={action.label}
+            defaultMessage={action.defaultLabel}
+          />
+        </a>
+      ))}
+    </div>
+  </>
+);
+
+export default ActionButton;
+
+ActionButton.propTypes = {
+  actions: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    defaultLabel: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  })),
+  buttonLabel: PropTypes.string.isRequired,
+  buttonDefaultLabel: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+};
+
+ActionButton.defaultProps = {
+  actions: [],
+  disabled: false,
+};
