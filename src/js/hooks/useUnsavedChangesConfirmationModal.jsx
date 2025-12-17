772cdce3c0d2d71f2f26c73dcab014863f614160
Alan Nadolny <alannadolnyy@gmail.com>
2025-12-17 13:28:21 +0100

    OBPIH-6273 Add hook for unsaved changes confirmation modal

---
+++ src/js/hooks/useUnsavedChangesConfirmationModal.jsx
@@ -0,0 +1,54 @@
+import { useCallback, useMemo } from 'react';
+
+import confirmationModal from 'utils/confirmationModalUtils';
+
+/**
+ * Hook to show a confirmation modal when there are unsaved changes.
+ * @param onConfirm - Function to call when the user confirms navigation away
+ * @returns {(function(): void)} - Function to open the confirmation modal
+ */
+const useUnsavedChangesConfirmationModal = ({ onConfirm }) => {
+  const modalLabels = useMemo(() => ({
+    title: {
+      label: 'react.default.unsavedChangesConfirmationModal.title',
+      default: 'Are you sure?',
+    },
+    content: {
+      label: 'react.default.unsavedChangesConfirmationModal.content',
+      default: 'Are you sure you want to go back without saving?',
+    },
+  }), []);
+
+  const modalButtons = useCallback((onClose) => ([
+    {
+      variant: 'transparent',
+      defaultLabel: 'Cancel',
+      label: 'react.default.unsavedChangesConfirmationModal.cancel',
+      onClick: () => {
+        onClose?.();
+      },
+    },
+    {
+      variant: 'primary',
+      defaultLabel: 'Confirm',
+      label: 'react.default.unsavedChangesConfirmationModal.confirm',
+      onClick: () => {
+        onConfirm?.();
+        onClose?.();
+      },
+    },
+  ]), []);
+
+  return useCallback(
+    () => {
+      confirmationModal({
+        buttons: modalButtons,
+        ...modalLabels,
+        hideCloseButton: true,
+        closeOnClickOutside: false,
+      });
+    }, [modalButtons, modalLabels],
+  );
+};
+
+export default useUnsavedChangesConfirmationModal;
