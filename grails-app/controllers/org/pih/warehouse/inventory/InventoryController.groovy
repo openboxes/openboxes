a24196275fdaa12043d40a9cb02755212d947147
Evan <evanwaterman@hotmail.com>
2026-02-02 12:35:49 -0800

    OBPIH-7606 fix transfer in (#5742)

--- grails-app/controllers/org/pih/warehouse/inventory/InventoryController.groovy
+++ grails-app/controllers/org/pih/warehouse/inventory/InventoryController.groovy
@@ -1086,8 +1086,10 @@ class InventoryController {
                 if (!transactionInstance.hasErrors() && transactionInstance.validate()) {
                     transactionInstance.save(failOnError: true)
                     flash.message = "Successfully saved transaction"
-                    //redirect(controller: "inventory", action: "browse")
-                    redirect(controller: "inventory", action: "browse")
+
+                    // A transfer-in only operates on a single product so redirect to its stock card.
+                    String productId = transactionInstance.getAssociatedProducts().first()
+                    redirect(controller: "inventoryItem", action: "showStockCard", id: productId)
                 }
             } catch (ValidationException e) {
                 log.debug("caught validation exception " + e)
