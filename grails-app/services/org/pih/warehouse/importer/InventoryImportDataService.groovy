da33df06d3548b43153b8c5045b47f670d700bd5
Alan Nadolny <alannadolnyy@gmail.com>
2026-02-12 16:18:24 +0100

    OBPIH-6969 Add inventory difference calculation before data import

--- grails-app/services/org/pih/warehouse/importer/InventoryImportDataService.groovy
+++ grails-app/services/org/pih/warehouse/importer/InventoryImportDataService.groovy
@@ -406,6 +406,26 @@ class InventoryImportDataService implements ImportDataService {
         return binLocation
     }
 
+    void calculateAndApplyInventoryDifferences(ImportDataCommand command) {
+        command.data = command.data.findResults { entry ->
+            Product product = Product.findByProductCode(entry['productCode'])
+            InventoryItem inventoryItem =
+                    inventoryService.findInventoryItemByProductAndLotNumber(product, entry['lotNumber'])
+
+            Integer currentQuantity =
+                    productAvailabilityService.getQuantityOnHand(inventoryItem)
+
+            Integer quantityToImport = entry['quantity'] as Integer
+            Integer quantity = quantityToImport - currentQuantity
+
+            if (quantity > 0) {
+                return entry
+            }
+            // Remove rows that have quantity 0, so that they won't be imported
+            return null
+        }
+    }
+
     /**
      * Convenience POJO for holding the results of the inventory import.
      */
