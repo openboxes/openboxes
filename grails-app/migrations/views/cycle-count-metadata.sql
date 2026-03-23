24179506615332999d0be585899218a426e40216
druchniewicz <51370022+druchniewicz@users.noreply.github.com>
2026-03-23 18:15:09 +0100

    OBLS-547 Taking precedence of cycle count frequency from Inventory Le… (#5823)

--- grails-app/migrations/views/cycle-count-metadata.sql
+++ grails-app/migrations/views/cycle-count-metadata.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE VIEW cycle_count_metadata AS
 (
-SELECT CRC32(CONCAT(inventory_id, product_id))                                                                        as id,
+SELECT CRC32(CONCAT(cycle_count_metadata_tmp.inventory_id, cycle_count_metadata_tmp.product_id))                                          as id,
        cycle_count_metadata_tmp.inventory_id                                                                          as inventory_id,
        cycle_count_metadata_tmp.product_id                                                                            as product_id,
        cycle_count_metadata_tmp.date_counted                                                                          as date_counted,
        cycle_count_metadata_tmp.abc_class                                                                             as abc_class,
-       cycle_count_frequency.frequency                                                                                as frequency,
-       datediff(now(), date_counted)                                                                                  as days_since_last_count,
-       date_add(cycle_count_metadata_tmp.date_counted, INTERVAL cycle_count_frequency.frequency
+       COALESCE(inventory_level.cycle_count_frequency_days, cycle_count_frequency.frequency)                        as frequency,
+       datediff(now(), cycle_count_metadata_tmp.date_counted)                                                         as days_since_last_count,
+       date_add(cycle_count_metadata_tmp.date_counted, INTERVAL COALESCE(inventory_level.cycle_count_frequency_days, cycle_count_frequency.frequency)
                 DAY)                                                                                                  as date_expected,
-       datediff(date_add(cycle_count_metadata_tmp.date_counted, INTERVAL cycle_count_frequency.frequency DAY),
+       datediff(date_add(cycle_count_metadata_tmp.date_counted, INTERVAL COALESCE(inventory_level.cycle_count_frequency_days, cycle_count_frequency.frequency) DAY),
                 now())                                                                                                as days_until_next_count
 FROM (SELECT product_count_history.inventory_id as inventory_id,
              product_count_history.product_id   as product_id,
@@ -25,5 +25,7 @@ FROM (SELECT product_count_history.inventory_id as inventory_id,
                                ON product_count_history.inventory_id = product_classification.inventory_id
                                    AND product_count_history.product_id =
                                        product_classification.product_id) as cycle_count_metadata_tmp
-         LEFT OUTER JOIN cycle_count_frequency ON cycle_count_frequency.abc_class = cycle_count_metadata_tmp.abc_class
-    );
+          LEFT OUTER JOIN cycle_count_frequency ON cycle_count_frequency.abc_class = cycle_count_metadata_tmp.abc_class
+          LEFT OUTER JOIN inventory_level ON inventory_level.inventory_id = cycle_count_metadata_tmp.inventory_id
+                                           AND inventory_level.product_id = cycle_count_metadata_tmp.product_id
+);
