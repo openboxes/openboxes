e460d7a16609b0952ac77458900141fe42a0e4e6
Evan <evanwaterman@hotmail.com>
2026-06-05 15:32:41 -0700

    remove NoOpValidator and DomainValidatableASTTransformation

--- src/main/groovy/org/pih/warehouse/core/validation/DomainValidatable.groovy
+++ src/main/groovy/org/pih/warehouse/core/validation/DomainValidatable.groovy
@@ -7,9 +7,6 @@ import org.grails.datastore.gorm.GormValidateable
  *
  * Hooks into Grails validation by wrapping GormValidateable.validate() methods with additional validation.
  *
- * We need to perform some AST Transformations on this trait in order for it to play nice with Grails.
- * See {@link DomainValidatableASTTransformation} for further details.
- *
  * @param <V> Optional. The {@link Validator} component containing additional validation to perform on this object.
  */
 trait DomainValidatable<V extends Validator> implements Validatable<V>, GormValidateable {
