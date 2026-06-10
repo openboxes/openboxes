8510d254fe62485990a6104675fb50ff97684b92
Alan Nadolny <alannadolnyy@gmail.com>
2026-06-10 16:54:43 +0200

    OBPIH-7852 Add filter fields

---
+++ src/js/components/receiving/FilterFields.jsx
@@ -0,0 +1,23 @@
+import FilterSelectField from 'components/form-elements/FilterSelectField';
+import receiptStatusOptions from 'consts/receiptStatusOptions';
+
+export default {
+  receiptStatusCode: {
+    type: FilterSelectField,
+    attributes: {
+      // top and the className are used to position this filter in the first row,
+      // next to the search field
+      top: true,
+      className: 'order-1',
+      multi: true,
+      filterElement: true,
+      placeholder: 'react.partialReceiving.filters.receiptStatus.label',
+      defaultPlaceholder: 'Receipt Status',
+      ariaLabel: 'Receipt Status',
+      showLabelTooltip: true,
+      options: receiptStatusOptions,
+      closeMenuOnSelect: false,
+      blurInputOnSelect: false,
+    },
+  },
+};
