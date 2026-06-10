3df10ed12b96e1cf9174913e39e66f45b35cc0c7
Alan Nadolny <alannadolnyy@gmail.com>
2026-06-10 16:54:29 +0200

    OBPIH-7852 Add consts with receipt stauses and receiving views

---
+++ src/js/consts/receiptStatusOptions.js
@@ -0,0 +1,8 @@
+const receiptStatusOptions = [
+  { value: 'QUANTITY_REMAINING', label: 'Quantity remaining' },
+  { value: 'OVER_RECEIVED', label: 'Over received' },
+  { value: 'NO_QUANTITY_ENTERED', label: 'No quantity entered' },
+  { value: 'COMPLETE', label: 'Complete / Fully Received' },
+];
+
+export default receiptStatusOptions;
---
+++ src/js/consts/receivingViewOptions.js
@@ -0,0 +1,6 @@
+const receivingViewOptions = [
+  { value: 'table', label: 'react.partialReceiving.tableView.label', defaultLabel: 'Table View' },
+  { value: 'packingList', label: 'react.partialReceiving.packingListView.label', defaultLabel: 'Packing List View' },
+];
+
+export default receivingViewOptions;
