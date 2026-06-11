34c769ce5ec4fd230f731000c7d65b5aa978843d
Kacper Chełstowski <kchelstowski@soldevelo.com>
2026-06-11 13:00:01 +0200

    OBPIH-7867 Implement an endpoint to start a receipt (#5968)

---
+++ src/main/groovy/org/pih/warehouse/receiving/ReceiptDto.groovy
@@ -0,0 +1,43 @@
+package org.pih.warehouse.receiving
+
+import org.pih.warehouse.receiving.Receipt
+import org.pih.warehouse.receiving.ReceiptStatusCode
+import org.pih.warehouse.shipping.Shipment
+
+class ReceiptDto {
+
+    String id
+    ReceiptStatusCode receiptStatus
+    ReceiptShipmentDto shipment
+    ReceiptLocationDto origin
+    ReceiptLocationDto destination
+    Date dateShipped
+    Date dateDelivered
+    ReceiptRequisitionDto requisition
+    ReceiptOrderDto order
+    String description
+    ReceiptRecipientDto recipient
+    // TODO: To be replaced by List<ReceiptItemDto> after the read endpoint is done
+    List receiptItems = []
+
+    static ReceiptDto toDto(Receipt receipt) {
+        if (!receipt) {
+            return null
+        }
+        Shipment shipment = receipt.shipment
+        return new ReceiptDto(
+                id: receipt.id,
+                receiptStatus: receipt.receiptStatusCode?.name(),
+                shipment: ReceiptShipmentDto.toDto(shipment),
+                origin: ReceiptLocationDto.toDto(shipment?.origin),
+                destination: ReceiptLocationDto.toDto(shipment?.destination),
+                dateShipped: shipment?.actualShippingDate,
+                dateDelivered: receipt.actualDeliveryDate,
+                requisition: ReceiptRequisitionDto.toDto(shipment?.requisition),
+                order: ReceiptOrderDto.toDto(shipment?.purchaseOrder),
+                description: shipment?.description,
+                recipient: ReceiptRecipientDto.toDto(receipt.recipient),
+                receiptItems: receipt.receiptItems
+        )
+    }
+}
