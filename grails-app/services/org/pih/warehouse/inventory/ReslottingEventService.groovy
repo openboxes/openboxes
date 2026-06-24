3e70554787e35aae56d6e55f6919022cc7784428
Artur Walkowiak <awalkowiak@soldevelo.com>
2026-06-24 09:58:06 +0200

    Apply suggestions from code review
    Co-authored-by: Artur Walkowiak <awalkowiak@soldevelo.com>

--- grails-app/services/org/pih/warehouse/inventory/ReslottingEventService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/ReslottingEventService.groovy
@@ -3,16 +3,12 @@ package org.pih.warehouse.inventory
 import org.pih.warehouse.jobs.PutawayLocationReslottingJob
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
-import org.springframework.transaction.support.TransactionSynchronizationManager
 
 class ReslottingEventService {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, fallbackExecution = true)
     void onReslottingEvent(ReslottingEvent event) {
         log.info "Application event $event has been published! " + event.properties
-
-        log.debug "isActualTransactionActive = ${TransactionSynchronizationManager.isActualTransactionActive()}"
-        log.debug "isSynchronizationActive = ${TransactionSynchronizationManager.isSynchronizationActive()}"
 
         PutawayLocationReslottingJob.triggerNow([inventoryLevelId: event.source])
     }
