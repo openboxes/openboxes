0a9cbda649953fae07d6a5d36a0a2727e9da7d7f
adambalcerzak <abalcerzak@soldevelo.com>
2026-06-26 11:00:24 +0200

    OBLS-451 Send IBT Status Updates to Exceed fix publishing shipment.received move createPutawayOrdersFromReceipt as in TODO

--- grails-app/services/org/pih/warehouse/core/RequisitionEventService.groovy
+++ grails-app/services/org/pih/warehouse/core/RequisitionEventService.groovy
@@ -1,14 +1,17 @@
 package org.pih.warehouse.core
 
 import grails.core.GrailsApplication
+import org.pih.warehouse.inboundSortation.InboundSortationService
 import org.pih.warehouse.jobs.AutomaticIssuanceJob
 import org.pih.warehouse.requisition.Requisition
+import org.pih.warehouse.shipping.Shipment
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
 class RequisitionEventService {
     GrailsApplication grailsApplication
     WebhookPublisherService webhookPublisherService
+    InboundSortationService inboundSortationService
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, fallbackExecution = true)
     void onRequisitionEvent(RequisitionEvent event) {
@@ -26,9 +29,23 @@ class RequisitionEventService {
                 Date runAt = new Date(System.currentTimeMillis() + delayInMilliseconds)
                 log.info "Triggering automaticIssuanceJob job with ${delayInMilliseconds} ms delay"
                 AutomaticIssuanceJob.schedule(runAt, [requisitionId: requisition.id])
-                // no break
+                webhookPublisherService.publishRequisitionEvent(requisition, event.eventType)
+                break
+            case WebhookEventType.SHIPMENT_RECEIVED:
+                // The AFTER_COMMIT phase runs after the original transaction has committed, so we need a
+                // new transaction for the putaway creation to write to the database.
+                Requisition.withNewTransaction {
+                    List<Shipment> shipments = Shipment.findAllByRequisition(requisition)
+                    shipments.each { Shipment shipment ->
+                        log.info "Creating putaway tasks for receipt ${shipment.receipt}"
+                        if (shipment.destination?.supports(ActivityCode.AUTOMATED_PUTAWAY_CREATION)) {
+                            inboundSortationService.createPutawayOrdersFromReceipt(shipment.receipt)
+                        }
+                    }
+                }
+                webhookPublisherService.publishRequisitionEvent(requisition, event.eventType)
+                break
             case WebhookEventType.REQUISITION_CREATED:
-            case WebhookEventType.REQUISITION_ISSUED:
                 webhookPublisherService.publishRequisitionEvent(requisition, event.eventType)
                 break
         }
