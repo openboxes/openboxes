81832bfe5063fb38de2fc77fa302094c688595e7
Evan <evanwaterman@hotmail.com>
2026-07-15 12:50:16 -0700

    OBPIH-7933 Initialize CycleCount.cycleCountItems to avoid null pointer (#6026)

--- grails-app/services/org/pih/warehouse/inventory/CycleCountService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/CycleCountService.groovy
@@ -568,6 +568,11 @@ class CycleCountService {
 
         List<AvailableItem> itemsToSave = cycleCountProductAvailabilityService.getAvailableItems(
                 facility, request.cycleCountRequest.product)
+        if (!itemsToSave) {
+            throw new RuntimeException("Cannot start a cycle count on a product with no stock. Please cancel the " +
+                    "count on product [${request.cycleCountRequest.product.productCode}] to proceed.")
+        }
+
         // 1:1 association between cycle count and cycle count request
         request.cycleCountRequest.cycleCount = newCycleCount
         request.cycleCountRequest.status = CycleCountRequestStatus.IN_PROGRESS
